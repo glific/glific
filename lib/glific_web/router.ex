033e33e822d9643f9d1586535d0a2dd4966e2d86
pankaj-ag <pankaj@coloredcow.in>
2020-06-02 20:18:58 +0530

    updates

--- lib/glific_web/router.ex
+++ lib/glific_web/router.ex
@@ -11,7 +11,7 @@ defmodule GlificWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, {GlificWeb.LayoutView, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
   end
 
   pipeline :api do
