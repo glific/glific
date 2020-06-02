40b129703645b52576e3f1a8ad33ccac4a7a011c
pankaj-ag <pankaj@coloredcow.in>
2020-06-02 19:46:24 +0530

    Add module doc for repo

--- lib/glific/repo.ex
+++ lib/glific/repo.ex
@@ -1,4 +1,7 @@
 defmodule Glific.Repo do
+  @moduledoc """
+  A repository that maps to an underlying data store, controlled by the Postgres adapter
+  """
   use Ecto.Repo,
     otp_app: :glific,
     adapter: Ecto.Adapters.Postgres
