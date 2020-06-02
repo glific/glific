dfe2ffd817e64bcb7a419cefd8ecaa7adc7a9e8c
pankaj-ag <pankaj@coloredcow.in>
2020-06-02 19:25:04 +0530

    CI check configs

---
+++ .doctor.exs
@@ -0,0 +1,12 @@
+%Doctor.Config{
+  ignore_modules: [],
+  ignore_paths: ["lib/glific_web/views/*.ex"],
+  min_module_doc_coverage: 40,
+  min_module_spec_coverage: 0,
+  min_overall_doc_coverage: 50,
+  min_overall_spec_coverage: 0,
+  moduledoc_required: true,
+  raise: false,
+  reporter: Doctor.Reporters.Full,
+  umbrella: false
+}
