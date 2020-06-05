73861e2450229e9255073c3db25bcdaced19f6ea
Donald A. Lobo <donald.lobo@gmail.com>
2020-06-05 11:19:19 -0700

    Slow and steady progress on full text search

--- priv/repo/migrations/20200605170830_full_text_search.exs
+++ priv/repo/migrations/20200605170830_full_text_search.exs
@@ -9,20 +9,20 @@ defmodule Glific.Repo.Migrations.FullTextSearch do
       """
       CREATE MATERIALIZED VIEW message_search AS
       SELECT
-      contact.id  AS contact_id,
-      messages.id AS messages_id,
-      contact.name AS contact_name,
+      contacts.id  AS id,
+      contacts.name || ' ' || contacts.phone AS name,
       (
-      setweight(to_tsvector(unaccent(contact.name || ' ' || contact.phone)), 'A') ||
-      setweight(to_tsvector(unaccent(coalesce(string_agg(tags.label, ' '), ' '))), 'B')
-      setweight(to_tsvector(unaccent(message.body || ' ' || message_media.caption)), 'C')
+      setweight(to_tsvector(unaccent(contacts.name || ' ' || contacts.phone)), 'A') ||
+      setweight(to_tsvector(unaccent(coalesce(string_agg(tags.label, ' '), ' '))), 'B') ||
+      setweight(to_tsvector(unaccent(coalesce(string_agg(messages.body, ' '), ' '))), 'C') ||
+      setweight(to_tsvector(unaccent(coalesce(string_agg(message_media.caption, ' '), ' '))), 'D')
       ) AS document
-      FROM contacts
-      LEFT JOIN messages ON (messages.sender_id == contact.id OR messages.receiver_id == contact.id)
-      LEFT JOIN message_media ON messages.media_id == message_media.id
-      LEFT JOIN messages_tags ON  messages.id == messages_tags.message_id
-      LEFT JOIN tags ON tags.id == messages_tags.tag_id
-      GROUP BY contact.id
+      FROM  contacts
+      LEFT  JOIN messages ON (messages.sender_id = contacts.id OR messages.recipient_id = contacts.id)
+      LEFT  JOIN message_media ON messages.media_id = message_media.id
+      LEFT  JOIN messages_tags ON  messages.id = messages_tags.message_id
+      LEFT  JOIN tags ON tags.id = messages_tags.tag_id
+      GROUP BY contacts.id
       """
     )
 
@@ -30,14 +30,20 @@ defmodule Glific.Repo.Migrations.FullTextSearch do
     create index("message_search", ["document"], using: :gin)
 
     # to support substring title matches with ILIKE
-    execute("CREATE INDEX recipe_search_title_trgm_index ON recipe_search USING gin (title gin_trgm_ops)")
+    execute("CREATE INDEX message_search_name_index ON message_search USING gin (name gin_trgm_ops)")
 
     # to support updating CONCURRENTLY
-    create unique_index("recipe_search", [:id])
+    create unique_index("message_search", [:id])
   end
 
   def down do
     execute("DROP EXTENSION IF EXISTS pg_trgm")
     execute("DROP EXTENSION IF EXISTS unaccent")
+
+    execute("DROP INDEX message_search_document")
+    execute("DROP INDEX message_search_name_index")
+    execute("DROP INDEX message_search_id")
+
+    execute("DROP MATERIALIZED VIEW message_search")
   end
 end
