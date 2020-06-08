d6d0422d3c7d217ea2f3651cb8c84ecd4a1ffd4e
Satendra Rawat <39792899+Satendra-SR@users.noreply.github.com>
2020-06-08 11:54:36 +0530

    Basic Github action for continuous integration (#24)
    * Basic Github action for continuous integration
    
    * Updated elixir version
    
    * Mix check for CI with PLT caching
    
    * dialyzer PLT caching
    
    * Added dialyzer_ignore
    
    * uncommented mix doctor command
    
    * halt exit status on dialyzer failure
    
    * uncommented mix doctor command
    
    * removed invalid CLI argument for dialyzer
    
    * Doctor issues
    
    * dialyzer fixes
    
    Co-authored-by: pankaj-ag <pankaj@coloredcow.in>

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -48,6 +48,7 @@ defmodule Glific.DataCase do
       assert %{password: ["password is too short"]} = errors_on(changeset)
 
   """
+  @spec errors_on(Ecto.Changeset.t()) :: map()
   def errors_on(changeset) do
     Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
       Regex.replace(~r"%{(\w+)}", message, fn _, key ->
