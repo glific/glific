ef15a8f9116ee6b761886f2f4ed5c40d89a07890
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-06-08 20:15:29 +0530

    BSP: foreign key constraint

--- lib/glific/partners/bsp.ex
+++ lib/glific/partners/bsp.ex
@@ -28,6 +28,8 @@ defmodule Glific.Partners.BSP do
     field :url, :string
     field :api_end_point, :string
 
+    has_many :organizations, Glific.Partners.Organization
+
     timestamps()
   end
 
@@ -40,5 +42,16 @@ defmodule Glific.Partners.BSP do
     |> cast(attrs, @required_fields ++ @optional_fields)
     |> validate_required(@required_fields)
     |> unique_constraint([:name])
+    |> foreign_key_constraint(:organizations)
+  end
+
+  @doc """
+  Delete changeset pattern we use for all data types
+  """
+  @spec delete_changeset(Organization.t()) :: Ecto.Changeset.t()
+  def delete_changeset(organization) do
+    organization
+    |> cast(%{}, @required_fields ++ @optional_fields)
+    |> foreign_key_constraint(:tags_organization_id_fk)
   end
 end
