ae779841ee59bc9450a1fc4a71347bf3e79a20b0
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-06-10 14:27:39 +0530

    Test cases

--- test/glific/templates_test.exs
+++ test/glific/templates_test.exs
@@ -26,11 +26,20 @@ defmodule Glific.TemplatesTest do
       body: "some updated body"
     }
 
-    @invalid_attrs %{label: nil, body: nil, language_id: nil}
+    @invalid_attrs %{
+      label: nil,
+      body: nil,
+      language_id: nil
+    }
 
     @valid_language_attrs %{
       label: "English (United States)",
       locale: "en_US",
+      is_active: true
+    }
+    @valid_language_attrs_1 %{
+      label: "Hindi (United States)",
+      locale: "hi_US",
       is_active: true
     }
 
@@ -83,6 +92,22 @@ defmodule Glific.TemplatesTest do
       assert length(session_template_list) == 2
     end
 
+    test "list_session_templates/1 with multiple items" do
+      session_template_fixture()
+      session_template_fixture(@valid_attrs_1)
+      session_templates = Templates.list_session_templates()
+      assert length(session_templates) == 2
+    end
+
+    test "list_session_templates/1 with multiple items sorted" do
+      session_template1 = session_template_fixture()
+      session_template2 = session_template_fixture(@valid_attrs_1)
+      session_templates = Templates.list_session_templates(%{order: :asc})
+      assert length(session_templates) == 2
+      [s1, s2] = session_templates
+      assert s1 == session_template1 && s2 == session_template2
+    end
+
     test "get_session_template!/1 returns the session_template with given id" do
       session_template = session_template_fixture()
       assert Templates.get_session_template!(session_template.id) == session_template
@@ -97,8 +122,11 @@ defmodule Glific.TemplatesTest do
 
       assert session_template.label == "some label"
       assert session_template.body == "some body"
+      assert session_template.shortcode == nil
       assert session_template.is_active == true
       assert session_template.is_reserved == true
+      assert session_template.is_source == false
+      assert session_template.language_id == language.id
     end
 
     test "create_session_template/1 with invalid data returns error changeset" do
@@ -107,12 +135,14 @@ defmodule Glific.TemplatesTest do
 
     test "update_session_template/2 with valid data updates the session_template" do
       session_template = session_template_fixture()
-
+      language = language_fixture(@valid_language_attrs_1)
+      attrs = Map.merge(@update_attrs, %{language_id: language.id})
       assert {:ok, %SessionTemplate{} = session_template} =
-               Templates.update_session_template(session_template, @update_attrs)
+               Templates.update_session_template(session_template, attrs)
 
       assert session_template.label == @update_attrs.label
       assert session_template.body == @update_attrs.body
+      assert session_template.language_id == language.id
     end
 
     test "update_session_template/2 with invalid data returns error changeset" do
