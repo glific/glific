56450d9932fd7223dc51c73d93146645a9844311
pankaj-ag <pankaj@coloredcow.in>
2020-06-10 23:50:43 +0530

    Update contact ID

--- lib/glific/messages.ex
+++ lib/glific/messages.ex
@@ -94,10 +94,20 @@ defmodule Glific.Messages do
     attrs =
       %{flow: :inbound, provider_status: :delivered}
       |> Map.merge(attrs)
+      |> put_contact_id()
 
     %Message{}
     |> Message.changeset(attrs)
     |> Repo.insert()
+  end
+
+  # Still need to improve this fucnation
+  defp put_contact_id(attrs) do
+    case attrs.flow do
+      :inbound -> Map.put(attrs, :contact_id, attrs[:sender_id])
+      :outbound -> Map.put(attrs, :contact_id, attrs[:receiver_id])
+      _ -> attrs
+    end
   end
 
   @doc """
