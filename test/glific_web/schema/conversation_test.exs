38da12d59fd3996a6a30d519206dc49c4c942c63
Donald A. Lobo <donald.lobo@gmail.com>
2020-06-10 19:42:06 -0700

    and we continue marching forward with api for conversations

--- test/glific_web/schema/conversation_test.exs
+++ test/glific_web/schema/conversation_test.exs
@@ -11,7 +11,12 @@ defmodule GlificWeb.Schema.Query.ConversationTest do
   load_gql(:list, GlificWeb.Schema, "assets/gql/conversations/list.gql")
 
   test "conversations always returns a few threads" do
-    {:ok, result} = query_gql_by(:list, variables: %{"nc" => 1, "sc" => 1})
+    {:ok, result} = query_gql_by(:list, variables: %{"nc" => 1, "sc" => 3})
+    assert get_in(result, [:data, "conversations"]) |> length >= 1
+
+    contact_id = get_in(result, [:data, "conversations", Access.at(0), "contact", "id"])
+
+    {:ok, result} = query_gql_by(:list, variables: %{"nc" => 1, "sc" => 1, "filter" => %{"id" => contact_id}})
     assert get_in(result, [:data, "conversations"]) |> length == 1
   end
 end
