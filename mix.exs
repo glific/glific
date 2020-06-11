93f85a44e0d1df443b4898cb5fe5c4a479dd23c3
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-06-11 11:46:48 +0530

    Checked all dependencies for latest version, updated doctor to latest minor version

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Glific.MixProject do
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22", only: [:dev, :test], runtime: false},
       {:inch_ex, "~> 2.0", only: [:dev, :test], runtime: false},
-      {:doctor, "~> 0.13.0"},
+      {:doctor, "~> 0.13"},
       {:httpoison, "~> 1.6"},
       {:poison, "~> 4.0"},
       {:ex_rated, "~> 1.2"},
