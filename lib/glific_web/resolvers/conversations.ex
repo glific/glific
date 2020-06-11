914824539abeef2ccfd7ea9129f4938bdd2018f7
Donald A. Lobo <donald.lobo@gmail.com>
2020-06-11 16:20:53 -0700

    get current_user exposed to absinthe context

--- lib/glific_web/resolvers/conversations.ex
+++ lib/glific_web/resolvers/conversations.ex
@@ -8,6 +8,8 @@ defmodule GlificWeb.Resolvers.Conversations do
 
   @doc """
   Get the list of conversations filtered by args
+  For an authenticated session, we can get the current user from the context via this pattern match
+  %{context: %{current_user: current_user}}
   """
   @spec conversations(Absinthe.Resolution.t(), map(), %{context: map()}) ::
           {:ok, any} | {:error, any}
--- lib/glific_web/router.ex
+++ lib/glific_web/router.ex
@@ -23,6 +23,7 @@ defmodule GlificWeb.Router do
 
   pipeline :api_protected do
     plug Pow.Plug.RequireAuthenticated, error_handler: GlificWeb.APIAuthErrorHandler
+    plug GlificWeb.Context
   end
 
   scope "/api/v1", GlificWeb.API.V1, as: :api_v1 do
@@ -45,13 +46,25 @@ defmodule GlificWeb.Router do
     live "/", PageLive, :index
   end
 
-  # Custom stack for Ansinthe
+  # Custom stack for Absinthe
   scope "/" do
-    pipe_through [:api, :api_protected]
+    pipe_through [:api]
 
     forward "/api", Absinthe.Plug, schema: GlificWeb.Schema
 
     forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: GlificWeb.Schema,
+      interface: :simple,
+      socket: GlificWeb.UserSocket
+  end
+
+  # Custom stack for Absinthe
+  scope "/" do
+    pipe_through [:api, :api_protected]
+
+    forward "/secure/api", Glific.Absinthe.Plug, schema: GlificWeb.Schema
+
+    forward "/secure/graphiql", Glific.Absinthe.Plug.GraphiQL,
       schema: GlificWeb.Schema,
       interface: :simple,
       socket: GlificWeb.UserSocket
