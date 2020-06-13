3c4eb7d9fc6c5a02de30e4f0152de33f083eb3bb
Donald A. Lobo <donald.lobo@gmail.com>
2020-06-12 18:21:16 -0700

    integrate vinod's changes to sql

--- priv/repo/migrations/20200605170830_full_text_search.exs
+++ priv/repo/migrations/20200605170830_full_text_search.exs
@@ -6,11 +6,11 @@ defmodule Glific.Repo.Migrations.FullTextSearch do
 
     create_view()
 
-    create_indexes()
+    search_messages()
+
+    create_functions()
 
     create_triggers()
-
-    full_text_search()
   end
 
   defp create_extensions do
@@ -20,44 +20,77 @@ defmodule Glific.Repo.Migrations.FullTextSearch do
 
   defp create_view do
     execute("""
-    CREATE MATERIALIZED VIEW message_search AS
-    SELECT
-    contacts.id  AS id,
-    contacts.name AS contact_name,
-    contacts.phone AS contacts_phone,
-    coalesce(string_agg(tags.label, ' '), ' ') AS tag_label,
-    (
-    setweight(to_tsvector(unaccent(contacts.name || ' ' || contacts.phone)), 'A') ||
-    setweight(to_tsvector(unaccent(coalesce(string_agg(tags.label, ' '), ' '))), 'B') ||
-    setweight(to_tsvector(unaccent(coalesce(string_agg(messages.body, ' '), ' '))), 'C')
-    ) AS document
-    FROM  contacts
-    LEFT  JOIN messages ON (messages.sender_id = contacts.id OR messages.receiver_id = contacts.id)
-    LEFT  JOIN messages_tags ON  messages.id = messages_tags.message_id
-    LEFT  JOIN tags ON tags.id = messages_tags.tag_id
-    GROUP BY contacts.id
+    DROP VIEW IF EXISTS partitioned_messages;
+    CREATE VIEW partitioned_messages AS (
+    SELECT *,
+    row_number() OVER (PARTITION BY contact_id ORDER BY updated_at DESC) AS rank
+    FROM messages
+    );
     """)
   end
 
-  defp create_indexes do
-    # to support full-text searches
-    create index("message_search", ["document"], using: :gin)
+  defp create_functions do
+    execute("""
+DROP FUNCTION IF EXISTS create_search_messages;
+CREATE OR REPLACE FUNCTION create_search_messages(N numeric) RETURNS TEXT AS
+$$
+BEGIN
+    EXECUTE ('DELETE FROM search_messages');
+    INSERT INTO search_messages(contact_id, name, phone, tag_label, document)
+    SELECT contacts.id,
+           contacts.name,
+           contacts.phone,
+           coalesce(string_agg(tags.label, ' '), ''),
+           (
+              setweight(to_tsvector(unaccent(contacts.name || ' ' || contacts.phone)), 'A') ||
+              setweight(to_tsvector(unaccent(
+                 coalesce(string_agg(tags.label, ' ' order by messages.inserted_at), ' '))), 'B') ||
+              setweight(to_tsvector(unaccent(coalesce(
+                 string_agg(messages.body, ' ' order by messages.inserted_at), ' '))), 'C')
+           )
+    FROM contacts
+    LEFT JOIN partitioned_messages messages
+           ON (messages.sender_id = contacts.id OR messages.receiver_id = contacts.id)
+    LEFT JOIN messages_tags ON messages.id = messages_tags.message_id
+    LEFT JOIN tags ON tags.id = messages_tags.tag_id
+    WHERE messages.rank <= N
+    GROUP BY contacts.id;
+    RETURN 'search_messages updated with last ' || N || ' messages';
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+    """)
 
-    # to support updating CONCURRENTLY
-    create unique_index("message_search", [:id])
+    execute("""
+    DROP FUNCTION IF EXISTS update_contact;
+    CREATE OR REPLACE FUNCTION update_contact(
+      contact_id numeric,
+      label text,
+      message text)
+    RETURNS BOOLEAN AS
+    $$
+    BEGIN
+    UPDATE search_messages
+    SET
+      tag_label = tag_label || ' ' || coalesce(update_contact.label, ''),
+      document  = (document || (setweight(to_tsvector(unaccent(coalesce(label, ' '))), 'B') ||
+      setweight(to_tsvector(unaccent(coalesce(message, ' '))), 'C')))::tsvector
+    WHERE contact_id = update_contact.contact_id;
+    RETURN FOUND;
+    END;
+    $$ LANGUAGE plpgsql SECURITY DEFINER;
+    """)
   end
-
 
   @doc """
   Create a table for full text search. This eliminates the need to do anything
   dynamically and we can return really quickly
   """
-  def full_text_search do
-    create table(:full_text_search) do
-      add :contact_name, :string
-      add :contact_phone, :string
-      add :tags_label, :text
-      add :document, :text
+  def search_messages do
+    create table(:search_messages) do
+      add :name, :string
+      add :phone, :string
+      add :tag_label, :text
+      add :document, :tsvector
 
       add :contact_id, references(:contacts, on_delete: :delete_all)
 
@@ -65,70 +98,58 @@ defmodule Glific.Repo.Migrations.FullTextSearch do
     end
 
     execute("""
-    CREATE INDEX full_text_search_index ON full_text_search using gin(to_tsvector('english', document))
+    CREATE INDEX search_messages_index ON search_messages using gin(to_tsvector('english', document))
     """)
 
-    create index(:full_text_search, :contact_name)
-    create index(:full_text_search, :contact_phone)
-    create index(:full_text_search, :tags_label)
+    create index(:search_messages, :name)
+    create index(:search_messages, :phone)
+    create index(:search_messages, :tag_label)
   end
 
   defp create_triggers do
     execute("""
-    CREATE OR REPLACE FUNCTION refresh_message_search()
-    RETURNS TRIGGER LANGUAGE plpgsql
-    AS $$
-    BEGIN
-    REFRESH MATERIALIZED VIEW CONCURRENTLY message_search;
-    RETURN NULL;
-    END $$;
-    """)
-
-    execute("""
-    CREATE TRIGGER refresh_message_search
+    CREATE TRIGGER message_search_update
     AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
     ON contacts
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE refresh_message_search();
+    EXECUTE PROCEDURE message_search_update();
     """)
 
     execute("""
-    CREATE TRIGGER refresh_message_search
+    CREATE TRIGGER message_search_update
     AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
     ON messages
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE refresh_message_search();
+    EXECUTE PROCEDURE message_search_update();
     """)
 
     execute("""
-    CREATE TRIGGER refresh_message_search
+    CREATE TRIGGER message_search_update
     AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
     ON tags
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE refresh_message_search();
+    EXECUTE PROCEDURE message_search_update();
     """)
 
     execute("""
-    CREATE TRIGGER refresh_message_search
+    CREATE TRIGGER message_search_update
     AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
     ON messages_tags
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE refresh_message_search();
+    EXECUTE PROCEDURE message_search_update();
     """)
   end
 
   def down do
-    execute("DROP TRIGGER IF EXISTS refresh_message_search ON contacts")
-    execute("DROP TRIGGER IF EXISTS refresh_message_search ON messages")
-    execute("DROP TRIGGER IF EXISTS refresh_message_search ON tags")
-    execute("DROP TRIGGER IF EXISTS refresh_message_search ON messages_tags")
+    execute("DROP TRIGGER IF EXISTS message_search_update ON contacts")
+    execute("DROP TRIGGER IF EXISTS message_search_update ON messages")
+    execute("DROP TRIGGER IF EXISTS message_search_update ON tags")
+    execute("DROP TRIGGER IF EXISTS message_search_update ON messages_tags")
 
-    execute("DROP INDEX IF EXISTS message_search_document_index")
-    execute("DROP INDEX IF EXISTS message_search_contact_index")
-    execute("DROP INDEX IF EXISTS message_search_tag_index")
-    execute("DROP INDEX IF EXISTS message_search_id_index")
+    execute("DROP VIEW IF EXISTS partitioned_message")
 
-    execute("DROP MATERIALIZED VIEW IF EXISTS message_search")
+    execute("DROP FUNCTION IF EXISTS create_search_messages")
+    execute("DROP FUNCTION IF EXISTS update_contact")
 
     execute("DROP EXTENSION IF EXISTS pg_trgm")
     execute("DROP EXTENSION IF EXISTS unaccent")
