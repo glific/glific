ba07d80943d8fded564f5b933735e3b07435adbf
Donald A. Lobo <donald.lobo@gmail.com>
2020-06-12 18:35:47 -0700

    integrate vinod's changes to sql

--- priv/repo/migrations/20200605170830_search_messages.exs
+++ priv/repo/migrations/20200605170830_search_messages.exs
@@ -20,7 +20,6 @@ defmodule Glific.Repo.Migrations.FullTextSearch do
 
   defp create_view do
     execute("""
-    DROP VIEW IF EXISTS partitioned_messages;
     CREATE VIEW partitioned_messages AS (
     SELECT *,
     row_number() OVER (PARTITION BY contact_id ORDER BY updated_at DESC) AS rank
@@ -31,7 +30,6 @@ defmodule Glific.Repo.Migrations.FullTextSearch do
 
   defp create_functions do
     execute("""
-DROP FUNCTION IF EXISTS create_search_messages;
 CREATE OR REPLACE FUNCTION create_search_messages(N numeric) RETURNS TEXT AS
 $$
 BEGIN
@@ -61,8 +59,7 @@ $$ LANGUAGE plpgsql SECURITY DEFINER;
     """)
 
     execute("""
-    DROP FUNCTION IF EXISTS update_contact;
-    CREATE OR REPLACE FUNCTION update_contact(
+    CREATE OR REPLACE FUNCTION update_search_message(
       contact_id numeric,
       label text,
       message text)
@@ -98,7 +95,7 @@ $$ LANGUAGE plpgsql SECURITY DEFINER;
     end
 
     execute("""
-    CREATE INDEX search_messages_index ON search_messages using gin(to_tsvector('english', document))
+    CREATE INDEX search_messages_index ON search_messages using gin(document)
     """)
 
     create index(:search_messages, :name)
@@ -108,43 +105,43 @@ $$ LANGUAGE plpgsql SECURITY DEFINER;
 
   defp create_triggers do
     execute("""
-    CREATE TRIGGER message_search_update
+    CREATE TRIGGER update_message_search
     AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
     ON contacts
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE message_search_update();
+    EXECUTE PROCEDURE update_search_message(1, NULL, NULL)
     """)
 
     execute("""
-    CREATE TRIGGER message_search_update
+    CREATE TRIGGER update_search_message
     AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
     ON messages
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE message_search_update();
+    EXECUTE PROCEDURE update_search_message(1, "", "");
     """)
 
     execute("""
-    CREATE TRIGGER message_search_update
+    CREATE TRIGGER update_search_message
     AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
     ON tags
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE message_search_update();
+    EXECUTE PROCEDURE update_search_message(1, "", "");
     """)
 
     execute("""
-    CREATE TRIGGER message_search_update
+    CREATE TRIGGER update_search_message
     AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
     ON messages_tags
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE message_search_update();
+    EXECUTE PROCEDURE update_search_message(1, "", "");
     """)
   end
 
   def down do
-    execute("DROP TRIGGER IF EXISTS message_search_update ON contacts")
-    execute("DROP TRIGGER IF EXISTS message_search_update ON messages")
-    execute("DROP TRIGGER IF EXISTS message_search_update ON tags")
-    execute("DROP TRIGGER IF EXISTS message_search_update ON messages_tags")
+    execute("DROP TRIGGER IF EXISTS update_search_message ON contacts")
+    execute("DROP TRIGGER IF EXISTS update_search_message ON messages")
+    execute("DROP TRIGGER IF EXISTS update_search_message ON tags")
+    execute("DROP TRIGGER IF EXISTS update_search_message ON messages_tags")
 
     execute("DROP VIEW IF EXISTS partitioned_message")
 
