ba7eb3b58799f62313ca49e1a44075ac72b63b13
pankaj-ag <pankaj@coloredcow.in>
2020-06-15 15:12:22 +0530

    Add seeds scale

--- priv/repo/migrations/20200615073630_add_parent_id_to_messages.exs
+++ priv/repo/migrations/20200615073630_add_parent_id_to_messages.exs
@@ -4,6 +4,7 @@ defmodule Glific.Repo.Migrations.AddParentIdToMessages do
   def up do
     alter table(:messages) do
       add :parent_id, references(:messages)
+      add :ancestors, {:array, :bigint}
     end
 
     execute """
@@ -12,6 +13,10 @@ defmodule Glific.Repo.Migrations.AddParentIdToMessages do
     BEGIN
       IF (TG_OP = 'INSERT') THEN
         UPDATE messages SET parent_id = (SELECT id from messages where contact_id = NEW.contact_id and id < NEW.id ORDER BY id DESC LIMIT 1) where id = NEW.id;
+
+        UPDATE messages SET ancestors = (Array(SELECT parent_id from messages where contact_id = NEW.contact_id and id < NEW.id and parent_id is not NULL ORDER BY id DESC LIMIT 10))
+        where id = NEW.id;
+
         RETURN NEW;
       END IF;
       RETURN NULL;
@@ -31,6 +36,7 @@ defmodule Glific.Repo.Migrations.AddParentIdToMessages do
   def down do
     alter table(:messages) do
       remove :parent_id
+      remove :ancestors
     end
     execute "DROP FUNCTION update_message_parent() CASCADE;"
   end
