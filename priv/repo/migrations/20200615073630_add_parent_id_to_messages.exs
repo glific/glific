b2ce6ba742ae9d35f995440660d330789c8e191f
pankaj-ag <pankaj@coloredcow.in>
2020-06-15 20:02:14 +0530

    make single query

--- priv/repo/migrations/20200615073630_add_parent_id_to_messages.exs
+++ priv/repo/migrations/20200615073630_add_parent_id_to_messages.exs
@@ -7,13 +7,12 @@ defmodule Glific.Repo.Migrations.AddParentIdToMessages do
     execute """
     CREATE OR REPLACE FUNCTION update_message_parent()
     RETURNS trigger AS $$
+    DECLARE message_ids BIGINT[];
     BEGIN
       IF (TG_OP = 'INSERT') THEN
-        UPDATE messages SET parent_id = (SELECT id from messages where contact_id = NEW.contact_id and id < NEW.id ORDER BY id DESC LIMIT 1) where id = NEW.id;
+        message_ids := Array(SELECT id from messages where contact_id = NEW.contact_id and id < NEW.id ORDER BY id DESC LIMIT #{@ancestors_limit} );
 
-        UPDATE messages SET ancestors = (Array(SELECT parent_id from messages where contact_id = NEW.contact_id and id < NEW.id and parent_id is not NULL ORDER BY id DESC LIMIT #{@ancestors_limit}))
-        where id = NEW.id;
-
+        UPDATE messages SET parent_id = message_ids[1], ancestors = message_ids where id = NEW.id;
         RETURN NEW;
       END IF;
       RETURN NULL;
