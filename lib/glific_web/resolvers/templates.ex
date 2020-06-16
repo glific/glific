8c1d6c142b91640fbc94cc74ad84708eb5e0279e
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-06-16 23:20:27 +0530

    Templates: cleanup

--- lib/glific_web/resolvers/templates.ex
+++ lib/glific_web/resolvers/templates.ex
@@ -4,8 +4,8 @@ defmodule GlificWeb.Resolvers.Templates do
   one or more calls to resolve the incoming queries.
   """
 
-  alias Glific.Communications.Message, as: Communications
   alias Glific.{Repo, Templates, Templates.SessionTemplate}
+  alias Glific.{Communications, Contacts, Messages}
 
   @doc """
   Get a specific session template by id
@@ -73,29 +73,19 @@ defmodule GlificWeb.Resolvers.Templates do
           {:ok, any} | {:error, any}
   def send_session_message(_, %{id: id, receiver_id: receiver_id}, _) do
     {:ok, session_template} = Repo.fetch(SessionTemplate, id)
-    {:ok, receiver} = Repo.fetch(Glific.Contacts.Contact, receiver_id)
+    {:ok, receiver} = Repo.fetch(Contacts.Contact, receiver_id)
 
     message_params = %{
       body: session_template.body,
       type: session_template.type,
       media_id: session_template.message_media_id,
-      sender_id: Glific.Communications.Message.organization_contact_id(),
+      sender_id: Communications.Message.organization_contact_id(),
       receiver_id: receiver.id,
       contact_id: receiver.id
     }
 
-    with {:ok, message} <- Glific.Messages.create_message(message_params) do
-      send_message(message)
+    with {:ok, message} <- Messages.create_and_send_message(message_params) do
+      {:ok, %{message: message}}
     end
-  end
-
-  @spec send_message(Glific.Messages.Message.t()) :: {:ok, any}
-  defp send_message(message) do
-    message
-    |> Repo.preload([:receiver, :sender, :media])
-    |> Communications.send_message()
-
-    Communications.publish_message({:ok, message}, :sent_message)
-    {:ok, %{message: message}}
   end
 end
