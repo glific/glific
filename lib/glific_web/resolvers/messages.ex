641f742b42ad88bce18e308b7b2f183aacf51119
pankaj-ag <pankaj@coloredcow.in>
2020-06-16 21:56:54 +0530

    Cleanups

--- lib/glific_web/resolvers/messages.ex
+++ lib/glific_web/resolvers/messages.ex
@@ -63,15 +63,20 @@ defmodule GlificWeb.Resolvers.Messages do
   end
 
   @doc false
-  @spec send_message(nil, %{:id => nil | non_neg_integer()}, nil) ::
-          {:ok, Messages.Message.t()}
-  def send_message(_, %{id: id}, _), do: Messages.fetch_and_send_message(%{id: id})
+  @spec send_message(Absinthe.Resolution.t(), %{id: non_neg_integer()}, %{context: map()}) ::
+          {:ok, %{message: Message.t()}}
+  def send_message(_, %{id: id}, _) do
+    with {:ok, message} <- Messages.fetch_and_send_message(%{id: id}),
+         do: {:ok, %{message: message}}
+  end
 
   @doc false
   @spec create_and_send_message(Absinthe.Resolution.t(), %{input: map()}, %{context: map()}) ::
-          {:ok, Messages.Message.t()}
-  def create_and_send_message(_, %{input: params}, _),
-    do: Messages.create_and_send_message(params)
+          {:ok, %{message: Message.t()}}
+  def create_and_send_message(_, %{input: params}, _) do
+    with {:ok, message} <- Messages.create_and_send_message(params),
+         do: {:ok, %{message: message}}
+  end
 
   # Message Media Resolver which sits between the GraphQL schema and Glific
   # Message Context API.
