33c0b28e1e3197bb215e4794f3a002d2df8fa90f
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-06-18 16:05:07 +0530

    Verify otp: WIP

--- lib/glific/authentication.ex
+++ lib/glific/authentication.ex
@@ -11,10 +11,22 @@ defmodule Glific.Authentication do
   def create_and_send_otp_to_phone(args \\ %{}) do
     %{phone: phone} = args
 
-    PasswordlessAuth.create_and_send_verification_code(
-      phone
+    {:ok, otp} = PasswordlessAuth.create_and_send_verification_code(phone)
+
+    {:ok, "OTP #{otp} sent successfully to #{phone}"}
+  end
+
+  @doc """
+  Verifies otp and updates database with new contact
+  Removes otp from agent
+  """
+  @spec verify_otp(map()) :: String.t()
+  def verify_otp(%{phone: phone, otp: otp}) do
+    PasswordlessAuth.verify_code(
+      phone,
+      otp
     )
 
-    {:ok, "OTP sent successfully to #{phone}"}
+    {:ok, "OTP verified successfully for #{phone}"}
   end
 end
--- lib/glific_web/resolvers/authentication.ex
+++ lib/glific_web/resolvers/authentication.ex
@@ -5,10 +5,19 @@ defmodule GlificWeb.Resolvers.Authentication do
   """
 
   alias Glific.{Authentication}
+
   @doc false
   @spec send_otp(Absinthe.Resolution.t(), %{input: map()}, %{context: map()}) :: {:ok, String.t()}
   def send_otp(_, %{input: params}, _) do
     with {:ok, response_message} <- Authentication.create_and_send_otp_to_phone(params),
+         do: {:ok, response_message}
+  end
+
+  @doc false
+  @spec verify_otp(Absinthe.Resolution.t(), %{input: map()}, %{context: map()}) ::
+          {:ok, String.t()}
+  def verify_otp(_, %{input: params}, _) do
+    with {:ok, response_message} <- Authentication.verify_otp(params),
          do: {:ok, response_message}
   end
 end
