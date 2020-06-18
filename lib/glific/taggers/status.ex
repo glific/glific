09b30fe08e79f428f5a32ef6a859b3ec1ebc2876
pankaj-ag <pankaj@coloredcow.in>
2020-06-18 17:42:08 +0530

    cleanuos

--- lib/glific/taggers/status.ex
+++ lib/glific/taggers/status.ex
@@ -5,13 +5,13 @@ defmodule Glific.Taggers.Status do
   alias Glific.Messages.Message
   alias Glific.Taggers
 
-  # hardcoding greeting as 5, since this is our testcase
-  # need to handle keywords in tags
 
   @doc false
   @spec get_status_map :: %{String.t() => integer}
   def get_status_map, do: Glific.Tags.status_map()
 
+  @doc false
+  @spec is_new_contact(integer()) :: boolean()
   def is_new_contact(contact_id), do: Glific.Contacts.is_new_contact(contact_id)
 
 end
