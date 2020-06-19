738d90cabe5f9113335a20924dca90e2f93960aa
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-06-19 17:06:29 +0530

    Fix

--- config/config.exs
+++ config/config.exs
@@ -49,7 +49,7 @@ config :glific, :pow,
   repo: Glific.Repo
 
 config :passwordless_auth,
-  sms_provider: Glific.SMSAdapter.Gupshup
+  sms_adapter: Glific.SMSAdapter.Gupshup
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
