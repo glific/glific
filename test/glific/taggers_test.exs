750c65fe8adbbf1bbce90fd5707a24e45f500e09
Donald A. Lobo <donald.lobo@gmail.com>
2020-06-20 07:49:17 -0700

    lets get some tests going

--- test/glific/taggers_test.exs
+++ test/glific/taggers_test.exs
@@ -4,8 +4,8 @@ defmodule Glific.TaggersTest do
   alias Glific.{
     Messages.Message,
     Taggers,
+    Taggers.Keyword,
     Taggers.Numeric,
-    Taggers.Keyword
   }
 
   # and some random ordinals, hindi numbers, and emojis
@@ -14,12 +14,12 @@ defmodule Glific.TaggersTest do
     1 => "one",
     2 => "\u0968",
     3 => "तीन",
-    4 => to_string(['\u0034', 65039, 8419]),
+    4 => to_string(['\u0034', 65_039, 8419]),
     5 => "five",
     6 => "\u096C",
     7 => "saat",
     8 => "आठ",
-    9 => to_string(['\u0039', 65039, 8419]),
+    9 => to_string(['\u0039', 65_039, 8419])
   }
 
   @checker_punct %{
@@ -27,55 +27,72 @@ defmodule Glific.TaggersTest do
     1 => ":.# o n e  ",
     2 => " \u0968  ",
     3 => "तीन",
-    4 => to_string(['\u0034', 65039, 8419]),
+    4 => to_string(['\u0034', 65_039, 8419]),
     5 => "!@#five!@#",
     6 => "%^&\u096C:;:",
     7 => "  saat%$^",
     8 => "<>,.आठ  ",
-    9 => to_string(['\u0039', 65039, 8419]),
+    9 => to_string(['\u0039', 65_039, 8419])
   }
 
   @invalid %{
     "23" => nil,
     "आठ and more" => nil,
     "hundred" => nil,
-    "some gibberirsh" => nil,
+    "some gibberirsh" => nil
   }
 
   test "tag maps has got at least all the number from zero to twenty" do
     numeric_map = Numeric.get_numeric_map()
     Enum.map(0..19, fn x -> assert x == Map.get(numeric_map, to_string(x)) end)
 
-    Enum.map(@checker,
-      fn {k, v} -> assert Map.get(numeric_map, v) == k end)
+    Enum.map(
+      @checker,
+      fn {k, v} -> assert Map.get(numeric_map, v) == k end
+    )
   end
 
   test "string clean does a good jonb of cleaning string, but leaves unicode and hindi intact" do
-    Enum.map(@checker,
-      fn {_, v} -> assert Taggers.string_clean(v) == String.downcase(v) end)
+    Enum.map(
+      @checker,
+      fn {_, v} -> assert Taggers.string_clean(v) == String.downcase(v) end
+    )
   end
 
   test "check numeric tag body matches things in numeric map, and skips non matches" do
     numeric_map = Numeric.get_numeric_map()
-    Enum.map(@checker,
-      fn {k, v} -> assert Numeric.tag_body(v, numeric_map) == {:ok, to_string(k)} end)
 
-    Enum.map(@invalid,
-      fn {k, _} -> assert Numeric.tag_body(k, numeric_map) == :error end)
+    Enum.map(
+      @checker,
+      fn {k, v} -> assert Numeric.tag_body(v, numeric_map) == {:ok, to_string(k)} end
+    )
+
+    Enum.map(
+      @invalid,
+      fn {k, _} -> assert Numeric.tag_body(k, numeric_map) == :error end
+    )
   end
 
   test "check numeric tag message matches things in numeric map, and skips non matches" do
     numeric_map = Numeric.get_numeric_map()
-    Enum.map(@checker,
-      fn {k, v} -> assert Numeric.tag_message(%Message{body: v}, numeric_map) == {:ok, to_string(k)} end)
 
-    Enum.map(@checker_punct,
-      fn {k, v} -> assert Numeric.tag_message(%Message{body: v}, numeric_map) == {:ok, to_string(k)} end)
+    Enum.map(
+      @checker,
+      fn {k, v} ->
+        assert Numeric.tag_message(%Message{body: v}, numeric_map) == {:ok, to_string(k)}
+      end
+    )
 
-    Enum.map(@invalid,
-      fn {k, _} -> assert Numeric.tag_message(%Message{body: k}, numeric_map) == :error end)
+    Enum.map(
+      @checker_punct,
+      fn {k, v} ->
+        assert Numeric.tag_message(%Message{body: v}, numeric_map) == {:ok, to_string(k)}
+      end
+    )
+
+    Enum.map(
+      @invalid,
+      fn {k, _} -> assert Numeric.tag_message(%Message{body: k}, numeric_map) == :error end
+    )
   end
-
-
-
 end
