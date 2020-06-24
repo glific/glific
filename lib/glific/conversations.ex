43eaa1fe8f009d87e9d84c31fc74999b5a60286d
pankaj-ag <pankaj@coloredcow.in>
2020-06-24 13:22:43 +0530

    removed

--- lib/glific/conversations.ex
+++ lib/glific/conversations.ex
@@ -16,35 +16,45 @@ defmodule Glific.Conversations do
     SELECT conversation_message_ids(ids => $1, contact_limit => $2, contact_offset => $3, message_limit => $4, message_offset => $5)
   """
 
+  # still thinking that should we put them into config or not
+  @default_opts %{
+      message_opts: %{offset: 0, limit: 25},
+      contact_opts: %{offset: 0, limit: 10}
+  }
+
+
   @doc """
   Returns the last M conversations, each conversation not more than N messages
   """
   @spec list_conversations(map()) :: list()
-  def list_conversations(%{number_of_conversations: nc, size_of_conversations: sc} = args) do
-    Messages.list_conversations(Map.put(args, :ids, get_message_ids(nc, sc, args)))
+  def list_conversations(args) do
+    args = Map.merge(@default_opts, args)
+    Messages.list_conversations(Map.put(args, :ids, get_message_ids(args.contact_opts, args.message_opts, args)))
   end
 
   @doc """
   Returns the filtered conversation by contact id
   """
-  @spec conversation_by_id(map()) :: Conversation.t()
-  def conversation_by_id(%{contact_id: contact_id, size_of_conversations: sc} = args) do
+
+  @spec conversation_by_id(map()) :: Conversation.t() | nil
+  def conversation_by_id(%{contact_id: contact_id} = args) do
     args = put_in(args, [Access.key(:filter, %{}), :id], contact_id)
+    message_opts = Map.merge(@default_opts.message_opts, args.message_opts)
 
     case args
-         |> Map.put(:ids, get_message_ids(1, sc, args))
+         |> Map.put(:ids, get_message_ids(%{limit: 1}, message_opts, args))
          |> Messages.list_conversations() do
       [conversation] -> conversation
       _ -> nil
     end
   end
 
-  @spec get_message_ids(integer(), integer(), map() | nil) :: list()
-  defp get_message_ids(nc, sc, %{filter: %{id: id}}), do: get_message_ids([[id], nc, 0, sc, 0])
+  @spec get_message_ids(map(), map(), map() | nil) :: list()
+  defp get_message_ids(contact_opts, message_opts, %{filter: %{id: id}}), do: get_message_ids([[id], contact_opts.limit, 0, message_opts.limit, message_opts.offset])
 
-  defp get_message_ids(nc, sc, %{filter: %{ids: ids}}), do: get_message_ids([ids, nc, 0, sc, 0])
+  defp get_message_ids(contact_opts, message_opts, %{filter: %{ids: ids}}), do: get_message_ids([ids, contact_opts.limit, 0, message_opts.limit, message_opts.offset])
 
-  defp get_message_ids(nc, sc, _), do: get_message_ids([[], nc, 0, sc, 0])
+  defp get_message_ids(contact_opts, message_opts, _), do: get_message_ids([[], contact_opts.limit, contact_opts.offset, message_opts.limit, message_opts.offset])
 
   defp get_message_ids(opts) do
     {:ok, results} = Repo.query(@sql_ids, opts)
