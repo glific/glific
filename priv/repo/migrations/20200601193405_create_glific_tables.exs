385f6fdcac25a17a88f2f1a9e23affbaa334b89a
pankaj-ag <pankaj@coloredcow.in>
2020-06-30 18:23:18 +0530

    claanups

--- priv/repo/migrations/20200601193405_create_glific_tables.exs
+++ priv/repo/migrations/20200601193405_create_glific_tables.exs
@@ -513,7 +513,7 @@ defmodule Glific.Repo.Migrations.GlificTables do
       add :message_id, references(:messages, on_delete: :delete_all), null: false
 
       add :question_id, references(:questions, on_delete: :delete_all), null: false
-      add :question_sets_id, references(:question_sets, on_delete: :delete_all), null: false
+      add :question_set_id, references(:question_sets, on_delete: :delete_all), null: false
 
       # for now all answers are stored as string
       # at some point, we might split it based on question type
