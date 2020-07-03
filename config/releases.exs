e51c1c028e6176bd9f8558c5f022dc69cffbcd7b
satendra-sr <officialsatendrarawat@gmail.com>
2020-07-03 18:42:46 +0530

    Updated release exs

--- config/releases.exs
+++ config/releases.exs
@@ -20,7 +20,5 @@ secret_key_base = System.get_env("SECRET_KEY_BASE") ||
   You can generate one by calling: mix phx.gen.secret
   """ 
 config :glific, GlificWeb.Endpoint,
-  server: true,
   http: [:inet6, port: 4000],
-  secret_key_base: secret_key_base,
-  url: [host: System.get_env("BASE_URL"), port: 80]
+  secret_key_base: secret_key_base
