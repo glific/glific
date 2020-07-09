c67c1ae990b2311c2f5b898331f8a858de8aa910
pankaj-ag <pankaj@coloredcow.in>
2020-07-09 19:18:27 +0530

    cleanups

---
+++ lib/glific/flows.ex
@@ -0,0 +1,126 @@
+defmodule Glific.Flows do
+  @moduledoc """
+  The Flows context.
+  """
+
+  import Ecto.Query, warn: false
+  alias Glific.Repo
+
+  alias Glific.Flows.Flow
+  alias Glific.Flows.FlowRevision
+
+  @doc """
+  Returns the list of flows.
+
+  ## Examples
+
+      iex> list_flows()
+      [%Flow{}, ...]
+
+  """
+  def list_flows do
+    Repo.all(Flow)
+  end
+
+  @doc """
+  Gets a single flow.
+
+  Raises `Ecto.NoResultsError` if the Flow does not exist.
+
+  ## Examples
+
+      iex> get_flow!(123)
+      %Flow{}
+
+      iex> get_flow!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_flow!(id), do: Repo.get!(Flow, id)
+
+  @doc """
+  Creates a flow.
+
+  ## Examples
+
+      iex> create_flow(%{field: value})
+      {:ok, %Flow{}}
+
+      iex> create_flow(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_flow(attrs \\ %{}) do
+    %Flow{}
+    |> Flow.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a flow.
+
+  ## Examples
+
+      iex> update_flow(flow, %{field: new_value})
+      {:ok, %Flow{}}
+
+      iex> update_flow(flow, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_flow(%Flow{} = flow, attrs) do
+    flow
+    |> Flow.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a flow.
+
+  ## Examples
+
+      iex> delete_flow(flow)
+      {:ok, %Flow{}}
+
+      iex> delete_flow(flow)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_flow(%Flow{} = flow) do
+    Repo.delete(flow)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking flow changes.
+
+  ## Examples
+
+      iex> change_flow(flow)
+      %Ecto.Changeset{data: %Flow{}}
+
+  """
+  def change_flow(%Flow{} = flow, attrs \\ %{}) do
+    Flow.changeset(flow, attrs)
+  end
+
+  def get_flow_revision(flow_id) do
+    get_flow!(flow_id)
+    |> Repo.preload(:revisions)
+  end
+
+  def create_flow_revision(definition) do
+    flow_id = 4
+    flow = get_flow!(flow_id) |> Repo.preload(:revisions)
+
+    attrs = %{
+      definition: definition,
+      flow_id: flow_id,
+      revision_number: length(flow.revisions) + 1
+    }
+
+    %FlowRevision{}
+    |> FlowRevision.changeset(attrs)
+    |> Repo.insert()
+
+  end
+end
