d0ec765ee91b70232ae3210a2fed43a411f155f8
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-07-21 11:57:01 +0530

    Action tests for execute

--- test/glific/flows/action_test.exs
+++ test/glific/flows/action_test.exs
@@ -1,10 +1,23 @@
 defmodule Glific.Flows.ActionTest do
   use Glific.DataCase, async: true
 
+  alias Glific.{
+    Contacts,
+    Settings
+  }
+
   alias Glific.Flows.{
     Action,
+    FlowContext,
     Node
   }
+
+  setup do
+    default_provider = Glific.SeedsDev.seed_providers()
+    Glific.SeedsDev.seed_organizations(default_provider)
+    Glific.SeedsDev.seed_contacts()
+    :ok
+  end
 
   test "process extracts the right values from json for enter_flow action" do
     node = %Node{uuid: "Test UUID"}
@@ -116,5 +129,140 @@ defmodule Glific.Flows.ActionTest do
 
     json = %{}
     assert_raise ArgumentError, fn -> Action.process(json, %{}, node) end
+  end
+
+  test "execute an action when type is send_msg" do
+    [contact | _] = Contacts.list_contacts(%{filter: %{name: "Default receiver"}})
+
+    # preload contact
+    context = %FlowContext{contact_id: contact.id} |> Repo.preload(:contact)
+
+    action = %Action{type: "send_msg", text: "This is a test message"}
+
+    message_stream = []
+
+    result = Action.execute(action, context, message_stream)
+
+    assert {:ok, updated_context, updated_message_stream} = result
+
+    message =
+      Glific.Messages.Message
+      |> where([m], m.contact_id == ^contact.id)
+      |> Ecto.Query.last()
+      |> Repo.one()
+
+    assert message.body == "This is a test message"
+  end
+
+  test "execute an action when type is set_contact_language" do
+    language_label = "English (United States)"
+    [language | _] = Settings.list_languages(%{label: language_label})
+
+    [contact | _] = Contacts.list_contacts(%{filter: %{name: "Default receiver"}})
+
+    # preload contact
+    context = %FlowContext{contact_id: contact.id} |> Repo.preload(:contact)
+
+    action = %Action{type: "set_contact_language", text: "English (United States)"}
+
+    message_stream = []
+
+    result = Action.execute(action, context, message_stream)
+
+    assert {:ok, updated_context, updated_message_stream} = result
+    assert updated_context.contact.language_id == language.id
+  end
+
+  test "execute an action when type is set_contact_name" do
+    [contact | _] = Contacts.list_contacts(%{filter: %{name: "Default receiver"}})
+
+    # preload contact
+    context = %FlowContext{contact_id: contact.id} |> Repo.preload(:contact)
+
+    action = %Action{type: "set_contact_name", value: "Updated Name"}
+
+    message_stream = []
+
+    result = Action.execute(action, context, message_stream)
+
+    assert {:ok, updated_context, updated_message_stream} = result
+    assert updated_context.contact.name == action.value
+  end
+
+  test "execute an action when type is set_contact_field to set contact preferences" do
+    [contact | _] = Contacts.list_contacts(%{filter: %{name: "Default receiver"}})
+
+    context =
+      %FlowContext{contact_id: contact.id, results: %{"test_result" => "preference1"}}
+      |> Repo.preload(:contact)
+
+    action = %Action{
+      type: "set_contact_field",
+      value: "@results.test_result",
+      field: %{key: "settings"}
+    }
+
+    message_stream = []
+
+    result = Action.execute(action, context, message_stream)
+
+    assert {:ok, updated_context, updated_message_stream} = result
+    assert updated_context.contact.settings["preferences"]["preference1"] == true
+  end
+
+  test "execute an action when type is set_contact_field to add contact field" do
+    [contact | _] = Contacts.list_contacts(%{filter: %{name: "Default receiver"}})
+
+    context =
+      %FlowContext{contact_id: contact.id, results: %{"test_result" => "field1"}}
+      |> Repo.preload(:contact)
+
+    action = %Action{
+      type: "set_contact_field",
+      value: "@results.test_result",
+      field: %{key: "not_settings"}
+    }
+
+    message_stream = []
+
+    result = Action.execute(action, context, message_stream)
+
+    assert {:ok, updated_context, updated_message_stream} = result
+
+    assert updated_context.contact.fields[action.field.key].value == "field1"
+    assert updated_context.contact.fields[action.field.key].type == "string"
+  end
+
+  test "execute an action when type is enter_flow" do
+    [contact | _] = Contacts.list_contacts(%{filter: %{name: "Default receiver"}})
+
+    # preload contact
+    context = %FlowContext{contact_id: contact.id} |> Repo.preload(:contact)
+
+    # using uuid of language flow
+    action = %Action{
+      type: "enter_flow",
+      uuid: "UUID 1",
+      enter_flow_uuid: "f5f0c89e-d5f6-4610-babf-ca0f12cbfcbf",
+      node_uuid: "Test UUID"
+    }
+
+    message_stream = []
+
+    result = Action.execute(action, context, message_stream)
+
+    assert {:ok, updated_context, updated_message_stream} = result
+
+    assert updated_context == context
+  end
+
+  test "execute an action when type is not supported" do
+    action = %Action{type: "others"}
+    context = %FlowContext{}
+    message_stream = []
+
+    assert_raise UndefinedFunctionError, fn ->
+      Action.execute(action, context, message_stream)
+    end
   end
 end
