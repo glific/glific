8e8ea79932d1ef03280bd54cc0a750331d90acfa
Donald A. Lobo <donald.lobo@gmail.com>
2020-07-21 10:10:09 -0700

    get localization in a format where we can send localized messages

--- lib/glific/flows/localization.ex
+++ lib/glific/flows/localization.ex
@@ -7,6 +7,8 @@ defmodule Glific.Flows.Localization do
 
   use Ecto.Schema
 
+  alias Glific.Settings
+
   @type t() :: %__MODULE__{
           localizations: map() | nil
         }
@@ -15,28 +17,35 @@ defmodule Glific.Flows.Localization do
     field :localizations, :map
   end
 
+  # given a json snippet containing all the translation for a specific language
+  # store them in a uuid map
+  @spec process_translation(map()) :: map()
+  defp process_translation(json) do
+    Enum.reduce(
+      json,
+      %{},
+      fn {uuid, values}, acc ->
+        Map.put(acc, uuid, hd(values["text"]))
+      end
+    )
+  end
+
   @doc """
   Process a json structure from floweditor to the Glific data types
   """
   @spec process(map()) :: Localization.t()
   def process(json) do
+    language_map = Settings.locale_id_map()
     %Localization{
       localizations:
         json
         |> Enum.reduce(
           %{},
           fn {language, translations}, acc ->
-            Map.put(
-              acc,
-              language,
-              Enum.reduce(
-                translations,
-                %{},
-                fn {uuid, values}, acc ->
-                  Map.put(acc, uuid, hd(values["text"]))
-                end
-              )
-            )
+            translated = process_translation(translations)
+            acc
+            |> Map.put(language, translated)
+            |> Map.put(language_map[language], translated)
           end
         )
     }
