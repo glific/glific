973fdfc6ab42e284ae9c681324a92a04b378b00e
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-07-22 20:21:34 +0530

    Cleanup

--- lib/glific/taggers/tagger_helper.ex
+++ lib/glific/taggers/tagger_helper.ex
@@ -25,6 +25,12 @@ defmodule Glific.Taggers.TaggerHelper do
     {:ok, message}
   end
 
+  @spec tag_outbound_message(Message.t()) :: :ok
+  def tag_outbound_message(message) do
+    Tags.remove_tag_from_all_message(message.contact_id, "Not Responded")
+    add_tag(message, "Not Responded")
+  end
+
   @spec add_tag(Message.t(), String.t()) :: Message.t()
   defp add_tag(message, tag_label) do
     {:ok, tag} = Repo.fetch_by(Tag, %{label: tag_label})
@@ -51,55 +57,6 @@ defmodule Glific.Taggers.TaggerHelper do
 
   @spec remove_not_responded_tag(Message.t()) :: Message.t()
   defp remove_not_responded_tag(message) do
-    # Remove "Not Responded" tag from last outbound message
-    {:ok, tag} = Repo.fetch_by(Glific.Tags.Tag, %{label: "Not Responded"})
-
-    # To fix: don't remove tag if message is not yet delivered
-    with last_outbound_message when last_outbound_message != nil <-
-           Message
-           |> where([m], m.receiver_id == ^message.contact_id)
-           |> where([m], m.flow == "outbound")
-           |> where([m], m.status == "sent")
-           |> Ecto.Query.last()
-           |> Repo.one(),
-         message_tag when message_tag != nil <-
-           Glific.Tags.MessageTag
-           |> where([mt], mt.tag_id == ^tag.id)
-           |> where([mt], mt.message_id == ^last_outbound_message.id)
-           |> Ecto.Query.last()
-           |> Repo.one(),
-         do: Glific.Tags.delete_message_tag(message_tag)
-  end
-
-  @spec tag_outbound_message(Message.t()) :: :ok
-  def tag_outbound_message(message) do
-    # Add "Not Responded" tag to message
-    {:ok, tag} = Repo.fetch_by(Glific.Tags.Tag, %{label: "Not Responded"})
-
-    {:ok, _} =
-      Glific.Tags.create_message_tag(%{
-        message_id: message.id,
-        tag_id: tag.id
-      })
-
-    # Remove not responded tag from last outbound message if any
-    # don't remove tag if message is not yet delivered
-    with last_outbound_message when last_outbound_message != nil <-
-           Message
-           |> where([m], m.id != ^message.id)
-           |> where([m], m.receiver_id == ^message.receiver_id)
-           |> where([m], m.flow == "outbound")
-           |> where([m], m.status == "sent")
-           |> Ecto.Query.last()
-           |> Repo.one(),
-         message_tag when message_tag != nil <-
-           Glific.Tags.MessageTag
-           |> where([m], m.tag_id == ^tag.id)
-           |> where([m], m.message_id == ^last_outbound_message.id)
-           |> Ecto.Query.last()
-           |> Repo.one(),
-         do: Glific.Tags.delete_message_tag(message_tag)
-
-    :ok
+    Tags.remove_tag_from_all_message(message.contact_id, "Not Responded")
   end
 end
