eaaeb441b64b32855af1c7b492e33573d3f1b8ff
satendra-sr <officialsatendrarawat@gmail.com>
2020-07-27 13:45:50 +0530

    ssl for backend

--- config/releases.exs
+++ config/releases.exs
@@ -10,6 +10,7 @@ db_database = System.get_env("DATABASE_DB") || "postgres"
 db_username = System.get_env("DATABASE_USER") || "postgres" 
 db_password = System.get_env("DATABASE_PASSWORD") || "postgres"
 db_url = "ecto://#{db_username}:#{db_password}@#{db_host}/#{db_database}"
+ssl_port = System.get_env("SSL_PORT") || 4001
 config :glific, Glific.Repo,
   url:  db_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
@@ -21,7 +22,12 @@ secret_key_base = System.get_env("SECRET_KEY_BASE") ||
   """ 
 config :glific, GlificWeb.Endpoint,
   server: true,
-  http: [:inet6, port: 4000],
-  https: false,
+  https: [
+    port: 4001, # 443 is already used by tides (laravel)
+    cipher_suite: :strong,
+    keyfile: System.get_env("TIDES_SSL_KEY_PATH"),
+    certfile: System.get_env("TIDES_APP_SSL_CERT_PATH"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
   secret_key_base: secret_key_base,
-  url: [host: System.get_env("BASE_URL"), port: 4000]
+  url: [host: System.get_env("BASE_URL"), port: ssl_port]
