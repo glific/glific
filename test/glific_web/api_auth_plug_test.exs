e002ecb6964da0fe31ab55c25b1602585f104c9b
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-07-31 20:18:30 +0530

    Test

--- test/glific_web/api_auth_plug_test.exs
+++ test/glific_web/api_auth_plug_test.exs
@@ -50,4 +50,25 @@ defmodule GlificWeb.APIAuthPlugTest do
   end
 
   defp with_auth_header(conn, token), do: Plug.Conn.put_req_header(conn, "authorization", token)
+
+  test "delete all existing sessions of a user", %{conn: conn, user: user} do
+    assert {%{private: %{api_access_token: access_token, api_renewal_token: _renewal_token}},
+            ^user} = APIAuthPlug.create(conn, user, @pow_config)
+
+    assert {%{private: %{api_access_token: access_token2, api_renewal_token: _renewal_token2}},
+            ^user} = APIAuthPlug.create(conn, user, @pow_config)
+
+    :timer.sleep(100)
+
+    assert {_conn, ^user} = APIAuthPlug.fetch(with_auth_header(conn, access_token), @pow_config)
+    assert {_conn, ^user} = APIAuthPlug.fetch(with_auth_header(conn, access_token2), @pow_config)
+
+    APIAuthPlug.delete_all_user_sessions(@pow_config, user)
+
+    :timer.sleep(100)
+
+    # existing sessions should be deleted
+    assert {_conn, nil} = APIAuthPlug.fetch(with_auth_header(conn, access_token), @pow_config)
+    assert {_conn, nil} = APIAuthPlug.fetch(with_auth_header(conn, access_token2), @pow_config)
+  end
 end
