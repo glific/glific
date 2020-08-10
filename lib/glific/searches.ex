04de2778d721d839e923c06063beee41f2f9bade
pankaj-ag <pankaj@coloredcow.in>
2020-08-10 12:15:56 +0530

    format

--- lib/glific/searches.ex
+++ lib/glific/searches.ex
@@ -125,13 +125,14 @@ defmodule Glific.Searches do
   end
 
   @spec do_save_search(map()) :: SavedSearch.t() | nil
-  defp do_save_search(%{save_search_input: save_search_input} = args) when save_search_input != nil,
-    do:
-      create_saved_search(%{
-        label: args.save_search_input.label,
-        shortcode: args.save_search_input.shortcode,
-        args: args
-      })
+  defp do_save_search(%{save_search_input: save_search_input} = args)
+       when save_search_input != nil,
+       do:
+         create_saved_search(%{
+           label: args.save_search_input.label,
+           shortcode: args.save_search_input.shortcode,
+           args: args
+         })
 
   defp do_save_search(_args), do: nil
 
