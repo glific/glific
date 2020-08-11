689bfb6ad14c2d34e66ca487ae3ff9f6e5da9a3a
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-08-11 18:35:17 +0530

    update group users test

--- test/glific_web/schema/user_group_test.exs
+++ test/glific_web/schema/user_group_test.exs
@@ -7,6 +7,7 @@ defmodule GlificWeb.Schema.UserGroupTest do
     Groups.Group,
     Repo,
     Seeds.SeedsDev,
+    Users,
     Users.User
   }
 
@@ -18,6 +19,46 @@ defmodule GlificWeb.Schema.UserGroupTest do
 
   load_gql(:create, GlificWeb.Schema, "assets/gql/user_groups/create.gql")
   load_gql(:delete, GlificWeb.Schema, "assets/gql/user_groups/delete.gql")
+  load_gql(:update_group_users, GlificWeb.Schema, "assets/gql/user_groups/update_group_users.gql")
+
+  test "update group users" do
+    label = "Default Group"
+    {:ok, group} = Repo.fetch_by(Group, %{label: label})
+
+    [user1, user2 | _] = Users.list_users()
+
+    # add group users
+    result =
+      query_gql_by(:update_group_users,
+        variables: %{
+          "input" => %{
+            "group_id" => group.id,
+            "add_user_ids" => [user1.id, user2.id],
+            "delete_user_ids" => []
+          }
+        }
+      )
+
+    assert {:ok, query_data} = result
+    group_users = get_in(query_data, [:data, "updateGroupUsers", "groupUsers"])
+    assert length(group_users) == 2
+
+    # delete group users
+    result =
+      query_gql_by(:update_group_users,
+        variables: %{
+          "input" => %{
+            "group_id" => group.id,
+            "add_user_ids" => [],
+            "delete_user_ids" => [user1.id]
+          }
+        }
+      )
+
+    assert {:ok, query_data} = result
+    numberDeleted = get_in(query_data, [:data, "updateGroupUsers", "numberDeleted"])
+    assert numberDeleted == 1
+  end
 
   test "create a user group and test possible scenarios and errors" do
     label = "Default Group"
