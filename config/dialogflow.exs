b588aae3b2cb3d809186e04aa49fe0bcdebebb85
Donald A. Lobo <donald.lobo@gmail.com>
2020-08-12 14:47:46 -0700

    get checks to pass

--- config/dialogflow.exs
+++ config/dialogflow.exs
@@ -8,23 +8,21 @@ config :glific,
 
 # config goth and fake it while we are still developing
 
-fake_goth_json =
-  """
-  {
-  "project_id": "DOES NOT EXISTS",
-  "private_key_id": "DOES NOT EXIST",
-  "private_key": "INVALID PRIVATE KEY"
-  }
-  """
+fake_goth_json = """
+{
+"project_id": "DOES NOT EXISTS",
+"private_key_id": "DOES NOT EXIST",
+"private_key": "INVALID PRIVATE KEY"
+}
+"""
 
 # for now this is optional, so we check for file exists, need
 # a more robust solution going forward
 goth_json =
-if File.exists?("config/.dialogflow.credentials.json"),
-  do: File.read!("config/.dialogflow.credentials.json"),
-  else: fake_goth_json
-
+  if File.exists?("config/.dialogflow.credentials.json"),
+    do: File.read!("config/.dialogflow.credentials.json"),
+    else: fake_goth_json
 
 # config goth
 config :goth,
-json: goth_json
+  json: goth_json
