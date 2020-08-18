025e5c4940c131ac93c041418b785e0d6fa43eaf
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-08-18 22:13:59 +0530

    table updated with flow_uuid

--- lib/glific/flows/flow_count.ex
+++ lib/glific/flows/flow_count.ex
@@ -14,14 +14,14 @@ defmodule Glific.Flows.FlowCount do
     Repo
   }
 
-  @required_fields [:uuid, :flow_id, :type]
+  @required_fields [:uuid, :flow_uuid, :type]
   @optional_fields [:destination_uuid, :recent_messages]
 
   @type t() :: %__MODULE__{
           __meta__: Ecto.Schema.Metadata.t(),
           id: non_neg_integer | nil,
           uuid: Ecto.UUID.t() | nil,
-          flow_id: non_neg_integer | nil,
+          flow_uuid: Ecto.UUID.t() | nil,
           flow: Flow.t() | Ecto.Association.NotLoaded.t() | nil,
           type: String.t() | nil,
           count: integer() | nil,
@@ -33,6 +33,7 @@ defmodule Glific.Flows.FlowCount do
 
   schema "flow_counts" do
     field :uuid, Ecto.UUID
+    field :flow_uuid, Ecto.UUID
     belongs_to :flow, Flow
     field :type, :string
     field :count, :integer
@@ -58,15 +59,11 @@ defmodule Glific.Flows.FlowCount do
   @spec upsert_flow_count(map()) :: :error | FlowCount.t()
   def upsert_flow_count(%{flow_uuid: nil} = _attrs), do: :error
 
-  def upsert_flow_count(%{flow_uuid: flow_uuid} = attrs) do
-    {:ok, flow} = Repo.fetch_by(Flow, %{uuid: flow_uuid})
-
-    attrs = Map.merge(attrs, %{flow_id: flow.id})
-
+  def upsert_flow_count(%{flow_uuid: _flow_uuid} = attrs) do
     Repo.insert!(
       FlowCount.changeset(%FlowCount{}, attrs),
       on_conflict: [inc: [count: 1]],
-      conflict_target: [:flow_id, :uuid, :type]
+      conflict_target: [:flow_uuid, :uuid, :type]
     )
     |> update_recent_messages(attrs)
   end
--- priv/repo/migrations/20200817115335_add_flow_counts_table.exs
+++ priv/repo/migrations/20200817115335_add_flow_counts_table.exs
@@ -2,12 +2,15 @@ defmodule Glific.Repo.Migrations.AddFlowCountsTable do
   use Ecto.Migration
 
   def change do
+    create unique_index(:flows, :uuid)
+
     create table(:flow_counts) do
       add :uuid, :uuid, null: false
 
       add :destination_uuid, :uuid, null: true
 
-      add :flow_id, references(:flows, on_delete: :delete_all), null: false
+      add :flow_uuid, references(:flows, column: :uuid, type: :uuid, on_delete: :delete_all),
+        null: false
 
       # Options are: node, exit, case
       add :type, :string
@@ -19,6 +22,6 @@ defmodule Glific.Repo.Migrations.AddFlowCountsTable do
       timestamps(type: :utc_datetime)
     end
 
-    create unique_index(:flow_counts, [:uuid, :flow_id, :type])
+    create unique_index(:flow_counts, [:uuid, :flow_uuid, :type])
   end
 end
