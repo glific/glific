6e1706652bbc96e919f1466394e1b3dab24b4465
pankaj-ag <pankaj@coloredcow.in>
2020-08-20 19:53:40 +0530

    cleanuos

--- lib/glific/users/roles.ex
+++ lib/glific/users/roles.ex
@@ -7,15 +7,24 @@ defmodule Glific.EctoRoles do
 
   alias Glific.Users.User
 
+  @doc false
+  @spec type :: :map
   def type, do: :map
 
   # Provide custom casting rules.
-  def cast(data) when is_list(data), do: {:ok, data}
+  @doc """
+  Cast the user roles to list
+  """
+  @spec cast(list()) :: {:ok, list()}
+  def cast(data), do: {:ok, data}
 
-  # When loading data from the database, as long as it's a list,
-  # we just put the data into a list of maps to be stored in
-  # the loaded schema struct.
-  def load(data) when is_list(data) do
+  @doc """
+    When loading data from the database, as long as it's a list,
+    we just put the data into a list of maps to be stored in
+    the loaded schema struct.
+  """
+  @spec load(list()) :: {:ok, list()}
+  def load(data) do
     list =
       User.get_roles_list()
       |> Enum.filter(fn role -> role.label in data end)
@@ -23,12 +32,13 @@ defmodule Glific.EctoRoles do
     {:ok, list}
   end
 
-  # When dumping data to the database, we *expect* a list of maps
-  # but any value could be inserted into the schema struct at runtime,
-  # so we need to guard against them.
-  def dump(roles_map_list) when is_list(roles_map_list) do
+  @doc """
+    When dumping data to the database, we *expect* a list of maps
+    but any value could be inserted into the schema struct at runtime,
+    so we need to guard against them.
+  """
+  @spec dump(list()) :: {:ok, list()}
+  def dump(roles_map_list) do
     {:ok, roles_map_list}
   end
-
-  def dump(_), do: :error
 end
