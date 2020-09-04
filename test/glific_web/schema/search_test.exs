0b2540213631477ad0385e172efaa482cae65f29
pankaj-ag <pankaj@coloredcow.in>
2020-09-04 19:54:12 +0530

    cleanups

--- test/glific_web/schema/search_test.exs
+++ test/glific_web/schema/search_test.exs
@@ -513,6 +513,15 @@ defmodule GlificWeb.Schema.SearchTest do
 
   test "search with the date range filters will returns the conversations", %{user: user} do
     message = Fixtures.message_fixture()
+              |> Repo.preload([:contact])
+
+    contact_count = Contacts.count_contacts(%{filter: %{organization_id: user.organization_id}})
+
+
+    {:ok, contact} =
+        Contacts.update_contact(message.contact,
+        %{last_message_at: DateTime.utc_now()}
+    )
 
     result =
       auth_query_gql_by(:search, user,
@@ -520,19 +529,21 @@ defmodule GlificWeb.Schema.SearchTest do
           "filter" => %{
             "term" => "",
             "dateRange" => %{
-              "from" =>
-                DateTime.utc_now() |> DateTime.to_date() |> Date.add(-2) |> Date.to_string(),
+              "from" => DateTime.utc_now() |> DateTime.to_date() |> Date.add(-2) |> Date.to_string(),
               "to" => DateTime.utc_now() |> DateTime.to_date() |> Date.to_string()
             }
           },
-          "contactOpts" => %{"limit" => 1},
+          "contactOpts" => %{"limit" => contact_count},
           "messageOpts" => %{"limit" => 1}
         }
       )
 
     assert {:ok, query_data} = result
 
-    assert get_in(query_data, [:data, "search", Access.at(0), "contact", "id"]) ==
-             "#{message.contact_id}"
+    conatct_ids = Enum.reduce(query_data[:data]["search"], [], fn row, acc ->
+      acc ++ [row["contact"]["id"]]
+      end)
+
+    assert "#{contact.id}" in conatct_ids
   end
 end
