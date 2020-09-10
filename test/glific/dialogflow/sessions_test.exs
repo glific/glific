308c6cb3829cd9e338edb681d38a29533d1150d6
pankaj-ag <pankaj@coloredcow.in>
2020-09-10 23:30:34 +0530

    clean

--- test/glific/dialogflow/sessions_test.exs
+++ test/glific/dialogflow/sessions_test.exs
@@ -2,10 +2,13 @@ defmodule Glific.Dialogflow.SessionsTest do
   use Glific.DataCase, async: true
   use Oban.Testing, repo: Glific.Repo
 
+  import Mock
+
   alias Glific.Dialogflow.Sessions
   alias Glific.Dialogflow.SessionWorker
   alias Glific.Fixtures
   alias Glific.Messages
+  alias Goth.Token
 
   @query %{
     "queryResult" => %{
@@ -36,18 +39,40 @@ defmodule Glific.Dialogflow.SessionsTest do
   end
 
   test "detect_intent/2 will add the message to queue" do
-    message =
+    with_mocks([
+      {
+        Token,
+        [:passthrough],
+        [for_scope: fn _url -> {:ok, %{token: "0xFAKETOKEN_Q="}} end]
+      },
+      {
+        HTTPoison,
+        [:passthrough],
+        [
+          request: fn _method, _url, _params, _headers ->
+            body = Poison.encode!(@query)
+            {:ok, %HTTPoison.Response{status_code: 200, body: body}}
+          end
+        ]
+      }
+    ]) do
+      message =
       Fixtures.message_fixture(%{body: "Hola"})
       |> Repo.preload(contact: [:language])
 
-    Sessions.detect_intent(message, "1e8118272e2f69ea6ec98acbb71ab959")
-    assert_enqueued(worker: SessionWorker)
-    Oban.drain_queue(queue: :dialogflow)
+      Sessions.detect_intent(message, "1e8118272e2f69ea6ec98acbb71ab959")
+      assert_enqueued(worker: SessionWorker)
+      assert %{success: 1, failure: 0} == Oban.drain_queue(queue: :dialogflow)
 
-    message =
-      Messages.get_message!(message.id)
-      |> Repo.preload([:tags])
+      message =
+        Messages.get_message!(message.id)
+        |> Repo.preload([:tags])
 
-    assert hd(message.tags).label == "Greeting"
+      assert hd(message.tags).label == "Greeting"
+
+    end
+
+
+
   end
 end
