f9f80a2ceaaa53f8f6bc7e0fa65b4d475636cb35
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-09-16 10:59:27 +0530

    Using Tesla instead of HTTPoison

--- priv/repo/seeds/20200723172939_add_glific_data.exs
+++ priv/repo/seeds/20200723172939_add_glific_data.exs
@@ -486,9 +486,9 @@ defmodule Glific.Repo.Seeds.AddGlificData do
 
   def opted_in_contacts(organization) do
     with {:ok, url} <- Application.fetch_env(:glific, :provider_optin_list_url),
-         {:ok, api_key} <- Application.fetch_env(:glific, :provider_key),
-         {:ok, response} <- HTTPoison.get(url, [{"apikey", api_key}]),
-         {:ok, response_data} <- Poison.decode(response.body),
+         {:ok, provider_key} <- Application.fetch_env(:glific, String.to_atom("provider_key_#{organization.id}")),
+         {:ok, response} <- Tesla.get(url, headers: [{"apikey", provider_key}]),
+         {:ok, response_data} <- Jason.decode(response.body),
          false <- is_nil(response_data["users"]) do
       users = response_data["users"]
 
