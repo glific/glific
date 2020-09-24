042c33a9530c988e2c4c3c733dc41783d0a37438
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-09-24 23:49:29 +0530

    migration for credentials table

---
+++ priv/repo/migrations/20200924193405_v0_4_1_alter_glific_tables.exs
@@ -0,0 +1,31 @@
+defmodule Glific.Repo.Migrations.V041AlterGlificTables do
+  use Ecto.Migration
+
+  @moduledoc """
+  v0.4.1 Alter Glific tables
+  """
+
+  def change do
+    organization_credentials()
+  end
+
+  defp organization_credentials do
+    create table(:organization_credentials) do
+      # shortcode for service name
+      add :shortcode, :string
+
+      # all the service keys which doesn't need ecryption
+      add :regular_keys, :jsonb, default: "{}"
+
+      # we will keep these keys encrypted
+      add :secret_keys, :jsonb, default: "{}"
+
+      # foreign key to organization restricting scope of this table to this organization only
+      add :organization_id, references(:organizations, on_delete: :delete_all), null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create unique_index(:organization_credentials, [:shortcode, :organization_id])
+  end
+end
