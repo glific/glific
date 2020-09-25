eecf2cfd54a5935234c328fd5e1c78e88b70a19f
pankaj-ag <pankaj@coloredcow.in>
2020-09-25 17:03:41 +0530

    Updates

--- lib/glific/flows/case.ex
+++ lib/glific/flows/case.ex
@@ -110,7 +110,7 @@ defmodule Glific.Flows.Case do
 
   def execute(%{type: type}, _context, msg)
       when type == "has_media",
-      do: Enum.member?([:text, :location, nil], msg.type)
+      do: if Enum.member?([:text, :location, nil], msg.type), do: false, else: true
 
   def execute(c, _context, _msg),
     do:
