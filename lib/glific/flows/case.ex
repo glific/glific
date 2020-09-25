7d91779b1dce62ee7d30861e8925b076abf5dc50
Pankaj Agrawal <pankaj@coloredcow.com>
2020-09-25 14:12:35 +0530

    fixes

--- lib/glific/flows/case.ex
+++ lib/glific/flows/case.ex
@@ -108,7 +108,7 @@ defmodule Glific.Flows.Case do
       when type == "has_only_phrase" or type == "has_only_text",
       do: strip(c.arguments) == strip(msg)
 
-  def execute(%{type: type} = c, _context, msg)
+  def execute(%{type: type}, _context, msg)
       when type == "has_media",
       do: Enum.member?([:text, :location, nil], msg.type)
 
