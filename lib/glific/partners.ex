f9a03302317fa3f853238d0dda007bf5ba6f630a
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-09-28 12:46:30 +0530

    Fix for fetch opted in contacts

--- lib/glific/partners.ex
+++ lib/glific/partners.ex
@@ -301,20 +301,6 @@ defmodule Glific.Partners do
     Organization.changeset(organization, attrs)
   end
 
-  @spec get_provider_key(non_neg_integer) :: String.t()
-  defp get_provider_key(organization_id) do
-    case get_credential(%{organization_id: organization_id, shortcode: "provider"}) do
-      {:ok, credentials} ->
-        credentials.secrets["provider_key"]
-
-      {:error, _} ->
-        # raise ArgumentError
-        # we need to decide if we want to put this key in the application
-        # environment, but this is the only place we read it and cache it
-        nil
-    end
-  end
-
   @doc """
   Cache the entire organization structure.
 
@@ -412,7 +398,7 @@ defmodule Glific.Partners do
   defp set_provider_info(organization) do
     {:ok, credential} = Repo.fetch_by(Credential,
       %{organization_id: organization.id,
-        provider_id: organization.provider.id})
+        provider_id: organization.provider_id})
     organization
     |> Map.put(:provider_key, credential.secrets["api_key"])
     |> Map.put(
@@ -454,9 +440,14 @@ defmodule Glific.Partners do
   """
   @spec fetch_opted_in_contacts(Organization.t()) :: :ok | any
   def fetch_opted_in_contacts(organization) do
+    {:ok, credential} = Repo.fetch_by(Credential,
+      %{organization_id: organization.id,
+        provider_id: organization.provider_id})
+
     organization = organization |> Repo.preload(:provider)
-    url = organization.provider.api_end_point <> "/users/" <> organization.provider_appname
-    api_key = get_provider_key(organization.id)
+    url = credential.keys["api_end_point"] <> "/users/" <> organization.provider_appname
+
+    api_key = credential.secrets["api_key"]
 
     with {:ok, response} <- Tesla.get(url, headers: [{"apikey", api_key}]),
          {:ok, response_data} <- Jason.decode(response.body),
@@ -495,10 +486,10 @@ defmodule Glific.Partners do
   """
   @spec get_credential(map()) ::
           {:ok, Credential.t()} | {:error, String.t()}
-  def get_credential(%{organization_id: organization_id, shortcode: shortcode}) do
+  def get_credential(%{organization_id: organization_id, provider_id: provider_id}) do
     Repo.fetch_by(Credential, %{
       organization_id: organization_id,
-      shortcode: shortcode
+      provider_id: provider_id
     })
   end
 
