489ff897a183478018d3a8ae41dcb0cb28d4503e
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-10-06 18:04:10 +0530

    Update for provider's description

--- priv/repo/seeds/20201006012223_v0_5_0_alter_glific_data.exs
+++ priv/repo/seeds/20201006012223_v0_5_0_alter_glific_data.exs
@@ -24,37 +24,37 @@ defmodule Glific.Repo.Seeds.AddGlificData_v0_5_0 do
     # update providers with description
     Repo.update!(
       Ecto.Changeset.change(gupshup, %{
-        description: "BSP provider: credentials of the BSP provider is required to send the messages to contacts"
+        description: "Setup for WhatsApp message provider"
       })
     )
 
     Repo.update!(
       Ecto.Changeset.change(glifproxy, %{
-        description: "Provider for Glific simulator of BSP"
+        description: "Setup for Glific simulator"
       })
     )
 
     Repo.update!(
       Ecto.Changeset.change(dialogflow, %{
-        description: "Provider for Dialogflow"
+        description: "Setup connection with Dialogflow for advanced conversation flows"
       })
     )
 
     Repo.update!(
       Ecto.Changeset.change(goth, %{
-        description: "Provider for GOTH which is required for Dialogflow and Chatbase"
+        description: "Setup for GOTH which is required for Dialogflow and Chatbase"
       })
     )
 
     Repo.update!(
       Ecto.Changeset.change(chatbase, %{
-        description: "Provider for Chatbase"
+        description: "Setup for Chatbase"
       })
     )
 
     Repo.update!(
       Ecto.Changeset.change(google_cloud_storage, %{
-        description: "Provider for Google Cloud Storage"
+        description: "Setup for Google Cloud Storage"
       })
     )
   end
