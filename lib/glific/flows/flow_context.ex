36a56d69aaac2869838e072c0d01095000d84c8a
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-10-08 20:27:02 +0530

    Fix for oban job

--- lib/glific/flows/flow_context.ex
+++ lib/glific/flows/flow_context.ex
@@ -347,6 +347,7 @@ defmodule Glific.Flows.FlowContext do
     # grabbing this context
     {:ok, context} = FlowContext.update_flow_context(context, %{wakeup_at: nil})
 
+    context = context |> Repo.preload([:flow])
     {:ok, flow} =
       Flows.get_cached_flow(context.flow.organization_id, {:flow_uuid, context.flow_uuid}, %{
         uuid: context.flow_uuid
