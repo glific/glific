7208d33163d238740a7501ca297d1a9faa79d40f
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-10-09 12:29:47 +0530

    comment for later changes

--- lib/glific/users/user.ex
+++ lib/glific/users/user.ex
@@ -27,6 +27,7 @@ defmodule Glific.Users.User do
 
   @required_fields [:phone, :name, :password, :contact_id, :organization_id]
   @optional_fields [:name, :roles]
+  # keeping the order alphabetical ASC for frontend dropdown display
   @user_roles ~w(Admin Manager None Staff)
 
   schema "users" do
