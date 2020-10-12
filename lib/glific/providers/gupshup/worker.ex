900e363b0e9f1ccddf83c01a0f20157c55b251e2
Pankaj Agrawal <pankaj@coloredcow.com>
2020-10-12 20:32:09 +0530

    Feature/check simulator msg (#557)
    * check for simulator
    
    * cleanups
    
    * cleanups
    
    * clearing functions
    
    * doctor tests
    
    * cleanups
    
    * clenaups
    
    * cleanups
    
    * mix doctor

--- lib/glific/providers/gupshup/worker.ex
+++ lib/glific/providers/gupshup/worker.ex
@@ -14,6 +14,7 @@ defmodule Glific.Providers.Gupshup.Worker do
     Providers.Gupshup.ApiClient
   }
 
+  @simulater_phone "9876543210"
   @doc """
   Standard perform method to use Oban worker
   """
@@ -31,19 +32,39 @@ defmodule Glific.Providers.Gupshup.Worker do
       {:ok, _} ->
         with credential <- organization.services["bsp"],
              false <- is_nil(credential),
-             do:
-               ApiClient.post(
-                 credential.keys["api_end_point"] <> "/msg",
-                 payload,
-                 headers: [{"apikey", credential.secrets["api_key"]}]
-               )
-               |> handle_response(message)
+             false <- is_simulter(payload["destination"], message),
+             do: process_to_gupshup(credential, payload, message)
 
       _ ->
         {:error, :rate_limit_exceeded}
     end
 
     :ok
+  end
+
+  defp is_simulter(destination, message) when destination == @simulater_phone do
+    message_id = Faker.String.base64(36)
+
+    {:ok,
+     %Tesla.Env{
+       __client__: %Tesla.Client{adapter: nil, fun: nil, post: [], pre: []},
+       __module__: Glific.Providers.Gupshup.ApiClient,
+       body: "{\"status\":\"submitted\",\"messageId\":\"simu-#{message_id}\"}",
+       method: :post,
+       status: 200
+     }}
+    |> handle_response(message)
+  end
+
+  defp is_simulter(_, _), do: false
+
+  defp process_to_gupshup(credential, payload, message) do
+    ApiClient.post(
+      credential.keys["api_end_point"] <> "/msg",
+      payload,
+      headers: [{"apikey", credential.secrets["api_key"]}]
+    )
+    |> handle_response(message)
   end
 
   @doc false
