2af0b653a7ed059e5e4adbb84776c030e446c299
Pankaj Agrawal <pankaj@coloredcow.com>
2020-10-14 15:29:36 +0530

    fixes for media msgs

--- lib/glific/providers/gupshup/api_client.ex
+++ lib/glific/providers/gupshup/api_client.ex
@@ -10,18 +10,4 @@ defmodule Glific.Providers.Gupshup.ApiClient do
   plug Tesla.Middleware.FormUrlencoded,
     encode: &Query.encode/1
 
-  @doc """
-  Returning mock Tesla value when message is send from simulator
-  """
-  @spec simulator_post :: {:ok, map()}
-  def simulator_post do
-    message_id = Faker.String.base64(36)
-
-    {:ok,
-     %Tesla.Env{
-       body: "{\"status\":\"submitted\",\"messageId\":\"simu-#{message_id}\"}",
-       method: :post,
-       status: 200
-     }}
-  end
 end
