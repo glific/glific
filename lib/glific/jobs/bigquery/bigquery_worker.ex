e1c94d7bb7d74d99c53e4af2db0dc95447b7060e
Pankaj Agrawal <pankaj@coloredcow.com>
2020-10-15 19:42:27 +0530

    Bug/contact field (#571)
    * cleanups
    
    * settings
    
    * credo errors

--- lib/glific/jobs/bigquery/bigquery_worker.ex
+++ lib/glific/jobs/bigquery/bigquery_worker.ex
@@ -114,14 +114,14 @@ defmodule Glific.Jobs.BigQueryWorker do
             id: row.id,
             name: row.name,
             phone: row.phone,
-            provider_status: row.provider_status,
+            provider_status: row.bsp_status,
             status: row.status,
             language: row.language.label,
             optin_time: format_date(row.optin_time),
             optout_time: format_date(row.optout_time),
             last_message_at: format_date(row.last_message_at),
             inserted_at: format_date(row.inserted_at),
-            fields: row.fields,
+            fields: Enum.map(row.fields, fn {_key, field} -> %{label: field["label"], inserted_at: format_date(field["inserted_at"]), type: field["type"], value: field["value"] }  end),
             settings: row.settings,
             groups: Enum.map(row.groups, fn group -> %{label: group.label} end),
             tags: Enum.map(row.tags, fn tag -> %{label: tag.label} end)
@@ -163,6 +163,12 @@ defmodule Glific.Jobs.BigQueryWorker do
   @spec format_date(DateTime.t() | nil) :: any()
   defp format_date(nil),
     do: nil
+
+  defp format_date(date) when is_binary(date),
+    do: Timex.parse(date, "{RFC3339z}")
+    |> elem(1)
+    |>Timex.format!("{YYYY}-{M}-{D} {h24}:{m}:{s}")
+
 
   defp format_date(date),
     do: Timex.format!(date, "{YYYY}-{M}-{D} {h24}:{m}:{s}")
