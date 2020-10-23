fe202ee0d9700cb613b01b56a74c885618517c9c
Pankaj Agrawal <pankaj@coloredcow.com>
2020-10-23 19:58:28 +0530

    Feature/flow label (#581)
    * schema and table

---
+++ priv/repo/migrations/20201023101018_create_flow_label_table.exs
@@ -0,0 +1,22 @@
+defmodule Glific.Repo.Migrations.CreateFlowLabelTable do
+  use Ecto.Migration
+
+  def change do
+    flow_label()
+  end
+
+  @doc """
+  Create flow label to associate flow messages with label
+  """
+  def flow_label do
+    create table(:flow_label) do
+      add :uuid, :uuid, null: false
+      add :name, :string
+
+      # foreign key to organization restricting scope of this table to this organization only
+      add :organization_id, references(:organizations, on_delete: :delete_all), null: false
+    end
+
+    create unique_index(:flow_label, [:name, :organization_id])
+  end
+end
