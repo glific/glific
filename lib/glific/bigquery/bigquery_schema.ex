eb4e329cb13144a4baa9fb88be12a662ac2b9c04
Anshul Gupta <42372297+anshulrr@users.noreply.github.com>
2020-10-23 16:47:20 +0530

    Feature/flows labels (#579)
    * Fix for multiple organizations
    
    * Tags and labels for flow responses
    
    * Language flow labels fix
    
    * Preferences tags and labels
    
    * Reverting preference flow tags
    
    * Fix
    
    * Flow uuid constant in all jsons
    
    * Seed flow labels for multiple organizations
    
    * Fix
    
    Co-authored-by: Anshul Gupta <42372297+ansh14j@users.noreply.github.com>

--- lib/glific/bigquery/bigquery_schema.ex
+++ lib/glific/bigquery/bigquery_schema.ex
@@ -1,241 +1,240 @@
 defmodule Glific.BigquerySchema do
-    @moduledoc """
-    Schema for tables to be created for a dataset
-    """
+  @moduledoc """
+  Schema for tables to be created for a dataset
+  """
 
-    @doc """
-    Schema for contact table
-    """
-    @spec contact_schema :: list()
-    def contact_schema do
+  @doc """
+  Schema for contact table
+  """
+  @spec contact_schema :: list()
+  def contact_schema do
     [
-        %{
-            name: "id",
-            type: "INTEGER",
-            mode: "REQUIRED"
-        },
-        %{
-            name: "name",
+      %{
+        name: "id",
+        type: "INTEGER",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "name",
+        type: "STRING",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "phone",
+        type: "STRING",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "provider_status",
+        type: "STRING",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "status",
+        type: "STRING",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "language",
+        type: "STRING",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "optin_time",
+        type: "DATETIME",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "optout_time",
+        type: "DATETIME",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "last_message_at",
+        type: "DATETIME",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "inserted_at",
+        type: "DATETIME",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "updated_at",
+        type: "DATETIME",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "fields",
+        type: "RECORD",
+        mode: "REPEATED",
+        fields: [
+          %{
+            name: "label",
             type: "STRING",
             mode: "NULLABLE"
-        },
-        %{
-            name: "phone",
-            type: "STRING",
-            mode: "REQUIRED"
-        },
-        %{
-            name: "provider_status",
-            type: "STRING",
-            mode: "REQUIRED"
-        },
-        %{
-            name: "status",
-            type: "STRING",
-            mode: "REQUIRED"
-        },
-        %{
-            name: "language",
+          },
+          %{
+            name: "value",
+            type: "string",
+            mode: "NULLABLE"
+          },
+          %{
+            name: "type",
             type: "STRING",
             mode: "NULLABLE"
-        },
-        %{
-            name: "optin_time",
-            type: "DATETIME",
-            mode: "NULLABLE"
-        },
-        %{
-            name: "optout_time",
-            type: "DATETIME",
-            mode: "NULLABLE"
-        },
-        %{
-            name: "last_message_at",
-            type: "DATETIME",
-            mode: "NULLABLE"
-        },
-        %{
+          },
+          %{
             name: "inserted_at",
             type: "DATETIME",
             mode: "NULLABLE"
-        },
-        %{
-            name: "updated_at",
-            type: "DATETIME",
-            mode: "NULLABLE"
-        },
-        %{
-            name: "fields",
-            type: "RECORD",
-            mode: "REPEATED",
-            fields: [
-                %{
-                    name: "label",
-                    type: "STRING",
-                    mode: "NULLABLE"
-                },
-                %{
-                    name: "value",
-                    type: "string",
-                    mode: "NULLABLE"
-                },
-                %{
-                    name: "type",
-                    type: "STRING",
-                    mode: "NULLABLE"
-                },
-                %{
-                    name: "inserted_at",
-                    type: "DATETIME",
-                    mode: "NULLABLE"
-                }
-            ]
-        },
-        %{
-            name: "settings",
-            type: "RECORD",
-            mode: "NULLABLE",
-            fields: [
-                %{
-                    name: "label",
-                    type: "STRING",
-                    mode: "NULLABLE"
-                },
-                %{
-                    name: "values",
-                    type: "RECORD",
-                    mode: "REPEATED",
-                    fields: [
-                        %{
-                            name: "key",
-                            type: "STRING",
-                            mode: "NULLABLE"
-                        },
-                        %{
-                            name: "value",
-                            type: "STRING",
-                            mode: "NULLABLE"
-                        }
-                    ]
-                }
-            ]
-        },
-        %{
-            name: "groups",
-            type: "RECORD",
-            mode: "REPEATED",
-            fields: [
-                %{
-                    name: "label",
-                    type: "STRING",
-                    mode: "REQUIRED"
-                }
-            ]
-        },
-        %{
-            name: "tags",
-            type: "RECORD",
-            mode: "REPEATED",
-            fields: [
-                %{
-                    name: "label",
-                    type: "STRING",
-                    mode: "REQUIRED"
-                }
-            ]
-        }
-    ]
-    end
-
-    @doc """
-    Schema for contact table
-    """
-    @spec message_schema :: list()
-    def message_schema do
-        [
-            %{
-                name: "id",
-                type: "INTEGER",
-                mode: "REQUIRED"
-            },
-            %{
-                name: "uuid",
-                type: "STRING",
-                mode: "NULLABLE"
-            },
-            %{
-                name: "body",
-                type: "STRING",
-                mode: "NULLABLE"
-            },
-            %{
-                name: "type",
-                type: "STRING",
-                mode: "REQUIRED"
-            },
-            %{
-                name: "flow",
-                type: "STRING",
-                mode: "REQUIRED"
-            },
-            %{
-                name: "status",
-                type: "STRING",
-                mode: "REQUIRED"
-            },
-            %{
-                name: "errors",
-                type: "STRING",
-                mode: "NULLABLE"
-            },
-            %{
-                name: "sender_phone",
-                type: "STRING",
-                mode: "REQUIRED"
-            },
-            %{
-                name: "receiver_phone",
-                type: "STRING",
-                mode: "REQUIRED"
-            },
-            %{
-                name: "contact_phone",
-                type: "STRING",
-                mode: "REQUIRED"
-            },
-            %{
-                name: "user_phone",
-                type: "STRING",
-                mode: "REQUIRED"
-            },
-            %{
-                name: "media_url",
-                type: "STRING",
-                mode: "NULLABLE"
-            },
-            %{
-                name: "sent_at",
-                type: "DATETIME",
-                mode: "NULLABLE"
-            },
-            %{
-                name: "inserted_at",
-                type: "DATETIME",
-                mode: "NULLABLE"
-            },
-            %{
-                name: "tags",
-                type: "RECORD",
-                mode: "REPEATED",
-                fields: [
-                    %{
-                        name: "label",
-                        type: "STRING",
-                        mode: "REQUIRED"
-                    }
-                ]
-            }
+          }
         ]
-    end
+      },
+      %{
+        name: "settings",
+        type: "RECORD",
+        mode: "NULLABLE",
+        fields: [
+          %{
+            name: "label",
+            type: "STRING",
+            mode: "NULLABLE"
+          },
+          %{
+            name: "values",
+            type: "RECORD",
+            mode: "REPEATED",
+            fields: [
+              %{
+                name: "key",
+                type: "STRING",
+                mode: "NULLABLE"
+              },
+              %{
+                name: "value",
+                type: "STRING",
+                mode: "NULLABLE"
+              }
+            ]
+          }
+        ]
+      },
+      %{
+        name: "groups",
+        type: "RECORD",
+        mode: "REPEATED",
+        fields: [
+          %{
+            name: "label",
+            type: "STRING",
+            mode: "REQUIRED"
+          }
+        ]
+      },
+      %{
+        name: "tags",
+        type: "RECORD",
+        mode: "REPEATED",
+        fields: [
+          %{
+            name: "label",
+            type: "STRING",
+            mode: "REQUIRED"
+          }
+        ]
+      }
+    ]
+  end
 
+  @doc """
+  Schema for contact table
+  """
+  @spec message_schema :: list()
+  def message_schema do
+    [
+      %{
+        name: "id",
+        type: "INTEGER",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "uuid",
+        type: "STRING",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "body",
+        type: "STRING",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "type",
+        type: "STRING",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "flow",
+        type: "STRING",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "status",
+        type: "STRING",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "errors",
+        type: "STRING",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "sender_phone",
+        type: "STRING",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "receiver_phone",
+        type: "STRING",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "contact_phone",
+        type: "STRING",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "user_phone",
+        type: "STRING",
+        mode: "REQUIRED"
+      },
+      %{
+        name: "media_url",
+        type: "STRING",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "sent_at",
+        type: "DATETIME",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "inserted_at",
+        type: "DATETIME",
+        mode: "NULLABLE"
+      },
+      %{
+        name: "tags",
+        type: "RECORD",
+        mode: "REPEATED",
+        fields: [
+          %{
+            name: "label",
+            type: "STRING",
+            mode: "REQUIRED"
+          }
+        ]
+      }
+    ]
+  end
 end
