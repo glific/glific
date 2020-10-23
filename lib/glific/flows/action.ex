ac96a2ea2c70aacfbd98eace5fdfd5dc86eb93bb
Pankaj Agrawal <pankaj@coloredcow.com>
2020-10-23 19:29:34 +0530

    clear

--- lib/glific/flows/action.ex
+++ lib/glific/flows/action.ex
@@ -258,9 +258,9 @@ defmodule Glific.Flows.Action do
 
   def execute(%{type: "add_input_labels"} = action, context, messages) do
     ## We will soon figure out how we will manage the UUID with tags
-    flow_label= action.labels
-                |>Enum.map(fn label -> label["name"] end)
-                |>Enum.join(", ")
+    flow_label = action.labels
+                  |>Enum.map(fn label -> label["name"] end)
+                  |>Enum.join(", ")
 
     Repo.get(Message, context.last_message.id)
     |> Message.changeset(%{flow_label: flow_label})
