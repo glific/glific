d2cfdbed01d9f173c4c4cf5305b68ad9d113f085
Donald A. Lobo <donald.lobo@gmail.com>
2020-10-26 17:25:18 -0700

    start making the content generation more flexible

--- lib/glific/csv/file.ex
+++ lib/glific/csv/file.ex
@@ -53,6 +53,7 @@ defmodule Glific.CSV.File do
       |> Enum.map(fn {:ok, l} -> l end)
       |> parse_header()
       |> parse_rows(%{})
+      |> IO.inspect()
 
     json_map = Flow.gen_flow(summary.menus[0], organization_id)
     {:ok, json} = Jason.encode_to_iodata(json_map, pretty: true)
