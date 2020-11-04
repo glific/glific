5846148fab635e149c1afb980ee653b27ec5ad0e
Pankaj Agrawal <pankaj@coloredcow.com>
2020-11-04 09:58:51 +0530

    restructuring

--- priv/repo/migrations/20200615073630_add_message_number_to_messages.exs
+++ priv/repo/migrations/20200615073630_add_message_number_to_messages.exs
@@ -16,28 +16,25 @@ defmodule Glific.Repo.Migrations.AddMessageNumberToMessages do
         UPDATE messages set message_number = message_number + 1 where contact_id = NEW.contact_id and id < NEW.id;
         UPDATE messages  set message_number = 0 where id = NEW.id;
 
-      IF (NEW.flow = 'inbound') THEN
-      sessionlimit := (SELECT session_limit * 60 FROM organizations WHERE id = NEW.organization_id LIMIT 1);
+        IF (NEW.flow = 'inbound') THEN
+          sessionlimit := (SELECT session_limit * 60 FROM organizations WHERE id = NEW.organization_id LIMIT 1);
+          current_diff := (SELECT EXTRACT(EPOCH FROM CURRENT_TIMESTAMP) - EXTRACT(EPOCH FROM (SELECT last_message_at FROM contacts WHERE id = NEW.contact_id AND organization_id = NEW.organization_id LIMIT 1)));
+          current_session_uuid := (SELECT session_uuid FROM messages
+          WHERE contact_id = NEW.contact_id
+          AND organization_id = NEW.organization_id
+          AND flow = 'inbound'
+          AND id != NEW.id
+          ORDER BY id DESC LIMIT 1);
 
-        current_diff := (SELECT EXTRACT(EPOCH FROM CURRENT_TIMESTAMP) - EXTRACT(EPOCH FROM (SELECT last_message_at FROM contacts WHERE id = NEW.contact_id AND organization_id = NEW.organization_id LIMIT 1)));
+          IF (current_diff < sessionlimit AND current_session_uuid is not null) THEN
+            UPDATE messages  set session_uuid = current_session_uuid where id = NEW.id;
+          ELSE
+            UPDATE messages set session_uuid = (SELECT uuid_generate_v4()) where id = NEW.id;
+          END IF;
 
-        current_session_uuid := (SELECT session_uuid FROM messages
-        WHERE contact_id = NEW.contact_id
-        AND organization_id = NEW.organization_id
-        AND flow = 'inbound'
-        AND id != NEW.id
-        ORDER BY id DESC LIMIT 1);
+        END IF;
 
-      IF (current_diff < sessionlimit AND current_session_uuid is not null) THEN
-        UPDATE messages  set session_uuid = current_session_uuid where id = NEW.id;
-      ELSE
-        UPDATE messages set session_uuid = (SELECT uuid_generate_v4()) where id = NEW.id;
-      END IF;
-
-      UPDATE contacts  set last_message_at = CURRENT_TIMESTAMP where id = NEW.contact_id;
-      END IF;
-
-
+        UPDATE contacts  set last_message_at = CURRENT_TIMESTAMP where id = NEW.contact_id;
         RETURN NEW;
       END IF;
       RETURN NULL;
