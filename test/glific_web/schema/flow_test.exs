40b395c8c377b20d860bb5a6d25b380f6822c366
Anshul Gupta <42372297+anshulrr@users.noreply.github.com>
2020-11-10 16:25:41 +0530

    1. Filter flows by status of flow revision (#640)
    2. Fix for listing only puplished flows in the flow editor's Enter a Flow popup
    
    Co-authored-by: Anshul Gupta <42372297+ansh14j@users.noreply.github.com>

--- test/glific_web/schema/flow_test.exs
+++ test/glific_web/schema/flow_test.exs
@@ -56,6 +56,17 @@ defmodule GlificWeb.Schema.FlowTest do
     assert flow_uuid == flow.uuid
   end
 
+  test "flows field returns list of flows filtered by status", %{manager: user} do
+    # Create a new flow
+    auth_query_gql_by(:create, user, variables: %{"input" => %{"name" => "New Flow"}})
+
+    result = auth_query_gql_by(:list, user, variables: %{"filter" => %{"status" => "draft"}})
+    assert {:ok, query_data} = result
+
+    flows = get_in(query_data, [:data, "flows"])
+    assert length(flows) == 1
+  end
+
   test "flow field id returns one flow or nil", %{staff: user} do
     name = "Test Workflow"
     {:ok, flow} = Repo.fetch_by(Flow, %{name: name, organization_id: user.organization_id})
