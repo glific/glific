406cab7a3e219e03ccc1dbea66293a4e15484ff1
Pankaj Agrawal <pankaj@coloredcow.com>
2020-11-11 16:03:21 +0530

    cleanups

--- lib/glific/providers/gupshup/gupshupbalance_worker.ex
+++ lib/glific/providers/gupshup/gupshupbalance_worker.ex
@@ -16,7 +16,6 @@ defmodule Glific.Jobs.GupshupbalanceWorker do
     Partners,
   }
 
-  Communications.publish_data(data, topic, organization_id)
   @spec perform_periodic(non_neg_integer) :: :ok
   @doc """
   periodic function for making calls to gupshup for remaining balance
@@ -29,8 +28,7 @@ defmodule Glific.Jobs.GupshupbalanceWorker do
     case Tesla.get(@gupshup_balance_url, headers: [{"apikey", api_key}]) do
       {:ok, %Tesla.Env{status: status, body: body}} when status in 200..299 ->
         {:ok, data} = Jason.decode(body)
-        Communications.publish_data(data["balance"], topic, organization_id)
-        IO.inspect(data["balance"])
+        Communications.publish_data(data["balance"], :glific, organization_id)
       _ ->
         {:error, "Invalid key"}
     end
