415f3bbdebffb0d1247f41e0dd3a2ccbb53b2d8d
Pankaj Agrawal <pankaj@coloredcow.com>
2020-11-11 17:47:27 +0530

    typos

--- lib/glific_web/schema/balance_types.ex
+++ lib/glific_web/schema/balance_types.ex
@@ -2,8 +2,14 @@ defmodule GlificWeb.Schema.BalanceTypes do
   @moduledoc """
   GraphQL Representation of Glific's Message DataType
   """
+  use Absinthe.Schema.Notation
+  import Absinthe.Resolution.Helpers, only: [dataloader: 1]
+
   alias Glific.Communications
 
+  alias GlificWeb.{
+    Schema,
+  }
   object :gupshup_balance do
     field :balance, :string do
       arg(:organization_id, non_null(:id))
@@ -12,5 +18,6 @@ defmodule GlificWeb.Schema.BalanceTypes do
 
       resolve(&Communications.publish_data/3)
     end
+  end
 
 end
