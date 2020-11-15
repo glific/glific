916fa656ad9a9463a99faf8c94042c6682b03eec
Donald A. Lobo <donald.lobo@gmail.com>
2020-11-14 17:32:21 -0800

    tweak checks

--- lib/glific_web/misc/body_reader.ex
+++ lib/glific_web/misc/body_reader.ex
@@ -4,7 +4,12 @@ defmodule GlificWeb.Misc.BodyReader do
   processed by Phoenix. Used to validate the signature
   """
 
-  @doc false
+  alias Plug.Conn
+
+  @doc """
+  Store the raw body in the conn assigns array
+  """
+  @spec cache_raw_body(Conn.t(), Keyword.t()) :: tuple()
   def cache_raw_body(conn, opts) do
     with {:ok, body, conn} <- Plug.Conn.read_body(conn, opts) do
       conn = update_in(conn.assigns[:raw_body], &[body | &1 || []])
