a36c50e76de942b83aa798cda6ae51f10937dd9a
Donald A. Lobo <donald.lobo@gmail.com>
2020-11-17 09:25:09 -0800

    unify migrations in 1 file

--- priv/repo/migrations/20201113072446_v0_7_2_alter_glific_tables.exs
+++ priv/repo/migrations/20201113072446_v0_7_2_alter_glific_tables.exs
@@ -2,7 +2,7 @@ defmodule Glific.Repo.Migrations.V0_7_2_AlterGlificTables do
   use Ecto.Migration
 
   @moduledoc """
-  v0.6.2 Alter Glific tables
+  v07.2 Alter Glific tables
   """
 
   import Ecto.Query, warn: false
@@ -13,6 +13,8 @@ defmodule Glific.Repo.Migrations.V0_7_2_AlterGlificTables do
     users()
 
     contacts()
+
+    organizations()
   end
 
   defp users() do
@@ -34,4 +36,21 @@ defmodule Glific.Repo.Migrations.V0_7_2_AlterGlificTables do
     )
     |> Repo.update_all([], skip_organization_id: true)
   end
+
+  defp organizations() do
+    alter table(:organizations) do
+      # add the signing phrase for webhooks
+      # we will keep this encrypted, and remove the default before release
+      add :signature_phrase, :string, default: "super secret"
+    end
+
+    # flush the change to the DB
+    flush()
+
+    from([o] in Organization,
+      update: [set: [signature_phrase: "super secret"]]
+    )
+    |> Repo.update_all([], skip_organization_id: true)
+  end
+
 end
