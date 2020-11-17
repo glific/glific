622cc032810f34982bee09248373aa919bf4f1f0
pankaj-ag <pankaj@coloredcow.in>
2020-11-17 18:57:55 +0530

    minute worker

--- config/config.exs
+++ config/config.exs
@@ -47,7 +47,7 @@ config :glific, Oban,
     {"*/5 * * * *", Glific.Jobs.MinuteWorker, args: %{job: :gcs}},
     {"0 * * * *", Glific.Jobs.MinuteWorker, args: %{job: :delete_completed_flow_contexts}},
     {"0 0 * * *", Glific.Jobs.MinuteWorker, args: %{job: :delete_old_flow_contexts}},
-    {"*/1 * * * *", Glific.Jobs.MinuteWorker, args: %{job: :bspbalance}}
+    {"*/30 * * * *", Glific.Jobs.MinuteWorker, args: %{job: :bspbalance}}
   ]
 
 config :tesla, adapter: Tesla.Adapter.Hackney
