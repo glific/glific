c9631f18d6b9b35deffd5f2ff2675fa31dd65f59
Pankaj Agrawal <pankaj@coloredcow.com>
2020-11-17 18:30:20 +0530

    cleanups

--- lib/glific/searches/collection_worker.ex
+++ lib/glific/searches/collection_worker.ex
@@ -15,9 +15,8 @@ defmodule Glific.Jobs.CollectionWorker do
   def perform_periodic(organization_id) do
     searches = Searches.list_saved_searches(%{filter: %{organization_id: organization_id}})
     searches
-    |> Enum.each( fn search ->
-
-      Communications.publish_data(%{key: "Collection_count", value: %{search.id => Searches.saved_search_count(%{id: search.id})}}, :periodic_info, organization_id)
+    |> Enum.each(fn search ->
+        Communications.publish_data(%{key: "Collection_count", value: %{search.id => Searches.saved_search_count(%{id: search.id})}}, :periodic_info, organization_id)
        end)
   end
 end
