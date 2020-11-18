6c12a8195d8318a45ec2f924e7d719f7d030a5b9
Pankaj Agrawal <pankaj@coloredcow.com>
2020-11-18 15:44:08 +0530

    deploy

--- config/gigalixir.env.sh
+++ config/gigalixir.env.sh
@@ -1,4 +1,4 @@
-gigalixir config:set BASE_URL='https://glifics.gigalixirapp.com/'
+gigalixir config:set BASE_URL='https://glific.gigalixirapp.com/'
 gigalixir config:set PROVIDER_KEY_1=''
 gigalixir config:set GOTH_JSON_CREDENTIALS='{"project_id": "DOES NOT EXISTS","private_key_id": "DOES NOT EXIST","private_key": "INVALID PRIVATE KEY"}'
 gigalixir config:set APPSIGNAL_PUSH_API_KEY=''
