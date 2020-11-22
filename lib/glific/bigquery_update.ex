98566f05554a7cd277340277cf07b271f53432fa
Pankaj Agrawal <pankaj@coloredcow.com>
2020-11-22 22:18:10 +0530

    minor updates

--- lib/glific/bigquery_update.ex
+++ lib/glific/bigquery_update.ex
@@ -2,9 +2,6 @@ defmodule Glific.BigqueryUpdate do
   @moduledoc """
   Glific Bigquery field updation
   """
-
-  import Ecto.Query, warn: false
-
   alias Glific.{
     Partners,
     Repo
@@ -16,9 +13,8 @@ defmodule Glific.BigqueryUpdate do
 
   @doc """
   Updating existing field in a table
-  Glific.BigqueryUpdate.sync_query("phone", 8881, 8882, 1)
   """
-  def sync_query(field, old_value, new_value, organization_id) do
+  def sync_query(field, table_name, old_value, new_value, organization_id) do
     organization = Partners.organization(organization_id)|> Repo.preload(:contact)
     dataset_id = organization.contact.phone
     organization.services["bigquery"]
@@ -27,10 +23,9 @@ defmodule Glific.BigqueryUpdate do
         nil
       credentials ->
         project_id = credentials.secrets["project_id"]
-        sql = "UPDATE `#{dataset_id}.same` SET #{field}= #{new_value} WHERE #{field}= #{old_value}"
+        sql = "UPDATE `#{dataset_id}.#{table_name}` SET #{field}= #{new_value} WHERE #{field}= #{old_value}"
         token = Partners.get_goth_token(organization_id, "bigquery")
         conn = Connection.new(token.token)
-        # Make the API request
         {:ok, response} = Jobs.bigquery_jobs_query(
           conn,
           project_id,
