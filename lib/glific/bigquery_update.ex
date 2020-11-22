e8a0766baebb6a7588b3b96d1ac1ae18968b2613
Pankaj Agrawal <pankaj@coloredcow.com>
2020-11-22 21:59:31 +0530

    dataset id from organization credential

--- lib/glific/bigquery_update.ex
+++ lib/glific/bigquery_update.ex
@@ -1,6 +1,6 @@
 defmodule Glific.BigqueryUpdate do
   @moduledoc """
-  Glific Bigquery Dataset and table creation
+  Glific Bigquery field updation
   """
   alias Glific.Partners
   alias GoogleApi.BigQuery.V2.{
@@ -10,20 +10,28 @@ defmodule Glific.BigqueryUpdate do
 
   @doc """
   Updating existing field in a table
-  Glific.BigqueryUpdate.sync_query("beginner-290513", "phone", 92099, 87098, 1)
+  Glific.BigqueryUpdate.sync_query("phone", 8888, 8881, 1)
   """
-  def sync_query(project_id, field, old_value, new_value, organization_id) do
-    sql = "UPDATE `demo.same` SET #{field}= #{new_value} WHERE #{field}= #{old_value}"
-    token = Partners.get_goth_token(organization_id, "bigquery")
-    conn = Connection.new(token.token)
-    # Make the API request
-    {:ok, response} = Jobs.bigquery_jobs_query(
-      conn,
-      project_id,
-      [body: %{ query: sql, useLegacySql: false}]
-    )
-    IO.inspect("response")
-    IO.inspect(response)
+  def sync_query(field, old_value, new_value, organization_id) do
+    organization = Partners.organization(organization_id)
+    organization.services["bigquery"]
+    |> case do
+      nil ->
+        nil
+      credentials ->
+        project_id = credentials.secrets["project_id"]
+        sql = "UPDATE `demo.same` SET #{field}= #{new_value} WHERE #{field}= #{old_value}"
+        token = Partners.get_goth_token(organization_id, "bigquery")
+        conn = Connection.new(token.token)
+        # Make the API request
+        {:ok, response} = Jobs.bigquery_jobs_query(
+          conn,
+          project_id,
+          [body: %{ query: sql, useLegacySql: false}]
+        )
+        IO.inspect("response")
+        IO.inspect(response)
+    end
     :ok
   end
 end
