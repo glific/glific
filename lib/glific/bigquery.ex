3ca69e286e101c117d04b60f3dc7e423f3a36a86
pankaj-ag <pankaj@coloredcow.in>
2020-11-23 17:27:40 +0530

    enhancemnts

--- lib/glific/bigquery.ex
+++ lib/glific/bigquery.ex
@@ -3,11 +3,15 @@ defmodule Glific.Bigquery do
   Glific Bigquery Dataset and table creation
   """
 
-  alias Glific.BigquerySchema
-  alias Glific.Partners
+  alias Glific.{
+    BigquerySchema,
+    Partners,
+    Repo
+  }
 
   alias GoogleApi.BigQuery.V2.{
     Api.Datasets,
+    Api.Jobs,
     Api.Tables,
     Connection
   }
@@ -72,6 +76,36 @@ defmodule Glific.Bigquery do
 
     :ok
   end
+
+  def update_contact(phone_no, updated_values, organization_id) do
+    organization = Partners.organization(organization_id)|> Repo.preload(:contact)
+    dataset_id = organization.contact.phone
+    organization.services["bigquery"]
+    |> case do
+      nil ->
+        nil
+      credentials ->
+        project_id = credentials.secrets["project_id"]
+        sql = "UPDATE `#{dataset_id}.contacts` SET #{format_update_values(updated_values)} WHERE phone= #{phone_no}"
+        token = Partners.get_goth_token(organization_id, "bigquery")
+        conn = Connection.new(token.token)
+        {:ok, response} = Jobs.bigquery_jobs_query(
+          conn,
+          project_id,
+          [body: %{ query: sql, useLegacySql: false}]
+        )
+        IO.inspect("response")
+        IO.inspect(response)
+    end
+    :ok
+  end
+
+  defp format_update_values(map) do
+    Map.keys(map)
+    |> Enum.map(fn key -> " #{key} = #{map[key]}" end)
+    |> Enum.join(",")
+  end
+
 
   defp create_dataset(conn, project_id, dataset_id) do
     Datasets.bigquery_datasets_insert(
@@ -196,4 +230,6 @@ defmodule Glific.Bigquery do
 
     response
   end
+
+
 end
