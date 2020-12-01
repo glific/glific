fbd159fb09c20725120414156cdc70b2352aea9b
pankaj-ag <pankaj@coloredcow.in>
2020-12-01 16:43:33 +0530

    update migrations

---
+++ priv/repo/migrations/20201201110824_v0_8_1_alter_glific_tables.exs
@@ -0,0 +1,21 @@
+defmodule Glific.Repo.Migrations.V0_8_1_AlterGlificTables do
+  @moduledoc """
+  v0.7.3 Alter Glific tables
+  """
+  use Ecto.Migration
+  @disable_ddl_transaction true
+
+  def up do
+    add_translations_to_session_templates()
+  end
+
+  def down do
+    # There is no easy way to drop an enum value. It is not supported.
+  end
+
+  defp add_translations_to_session_templates() do
+    alter table(:session_templates) do
+      add :translations, :jsonb, default: "[]"
+    end
+  end
+end
