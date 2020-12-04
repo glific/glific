2ad6520713eb74b7cfe60991b75c5a5c2b7f65b6
Pankaj Agrawal <pankaj@coloredcow.com>
2020-12-04 12:06:28 +0530

    fix test case

--- test/glific_web/schema/flow_test.exs
+++ test/glific_web/schema/flow_test.exs
@@ -58,11 +58,11 @@ defmodule GlificWeb.Schema.FlowTest do
 
   test "flows field returns list of flows filtered by status", %{manager: user} do
     # Create a new flow
-    auth_query_gql_by(:create, user, variables: %{"input" => %{"name" => "New Flow"}})
+    auth_query_gql_by(:create, user, variables: %{"input" => %{"name" => "New Flow", "keywords" => "new"}})
 
     result = auth_query_gql_by(:list, user, variables: %{"filter" => %{"status" => "draft"}})
     assert {:ok, query_data} = result
-
+IO.inspect(query_data)
     flows = get_in(query_data, [:data, "flows"])
     assert length(flows) == 1
   end
