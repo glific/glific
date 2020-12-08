d2acda7546e3bb9fe69037b9d7a75f9a6b8c85a3
Pankaj Agrawal <pankaj@coloredcow.com>
2020-12-08 14:26:12 +0530

    fixes

--- lib/glific/vault.ex
+++ lib/glific/vault.ex
@@ -3,21 +3,4 @@ defmodule Glific.Vault do
   Cloak Vault
   """
   use Cloak.Vault, otp_app: :glific
-
-  @impl GenServer
-  def init(config) do
-    config =
-      Keyword.put(config, :ciphers,
-        default: {Cloak.Ciphers.AES.GCM, tag: "AES.GCM.V2", key: decode_env!("NEW_CIPHER_KEY")},
-        old_key: {Cloak.Ciphers.AES.GCM, tag: "AES.GCM.V1", key: decode_env!("CIPHER_KEY")}
-      )
-
-    {:ok, config}
-  end
-
-  defp decode_env!(var) do
-    var
-    |> System.get_env()
-    |> Base.decode64!()
-  end
 end
