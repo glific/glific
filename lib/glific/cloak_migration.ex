a661077514c6d48ffdba9703d1861eb272a10086
Pankaj Agrawal <pankaj@coloredcow.com>
2020-12-12 18:44:58 +0530

    clearing caches

--- lib/glific/cloak_migration.ex
+++ lib/glific/cloak_migration.ex
@@ -10,29 +10,49 @@ defmodule Glific.CloakMigration do
     Repo
   }
 
+  @global_organization_id 0
+
   @doc """
   migrate to new key for encryption
   """
   @spec cloak_migrate :: :ok
   def cloak_migrate do
-    Repo.all(Organization, skip_organization_id: true)
-    |> Enum.each(fn organization -> update_organization(organization) end)
+    organizations = Glific.Repo.all(Glific.Partners.Organization, skip_organization_id: true)
+
+    organizations |> Enum.each(fn organization -> update_organization(organization) end)
+
+    organizations_list =
+      Enum.reduce(organizations, %{}, fn organization, acc ->
+        Map.put(acc, organization.id, organization.shortcode)
+      end)
 
     Repo.all(Credential, skip_organization_id: true)
-    |> Enum.each(fn credential -> update_credential(credential) end)
+    |> Enum.each(fn credential -> update_credential(credential, organizations_list) end)
 
     :ok
   end
 
-  @spec update_credential(Credential.t()) :: {:ok, Credential.t()}
-  defp update_credential(record) do
-    Partners.update_credential(record, %{secrets: %{temp: nil}})
-    Partners.update_credential(record, %{secrets: record.secrets}})
+  @spec update_credential(Credential.t(), map()) :: any()
+  defp update_credential(credential, organizations_list) do
+    {:ok, updated} =
+      credential
+      |> Credential.changeset(%{secrets: %{api_key: nil}})
+      |> Repo.update(force: true)
+
+    updated
+    |> Credential.changeset(%{secrets: credential.secrets})
+    |> Repo.update(force: true)
+
+    Partners.remove_organization_cache(
+      credential.organization_id,
+      Map.get(organizations_list, credential.organization_id)
+    )
   end
 
-  @spec update_credential(Organization.t()) :: {:ok, Organization.t()}
-  defp update_organization(record) do
-    Partners.update_organization(record, %{signature_phrase: nil})
-    Partners.update_organization(record, %{signature_phrase: record.signature_phrase})
+  @spec update_organization(Organization.t()) ::
+          {:ok, Organization.t()} | {:error, Ecto.Changeset.t()}
+  defp update_organization(organization) do
+    Partners.update_organization(organization, %{signature_phrase: nil})
+    Partners.update_organization(organization, %{signature_phrase: organization.signature_phrase})
   end
 end
