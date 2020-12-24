1b869ae48b7307606d59f0b0f2092f0a1597d66f
Anshul Gupta <42372297+ansh14j@users.noreply.github.com>
2020-12-24 22:39:26 +0530

    More tests

--- test/glific/conversations_group_test.exs
+++ test/glific/conversations_group_test.exs
@@ -13,7 +13,6 @@ defmodule Glific.ConversationsGroupTest do
       default_provider = SeedsDev.seed_providers()
       SeedsDev.seed_organizations(default_provider)
       SeedsDev.seed_contacts()
-      SeedsDev.seed_messages()
       SeedsDev.seed_groups()
       SeedsDev.seed_group_contacts()
       SeedsDev.seed_group_messages()
@@ -42,6 +41,40 @@ defmodule Glific.ConversationsGroupTest do
 
       assert [conversation] = conversations
       assert conversation.group.id == group.id
+    end
+
+    test "list_conversations/2 will return a list with messages filtered by opts" do
+      messages_limit = 1
+
+      [c1, c2] =
+        ConversationsGroup.list_conversations(nil, %{
+          filter: %{search_group: true},
+          contact_opts: %{limit: 10, offset: 0},
+          message_opts: %{limit: messages_limit, offset: 0}
+        })
+
+      assert length(c1.messages) == messages_limit
+      assert length(c2.messages) == messages_limit
+    end
+
+    test "list_conversations/2 will return a list with groups filtered by opts" do
+      conversations =
+        ConversationsGroup.list_conversations(nil, %{
+          filter: %{search_group: true},
+          contact_opts: %{limit: 1, offset: 0},
+          message_opts: %{limit: 1, offset: 0}
+        })
+
+      assert length(conversations) == 1
+
+      conversations =
+        ConversationsGroup.list_conversations(nil, %{
+          filter: %{search_group: true},
+          contact_opts: %{limit: 1, offset: 2},
+          message_opts: %{limit: 1, offset: 0}
+        })
+
+      assert conversations == []
     end
   end
 end
