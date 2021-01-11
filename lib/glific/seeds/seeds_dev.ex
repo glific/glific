540d78073b07b0409e50940818904afa3558f8a6
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-01-11 18:18:00 +0530

    updating specs

--- lib/glific/seeds/seeds_dev.ex
+++ lib/glific/seeds/seeds_dev.ex
@@ -843,95 +843,95 @@ if Code.ensure_loaded?(Faker) do
         else: organization
     end
 
+    @doc false
+    @spec hsm_templates(Organization.t()) :: nil
+    def hsm_templates(organization) do
+      [hi | _] = Settings.list_languages(%{filter: %{label: "hindi"}})
+      [en_us | _] = Settings.list_languages(%{filter: %{label: "english"}})
 
-  def hsm_templates(organization) do
-
-    [hi | _] = Settings.list_languages(%{filter: %{label: "hindi"}})
-    [en_us | _] = Settings.list_languages(%{filter: %{label: "english"}})
-
-    translations = %{
-      hi.id => %{
-        body: " मुझे खेद है कि मैं कल आपकी चिंताओं का जवाब देने में सक्षम नहीं था, लेकिन मैं अब आपकी सहायता करने में प्रसन्न हूं।
+      translations = %{
+        hi.id => %{
+          body: " मुझे खेद है कि मैं कल आपकी चिंताओं का जवाब देने में सक्षम नहीं था, लेकिन मैं अब आपकी सहायता करने में प्रसन्न हूं।
           यदि आप इस चर्चा को जारी रखना चाहते हैं, तो कृपया 'हां' के साथ उत्तर दें।",
-        language_id: hi.id,
-        number_parameters: 0
+          language_id: hi.id,
+          number_parameters: 0
+        }
       }
-    }
 
-    Repo.insert!(%SessionTemplate{
-      label: "Missed Message Apology",
-      type: :text,
-      shortcode: "missed_message",
-      is_hsm: true,
-      number_parameters: 0,
-      language_id: en_us.id,
-      organization_id: organization.id,
-      body: """
-      I'm sorry that I wasn't able to respond to your concerns yesterday but I’m happy to assist you now.
-      If you’d like to continue this discussion, please reply with ‘yes’
-      """,
-      translations: translations,
-      status: "PENDING",
-      category: "ALERT_UPDATE",
-      uuid: Ecto.UUID.generate()
-    })
+      Repo.insert!(%SessionTemplate{
+        label: "Missed Message Apology",
+        type: :text,
+        shortcode: "missed_message",
+        is_hsm: true,
+        number_parameters: 0,
+        language_id: en_us.id,
+        organization_id: organization.id,
+        body: """
+        I'm sorry that I wasn't able to respond to your concerns yesterday but I’m happy to assist you now.
+        If you’d like to continue this discussion, please reply with ‘yes’
+        """,
+        translations: translations,
+        status: "PENDING",
+        category: "ALERT_UPDATE",
+        uuid: Ecto.UUID.generate()
+      })
 
-    translations = %{
-      hi.id => %{
-        body: "{{1}} के लिए आपका OTP {{2}} है। यह {{3}} के लिए मान्य है।",
-        language_id: hi.id,
-        number_parameters: 3
+      translations = %{
+        hi.id => %{
+          body: "{{1}} के लिए आपका OTP {{2}} है। यह {{3}} के लिए मान्य है।",
+          language_id: hi.id,
+          number_parameters: 3
+        }
       }
-    }
 
-    Repo.insert!(%SessionTemplate{
-      label: "OTP Message",
-      type: :text,
-      shortcode: "otp",
-      is_hsm: true,
-      number_parameters: 3,
-      language_id: en_us.id,
-      organization_id: organization.id,
-      translations: translations,
-      status: "REJECTED",
-      category: "ALERT_UPDATE",
-      body: "Your OTP for {{1}} is {{2}}. This is valid for {{3}}.",
-      example: "Your OTP for [adding Anil as a payee] is [1234]. This is valid for [15 minutes].",
-      uuid: Ecto.UUID.generate()
-    })
+      Repo.insert!(%SessionTemplate{
+        label: "OTP Message",
+        type: :text,
+        shortcode: "otp",
+        is_hsm: true,
+        number_parameters: 3,
+        language_id: en_us.id,
+        organization_id: organization.id,
+        translations: translations,
+        status: "REJECTED",
+        category: "ALERT_UPDATE",
+        body: "Your OTP for {{1}} is {{2}}. This is valid for {{3}}.",
+        example:
+          "Your OTP for [adding Anil as a payee] is [1234]. This is valid for [15 minutes].",
+        uuid: Ecto.UUID.generate()
+      })
 
-    translations = %{
-      hi.id => %{
-        body:
-          " कृपया फोन नंबर @ contact.phone के साथ पंजीकरण करने के लिए लिंक पर क्लिक करें @ global.registration.url",
-        language_id: hi.id,
-        number_parameters: 0
+      translations = %{
+        hi.id => %{
+          body:
+            " कृपया फोन नंबर @ contact.phone के साथ पंजीकरण करने के लिए लिंक पर क्लिक करें @ global.registration.url",
+          language_id: hi.id,
+          number_parameters: 0
+        }
       }
-    }
 
-    Repo.insert!(%SessionTemplate{
-      label: "User Registration",
-      body: """
-      Please click on the link to register with the phone number @contact.phone
-      @global.registration.url
-      """,
-      example: """
-      Please click on the link to register with the phone number @contact.phone
-      [https://www.gupshup.io/developer/register]
-      """,
-      type: :text,
-      shortcode: "user-registration",
-      is_reserved: true,
-      language_id: en_us.id,
-      translations: translations,
-      status: "REJECTED",
-      category: "ALERT_UPDATE",
-      organization_id: organization.id,
-      number_parameters: 0,
-      uuid: Ecto.UUID.generate()
-    })
-  end
-
+      Repo.insert!(%SessionTemplate{
+        label: "User Registration",
+        body: """
+        Please click on the link to register with the phone number @contact.phone
+        @global.registration.url
+        """,
+        example: """
+        Please click on the link to register with the phone number @contact.phone
+        [https://www.gupshup.io/developer/register]
+        """,
+        type: :text,
+        shortcode: "user-registration",
+        is_reserved: true,
+        language_id: en_us.id,
+        translations: translations,
+        status: "REJECTED",
+        category: "ALERT_UPDATE",
+        organization_id: organization.id,
+        number_parameters: 0,
+        uuid: Ecto.UUID.generate()
+      })
+    end
 
     @doc """
     Function to populate some basic data that we need for the system to operate. We will
