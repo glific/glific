3aa5269400000dd148ac24a90cd8cf17fb9a3dac
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-01-19 19:59:23 +0530

    updated groups and languages description

--- priv/repo/migrations/20210119132444_v0_9_5_alter_glific_tables.exs
+++ priv/repo/migrations/20210119132444_v0_9_5_alter_glific_tables.exs
@@ -3,10 +3,24 @@ defmodule Glific.Repo.Migrations.V0_9_5_AlterGlificTables do
 
   def change do
     tags()
+    languages()
+    groups()
   end
 
   defp tags do
     alter table(:tags) do
+      modify :description, :text
+    end
+  end
+
+  defp languages do
+    alter table(:languages) do
+      modify :description, :text
+    end
+  end
+
+  defp groups do
+    alter table(:groups) do
       modify :description, :text
     end
   end
