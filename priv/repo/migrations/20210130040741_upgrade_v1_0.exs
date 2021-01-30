f0850a13fb7b56d442e233b2c05ba72cd5e16941
Donald A. Lobo <donald.lobo@gmail.com>
2021-01-30 12:36:52 -0800

    add index to messages, bsp_message_id

--- priv/repo/migrations/20210130040741_upgrade_v1_0.exs
+++ priv/repo/migrations/20210130040741_upgrade_v1_0.exs
@@ -2,10 +2,12 @@ defmodule Glific.Repo.Migrations.UpgradeV10 do
   use Ecto.Migration
 
   def change do
-    messages()
+    indexes()
   end
 
-  defp messages do
-    create unique_index(:messages, :bsp_message_id)
+  defp indexes do
+    create index(:contacts, :status)
+    create index(:messages, :message_number)
+    create unique_index(:messages, [:bsp_message_id, :organization_id])
   end
 end
