b14fb5c75d095d31eb090c552bee7dd44ae07edf
Donald A Lobo <409069+dlobo@users.noreply.github.com>
2021-02-01 09:32:27 -0800

    restart oban from scratch (#1024)

--- priv/repo/migrations/20210127011934_upgrade_oban_jobs_to_v10.exs
+++
@@ -1,9 +0,0 @@
-defmodule MyApp.Repo.Migrations.UpdateObanJobsToV10 do
-  use Ecto.Migration
-
-  def change do
-    # do nothing, we had made an error in our previous migration
-    # and forgot to include the global prefix
-    # the new migration in in the upgrade_oban_jobs_to_v10_1
-  end
-end
