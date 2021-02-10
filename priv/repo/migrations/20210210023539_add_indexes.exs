863f65e3a8d67bd4d7dea78c23688b5790811270
Donald A. Lobo <donald.lobo@gmail.com>
2021-02-09 18:51:47 -0800

    add indexes for some slow queries

--- priv/repo/migrations/20210210023539_add_indexes.exs
+++ priv/repo/migrations/20210210023539_add_indexes.exs
@@ -5,6 +5,10 @@ defmodule Glific.Repo.Migrations.AddIndexes do
     # We might have manually created this on our production servers
     # hence using if not exists
     create_if_not_exists index(:search_messages, :contact_id)
+
     create_if_not_exists index(:flow_contexts, [:wakeup_at, :completed_at])
+
+    create_if_not_exists index(:messages, :inserted_at)
+    create_if_not_exists index(:messages, :updated_at)
   end
 end
