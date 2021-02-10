99adf4934121fc5addae374e0ebaefd25577d093
Donald A. Lobo <donald.lobo@gmail.com>
2021-02-10 06:48:55 -0800

    add indexes

--- priv/repo/migrations/20210210023539_add_indexes.exs
+++ priv/repo/migrations/20210210023539_add_indexes.exs
@@ -8,6 +8,10 @@ defmodule Glific.Repo.Migrations.AddIndexes do
 
     create_if_not_exists index(:flow_contexts, [:wakeup_at, :completed_at])
 
+    create_if_not_exists index(:flow_results, :organization_id)
+    create_if_not_exists index(:flow_results, :inserted_at)
+    create_if_not_exists index(:flow_results, :updated_at)
+
     create_if_not_exists index(:flow_revisions, :flow_id)
     create_if_not_exists index(:flow_revisions, :status)
     create_if_not_exists index(:flow_revisions, :organization_id)
