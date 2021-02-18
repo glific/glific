5586670546fbf64417c7b34395fd94753f9348bb
pankaj-ag <pankaj@coloredcow.in>
2021-02-18 14:17:49 +0530

    fixed saved search

---
+++ priv/repo/migrations/20210218084225_update_all_saved_search_args.exs
@@ -0,0 +1,25 @@
+defmodule Glific.Repo.Migrations.UpdateAllSavedSearchArgs do
+  use Ecto.Migration
+
+  import Ecto.Query
+
+  alias Glific.{Repo, Searches.SavedSearch}
+
+  def change do
+    update_all_saved_search_args()
+  end
+
+  def update_all_saved_search_args() do
+    shortcode = "All"
+    args = %{
+      filter: %{},
+      contactOpts: %{limit: 25},
+      messageOpts: %{limit: 20}
+    }
+
+    SavedSearch
+    |> where([s], s.shortcode == ^shortcode)
+    |> Repo.update_all([set: [args: args]], skip_organization_id: true)
+  end
+
+end
