b7148dceb61a929623d9a81f9f638cd5950ecb3d
pankaj-ag <pankaj@coloredcow.in>
2021-02-20 16:30:29 +0530

    cleanups

--- build_scripts/staging_build.sh
+++ build_scripts/staging_build.sh
@@ -13,10 +13,6 @@ git pull staging master
 echo "pushing data to staging server"
 git push staging tag_$1:master
 
-
 # echo "SSH to portal"
 echo "clean SSH server for Staging"
 gigalixir ps:ssh -a glific-staging < ./build_scripts/ssh_profile.sh
-
-# echo "Clean SSH sever for production"
-# gigalixir ps:ssh -a glific < ./build_scripts/ssh_profile.sh
