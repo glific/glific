b99dd717f1ae97a9b6e20b756142c0c5951d5f0e
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-02-22 17:35:21 +0530

    added migration for updating chatbase field

--- priv/repo/migrations/20210222105752_v0_11_6_alter_glific_tables.exs
+++ priv/repo/migrations/20210222105752_v0_11_6_alter_glific_tables.exs
@@ -1,8 +1,16 @@
 defmodule Glific.Repo.Migrations.V0_11_6_AlterGlificTables do
+  @moduledoc """
+  Updating glific tables
+  """
   use Ecto.Migration
 
   def change do
+    chatbase_jobs()
+  end
 
-
+  defp chatbase_jobs do
+    alter table(:chatbase_jobs) do
+      modify :message_id, :integer, default: 0
+    end
   end
 end
