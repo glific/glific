8292cb006af24e2af81a5f155541f8173ade0c92
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-02-25 15:19:25 +0530

    resetting table

--- priv/repo/migrations/20210225083751_alter_data.exs
+++ priv/repo/migrations/20210225083751_alter_data.exs
@@ -2,6 +2,14 @@ defmodule Glific.Repo.Migrations.AlterData do
   use Ecto.Migration
 
   def change do
+    users()
+  end
 
+  defp users do
+    drop constraint(:users, "users_contact_id_fkey")
+    drop_if_exists index(:users, [:contact_id])
+    alter table(:users) do
+      modify :contact_id, references(:contacts,  on_delete: :delete_all)
+    end
   end
 end
