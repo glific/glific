0a4b0a0a01aec24607c4cd4ed74265cecb81e20e
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-02-25 15:40:03 +0530

    cleanups

--- priv/repo/migrations/20210225083751_v0_11_6_alter_glific_tables.exs
+++ priv/repo/migrations/20210225083751_v0_11_6_alter_glific_tables.exs
@@ -1,4 +1,4 @@
-defmodule Glific.Repo.Migrations.AlterData do
+defmodule Glific.Repo.Migrations.V0_11_6_AlterGlificTables do
   use Ecto.Migration
 
   def change do
@@ -8,8 +8,9 @@ defmodule Glific.Repo.Migrations.AlterData do
   defp users do
     drop constraint(:users, "users_contact_id_fkey")
     drop_if_exists index(:users, [:contact_id])
+
     alter table(:users) do
-      modify :contact_id, references(:contacts,  on_delete: :delete_all)
+      modify :contact_id, references(:contacts, on_delete: :delete_all)
     end
   end
 end
