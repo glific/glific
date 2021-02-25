f7164a999d7ecb33fd6ea8f5d8f95a8134a1e404
pankaj-ag <pankaj@coloredcow.com>
2021-02-25 18:07:17 +0530

    making optin flow as seeded

--- priv/repo/seeds/20210225121554_optin_optout_flows.exs
+++ priv/repo/seeds/20210225121554_optin_optout_flows.exs
@@ -3,10 +3,61 @@ defmodule Glific.Repo.Seeds.OptinOptoutFlows do
 
   envs([:dev])
 
+  alias Glific.{
+    Contacts.Contact,
+    Contacts.ContactsField,
+    Flows.Flow,
+    Flows.FlowLabel,
+    Jobs.BigqueryJob,
+    Partners,
+    Partners.Organization,
+    Partners.Provider,
+    Repo,
+    Searches.SavedSearch,
+    Seeds.SeedsDev,
+    Seeds.SeedsSim,
+    Settings.Language,
+    Tags.Tag,
+    Users
+  }
+
   def up(_repo) do
-    add_optin_flow()
+    Partners.list_organizations()
+    |> Enum.each(fn organization ->
+      Glific.Repo.put_organization_id(organization.id)
+      add_optin_flow(organization)
+    end)
   end
 
-  defp add_optin_flow() do
+  defp add_optin_flow(organization) do
+    uuid_map = %{
+      optin: Ecto.UUID.generate()
+    }
+
+    flow_labels_id_map =
+      FlowLabel.get_all_flowlabel(organization.id)
+      |> Enum.reduce(%{}, fn flow_label, acc ->
+        acc |> Map.merge(%{flow_label.name => flow_label.uuid})
+      end)
+
+    data = [
+      {"Optin", ["optin"], uuid_map.optin, true, "optin.json"}
+    ]
+
+    Enum.map(data, &flow(&1, organization, uuid_map, flow_labels_id_map))
+  end
+
+  defp flow({name, keywords, uuid, ignore_keywords, file}, organization, uuid_map, id_map) do
+    f =
+      Repo.insert!(%Flow{
+        name: name,
+        keywords: keywords,
+        ignore_keywords: ignore_keywords,
+        version_number: "13.1.0",
+        uuid: uuid,
+        organization_id: organization.id
+      })
+
+    SeedsDev.flow_revision(f, organization, file, uuid_map, id_map)
   end
 end
