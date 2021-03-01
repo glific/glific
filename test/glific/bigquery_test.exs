c4911f279eaf60d9b05789724d4f9854b6f1b5fb
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-03-01 17:54:00 +0530

    added queue check for flows

--- test/glific/bigquery_test.exs
+++ test/glific/bigquery_test.exs
@@ -12,16 +12,19 @@ defmodule Glific.BigqueryTest do
     organization = SeedsDev.seed_organizations()
     SeedsDev.seed_contacts(organization)
     SeedsDev.seed_messages()
+    SeedsDev.seed_flows()
     :ok
   end
 
-  test "queue_table_data/4 should create job for messages", attrs do
+  test "queue_table_data/4 should create job", attrs do
     BigQueryWorker.queue_table_data("messages", attrs.organization_id, 0, 10)
+    assert %{success: 1, failure: 0} = Oban.drain_queue(queue: :bigquery)
+
+    BigQueryWorker.queue_table_data("contacts", attrs.organization_id, 0, 10)
+    assert %{success: 1, failure: 0} = Oban.drain_queue(queue: :bigquery)
+
+    BigQueryWorker.queue_table_data("flows", attrs.organization_id, 0, 10)
     assert %{success: 1, failure: 0} = Oban.drain_queue(queue: :bigquery)
   end
 
-  test "queue_table_data/4 should create job for contacts", attrs do
-    BigQueryWorker.queue_table_data("contacts", attrs.organization_id, 0, 10)
-    assert %{success: 1, failure: 0} = Oban.drain_queue(queue: :bigquery)
-  end
 end
