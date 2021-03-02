9d59c29b9d071fea403a10440483ac70be8f83d7
Madhav Malhotra <mmalhotra@securitycompass.com>
2021-03-02 17:04:33 -0500

    Improve the contact creation method

--- lib/glific/contacts/import.ex
+++ lib/glific/contacts/import.ex
@@ -1,26 +1,23 @@
 defmodule Glific.Contacts.Import do
-    alias Glific.{Contacts, Partners, Groups}
+    alias Glific.{Contacts, Groups}
 
-    def import_contact({_, [name,mobile,opt_in,language_id]}) do
-        Contacts.create_contact(%{
-            name: name,
-            phone: mobile,
-            organization_id: 1,
-            language_id: String.to_integer(language_id),
-            optin_time: opt_in
-          })
-    end
+    @doc """
+    This method allows importing of contacts to a particular organization and group
 
+    The method takes in a csv file path and adds the contacts to the particular organization
+    and group.
+    """
     def import_contacts(file_path, organization_id, group_id) do
-        organization = Partners.organization(organization_id)
         group = Groups.get_group!(group_id)
 
         File.stream!(file_path)
         |> CSV.decode
-        |> Enum.map(&import_contact/1)
-
+        |> Enum.map(fn {_, [name,mobile,opt_in,language_id]} -> Contacts.create_or_update_contact(%{
+            name: name,
+            phone: mobile,
+            organization_id: organization_id,
+            language_id: String.to_integer(language_id),
+            optin_time: opt_in
+          }) end)
     end
 end
-
-# alias Glific.Contacts.Import
-# Import.import_contacts("/Users/mmalhotra/apps/glific/lib/glific/contacts/test.csv", 1, 1)
