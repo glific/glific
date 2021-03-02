2a14bb645dc3cbbe3337bae0d8f8782de0d7d25b
Madhav Malhotra <mmalhotra@securitycompass.com>
2021-03-02 18:43:32 -0500

    Improve code

--- lib/glific/contacts/import.ex
+++ lib/glific/contacts/import.ex
@@ -1,5 +1,5 @@
 defmodule Glific.Contacts.Import do
-    alias Glific.{Contacts, Groups}
+    alias Glific.{Contacts, Groups, Settings}
 
     @doc """
     This method allows importing of contacts to a particular organization and group
@@ -8,26 +8,34 @@ defmodule Glific.Contacts.Import do
     and group.
     """
     def import_contacts(file_path, organization_id, group_id) do
-        group = Groups.get_group!(group_id)
-
         File.stream!(file_path)
-        |> CSV.decode
-        |> Enum.map(fn {_, [name,mobile,opt_in,language_id]} -> Contacts.create_or_update_contact(%{
-            name: name,
-            phone: mobile,
+        |> CSV.decode(headers: true, strip_fields: true)
+        |> Enum.map(fn {_, contact} -> %{
+            name: contact["name"],
+            phone: contact["phone"],
             organization_id: organization_id,
-            language_id: String.to_integer(language_id),
-            optin_time: opt_in
+            language_id: Enum.at(Settings.get_language_by_label_or_locale(contact["language"]),0).id,
+            optin_time: elem(Timex.parse(contact["opt_in"], "{YYYY}-{0M}-{0D}"),1)
+        }
+    end )
+        |> Enum.map(fn contact -> Contacts.create_or_update_contact(%{
+            name: contact.name,
+            phone: contact.phone,
+            organization_id: organization_id,
+            language_id: contact.language_id,
+            optin_time: contact.optin_time
           }) end)
         |> Enum.reduce(
             [],
             fn {_,contact}, acc ->
               [
-                %{contact_id: contact.id, group_id: group.id, organization_id: organization_id}
+                %{contact_id: contact.id, group_id: group_id, organization_id: organization_id}
                 | acc
               ]
             end
           )
         |> Enum.map(&Groups.create_contact_group/1)
+
+        :ok
     end
 end
