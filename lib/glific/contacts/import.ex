0ed426b20e9fcd66595a417bdd10d5bbb87daef5
Madhav Malhotra <mmalhotra@securitycompass.com>
2021-03-04 17:33:34 -0500

    simplify code

--- lib/glific/contacts/import.ex
+++ lib/glific/contacts/import.ex
@@ -5,6 +5,26 @@ defmodule Glific.Contacts.Import do
 
   alias Glific.{Contacts, Groups, Settings}
 
+  defp cleanup_contact_data(data, organization_id) do
+    %{
+      name: data["name"],
+      phone: data["phone"],
+      organization_id: organization_id,
+      language_id: Enum.at(Settings.get_language_by_label_or_locale(data["language"]), 0).id,
+      optin_time: elem(Timex.parse(data["opt_in"], "{YYYY}-{0M}-{0D}"), 1)
+    }
+  end
+
+  defp insert_or_update_contact_data(contact, group_id) do
+    {_,contact} = Contacts.create_or_update_contact(contact)
+
+    Groups.create_contact_group(%{
+      contact_id: contact.id,
+      group_id: group_id,
+      organization_id: contact.organization_id
+    })
+  end
+
   @doc """
   This method allows importing of contacts to a particular organization and group
 
@@ -15,34 +35,9 @@ defmodule Glific.Contacts.Import do
   def import_contacts(file_path, organization_id, group_id) do
     File.stream!(file_path)
     |> CSV.decode(headers: true, strip_fields: true)
-    |> Enum.map(fn {_, contact} ->
-      %{
-        name: contact["name"],
-        phone: contact["phone"],
-        organization_id: organization_id,
-        language_id: Enum.at(Settings.get_language_by_label_or_locale(contact["language"]), 0).id,
-        optin_time: elem(Timex.parse(contact["opt_in"], "{YYYY}-{0M}-{0D}"), 1)
-      }
-    end)
-    |> Enum.map(fn contact ->
-      Contacts.create_or_update_contact(%{
-        name: contact.name,
-        phone: contact.phone,
-        organization_id: organization_id,
-        language_id: contact.language_id,
-        optin_time: contact.optin_time
-      })
-    end)
-    |> Enum.reduce(
-      [],
-      fn {_, contact}, acc ->
-        [
-          %{contact_id: contact.id, group_id: group_id, organization_id: organization_id}
-          | acc
-        ]
-      end
-    )
-    |> Enum.map(&Groups.create_contact_group/1)
+    |> Enum.map(fn {_, data} -> cleanup_contact_data(data, organization_id) end)
+    |> Enum.map(fn contact -> insert_or_update_contact_data(contact, group_id) end)
+    |> IO.inspect
 
     :ok
   end
