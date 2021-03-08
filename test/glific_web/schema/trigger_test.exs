11c42ad08fc20df8ab42171d1ff53e6516fc7e60
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-03-08 17:44:02 +0530

    added test case for trigger list

---
+++ test/glific_web/schema/trigger_test.exs
@@ -0,0 +1,21 @@
+defmodule GlificWeb.Schema.WebhookLogTest do
+  use GlificWeb.ConnCase
+  use Wormwood.GQLCase
+
+  alias Glific.Fixtures
+
+  # load_gql(:count, GlificWeb.Schema, "assets/gql/triggers/count.gql")
+  load_gql(:list, GlificWeb.Schema, "assets/gql/triggers/list.gql")
+
+  test "triggers field returns list of triggers", %{staff: user} = attrs do
+    wl = Fixtures.trigger_fixture(attrs)
+
+    result = auth_query_gql_by(:list, user, variables: %{})
+    assert {:ok, query_data} = result
+    triggers = get_in(query_data, [:data, "triggers"])
+    assert length(triggers) > 0
+    [trigger | _] = triggers
+    assert trigger["start_at"] == wl.start_at
+  end
+
+end
