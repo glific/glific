bcc3ebc03b59e4405b08e35ba4265830ff773730
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-03-08 14:54:45 +0530

    new migration for updating table trigger

---
+++ priv/repo/migrations/20210308092147_update_trigger.exs
@@ -0,0 +1,14 @@
+defmodule Glific.Repo.Migrations.UpdateTrigger do
+  use Ecto.Migration
+
+  def change do
+    triggers()
+  end
+
+  defp triggers do
+    alter table(:triggers) do
+      remove :contact_id
+      remove :name
+    end
+  end
+end
