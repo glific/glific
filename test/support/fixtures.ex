a9f3714dd9e9428608649e89d5ad8bf22b4b013b
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-03-09 13:16:54 +0530

    adding test case to return triggers in desc order

--- test/support/fixtures.ex
+++ test/support/fixtures.ex
@@ -574,14 +574,20 @@ defmodule Glific.Fixtures do
       start_at: ~U[2021-03-08 08:22:51Z]
     }
 
-    flow = flow_fixture(Map.merge(attrs, %{keywords: [], name: Person.name()}))
-    group = group_fixture(attrs)
+    [g1 | _] = Groups.list_groups(attrs)
+    [f1 | _] = Flows.list_flows(attrs)
+
+    {:ok, group} =
+      Repo.fetch_by(Groups.Group, %{id: g1.id, organization_id: attrs.organization_id})
+
+      {:ok, flow} =
+      Repo.fetch_by(Flows.Flow, %{id: f1.id, organization_id: attrs.organization_id})
 
     valid_attrs =
       valid_attrs
       |> Map.merge(attrs)
-      |> Map.put(:flow_id, flow.id)
-      |> Map.put(:group_id, group.id)
+      |> Map.put(:flow_id, f1.id)
+      |> Map.put(:group_id, g1.id)
       |> Map.put(:organization_id, attrs.organization_id)
 
     {:ok, trigger} = Trigger.create_trigger(valid_attrs)
