581d90c3e572763b467c9224f27acfb2a6eed611
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-03-17 14:31:37 +0530

    updating test case with frequency

--- test/glific/triggers_test.exs
+++ test/glific/triggers_test.exs
@@ -1,5 +1,4 @@
 defmodule Glific.TriggersTest do
-
   use Glific.DataCase
   use Oban.Testing, repo: Glific.Repo
 
@@ -10,9 +9,8 @@ defmodule Glific.TriggersTest do
     Triggers
   }
 
-
   setup do
-  organization = SeedsDev.seed_organizations()
+    organization = SeedsDev.seed_organizations()
     SeedsDev.seed_contacts(organization)
     SeedsDev.seed_flows(organization)
     SeedsDev.seed_users(organization)
@@ -27,9 +25,15 @@ defmodule Glific.TriggersTest do
       start_at = Timex.shift(DateTime.utc_now(), days: -1)
       end_date = Timex.shift(DateTime.utc_now(), days: 1)
 
-      _trigger = Fixtures.trigger_fixture(%{start_at: start_at, organization_id: attrs.organization_id, end_date: end_date})
+      _trigger =
+        Fixtures.trigger_fixture(%{
+          start_at: start_at,
+          organization_id: attrs.organization_id,
+          end_date: end_date
+        })
+
       msg_count1 = Messages.count_messages(%{filter: attrs})
-       Triggers.execute_triggers(attrs.organization_id)
+      Triggers.execute_triggers(attrs.organization_id)
       msg_count2 = Messages.count_messages(%{filter: attrs})
       assert msg_count2 > msg_count1
     end
@@ -37,9 +41,37 @@ defmodule Glific.TriggersTest do
     test "execute_triggers/2 should execute a trigger with last_trigger_at not nil", attrs do
       start_at = Timex.shift(DateTime.utc_now(), days: -1)
       end_date = Timex.shift(DateTime.utc_now(), days: 2)
-      _trigger = Fixtures.trigger_fixture(%{start_at: start_at, organization_id: attrs.organization_id, last_trigger_at: time, end_date: end_date})
+
+      _trigger =
+        Fixtures.trigger_fixture(%{
+          start_at: start_at,
+          organization_id: attrs.organization_id,
+          last_trigger_at: start_at,
+          end_date: end_date
+        })
+
       msg_count1 = Messages.count_messages(%{filter: attrs})
-       Triggers.execute_triggers(attrs.organization_id)
+      Triggers.execute_triggers(attrs.organization_id)
+      msg_count2 = Messages.count_messages(%{filter: attrs})
+      assert msg_count2 > msg_count1
+    end
+
+    test "execute_triggers/2 should execute a trigger with frequency as daily", attrs do
+      start_at = Timex.shift(DateTime.utc_now(), days: -1)
+      end_date = Timex.shift(DateTime.utc_now(), days: 5)
+
+      _trigger =
+        Fixtures.trigger_fixture(%{
+          start_at: start_at,
+          frequency: ["daily"],
+          is_repeating: true,
+          organization_id: attrs.organization_id,
+          last_trigger_at: start_at,
+          end_date: end_date
+        })
+
+      msg_count1 = Messages.count_messages(%{filter: attrs})
+      Triggers.execute_triggers(attrs.organization_id)
       msg_count2 = Messages.count_messages(%{filter: attrs})
       assert msg_count2 > msg_count1
     end
