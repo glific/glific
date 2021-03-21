df4eb05c0387a1e6c18fb1808d0f47adb156bf2d
Donald A. Lobo <donald.lobo@gmail.com>
2021-03-21 07:46:53 -0700

    dont fall into atoms vs strings trap, be consistent

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Glific.MixProject do
   def project do
     [
       app: :glific,
-      version: "1.2.1",
+      version: "1.2.2",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       dialyzer: [
