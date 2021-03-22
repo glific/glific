b7cdb2f7ef20595187a8322c5f67e40a44cd0e52
Donald A. Lobo <donald.lobo@gmail.com>
2021-03-22 12:27:38 -0700

    add stats table to gather and collect stats

--- priv/repo/migrations/20210322182605_stats.exs
+++ priv/repo/migrations/20210322182605_stats.exs
@@ -10,8 +10,10 @@ defmodule Glific.Repo.Migrations.Stats do
     # since we are starting off with a blank slate
     drop_if_exists table(:stats)
 
-    # daily trends, capture once a day
-    create table(:stats_daily) do
+    # period trends, capture hourly, daily, weekly, monthly, summary
+    # The total is specific only for that period
+    # The summary total is the grand total in the DB, and keeps track of contact data only
+    create table(:stats) do
       # contact related data
       add :contacts, :integer, comment: "Total number of contacts"
       add :active, :integer, comment: "Total number of active contacts"
@@ -22,12 +24,15 @@ defmodule Glific.Repo.Migrations.Stats do
       add :messages, :integer, comment: "Total number of messages"
       add :inbound, :integer, comment: "Total number of inbound messages"
       add :outbound, :integer, comment: "Total number of outbound messages"
-      add :hsm, :integer, comment: "Total number of outbound messages"
+      add :hsm, :integer, comment: "Total number of HSM messages (outbound only)"
 
       add :flows_started, :integer, comment: "Total number of flows started today"
       add :flows_completed, :integer, comment: "Total number of flows completed today"
 
+      add :period, :string, comment: "The period for this record: hour, day, week, month, summary"
+
       add :date, :date, comment: "All stats are measured with respect to UTC time, to keep things timezone agnostic"
+      add :time, :integer, comment: "The start of the hour that this record represents, 0..23, only for PERIOD: hour"
 
       # foreign key to organization restricting scope of this table to this organization only
       add :organization_id, references(:organizations, on_delete: :delete_all), null: false
@@ -35,31 +40,8 @@ defmodule Glific.Repo.Migrations.Stats do
       timestamps(type: :utc_datetime)
     end
 
-    create index(:stats_daily, :organization_id)
-    create index(:stats_daily, :date)
-
-    # hourly trends
-    create table(:stats_hourly) do
-      add :active, :integer, comment: "Number of contacts active this period"
-
-      add :inbound, :integer, comment: "Number of inbound messages this period"
-      add :outbound, :integer, comment: "Number of outbound messages this period"
-      add :hsm, :integer, comment: "Number of HSM messages (outbound only) this period"
-
-      add :flows_started, :integer, comment: "Number of flows started this period"
-      add :flows_completed, :integer, comment: "Number of flows completed this period"
-
-      add :date, :date, comment: "All stats are measured with respect to UTC time, to keep things timezone agnostic"
-      add :time, :integer, comment: "The start of the hour that this record represents, 0..23"
-
-      # foreign key to organization restricting scope of this table to this organization only
-      add :organization_id, references(:organizations, on_delete: :delete_all), null: false
-
-      timestamps(type: :utc_datetime)
-    end
-
-    create index(:stats_hourly, :organization_id)
-    create index(:stats_hourly, :date)
+    create index(:stats, :organization_id)
+    create index(:stats, :date)
   end
 
   def down do
