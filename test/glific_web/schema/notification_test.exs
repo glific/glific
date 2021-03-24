8e567f2fea2f0c770f021cd4ca2134f8bb15280d
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-03-24 12:55:18 +0530

    added GraphQL testcases

--- test/glific_web/schema/notification_test.exs
+++ test/glific_web/schema/notification_test.exs
@@ -17,83 +17,84 @@ defmodule GlificWeb.Schema.NotificationTest do
     assert notification["category"] == notify.category
   end
 
-  # test "notifications field returns list of notifications in desc order", %{staff: user} = attrs do
-  #   _wl_1 = Fixtures.notification_fixture(attrs)
-  #   :timer.sleep(1000)
-  #   valid_attrs_2 = Map.merge(attrs, %{category: "test_category_2", status_code: 500})
-  #   wl_2 = Fixtures.notification_fixture(valid_attrs_2)
+  test "notifications field returns list of notifications in desc order", %{staff: user} = attrs do
+    _notify_1 = Fixtures.notification_fixture(attrs)
+    :timer.sleep(1000)
+    valid_attrs_2 = Map.merge(attrs, %{category: "Provider Error", message: "No balance in wallet"})
+    notify_2 = Fixtures.notification_fixture(valid_attrs_2)
 
-  #   result = auth_query_gql_by(:list, user, variables: %{"opts" => %{"order" => "DESC"}})
-  #   assert {:ok, query_data} = result
-  #   notifications = get_in(query_data, [:data, "notifications"])
-  #   assert length(notifications) > 0
-  #   [notification | _] = notifications
-  #   assert notification["category"] == wl_2.category
-  # end
+    result = auth_query_gql_by(:list, user, variables: %{"opts" => %{"order" => "DESC"}})
+    assert {:ok, query_data} = result
+    notifications = get_in(query_data, [:data, "notifications"])
+    assert length(notifications) > 0
+    [notification | _] = notifications
+    assert notification["category"] == notify_2.category
+  end
 
-  # test "notifications field returns list of notifications in various filters",
-  #      %{staff: user} = attrs do
-  #   wl_1 = Fixtures.notification_fixture(attrs)
-  #   valid_attrs_2 = Map.merge(attrs, %{category: "test_category_2", status_code: 500})
-  #   _wl_2 = Fixtures.notification_fixture(valid_attrs_2)
+  test "notifications field returns list of notifications in various filters",
+       %{staff: user} = attrs do
+    notify_1 = Fixtures.notification_fixture(attrs)
+    valid_attrs_2 = Map.merge(attrs, %{category: "Provider Error", message: "No balance in wallet"})
+    _notify_2 = Fixtures.notification_fixture(valid_attrs_2)
 
-  #   result = auth_query_gql_by(:list, user, variables: %{"filter" => %{"category" => wl_1.category}})
-  #   assert {:ok, query_data} = result
-  #   notifications = get_in(query_data, [:data, "notifications"])
-  #   assert length(notifications) > 0
-  #   [notification | _] = notifications
-  #   assert get_in(notification, ["category"]) == wl_1.category
+    result = auth_query_gql_by(:list, user, variables: %{"filter" => %{"category" => notify_1.category}})
+    assert {:ok, query_data} = result
+    notifications = get_in(query_data, [:data, "notifications"])
+    assert length(notifications) > 0
+    [notification | _] = notifications
+    assert get_in(notification, ["category"]) == notify_1.category
 
-  #   result =
-  #     auth_query_gql_by(:list, user,
-  #       variables: %{"filter" => %{"status_code" => wl_1.status_code}}
-  #     )
+    result =
+      auth_query_gql_by(:list, user,
+        variables: %{"filter" => %{"message" => notify_1.message}}
+      )
 
-  #   assert {:ok, query_data} = result
-  #   notifications = get_in(query_data, [:data, "notifications"])
-  #   assert length(notifications) > 0
-  #   [notification | _] = notifications
-  #   assert get_in(notification, ["statusCode"]) == wl_1.status_code
-  # end
+    assert {:ok, query_data} = result
+    notifications = get_in(query_data, [:data, "notifications"])
+    assert length(notifications) > 0
+    [notification | _] = notifications
+    assert get_in(notification, ["message"]) == notify_1.message
+  end
 
-  # test "notifications field obeys limit and offset", %{staff: user} = attrs do
-  #   _wl_1 = Fixtures.notification_fixture(attrs)
-  #   valid_attrs_2 = Map.merge(attrs, %{category: "test_category_2", status_code: 500})
-  #   _wl_2 = Fixtures.notification_fixture(valid_attrs_2)
+  test "notifications field obeys limit and offset", %{staff: user} = attrs do
+    _notify_1 = Fixtures.notification_fixture(attrs)
+    valid_attrs_2 = Map.merge(attrs, %{category: "Provider Error", message: "No balance in wallet"})
+    _notify_2 = Fixtures.notification_fixture(valid_attrs_2)
 
-  #   result =
-  #     auth_query_gql_by(:list, user, variables: %{"opts" => %{"limit" => 1, "offset" => 0}})
+    result =
+      auth_query_gql_by(:list, user, variables: %{"opts" => %{"limit" => 1, "offset" => 0}})
 
-  #   assert {:ok, query_data} = result
-  #   assert length(get_in(query_data, [:data, "notifications"])) == 1
+    assert {:ok, query_data} = result
+    assert length(get_in(query_data, [:data, "notifications"])) == 1
 
-  #   result =
-  #     auth_query_gql_by(:list, user, variables: %{"opts" => %{"limit" => 1, "offset" => 1}})
+    result =
+      auth_query_gql_by(:list, user, variables: %{"opts" => %{"limit" => 1, "offset" => 1}})
 
-  #   assert {:ok, query_data} = result
+    assert {:ok, query_data} = result
 
-  #   notifications = get_in(query_data, [:data, "notifications"])
-  #   assert length(notifications) == 1
-  # end
+    notifications = get_in(query_data, [:data, "notifications"])
+    assert length(notifications) == 1
+  end
 
-  # test "count returns the number of notifications", %{staff: user} = attrs do
-  #   _wl_1 = Fixtures.notification_fixture(attrs)
-  #   valid_attrs_2 = Map.merge(attrs, %{category: "test_category_2"})
-  #   wl_2 = Fixtures.notification_fixture(valid_attrs_2)
+  test "count returns the number of notifications", %{staff: user} = attrs do
+    _notify_1 = Fixtures.notification_fixture(attrs)
+    valid_attrs_2 = Map.merge(attrs, %{category: "test_category_2"})
+    notify_2 = Fixtures.notification_fixture(valid_attrs_2)
 
-  #   {:ok, query_data} = auth_query_gql_by(:count, user)
-  #   assert get_in(query_data, [:data, "countnotifications"]) == 2
+    {:ok, query_data} = auth_query_gql_by(:count, user)
+    IO.inspect(query_data)
+    assert get_in(query_data, [:data, "countNotifications"]) == 2
 
-  #   {:ok, query_data} =
-  #     auth_query_gql_by(:count, user,
-  #       variables: %{"filter" => %{"category" => "This notification doesn't exist"}}
-  #     )
+    {:ok, query_data} =
+      auth_query_gql_by(:count, user,
+        variables: %{"filter" => %{"category" => "This notification doesn't exist"}}
+      )
 
-  #   assert get_in(query_data, [:data, "countnotifications"]) == 0
+    assert get_in(query_data, [:data, "countNotifications"]) == 0
 
-  #   {:ok, query_data} =
-  #     auth_query_gql_by(:count, user, variables: %{"filter" => %{"category" => wl_2.category}})
+    {:ok, query_data} =
+      auth_query_gql_by(:count, user, variables: %{"filter" => %{"category" => notify_2.category}})
 
-  #   assert get_in(query_data, [:data, "countnotifications"]) == 1
-  # end
+    assert get_in(query_data, [:data, "countNotifications"]) == 1
+  end
 end
