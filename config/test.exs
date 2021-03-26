9ffe5e4a8a48f72d39d8324bbf3e1e938a3ec136
Pankaj Agrawal <pankaj@coloredcow.com>
2021-03-26 20:56:48 +0530

    Update test.exs

--- config/test.exs
+++ config/test.exs
@@ -25,7 +25,7 @@ config :glific, GlificWeb.Endpoint,
 
 # Print only warnings and errors during test
 config :logger,
-  level: :emergency,
+  level: :info,
   compile_time_purge_matching: [
     [level_lower_than: :emergency]
   ]
