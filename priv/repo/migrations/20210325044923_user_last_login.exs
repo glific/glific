d17ad3ff3434dd3e91963f11f81d1800ed84ff63
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-03-26 14:12:37 +0530

    User-login-track (#1326)
    Co-authored-by: pankaj-ag <pankaj@coloredcow.in>

---
+++ priv/repo/migrations/20210325044923_user_last_login.exs
@@ -0,0 +1,14 @@
+defmodule Glific.Repo.Migrations.UserLastLogin do
+  use Ecto.Migration
+
+  def change do
+    users()
+  end
+
+  defp users() do
+    alter table(:users) do
+      add :last_login_at, :utc_datetime, default: nil
+      add :last_login_from, :string, default: nil
+    end
+  end
+end
