5fe3f3d562050a39e0fc1b00084af8be477cadd3
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-04-08 11:10:48 +0530

    updating with schema

--- priv/repo/migrations/20210406052407_v1_3_1_alter_glific_tables.exs
+++ priv/repo/migrations/20210406052407_v1_3_1_alter_glific_tables.exs
@@ -1,5 +1,6 @@
 defmodule Glific.Repo.Migrations.V131AlterGlificTables do
   use Ecto.Migration
+  @global_schema Application.fetch_env!(:glific, :global_schema)
 
   def change do
     session_templates()
@@ -15,12 +16,14 @@ defmodule Glific.Repo.Migrations.V131AlterGlificTables do
 
   defp users() do
     alter table(:users) do
-      add :language_id, references(:languages, on_delete: :nothing), default: null
+      add :language_id, references(:languages, on_delete: :restrict, prefix: @global_schema),
+        null: true,
+        comment: "Foreign key for the language"
     end
   end
 
   defp languages() do
-    alter table(:languages) do
+    alter table(:languages, prefix: @global_schema) do
       add :localized, :boolean, default: false
     end
   end
