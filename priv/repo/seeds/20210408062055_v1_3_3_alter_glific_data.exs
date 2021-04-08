eaa77f97c3c88c2875adbb5e47485a2814ce4bb8
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-04-08 12:29:39 +0530

    cleanup

--- priv/repo/seeds/20210408062055_v1_3_3_alter_glific_data.exs
+++ priv/repo/seeds/20210408062055_v1_3_3_alter_glific_data.exs
@@ -13,9 +13,7 @@ defmodule Glific.Repo.Seeds.V133AlterGlificData do
   end
 
   defp update_language_localized() do
-    languages = Repo.all(Language, skip_organization_id: true)
-IO.inspect(languages)
-    languages
+    Repo.all(Language, skip_organization_id: true)
     |> Enum.each(fn language ->
       if Enum.member?(["en_US", "hi"], language.locale) do
         Repo.update!(Ecto.Changeset.change(language, %{localized: true}))
