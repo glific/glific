582fc0923aae421275fff348a0f709722e6995ec
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-04-13 14:50:01 +0530

    added random string generater

--- test/support/fixtures.ex
+++ test/support/fixtures.ex
@@ -650,4 +650,45 @@ defmodule Glific.Fixtures do
 
     notification
   end
+
+  @doc """
+  Generate random string based on the given legth. It is also possible to generate certain type of randomise string using the options below:
+  * :all - generate alphanumeric random string
+  * :alpha - generate nom-numeric random string
+  * :numeric - generate numeric random string
+  * :upcase - generate upper case non-numeric random string
+  * :downcase - generate lower case non-numeric random string
+  ## Example
+      iex> Fixtures.randomizer(20) //"Je5QaLj982f0Meb0ZBSK"
+  """
+  @spec randomizer(non_neg_integer(), atom()) :: String.t()
+  def randomizer(length, type \\ :all) do
+    alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "0123456789"
+
+    lists =
+      cond do
+        type == :alpha -> alphabets <> String.downcase(alphabets)
+        type == :numeric -> numbers
+        type == :upcase -> alphabets
+        type == :downcase -> String.downcase(alphabets)
+        true -> alphabets <> String.downcase(alphabets) <> numbers
+      end
+      |> String.split("", trim: true)
+
+    do_randomizer(length, lists)
+  end
+
+  @doc false
+  @spec get_range(non_neg_integer()) :: [any()]
+  defp get_range(length) when length > 1, do: 1..length
+  defp get_range(_length), do: [1]
+
+  @doc false
+  @spec do_randomizer(non_neg_integer(), list()) :: String.t()
+  defp do_randomizer(length, lists) do
+    get_range(length)
+    |> Enum.reduce([], fn _, acc -> [Enum.random(lists) | acc] end)
+    |> Enum.join("")
+  end
 end
