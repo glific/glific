a41a1089b5b2c383f1dabff8c8aa1b57ee2a1237
pankaj-ag <pankaj@coloredcow.in>
2021-04-14 18:21:54 +0530

    login issues

--- test/glific_web/flows/flow_editor_controller_test.exs
+++ test/glific_web/flows/flow_editor_controller_test.exs
@@ -50,7 +50,7 @@ defmodule GlificWeb.Flows.FlowEditorControllerTest do
   defp get_token(valid_params, organization_id, conn) do
     params = put_in(valid_params, ["user", "organization_id"], organization_id)
     authed_conn = post(conn, Routes.api_v1_session_path(conn, :create, params))
-    {:ok, access_token: authed_conn.private[:api_access_token], conn: conn}
+    {:ok, access_token: authed_conn.private[:api_access_token]}
   end
 
   describe "flow_editor_routes" do
