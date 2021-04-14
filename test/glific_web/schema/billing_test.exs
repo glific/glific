0f862be375ee05f05fc93e7d4313991fbc068dec
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-04-14 18:51:51 +0530

    Feature/stripe testcases (#1405)
    * using exvcr for testing
    
    * added test case for create billing
    
    * added fixture for billing
    
    * added testcase for get billing
    
    * added test case for subscription
    
    * invalid attrs subscription
    
    * adding GraphQL test
    
    * added graphql subscription test
    
    * Update billing.ex

--- test/glific_web/schema/billing_test.exs
+++ test/glific_web/schema/billing_test.exs
@@ -1,6 +1,8 @@
 defmodule GlificWeb.Schema.BillingTest do
   use GlificWeb.ConnCase
   use Wormwood.GQLCase
+  use ExUnit.Case
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   alias Glific.{
     Partners.Billing,
@@ -11,6 +13,7 @@ defmodule GlificWeb.Schema.BillingTest do
   setup do
     organization = SeedsDev.seed_organizations()
     SeedsDev.seed_billing(organization)
+    HTTPoison.start()
     :ok
   end
 
@@ -18,6 +21,7 @@ defmodule GlificWeb.Schema.BillingTest do
   load_gql(:list, GlificWeb.Schema, "assets/gql/billings/list.gql")
   load_gql(:by_id, GlificWeb.Schema, "assets/gql/billings/by_id.gql")
   load_gql(:create, GlificWeb.Schema, "assets/gql/billings/create.gql")
+  load_gql(:create_subscription, GlificWeb.Schema, "assets/gql/billings/create_subscription.gql")
   load_gql(:update, GlificWeb.Schema, "assets/gql/billings/update.gql")
   load_gql(:delete, GlificWeb.Schema, "assets/gql/billings/delete.gql")
 
@@ -46,5 +50,57 @@ defmodule GlificWeb.Schema.BillingTest do
     assert {:ok, query_data} = result
     currency = get_in(query_data, [:data, "updateBilling", "billing", "currency"])
     assert currency == "usd"
+  end
+
+  test "create a billing", %{user: user} do
+    use_cassette "create_billing" do
+      result =
+        auth_query_gql_by(:create, user,
+          variables: %{
+            "input" => %{
+              "name" => "test billing name",
+              "email" => "testbilling@gmail.com",
+              "currency" => "inr"
+            }
+          }
+        )
+
+      assert {:ok, query_data} = result
+      currency = get_in(query_data, [:data, "createBilling", "billing", "currency"])
+      assert currency == "inr"
+      email = get_in(query_data, [:data, "createBilling", "billing", "email"])
+      assert email == "testbilling@gmail.com"
+    end
+  end
+
+  test "create a billing subscription", %{user: user} do
+    use_cassette "create_subscription" do
+      result =
+        auth_query_gql_by(:create_subscription, user,
+          variables: %{
+            "input" => %{
+              "stripe_payment_method_id" => "some_stripe_payment_method_id"
+            }
+          }
+        )
+
+      assert {:ok, query_data} = result
+      assert get_in(query_data, [:data, "createBillingSubscription", "errors"]) == nil
+    end
+  end
+
+  test "fetch a billing by id", %{user: user} do
+    stripe_customer_id = "test_cus_JIdQjmJcjq"
+
+    {:ok, billing} =
+      Repo.fetch_by(Billing, %{
+        stripe_customer_id: stripe_customer_id,
+        organization_id: user.organization_id
+      })
+
+    result = auth_query_gql_by(:by_id, user, variables: %{"id" => billing.id})
+    assert {:ok, query_data} = result
+    billing = get_in(query_data, [:data, "billing", "billing"])
+    assert billing["stripe_customer_id"] == stripe_customer_id
   end
 end
