de3d948e8535a1678fe207905d01e1b006a7f3c6
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-04-14 16:16:44 +0530

    adding GraphQL test

--- test/glific_web/schema/billing_test.exs
+++ test/glific_web/schema/billing_test.exs
@@ -1,6 +1,8 @@
 defmodule GlificWeb.Schema.BillingTest do
   use GlificWeb.ConnCase
   use Wormwood.GQLCase
+  use ExUnit.Case
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   alias Glific.{
     Partners.Billing,
@@ -11,6 +13,7 @@ defmodule GlificWeb.Schema.BillingTest do
   setup do
     organization = SeedsDev.seed_organizations()
     SeedsDev.seed_billing(organization)
+    HTTPoison.start()
     :ok
   end
 
@@ -46,5 +49,41 @@ defmodule GlificWeb.Schema.BillingTest do
     assert {:ok, query_data} = result
     currency = get_in(query_data, [:data, "updateBilling", "billing", "currency"])
     assert currency == "usd"
+  end
+
+  test "create a billing", %{user: user} do
+    use_cassette "create_billing" do
+      result =
+        auth_query_gql_by(:create, user,
+          variables: %{
+            "input" => %{
+              "name" => "test billing name",
+              "email" => "testbilling@gmail.com",
+              "currency" => "inr"
+            }
+          }
+        )
+
+      assert {:ok, query_data} = result
+      currency = get_in(query_data, [:data, "createBilling", "billing", "currency"])
+      assert currency == "inr"
+      email = get_in(query_data, [:data, "createBilling", "billing", "email"])
+      assert email == "testbilling@gmail.com"
+    end
+  end
+
+  test "fetch a billing by id", %{user: user} do
+    stripe_customer_id = "test_cus_JIdQjmJcjq"
+
+    {:ok, billing} =
+      Repo.fetch_by(Billing, %{
+        stripe_customer_id: stripe_customer_id,
+        organization_id: user.organization_id
+      })
+
+    result = auth_query_gql_by(:by_id, user, variables: %{"id" => billing.id})
+    assert {:ok, query_data} = result
+    billing = get_in(query_data, [:data, "billing", "billing"])
+    assert billing["stripe_customer_id"] == stripe_customer_id
   end
 end
