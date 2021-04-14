b922f7a60bd76bb79950baec11d608ff7bfbbea1
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-04-14 16:55:35 +0530

    added graphql subscription test

--- test/glific_web/schema/billing_test.exs
+++ test/glific_web/schema/billing_test.exs
@@ -21,6 +21,7 @@ defmodule GlificWeb.Schema.BillingTest do
   load_gql(:list, GlificWeb.Schema, "assets/gql/billings/list.gql")
   load_gql(:by_id, GlificWeb.Schema, "assets/gql/billings/by_id.gql")
   load_gql(:create, GlificWeb.Schema, "assets/gql/billings/create.gql")
+  load_gql(:create_subscription, GlificWeb.Schema, "assets/gql/billings/create_subscription.gql")
   load_gql(:update, GlificWeb.Schema, "assets/gql/billings/update.gql")
   load_gql(:delete, GlificWeb.Schema, "assets/gql/billings/delete.gql")
 
@@ -69,6 +70,22 @@ defmodule GlificWeb.Schema.BillingTest do
       assert currency == "inr"
       email = get_in(query_data, [:data, "createBilling", "billing", "email"])
       assert email == "testbilling@gmail.com"
+    end
+  end
+
+  test "create a billing subscription", %{user: user} do
+    use_cassette "create_subscription" do
+      result =
+        auth_query_gql_by(:create_subscription, user,
+          variables: %{
+            "input" => %{
+              "stripe_payment_method_id" => "some_stripe_payment_method_id"
+            }
+          }
+        )
+
+      assert {:ok, query_data} = result
+      assert get_in(query_data, [:data, "createBillingSubscription", "errors"]) == nil
     end
   end
 
