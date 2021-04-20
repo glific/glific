ce9b24699c1e5223a81e1c67dd021f173989f440
Donald A. Lobo <donald.lobo@gmail.com>
2021-04-20 13:25:48 -0700

    try to debug, not returning

--- lib/glific_web/plugs/stripe_webhook.ex
+++ lib/glific_web/plugs/stripe_webhook.ex
@@ -13,11 +13,12 @@ defmodule GlificWeb.StripeWebhook do
   @doc false
   def call(%{request_path: "/webhook/stripe"} = conn, _) do
     signing_secret = Application.fetch_env!(:stripity_stripe, :signing_secret)
-    [stripe_signature] = Conn.get_req_header(conn, "stripe-signature")
+    [stripe_signature] = Conn.get_req_header(conn, "stripe-signature") |> IO.inspect(label: "Signature")
 
     with {:ok, body, _} <- Conn.read_body(conn),
          {:ok, stripe_event} <-
-           Stripe.Webhook.construct_event(body, stripe_signature, signing_secret) do
+    Stripe.Webhook.construct_event(body, stripe_signature, signing_secret) do
+      IO.inspect(stripe_event.type, label: "EVENT")
       conn |> Plug.Conn.assign(:stripe_event, stripe_event)
     else
       {:error, error} ->
