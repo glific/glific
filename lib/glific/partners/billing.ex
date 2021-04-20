a1136f0d462e17292ce9717d299fa3b54bca2759
Donald A. Lobo <donald.lobo@gmail.com>
2021-04-20 11:30:12 -0700

    fix stripe webhooks

--- lib/glific/partners/billing.ex
+++ lib/glific/partners/billing.ex
@@ -462,6 +462,12 @@ defmodule Glific.Partners.Billing do
             organization_id
           }"
         )
+
+        message = """
+        Did not find Billing object for Subscription: #{subscription.id}, org: #{organization_id}
+        """
+
+        {:error, message}
     end
   end
 
