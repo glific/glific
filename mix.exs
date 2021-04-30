b51f54fbf15341dfd8a15072df57046d06bc0686
Donald A. Lobo <donald.lobo@gmail.com>
2021-04-30 13:08:23 -0700

    upgrade version number for release

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Glific.MixProject do
   def project do
     [
       app: :glific,
-      version: "1.4.7",
+      version: "1.4.8",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       dialyzer: [
