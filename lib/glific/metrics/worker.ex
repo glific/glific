496eac4550f4c4ef192ac04071f903e2ec70a55a
Donald A. Lobo <donald.lobo@gmail.com>
2021-05-04 14:27:51 -0700

    optimize the implementations, maps == good, lists == bad

--- lib/glific/metrics/worker.ex
+++ lib/glific/metrics/worker.ex
@@ -52,7 +52,7 @@ defmodule Glific.Metrics.Worker do
   @doc false
   defp schedule_upsert do
     # store to database between 1 to 3 minutes
-    Process.send_after(self(), :upsert, Enum.random(60..180) * 1_000)
+    Process.send_after(self(), :upsert, Enum.random(60..90) * 1_000)
   end
 
   @doc false
