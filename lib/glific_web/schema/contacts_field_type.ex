0d4ff2c762ceb0e5e8cc13aa0c7769fc72fe0693
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-05-13 16:39:11 +0530

    adding contact field type in schema

--- lib/glific_web/schema/contacts_field_type.ex
+++ lib/glific_web/schema/contacts_field_type.ex
@@ -24,8 +24,8 @@ defmodule GlificWeb.Schema.ContactsFieldTypes do
     field :id, :id
     field :name, :string
     field :shortcode, :string
-    field :value_type, :string
-    field :scope, :string
+    field :value_type, :contact_field_value_type_enum
+    field :scope, :contact_field_scope_enum
     field :inserted_at, :datetime
     field :updated_at, :datetime
 
@@ -46,8 +46,8 @@ defmodule GlificWeb.Schema.ContactsFieldTypes do
   input_object :contacts_field_input do
     field :name, :string
     field :shortcode, :string
-    field :value_type, :string
-    field :scope, :string
+    field :value_type, :contact_field_value_type_enum
+    field :scope, :contact_field_scope_enum
   end
 
   object :contacts_field_queries do
