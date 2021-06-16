bf012668762b1c1042d07b8e90e729641044eef5
pankaj-ag <pankaj@coloredcow.in>
2021-06-16 14:21:31 +0530

    migration

--- priv/repo/migrations/20210616074312_v1_7_0.exs
+++ priv/repo/migrations/20210616074312_v1_7_0.exs
@@ -1,16 +1,23 @@
 defmodule Glific.Repo.Migrations.V170 do
   use Ecto.Migration
 
-  def change do
+  alias Glific.Enums.OrganizationStatus
+
+  def up do
     alter_organization_with_status()
   end
 
-  def abc do
+  def alter_organization_with_status do
+    OrganizationStatus.create_type()
     alter table(:organizations) do
-      add :status, :boolean,
-        default: false,
-        comment: "Manual approval of an organization to trigger onboarding workflow"
+      add_if_not_exists :status, :organization_status_enum,
+        default: "inactive",
+        comment: "organization status to define different states of the organizations"
     end
+  end
+
+  def down do
+     OrganizationStatus.drop_type()
   end
 
 end
