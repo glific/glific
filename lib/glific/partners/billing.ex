805e0c732c1824e42ca75068eb61b8a35c5b8a03
Pankaj Agrawal <pankaj@coloredcow.com>
2021-06-18 23:38:46 +0530

    Update billing.ex

--- lib/glific/partners/billing.ex
+++ lib/glific/partners/billing.ex
@@ -258,7 +258,7 @@ defmodule Glific.Partners.Billing do
       customer: billing.stripe_customer_id,
       # Temporary for existing customers.
       billing_cycle_anchor: anchor_timestamp,
-      prorate: true,
+      prorate: false,
       items: [
         %{
           price: prices["monthly"],
