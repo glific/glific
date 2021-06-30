84ec49cea974d2658ad2aac9683ae088ae5b0e83
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-06-30 17:42:16 +0530

    doc updates feature/function

--- lib/glific/third_party/navanatech/navanatech.ex
+++ lib/glific/third_party/navanatech/navanatech.ex
@@ -3,6 +3,10 @@ defmodule Glific.Navanatech do
   Glific Navanatech for all api calls to navatech
   """
 
+  @doc """
+  Creating a dataset with messages and contacts as tables
+  """
+  @spec navatech_post(map()) :: map()
   def navatech_post(fields) do
     Process.sleep(2000)
     %{response: fields["response"]}
