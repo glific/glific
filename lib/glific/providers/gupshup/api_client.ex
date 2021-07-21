a642320222e636ad43f523f670a443920df6186c
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-07-21 16:41:55 +0530

    added delay of 30 seconds

--- lib/glific/providers/gupshup/api_client.ex
+++ lib/glific/providers/gupshup/api_client.ex
@@ -71,9 +71,9 @@ defmodule Glific.Providers.Gupshup.ApiClient do
 
     with {:ok, credentials} <- get_credentials(org_id) do
       template_url = @gupshup_url <> "/template/add/" <> credentials.app_name
-      # Adding a delay of 1 min when applying for template
+      # Adding a delay of 30 seconds when applying for template
       post(template_url, payload,
-        headers: [{"apikey", credentials.api_key}, opts: [adapter: [recv_timeout: 60_000]]]
+        headers: [{"apikey", credentials.api_key}, opts: [adapter: [recv_timeout: 30_000]]]
       )
     end
   end
