bcd0bb17f29f51bb28ccff8477628132e8da5e0c
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-08-04 15:00:08 +0530

    reverting time changes

--- lib/glific/providers/gupshup/api_client.ex
+++ lib/glific/providers/gupshup/api_client.ex
@@ -71,10 +71,7 @@ defmodule Glific.Providers.Gupshup.ApiClient do
 
     with {:ok, credentials} <- get_credentials(org_id) do
       template_url = @gupshup_url <> "/template/add/" <> credentials.app_name
-      # Adding a delay of 30 seconds when applying for template
-      post(template_url, payload,
-        headers: [{"apikey", credentials.api_key}, opts: [adapter: [recv_timeout: 30_000]]]
-      )
+      gupshup_post(template_url, payload, credentials.api_key)
     end
   end
 
