95400c291d590ac13299cb16177f43b3e3f81bb3
pankaj-ag <pankaj@coloredcow.in>
2021-08-17 21:58:24 +0530

    mix contacts

--- test/glific/processor/consumer_flow_test.exs
+++ test/glific/processor/consumer_flow_test.exs
@@ -2,6 +2,7 @@ defmodule Glific.Processor.ConsumerFlowTest do
   use Glific.DataCase
 
   alias Glific.{
+    Contacts,
     Contacts.Contact,
     Fixtures,
     Messages.Message,
@@ -117,8 +118,9 @@ defmodule Glific.Processor.ConsumerFlowTest do
   test "check optin/optout sequence" do
     # keep track of current messages
     message_count = Repo.aggregate(Message, :count)
-
-    sender = Repo.get_by(Contact, %{name: "Chrissy Cron"})
+    {:ok, sender} =
+      Repo.get_by(Contact, %{name: "Chrissy Cron"})
+      |> Contacts.update_contact(%{phone: "919917443332"})
     receiver = Repo.get_by(Contact, %{name: "NGO Main Account"})
 
     send_messages(@checks_1, sender, receiver)
