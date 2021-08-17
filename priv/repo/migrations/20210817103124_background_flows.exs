080f009ec3e22087de19f924cc3f6e7c611a9f6b
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-08-17 16:02:51 +0530

    migration for is_background

---
+++ priv/repo/migrations/20210817103124_background_flows.exs
@@ -0,0 +1,15 @@
+defmodule Glific.Repo.Migrations.BackgroundFlows do
+  use Ecto.Migration
+
+  def change do
+    flows()
+  end
+
+  defp flows do
+    alter table(:flows) do
+      add :is_background, :boolean,
+        default: true,
+        comment: "Whether flows are background flows or not"
+    end
+  end
+end
