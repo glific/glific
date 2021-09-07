98a4958ca646c5ecb0ec1142f6e7516d4a8843cb
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-09-07 15:04:44 +0530

    added first draft for function check_if_existing_teacher

--- lib/glific/clients/avanti.ex
+++ lib/glific/clients/avanti.ex
@@ -2,38 +2,44 @@ defmodule Glific.Clients.Avanti do
   @moduledoc """
   Fetches data from Plio Bigquery dataset to send reports to users
   """
-  alias Glific.{
-    Contacts,
-    Flows.ContactField,
-    Groups,
-    Groups.Group,
-    Repo
-  }
+  alias GoogleApi.BigQuery.V2.Api.Jobs
 
   @plio %{
     "dataset" => "917302307943",
     "table" => "flows"
   }
+  @doc """
+  Create a webhook with different signatures, so we can easily implement
+  additional functionality as needed
+  """
+  @spec webhook(String.t(), map()) :: map()
+  def webhook("check_if_existing_teacher", fields) do
+    %{is_new_teacher: false}
+  end
 
   def webhook("fetch_report", fields) do
     Glific.BigQuery.fetch_bigquery_credentials(fields["organization_id"])
     |> case do
       {:ok, %{conn: conn, project_id: project_id, dataset_id: _dataset_id} = _credentials} ->
-        sql = get_sql()
+        sql = get_report_sql()
 
         with {:ok, response} <-
-               GoogleApi.BigQuery.V2.Api.Jobs.bigquery_jobs_query(conn, project_id,
+               Jobs.bigquery_jobs_query(conn, project_id,
                  body: %{query: sql, useLegacySql: false, timeoutMs: 120_000}
                ) do
-          response.rows
-          |> Enum.map(fn row ->
-            row.f
-            |> Enum.with_index()
-            |> Enum.reduce(%{}, fn {cell, i}, acc ->
-              acc
-              |> Map.put_new("#{Enum.at(response.schema.fields, i).name}", cell.v)
+          data =
+            response.rows
+            |> Enum.map(fn row ->
+              row.f
+              |> Enum.with_index()
+              |> Enum.reduce(%{}, fn {cell, i}, acc ->
+                acc
+                |> Map.put_new("#{Enum.at(response.schema.fields, i).name}", cell.v)
+              end)
             end)
-          end)
+            |> List.first()
+
+          %{is_valid: true, data: data}
         else
           _ -> %{is_valid: false, message: "Permission issue while fetching data"}
         end
@@ -43,14 +49,17 @@ defmodule Glific.Clients.Avanti do
     end
   end
 
-  defp get_sql do
+  defp get_report_sql do
     time =
       DateTime.utc_now()
-      |> Timex.shift(days: -6)
+      # |> Timex.shift(days: -6)
       |> Timex.format!("{YYYY}-{0M}-{0D} {h24}:{m}:{s}")
 
     """
-    SELECT name, uuid FROM `#{@plio["dataset"]}.#{@plio["table"]}` where inserted_at > '#{time}' ;
+    SELECT * FROM `#{@plio["dataset"]}.#{@plio["table"]}` where inserted_at > '#{time}' ;
     """
+  end
+
+  defp check_in_bigquery(organization_id) do
   end
 end
