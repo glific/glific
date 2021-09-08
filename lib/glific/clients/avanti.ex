7437d485781b5b3d14a09621de17ba3842b44130
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-09-08 15:54:03 +0530

    added function to check if teacher is existing

--- lib/glific/clients/avanti.ex
+++ lib/glific/clients/avanti.ex
@@ -6,7 +6,8 @@ defmodule Glific.Clients.Avanti do
 
   @plio %{
     "dataset" => "917302307943",
-    "table" => "flows"
+    "analytics_table" => "flows",
+    "teachers_table" => "contacts"
   }
   @doc """
   Create a webhook with different signatures, so we can easily implement
@@ -14,14 +15,27 @@ defmodule Glific.Clients.Avanti do
   """
   @spec webhook(String.t(), map()) :: map()
   def webhook("check_if_existing_teacher", fields) do
-    %{is_new_teacher: false}
+    phone = fields["phone"]
+
+    with %{is_valid: true, data: data} <- fetch_bigquery_data(fields, :teachers) do
+      data
+      |> Enum.reduce(%{found: false}, fn teacher, acc ->
+        if teacher["phone"] == phone, do: acc |> Map.merge(%{found: true}), else: acc
+      end)
+    end
   end
 
   def webhook("fetch_report", fields) do
+    fetch_bigquery_data(fields, :analytics)
+  end
+
+  # returns data queried from bigquery in the form %{data: data, is_valid: true} or returns error as %{is_valid: false, message: error_message}
+  @spec fetch_bigquery_data(map(), atom()) :: map()
+  defp fetch_bigquery_data(fields, query_type) do
     Glific.BigQuery.fetch_bigquery_credentials(fields["organization_id"])
     |> case do
       {:ok, %{conn: conn, project_id: project_id, dataset_id: _dataset_id} = _credentials} ->
-        sql = get_report_sql()
+        sql = get_report_sql(query_type)
 
         with {:ok, response} <-
                Jobs.bigquery_jobs_query(conn, project_id,
@@ -37,7 +51,6 @@ defmodule Glific.Clients.Avanti do
                 |> Map.put_new("#{Enum.at(response.schema.fields, i).name}", cell.v)
               end)
             end)
-            |> List.first()
 
           %{is_valid: true, data: data}
         else
@@ -49,17 +62,22 @@ defmodule Glific.Clients.Avanti do
     end
   end
 
-  defp get_report_sql do
+  # returns query that need to be run in bigquery instance
+  @spec get_report_sql(atom()) :: String.t()
+  defp get_report_sql(:analytics) do
     time =
       DateTime.utc_now()
-      # |> Timex.shift(days: -6)
+      |> Timex.shift(days: -6)
       |> Timex.format!("{YYYY}-{0M}-{0D} {h24}:{m}:{s}")
 
     """
-    SELECT * FROM `#{@plio["dataset"]}.#{@plio["table"]}` where inserted_at > '#{time}' ;
+    SELECT * FROM `#{@plio["dataset"]}.#{@plio["analytics_table"]}` where inserted_at > '#{time}' ;
     """
   end
 
-  defp check_in_bigquery(organization_id) do
+  defp get_report_sql(:teachers) do
+    """
+    SELECT phone FROM `#{@plio["dataset"]}.#{@plio["teachers_table"]}` ;
+    """
   end
 end
