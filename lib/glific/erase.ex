ac8349cc5e6d6817c9a05c3fb8a767455eec9583
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-09-13 17:55:38 +0530

    added query for deleting flow revisions

--- lib/glific/erase.ex
+++ lib/glific/erase.ex
@@ -2,15 +2,14 @@ defmodule Glific.Erase do
   @moduledoc """
   A simple module to periodically delete old data to clean up db
   """
-import Ecto.Query
+  import Ecto.Query
+
   alias Glific.{
     Flows.WebhookLog,
-    Flows.FlowRevision,
     Notifications,
     Repo
   }
-
-
+  @max_age 1
   @doc """
   This is called from the cron job on a regular schedule and cleans database periodically
   """
@@ -20,13 +19,17 @@ import Ecto.Query
     webhook_logs(organization_id)
     flow_revision(organization_id)
   end
+
   @doc """
   Deleting notification older than a month
   """
   @spec clean_notifications(non_neg_integer()) :: String.t()
   def clean_notifications(organization_id) do
-    from n in Notification, where: n.inserted_at < ago(1, "month")
-    |> Repo.delete_all
+    from n in Notification,
+      where:
+        n.inserted_at <
+          ago(@max_age, "month")
+          |> Repo.delete_all()
   end
 
   @doc """
@@ -34,8 +37,11 @@ import Ecto.Query
   """
   @spec webhook_logs(non_neg_integer()) :: String.t()
   def webhook_logs(organization_id) do
-    from w in WebhookLog, where: w.inserted_at < ago(1, "month")
-    |> Repo.delete_all
+    from w in WebhookLog,
+      where:
+        w.inserted_at <
+          ago(@max_age, "month")
+          |> Repo.delete_all()
   end
 
   @doc """
@@ -43,7 +49,25 @@ import Ecto.Query
   """
   @spec flow_revision(non_neg_integer()) :: String.t()
   def flow_revision(organization_id) do
-    from w in FlowRevision, where: w.inserted_at < ago(1, "month")
-    |> Repo.delete_all
+    clean_drafted_flow_revisions()
+    clean_drafted_archived_revisions()
+  end
+
+  defp clean_drafted_flow_revisions do
+    """
+    DELETE FROM flow_revisions fr1
+    WHERE fr1.status = 'draft' AND id
+    NOT IN( SELECT fr2.id FROM flow_revisions fr2 WHERE fr2.flow_id = fr1.flow_id and fr2.status = 'draft' ORDER BY fr2.id DESC LIMIT 10);
+    """
+    |> Repo.query!([], timeout: 60_000, skip_organization_id: true)
+  end
+
+  defp clean_drafted_archived_revisions do
+    """
+    DELETE FROM flow_revisions fr1
+    WHERE fr1.status = 'archived' AND id
+    NOT IN( SELECT fr2.id FROM flow_revisions fr2 WHERE fr2.flow_id = fr1.flow_id  and fr2.status = 'archived' ORDER BY fr2.id DESC LIMIT 10);
+    """
+    |> Repo.query!([], timeout: 60_000, skip_organization_id: true)
   end
 end
