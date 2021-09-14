7dce81e5eac2e1788667dd884fc080c056fa746f
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-09-14 12:29:06 +0530

    added testcase for cleaning flow revisions with status as draft

--- test/glific/erase_test.exs
+++ test/glific/erase_test.exs
@@ -5,15 +5,13 @@ defmodule Glific.EraseTest do
     Erase,
     Fixtures,
     Flows.WebhookLog,
+    Flows.FlowRevision,
     Notifications.Notification,
     Notifications,
     Repo
   }
 
-  test "perform_periodic with clears webhook log older than a month",
-       %{
-         organization_id: _organization_id
-       } = attrs do
+  test "perform_periodic clears webhook log older than a month", attrs do
     Fixtures.webhook_log_fixture(attrs)
     time = DateTime.truncate(DateTime.utc_now(), :second)
 
@@ -28,10 +26,7 @@ defmodule Glific.EraseTest do
     assert WebhookLog.count_webhook_logs(%{filter: attrs}) == logs_count - 1
   end
 
-  test "perform_periodic with clears notifications log older than a month",
-       %{
-         organization_id: _organization_id
-       } = attrs do
+  test "perform_periodic clears notifications log older than a month", attrs do
     Fixtures.notification_fixture(attrs)
     time = DateTime.truncate(DateTime.utc_now(), :second)
 
@@ -44,5 +39,22 @@ defmodule Glific.EraseTest do
     notification_count = Notifications.count_notifications(%{filter: attrs})
     Erase.perform_periodic()
     assert Notifications.count_notifications(%{filter: attrs}) == notification_count - 1
+  end
+
+  test "perform_periodic clears flow revisions with status as draft, and saving only recent 10",
+       attrs do
+    flow = Fixtures.flow_fixture(attrs)
+
+    Enum.each(1..15, fn _x ->
+      FlowRevision.create_flow_revision(%{
+        definition: FlowRevision.default_definition(flow),
+        flow_id: flow.id,
+        organization_id: flow.organization_id
+      })
+    end)
+
+    flow_revision_count = Repo.count_filter(%{}, FlowRevision, &Repo.filter_with/2)
+    Erase.perform_periodic()
+    assert Repo.count_filter(%{}, FlowRevision, &Repo.filter_with/2) == flow_revision_count - 6
   end
 end
