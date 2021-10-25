19da395e6c20b85feacb8d1957b0110c5f19da14
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-10-25 23:09:04 +0530

    Initiate webhook implementation for Balajanaagraha (#1835)
    * initiating custom webhook implementatio for Balajanaagraha
    
    * added webhook implementation for janaagraha
    
    * initiating client extension for Lahi
    
    * marking testcase as pending

---
+++ lib/glific/clients/balajanaagraha.ex
@@ -0,0 +1,29 @@
+defmodule Glific.Clients.Balajanaagraha do
+  @moduledoc """
+  Custom webhook implementation specific to balajanaagraha usecase
+  """
+
+  import Ecto.Query, warn: false
+
+  alias Glific.{
+    Contacts,
+    Flows.ContactField
+  }
+
+  @doc """
+  Create a webhook with different signatures, so we can easily implement
+  additional functionality as needed
+  """
+  @spec webhook(String.t(), map()) :: map()
+  def webhook("save_team_member_details", fields) do
+    {:ok, contact_id} = Glific.parse_maybe_integer(fields["contact"]["id"])
+    contact = Contacts.get_contact!(contact_id)
+    {:ok, team_size} = Glific.parse_maybe_integer(fields["team_size"])
+    contact
+    |> ContactField.do_add_contact_field("team_member_#{team_size}", "team_member_#{team_size}", fields["details"], "string")
+    %{updated_details: "team_member_#{team_size}"}
+  end
+
+  def webhook(_, _fields),
+    do: %{}
+end
