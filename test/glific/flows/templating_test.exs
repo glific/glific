c208e131f35b29a40a360c18ac445862cb32121d
Pankaj Agrawal <pankaj@coloredcow.com>
2021-11-01 14:32:00 +0530

    test cleanups

--- test/glific/flows/templating_test.exs
+++ test/glific/flows/templating_test.exs
@@ -15,6 +15,8 @@ defmodule Glific.Flows.TemplatingTest do
 
   test "process extracts the right values from json for templating action",
        %{organization_id: organization_id} = _attrs do
+    template_node_uuid = "5d47b971-aafd-4fcf-9917-2295f0b0176d"
+
     [template | _] =
       Templates.list_session_templates(%{
         filter: %{
@@ -25,13 +27,14 @@ defmodule Glific.Flows.TemplatingTest do
       })
 
     json = %{
+      "uuid" => template_node_uuid,
       "template" => %{"uuid" => template.uuid, "name" => "test name"},
       "variables" => ["variable_1", "variable_2"]
     }
 
     {templating, uuid_map} = Templating.process(json, %{})
 
-    assert templating.uuid == template.uuid
+    assert templating.uuid == template_node_uuid
     assert templating.name == "test name"
     assert templating.template == template
     assert templating.variables == ["variable_1", "variable_2"]
