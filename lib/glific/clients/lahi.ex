04c51dcb466dc834448c7029c8cf1c97e4971822
Mohit Gusain <mohit.gusain@coloredcow.com>
2021-11-10 17:33:31 +0530

    Update lahi.ex

--- lib/glific/clients/lahi.ex
+++ lib/glific/clients/lahi.ex
@@ -1,28 +1,32 @@
 defmodule Glific.Clients.Lahi do
   @moduledoc """
-    Implementation for the Lahi
+  Custom webhook implementation specific to Lahi usecase
   """
+  alias Glific.{
+    Contacts.Contact,
+    Repo
+  }
+
   import Ecto.Query, warn: false
 
-  alias Glific.{Contacts.Contact, Groups.ContactGroup, Groups.Group, Repo}
-
   @doc """
-    In the case of LAHI we retrieve image and will format the name of the image
+  Tweak GCS Bucket name based Lahi usecase
   """
   @spec gcs_file_name(map()) :: String.t()
   def gcs_file_name(media) do
-    contact = Contact|> where([c], c.id == ^media["contact_id"])|> Repo.one()
+    contact = Contact |> where([c], c.id == ^media["contact_id"]) |> Repo.one()
 
     phone_number = contact.phone
     datetime = Timex.now("Asia/Calcutta")
     strftime_str = Timex.format!(datetime, "%FT%T%:z", :strftime)
-    phone_number <> "/" <> strftime_str <>  Path.extname(media["remote_name"]) <> "1"
+    phone_number <> "/" <> strftime_str <> Path.extname(media["remote_name"]) <> "1"
   end
 
-
-  @doc "webhook call to store the file in the gcs"
+  @doc """
+  Create a webhook with different signatures, so we can easily implement
+  additional functionality as needed
+  """
   @spec webhook(String.t(), map()) :: map()
-  def webhook(_save_file_into_gcs, fields) do
-    %{"status": true}
-  end
+  def webhook(_, _fields),
+    do: %{}
 end
