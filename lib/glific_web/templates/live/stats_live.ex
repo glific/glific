ee4bc3bebdafc433e657f8ca68b8af6e3b1a8bb8
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-11-23 18:08:20 +0530

    added specs

--- lib/glific_web/templates/live/stats_live.ex
+++ lib/glific_web/templates/live/stats_live.ex
@@ -1,14 +1,23 @@
 defmodule GlificWeb.StatsLive do
-  # If you generated an app with mix phx.new --live,
-  # the line below would be: use MyAppWeb, :live_view
+  @moduledoc """
+  StatsLive uses phoenix live view to show current stats
+  """
   use GlificWeb, :live_view
 
+  @doc """
+  Receives the socket.assigns and is responsible for returning rendered content
+  """
+  @spec render(Plug.Conn.t()) :: any()
   def render(assigns) do
     ~H"""
     <h1> Current temperature: <%= @temperature %> </h1>
     """
   end
 
+  @doc """
+  Wires up socket assigns necessary for rendering the view
+  """
+  @spec mount(map(), map(), Plug.Conn.t()) :: {:ok, Plug.Conn.t()}
   def mount(_params, _session, socket) do
     # temperature = Thermostat.get_user_reading(user_id)
     {:ok, assign(socket, :temperature, 10)}
