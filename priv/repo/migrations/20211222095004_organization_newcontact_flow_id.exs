ed26228a58b2c0b03f74d94b1f285c5df5f4fabe
AkhileshNegi <akhilesh.negi@coloredcow.in>
2021-12-22 15:25:25 +0530

    added migration for newcontact_flow_id

---
+++ priv/repo/migrations/20211222095004_organization_newcontact_flow_id.exs
@@ -0,0 +1,14 @@
+defmodule Glific.Repo.Migrations.OrganizationNewcontactFlowId do
+  use Ecto.Migration
+
+  def change do
+    organization_newcontact_flow_id()
+  end
+
+  defp organization_newcontact_flow_id() do
+    alter table(:organizations) do
+      add :newcontact_flow_id, references(:flows, on_delete: :nilify_all),
+        null: true,
+        comment: "Flow which will trigger when newcontact joins the bot"
+    end
+end
