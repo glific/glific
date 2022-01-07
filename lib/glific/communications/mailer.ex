55772d4147b3a60e6070b0cf4ccb5e256f51f3df
Pankaj Agrawal <pankaj@coloredcow.com>
2022-01-07 15:41:57 +0530

    updates

--- lib/glific/communications/mailer.ex
+++ lib/glific/communications/mailer.ex
@@ -1,3 +1,11 @@
 defmodule Glific.Communications.Mailer do
   use Swoosh.Mailer, otp_app: :glific
+
+  @doc """
+  Default sender for all the emails
+  """
+  @spec sender() :: tuple()
+  def sender() do
+    {"Pankaj Agrawal", "glific-tides-support@coloredcow.com"}
+  end
 end
--- lib/glific/mails/user_mail.ex
+++ lib/glific/mails/user_mail.ex
@@ -1,15 +1,12 @@
 defmodule Glific.Mails.UserEmail do
   import Swoosh.Email
-
-  defp sender() do
-    {"Pankaj Agrawal", "glific-tides@coloredcow.com"}
-  end
+  alias Glific.Communications.Mailer
 
   def welcome(user) do
     new()
     |> to({user.name, user.email})
-    |> from(sender())
-    |> subject("Hello, Avengers!")
+    |> from(Mailer.sender())
+    |> subject("Hello, New Organization Added")
     |> html_body("<h1>Hello #{user.name}</h1>")
     |> text_body("Hello #{user.name}\n")
   end
