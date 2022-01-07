08a6f47b0d45abda63a262dc882c3be3528d6624
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-01-07 18:21:53 +0530

    added message and worker to send message

---
+++ lib/glific/providers/gupshup_enterprise/message.ex
@@ -0,0 +1,90 @@
+defmodule Glific.Providers.Gupshup.Enterprise.Message do
+  @moduledoc """
+  Message API layer between application and Gupshup
+  """
+
+  @channel "whatsapp"
+  @behaviour Glific.Providers.MessageBehaviour
+
+  alias Glific.{
+    Communications,
+    Messages.Message,
+    Partners
+  }
+
+  require Logger
+
+  @doc false
+  @impl Glific.Providers.MessageBehaviour
+  @spec send_text(Message.t(), map()) ::
+          {:ok, Oban.Job.t()} | {:error, Ecto.Changeset.t()} | {:error, String.t()}
+  def send_text(message, attrs \\ %{}) do
+    %{type: :text, text: message.body, isHSM: message.is_hsm}
+    |> check_size()
+    |> send_message(message, attrs)
+  end
+
+  @doc false
+  @spec format_sender(Message.t()) :: map()
+  defp format_sender(message) do
+    organization = Partners.organization(message.organization_id)
+
+    %{
+      "source" => message.sender.phone,
+      "src.name" => organization.services["bsp"].secrets["app_name"]
+    }
+  end
+
+  @max_size 4096
+  @doc false
+  @spec check_size(map()) :: map()
+  defp check_size(%{text: text} = attrs) do
+    if String.length(text) < @max_size,
+      do: attrs,
+      else: attrs |> Map.merge(%{error: "Message size greater than #{@max_size} characters"})
+  end
+
+  defp check_size(%{caption: caption} = attrs) do
+    if String.length(caption) < @max_size,
+      do: attrs,
+      else: attrs |> Map.merge(%{error: "Message size greater than #{@max_size} characters"})
+  end
+
+  @doc false
+  @spec send_message(map(), Message.t(), map()) ::
+          {:ok, Oban.Job.t()} | {:error, Ecto.Changeset.t()} | {:error, String.t()}
+  defp send_message(%{error: error} = _payload, _message, _attrs), do: {:error, error}
+
+  defp send_message(payload, message, attrs) do
+    request_body =
+      %{"channel" => @channel}
+      |> Map.merge(format_sender(message))
+      |> Map.put(:destination, message.receiver.phone)
+      |> Map.put("message", Jason.encode!(payload))
+
+    ## gupshup does not allow null in the caption.
+    attrs =
+      if Map.has_key?(attrs, :caption) and is_nil(attrs[:caption]),
+        do: Map.put(attrs, :caption, ""),
+        else: attrs
+
+    create_oban_job(message, request_body, attrs)
+  end
+
+  @doc false
+  @spec to_minimal_map(map()) :: map()
+  defp to_minimal_map(attrs) do
+    Map.take(attrs, [:params, :template_id, :template_uuid, :is_hsm, :template_type])
+  end
+
+  @spec create_oban_job(Message.t(), map(), map()) ::
+          {:ok, Oban.Job.t()} | {:error, Ecto.Changeset.t()}
+  defp create_oban_job(message, request_body, attrs) do
+    attrs = to_minimal_map(attrs)
+    worker_module = Communications.provider_worker(message.organization_id)
+    worker_args = %{message: Message.to_minimal_map(message), payload: request_body, attrs: attrs}
+
+    worker_module.new(worker_args, scheduled_at: message.send_at)
+    |> Oban.insert()
+  end
+end
---
+++ lib/glific/providers/gupshup_enterprise/worker.ex
@@ -0,0 +1,122 @@
+defmodule Glific.Providers.Gupshup.Enterprise.Worker do
+  @moduledoc """
+  A worker to handle send message processes
+  """
+
+  use Oban.Worker,
+    queue: :gupshup,
+    max_attempts: 2,
+    priority: 0
+
+  alias Glific.{
+    Communications,
+    Contacts,
+    Messages.Message,
+    Partners,
+    Partners.Organization,
+    Providers.Gupshup.Enterprise.ApiClient
+  }
+
+  @doc """
+  Standard perform method to use Oban worker
+  """
+  @impl Oban.Worker
+  @spec perform(Oban.Job.t()) :: :ok | {:error, String.t()} | {:snooze, pos_integer()}
+  def perform(%Oban.Job{args: %{"message" => message}} = job) do
+    organization = Partners.organization(message["organization_id"])
+
+    if is_nil(organization.services["bsp"]) do
+      handle_fake_response(
+        message,
+        "{\"message\": \"API Key does not exist\"}",
+        401
+      )
+    else
+      perform(job, organization)
+    end
+  end
+
+  @spec perform(Oban.Job.t(), Organization.t()) ::
+          :ok | {:error, String.t()} | {:snooze, pos_integer()}
+  defp perform(
+         %Oban.Job{args: %{"message" => message, "payload" => payload, "attrs" => attrs}},
+         organization
+       ) do
+    # ensure that we are under the rate limit, all rate limits are in requests/minutes
+    # Refactoring because of credo warning
+    case ExRated.check_rate(
+           organization.shortcode,
+           # the bsp limit is per organization per shortcode
+           1000,
+           organization.services["bsp"].keys["bsp_limit"]
+         ) do
+      {:ok, _} ->
+        if Contacts.is_simulator_contact?(payload["destination"]) do
+          process_simulator(payload["destination"], message)
+        else
+          process_gupshup(organization.id, payload, message, attrs)
+        end
+
+      _ ->
+        # lets sleep real briefly, so that we are not firing off many
+        # jobs to the BSP after exceeding the rate limit for this second
+        # so we are artifically slowing down the send rate
+        Process.sleep(50)
+        # we also want this job scheduled as soon as possible
+        {:snooze, 1}
+    end
+  end
+
+  @spec process_simulator(String.t(), Message.t()) :: :ok | {:error, String.t()}
+  defp process_simulator(_destination, message) do
+    message_id = Faker.String.base64(36)
+
+    handle_fake_response(
+      message,
+      "{\"status\":\"submitted\",\"messageId\":\"simu-#{message_id}\"}",
+      200
+    )
+  end
+
+  @spec handle_fake_response(Message.t(), String.t(), non_neg_integer) ::
+          :ok | {:error, String.t()}
+  defp handle_fake_response(message, body, status) do
+    {:ok,
+     %Tesla.Env{
+       __client__: %Tesla.Client{adapter: nil, fun: nil, post: [], pre: []},
+       __module__: Glific.Providers.Gupshup.ApiClient,
+       body: body,
+       method: :post,
+       status: status
+     }}
+    |> handle_response(message)
+  end
+
+  defp process_gupshup(org_id, payload, message, _attrs) do
+    ApiClient.send_message(
+      org_id,
+      payload
+    )
+    |> handle_response(message)
+  end
+
+  @doc false
+  @spec handle_response({:ok, Tesla.Env.t()}, Message.t()) ::
+          :ok | {:error, String.t()}
+  defp handle_response({:ok, response}, message) do
+    case response do
+      %Tesla.Env{status: status} when status in 200..299 ->
+        Communications.Message.handle_success_response(response, message)
+        :ok
+
+      # Not authorized, Job succeeded, we should return an ok, so we dont retry
+      %Tesla.Env{status: status} when status in 400..499 ->
+        Communications.Message.handle_error_response(response, message)
+        :ok
+
+      # We dont know why this failed, so we should try again
+      _ ->
+        Communications.Message.handle_error_response(response, message)
+    end
+  end
+end
