bb933a7e8bd5d1166590093d4e6758c0f378fec6
Donald A Lobo <409069+dlobo@users.noreply.github.com>
2022-01-24 01:07:08 -0800

    add weekly DB vacuuming tasks (#1995)
    * add weekly DB vacuuming tasks
    
    * add weekly DB vacuuming tasks
    
    Co-authored-by: Pankaj Agrawal <pankaj@coloredcow.com>

--- lib/glific/erase.ex
+++ lib/glific/erase.ex
@@ -7,22 +7,20 @@ defmodule Glific.Erase do
   alias Glific.Repo
 
   @period "month"
+
   @doc """
   This is called from the cron job on a regular schedule and cleans database periodically
   """
-  @spec perform_periodic() :: :ok
+  @spec perform_periodic() :: any
   def perform_periodic do
     clean_notifications()
     clean_webhook_logs()
     clean_flow_revision()
-    :ok
   end
 
-  @doc """
-  Deleting notification older than a month
-  """
+  # Deleting notification older than a month
   @spec clean_notifications() :: {integer(), nil | [term()]}
-  def clean_notifications do
+  defp clean_notifications do
     Repo.delete_all(
       from(n in "notifications",
         where: n.inserted_at < fragment("CURRENT_DATE - ('1' || ?)::interval", ^@period)
@@ -31,11 +29,9 @@ defmodule Glific.Erase do
     )
   end
 
-  @doc """
-  Deleting webhook logs older than a month
-  """
+  # Deleting webhook logs older than a month
   @spec clean_webhook_logs() :: {integer(), nil | [term()]}
-  def clean_webhook_logs do
+  defp clean_webhook_logs do
     Repo.delete_all(
       from(w in "webhook_logs",
         where: w.inserted_at < fragment("CURRENT_DATE - ('1' || ?)::interval", ^@period)
@@ -44,14 +40,11 @@ defmodule Glific.Erase do
     )
   end
 
-  @doc """
-  Deleting flow_revision older than a month
-  """
-  @spec clean_flow_revision() :: :ok
-  def clean_flow_revision do
+  # Deleting flow_revision older than a month
+  @spec clean_flow_revision() :: any
+  defp clean_flow_revision do
     clean_drafted_flow_revisions()
     clean_archived_flow_revisions()
-    :ok
   end
 
   defp clean_drafted_flow_revisions do
@@ -70,5 +63,30 @@ defmodule Glific.Erase do
     NOT IN( SELECT fr2.id FROM flow_revisions fr2 WHERE fr2.flow_id = fr1.flow_id  and fr2.status = 'archived' ORDER BY fr2.id DESC LIMIT 10);
     """
     |> Repo.query!([], timeout: 60_000, skip_organization_id: true)
+  end
+
+  @doc """
+  Do the weekly DB cleaner tasks, typically in the middle of the night on sunday morning
+  """
+  @spec perform_weekly() :: any
+  def perform_weekly do
+    [
+      "vacuum (FULL, ANALYZE) webhook_logs",
+      "vacuum (FULL, ANALYZE) organizations",
+      "vacuum (FULL, ANALYZE) messages_tags",
+      "vacuum (FULL, ANALYZE) notifications",
+      "vacuum (FULL, ANALYZE) flow_counts",
+      "vacuum (FULL, ANALYZE) bigquery_jobs",
+      "vacuum (FULL, ANALYZE) global.oban_producers",
+      "reindex table global.oban_jobs",
+      "vacuum (FULL, ANALYZE) contacts_groups",
+      "vacuum (FULL, ANALYZE) flow_results",
+      "vacuum (FULL, ANALYZE) contacts",
+      "vacuum (ANALYZE) messages"
+    ]
+    |> Enum.each(
+      # need such a large timeout specifically to vacuum the messages
+      &Repo.query!(&1, timeout: 300_000, skip_organization_id: true)
+    )
   end
 end
