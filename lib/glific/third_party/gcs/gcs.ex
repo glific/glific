07ec3e1d6566c416b92d785d49c9cb36733d155c
Pankaj Agrawal <pankaj@coloredcow.com>
2022-02-02 18:40:19 +0530

    private gcs

--- lib/glific/third_party/gcs/gcs.ex
+++ lib/glific/third_party/gcs/gcs.ex
@@ -12,6 +12,8 @@ defmodule Glific.GCS do
     Repo
   }
 
+  alias Waffle.Storage.Google.CloudStorage
+
   @doc """
   Fetch token for GCS
   """
@@ -54,5 +56,65 @@ defmodule Glific.GCS do
     end
 
     :ok
+  end
+
+  @spec upload_file(String.t(), String.t(), non_neg_integer) ::
+          {:ok, GoogleApi.Storage.V1.Model.Object.t()} | {:error, Tesla.Env.t()} | {:error, map()}
+  def upload_file(local, remote, organization_id) do
+    Logger.info("Uploading to GCS, org_id: #{organization_id}, file_name: #{remote}")
+
+    CloudStorage.put(
+      Glific.PrivateMedia,
+      :original,
+      {%Waffle.File{path: local, file_name: remote}, "#{organization_id}"}
+    )
+    |> case do
+      {:ok, response} ->
+        {:ok, response}
+
+      {:error, error} when is_map(error) == true ->
+        {:error, error}
+
+      response ->
+        {:error, %{body: response}}
+    end
+  end
+
+  @spec download_file_to_temp(String.t(), String.t(), non_neg_integer) ::
+          {:ok, String.t()} | {:error, any()}
+  def download_file_to_temp(url, path, org_id) do
+    Logger.info("Downloading file: org_id: #{org_id}, url: #{url}")
+
+    Tesla.get(url)
+    |> case do
+      {:ok, %Tesla.Env{status: status, body: body} = _env} when status in 200..299 ->
+        File.write!(path, body)
+        {:ok, path}
+
+      {:error, :timeout} ->
+        {:error, :timeout}
+
+      {:error, %Tesla.Error{reason: reason}} ->
+        {:error, reason}
+
+      error ->
+        {:error, error}
+    end
+  end
+
+  def get_signed_url(file_name, organization_id, opts \\ []) do
+    Repo.put_organization_id(organization_id)
+    Partners.get_goth_token(organization_id, "google_cloud_storage")
+
+    opts =
+      [signed: true, url_builder: Glific.GCS.SignedUrl, expires_in: 300]
+      |> Keyword.merge(opts)
+
+    CloudStorage.url(
+      Glific.PrivateMedia,
+      :original,
+      {%Waffle.File{file_name: file_name}, "#{organization_id}"},
+      opts
+    )
   end
 end
---
+++ lib/glific/third_party/gcs/signed_url.ex
@@ -0,0 +1,149 @@
+defmodule Glific.GCS.SignedUrl do
+  @moduledoc """
+  This is an implementation of the v2 URL signing for Google Cloud Storage. See
+  [the Google documentation](https://cloud.google.com/storage/docs/access-control/signed-urls-v2)
+  for more details.
+
+  The bulk of the major logic is taken from Martide's `arc_gcs` work:
+  https://github.com/martide/arc_gcs.
+  """
+
+  use Waffle.Storage.Google.Url
+
+  alias Waffle.Types
+  alias Waffle.Storage.Google.{CloudStorage, Util}
+
+  # Default expiration time is 3600 seconds, or 1 hour
+  @default_expiry 3600
+
+  # It's unlikely, but in the event that someone accidentally tries to give a
+  # zero or negative expiration time, this will be used to correct that mistake
+  @min_expiry 1
+
+  # Maximum expiration time is 7 days from the creation of the signed URL
+  @max_expiry 604_800
+
+  # The official Google Cloud Storage host
+  @endpoint "storage.googleapis.com"
+
+  @doc """
+  Returns the amount of time, in seconds, before a signed URL becomes invalid.
+  Assumes the key for the option is `:expires_in`.
+  """
+  @spec expiry(Keyword.t()) :: pos_integer
+  def expiry(opts \\ []) do
+    case Util.option(opts, :expires_in, @default_expiry) do
+      val when val < @min_expiry -> @min_expiry
+      val when val > @max_expiry -> @max_expiry
+      val -> val
+    end
+  end
+
+  @doc """
+  Determines whether or not the URL should be signed. Assumes the key for the
+  option is `:signed`.
+  """
+  @spec signed?(Keyword.t()) :: boolean
+  def signed?(opts \\ []), do: Util.option(opts, :signed, false)
+
+  @doc """
+  Returns the remote asset host. The config key is assumed to be `:asset_host`.
+  """
+  @spec endpoint(Keyword.t()) :: String.t()
+  def endpoint(opts \\ []) do
+    opts
+    |> Util.option(:asset_host, @endpoint)
+    |> Util.var()
+  end
+
+  @impl Waffle.Storage.Google.Url
+  def build(definition, version, meta, options) do
+    path = CloudStorage.path_for(definition, version, meta)
+    get_goth_config(1)
+
+    if signed?(options) do
+      build_signed_url(definition, path, options)
+    else
+      build_url(definition, path)
+    end
+  end
+
+  @spec build_url(Types.definition(), String.t()) :: String.t()
+  defp build_url(definition, path) do
+    %URI{
+      host: endpoint(),
+      path: build_path(definition, path),
+      scheme: "https"
+    }
+    |> URI.to_string()
+  end
+
+  @spec build_signed_url(Types.definition(), String.t(), Keyword.t()) :: String.t()
+  defp build_signed_url(definition, path, options) do
+    {:ok, client_id} = Goth.Config.get(:client_email)
+
+    expiration = System.os_time(:second) + expiry(options)
+
+    signature =
+      definition
+      |> build_path(path)
+      |> canonical_request(expiration)
+      |> sign_request()
+
+    base_url = build_url(definition, path)
+
+    "#{base_url}?GoogleAccessId=#{client_id}&Expires=#{expiration}&Signature=#{signature}"
+  end
+
+  @spec build_path(Types.definition(), String.t()) :: String.t()
+  defp build_path(definition, path) do
+    path =
+      if endpoint() != @endpoint do
+        path
+      else
+        bucket_and_path(definition, path)
+      end
+
+    path
+    |> Util.prepend_slash()
+    |> URI.encode()
+  end
+
+  @spec bucket_and_path(Types.definition(), String.t()) :: String.t()
+  defp bucket_and_path(definition, path) do
+    definition
+    |> CloudStorage.bucket()
+    |> Path.join(path)
+  end
+
+  @spec canonical_request(String.t(), pos_integer) :: String.t()
+  defp canonical_request(resource, expiration) do
+    "GET\n\n\n#{expiration}\n#{resource}"
+  end
+
+  @spec sign_request(String.t()) :: String.t()
+  defp sign_request(request) do
+    {:ok, pem_bin} = Goth.Config.get("private_key")
+    [pem_key_data] = :public_key.pem_decode(pem_bin)
+    otp_release = System.otp_release() |> String.to_integer()
+
+    rsa_key =
+      case otp_release do
+        n when n >= 21 ->
+          :public_key.pem_entry_decode(pem_key_data)
+
+        _ ->
+          pem_key = :public_key.pem_entry_decode(pem_key_data)
+          :public_key.der_decode(:RSAPrivateKey, elem(pem_key, 3))
+      end
+
+    request
+    |> :public_key.sign(:sha256, rsa_key)
+    |> Base.encode64()
+    |> URI.encode_www_form()
+  end
+
+  def get_goth_config(org_id) do
+    Glific.Partners.get_goth_token(org_id, "google_cloud_storage")
+  end
+end
