dae6c116c6ca305969998cb84770a05a4c581786
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-02-16 16:13:07 +0530

    added log for trigger deletion

--- lib/glific_web/resolvers/triggers.ex
+++ lib/glific_web/resolvers/triggers.ex
@@ -6,6 +6,7 @@ defmodule GlificWeb.Resolvers.Triggers do
   import GlificWeb.Gettext
 
   alias Glific.{Repo, Triggers.Trigger}
+  require Logger
 
   @doc false
   @spec trigger(Absinthe.Resolution.t(), %{id: integer}, %{context: map()}) ::
@@ -66,6 +67,10 @@ defmodule GlificWeb.Resolvers.Triggers do
   def delete_trigger(_, %{id: id}, %{context: %{current_user: user}}) do
     with {:ok, trigger} <-
            Repo.fetch_by(Trigger, %{id: id, organization_id: user.organization_id}) do
+      Logger.info(
+        "Trigger for org_id: #{user.organization_id} has been deleted by #{user.name} phone: #{user.phone}"
+      )
+
       Trigger.delete_trigger(trigger)
     end
   end
