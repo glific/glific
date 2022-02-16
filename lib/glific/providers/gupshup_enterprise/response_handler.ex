9c946fb94d0cbbba5debd9cc0b6afa34d03c1564
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-02-16 15:51:33 +0530

    added spec

--- lib/glific/providers/gupshup_enterprise/response_handler.ex
+++ lib/glific/providers/gupshup_enterprise/response_handler.ex
@@ -41,6 +41,7 @@ defmodule Glific.Providers.Gupshup.Enterprise.ResponseHandler do
     end
   end
 
+  @spec add_error_payload(Tesla.Env.t()) :: Tesla.Env.t()
   defp add_error_payload(response) do
     %{"response" => error} = Jason.decode!(response.body)
 
@@ -48,6 +49,7 @@ defmodule Glific.Providers.Gupshup.Enterprise.ResponseHandler do
     |> then(&Map.put(response, :body, &1))
   end
 
+  @spec add_success_payload(Tesla.Env.t()) :: Tesla.Env.t()
   defp add_success_payload(response) do
     %{"response" => success_response} = Jason.decode!(response.body)
 
@@ -55,6 +57,7 @@ defmodule Glific.Providers.Gupshup.Enterprise.ResponseHandler do
     |> then(&Map.put(response, :body, Jason.encode!(&1)))
   end
 
+  @spec add_success_payload(Tesla.Env.t()) :: String.t()
   defp check_message_status(%{body: body} = _response) do
     %{"response" => response} = Jason.decode!(body)
     response["status"]
