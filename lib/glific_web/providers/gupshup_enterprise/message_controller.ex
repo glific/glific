80af215fcb4cddd108600427f7d7cbec598f08f3
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2022-03-24 20:57:11 +0530

    Gupshup-Enterprise/webhook support for buttons (#2132)
    * inspecting results
    
    * inspecting button params
    
    * receive text inspect
    
    * parsing button text
    
    * rephrasing context id
    
    * cleanups

--- lib/glific_web/providers/gupshup_enterprise/message_controller.ex
+++ lib/glific_web/providers/gupshup_enterprise/message_controller.ex
@@ -67,6 +67,26 @@ defmodule GlificWeb.Providers.Gupshup.Enterprise.Controllers.MessageController d
     handler(conn, params, "location handler")
   end
 
+  @doc """
+  Parse button message payload and convert that into Glific message struct
+  """
+  @spec button(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def button(conn, params) do
+    params
+    |> parse_button_text()
+    |> Gupshup.Enterprise.Message.receive_text()
+    |> Map.put(:organization_id, conn.assigns[:organization_id])
+    |> Communications.Message.receive_message()
+
+    handler(conn, params, "text handler")
+  end
+
+  defp parse_button_text(params),
+    do:
+      params["button"]
+      |> Jason.decode!()
+      |> then(&Map.put(params, "text", &1["text"]))
+
   @doc false
   # Handle Gupshup media message and convert them into Glific Message struct
   @spec media(Plug.Conn.t(), map(), atom()) :: Plug.Conn.t()
