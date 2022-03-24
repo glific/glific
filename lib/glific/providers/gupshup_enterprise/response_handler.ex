5b38c5b3eb59b2fb7fab4ee7dd8d72433446aafd
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-03-24 13:46:37 +0530

    cleanups

--- lib/glific/providers/gupshup_enterprise/response_handler.ex
+++ lib/glific/providers/gupshup_enterprise/response_handler.ex
@@ -68,9 +68,6 @@ defmodule Glific.Providers.Gupshup.Enterprise.ResponseHandler do
   @spec add_success_payload(Tesla.Env.t()) :: Tesla.Env.t()
   defp add_success_payload(response) do
     %{"response" => success_response} = Jason.decode!(response.body)
-    IO.inspect("debug002add_success_payload")
-    IO.inspect(success_response)
-    IO.inspect(response)
 
     %{"messageId" => success_response["id"]}
     |> then(&Map.put(response, :body, Jason.encode!(&1)))
