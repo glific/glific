996f06a86a855861a116c9b3c0efe207813b1056
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-03-24 13:42:40 +0530

    rephrasing context id

--- lib/glific/providers/gupshup_enterprise/response_handler.ex
+++ lib/glific/providers/gupshup_enterprise/response_handler.ex
@@ -70,6 +70,7 @@ defmodule Glific.Providers.Gupshup.Enterprise.ResponseHandler do
     %{"response" => success_response} = Jason.decode!(response.body)
     IO.inspect("debug002add_success_payload")
     IO.inspect(success_response)
+    IO.inspect(response)
 
     %{"messageId" => success_response["id"]}
     |> then(&Map.put(response, :body, Jason.encode!(&1)))
