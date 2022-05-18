b5e685c16aa06824e24b15796047b950ddaee261
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-05-18 11:09:50 +0530

    migration cleanups

--- priv/repo/migrations/20220509103325_create_role_permissions.exs
+++ priv/repo/migrations/20220509103325_create_role_permissions.exs
@@ -3,6 +3,38 @@ defmodule Glific.Repo.Migrations.CreateRolePermissions do
   @global_schema Application.fetch_env!(:glific, :global_schema)
 
   def change do
+    create_role()
+    create_permission()
+    create_role_permissions()
+  end
+
+  defp create_role() do
+    create table(:roles) do
+      add :label, :string
+      add :description, :string
+      add :is_reserved, :boolean, default: false, null: false
+
+      # foreign key to organization restricting scope of this table to this organization only
+      add(:organization_id, references(:organizations, on_delete: :delete_all),
+        null: false,
+        comment: "Unique organization ID"
+      )
+
+      timestamps()
+    end
+
+    create unique_index(:roles, [:label, :organization_id])
+  end
+
+  defp create_permission() do
+    create table(:permissions, prefix: @global_schema) do
+      add :entity, :string
+
+      timestamps()
+    end
+  end
+
+  defp create_role_permissions() do
     create table(:role_permissions) do
       add :role_id, references(:roles, on_delete: :delete_all), null: false
 
