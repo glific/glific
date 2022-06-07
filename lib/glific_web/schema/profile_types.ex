a40360fd8280e4293b490262e05a8f07be768b4a
siddhant3030 <ssiddhant3030@gmail.com>
2022-06-07 12:20:16 +0530

    schema added

--- lib/glific_web/schema/profile_types.ex
+++ lib/glific_web/schema/profile_types.ex
@@ -11,6 +11,12 @@ defmodule GlificWeb.Schema.ProfileTypes do
   alias GlificWeb.Resolvers
   alias GlificWeb.Schema.Middleware.Authorize
 
+
+  object :profile_result do
+    field :profile, :profile
+    field :errors, list_of(:input_error)
+  end
+
   object :profile do
     field :id, :id
     field :name, :string
@@ -19,5 +25,23 @@ defmodule GlificWeb.Schema.ProfileTypes do
     field :contact_profile_fields, :map
     field :inserted_at, :datetime
     field :updated_at, :datetime
+  end
+
+  input_object :profile_input do
+    field :name, :string
+    field :profile_type, :string
+    field :profile_registration_fields, :map
+    field :contact_profile_fields, :map
+    field :inserted_at, :datetime
+    field :updated_at, :datetime
+  end
+
+
+  object :profile_mutations do
+    field :create_profile, :profile_result do
+      arg(:input, non_null(:profile_input))
+      middleware(Authorize, :manager)
+      resolve(&Resolvers.Contacts.create_contact/3)
+    end
   end
 end
