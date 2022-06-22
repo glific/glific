bdd63a3c9739b38fbc2896ddc8b7fc0a23986747
Arun Kadekodi <arun@Aruns-MacBook-Pro.local>
2022-06-22 23:15:07 +0530

    fixed webhook

--- lib/glific/clients/suno_sunao.ex
+++ lib/glific/clients/suno_sunao.ex
@@ -3,8 +3,12 @@ defmodule Glific.Clients.SunoSunao do
   This module will focus on suno sunao usecase
   """
 
-  alias Glific.{GoogleASR}
+  alias Glific.{ASR.GoogleASR}
 
+  @doc """
+  This is a webhook which will call into google speech to text api
+  """
+  @spec webhook(String.t(), map()) :: map()
   def webhook("speech_to_text", fields) do
     Glific.parse_maybe_integer!(fields["organization_id"])
     |> GoogleASR.speech_to_text(fields["results"])
