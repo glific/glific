90c6eb31fa5d35e5d14244151122529e3464ee7f
Arun Kadekodi <arun@Aruns-MacBook-Pro.local>
2022-06-22 10:18:24 -0700

    more filter added

--- lib/glific/third_party/asr/google_asr.ex
+++ lib/glific/third_party/asr/google_asr.ex
@@ -11,6 +11,24 @@ defmodule Glific.ASR.GoogleASR do
 
   @doc """
   This function will take organization_id and the url for audio.
+     * encoding: We have a list of endoing which is supported with these google api for more information check here
+       https://cloud.google.com/speech-to-text/docs/encoding
+
+     * Sample rate in Hertz of the audio data sent in all RecognitionAudio messages. Valid values are: 8000-48000. 16000 is optimal.
+       For best results, set the sampling rate of the audio source to 16000 Hz. If that's not possible, use the native sample rate
+       of the audio source (instead of re-sampling). This field is optional for FLAC and WAV audio files, but is required for all
+       other audio formats
+
+     * languageCode: This field support multiple languages to more info check this links
+          1. https://www.rfc-editor.org/rfc/bcp/bcp47.txt
+          2. https://cloud.google.com/speech-to-text/docs/languages
+
+     *  profanityFilter: If enabled, Speech-to-Text will attempt to detect profane words and return only the first letter followed by asterisks in the
+        transcript (for example, f***).
+
+     *  enableWordConfidence: You can specify that Speech-to-Text indicate a value of accuracy, or confidence level, for individual words in a transcription.
+
+     *  enableAutomaticPunctuation:  When you enable this feature, Speech-to-Text automatically infers the presence of periods, commas, and question marks in your audio data and adds them to the transcript.
   """
 
   @spec speech_to_text(non_neg_integer, String.t()) :: any
@@ -25,7 +43,9 @@ defmodule Glific.ASR.GoogleASR do
         "encoding" => "OGG_OPUS",
         "sampleRateHertz" => 16_000,
         "languageCode" => "hi-IN",
-        "profanityFilter" => true
+        "profanityFilter" => true,
+        "enableWordConfidence" => true,
+        "enableAutomaticPunctuation" => true
       },
       "audio" => %{
         "content" => content
