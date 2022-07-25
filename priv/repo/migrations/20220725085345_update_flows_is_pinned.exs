ac243838cda18a6257d1551019367542b2af3d8c
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-07-25 15:00:34 +0530

    updating the commetn

--- priv/repo/migrations/20220725085345_update_flows_is_pinned.exs
+++ priv/repo/migrations/20220725085345_update_flows_is_pinned.exs
@@ -3,7 +3,9 @@ defmodule Glific.Repo.Migrations.UpdateFlowsIsPinned do
 
   def change do
     alter table(:flows) do
-      add :is_pinned, :boolean, comment: "This is optional and depends on NGO usecase"
+      add :is_pinned, :boolean,
+        default: false,
+        comment: "This is for showing the pinned flows at the top of flow screen"
     end
 
     create index(:flows, [:is_pinned, :organization_id])
