ad16c3498f9874af8cef60fcab755813e4f950c4
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-08-18 13:16:03 +0530

    adding fetch functionaliyt

--- lib/glific/clients/reap_benefit.ex
+++ lib/glific/clients/reap_benefit.ex
@@ -42,8 +42,7 @@ defmodule Glific.Clients.ReapBenefit do
     token = fields["token"]
     header = get_header(token)
 
-    url =
-      @frappe_open_civic_api_url <> "User/" <> fields["contact"]["phone"] <> "@solveninja.org"
+    url = @frappe_open_civic_api_url <> "User/" <> fields["contact"]["phone"] <> "@solveninja.org"
 
     Tesla.get(url, headers: header)
     |> case do
@@ -67,7 +66,7 @@ defmodule Glific.Clients.ReapBenefit do
       %{
         "email" => fields["contact"]["phone"] <> "@solveninja.org ",
         "first_name" => fields["contact"]["name"],
-        "mobile_no" => fields["contact"]["phone"],
+        "mobile_no" => fields["contact"]["phone"]
       }
       |> Jason.encode!()
 
@@ -102,6 +101,27 @@ defmodule Glific.Clients.ReapBenefit do
     end
   end
 
+  def webhook("fetch_from_frappe", fields) do
+    with %{is_valid: true, attrs: attrs} <- validate_fetch_attrs(fields) do
+      {doctype, doctype_id, token} = attrs
+      header = get_header(token)
+      url = @frappe_open_civic_api_url <> "#{doctype}/" <> doctype_id
+
+      Tesla.get(url, headers: header)
+      |> case do
+        {:ok, %Tesla.Env{status: 200, body: _body}} ->
+          %{is_found: true, response: "Logged In"}
+
+        {:ok, %Tesla.Env{status: 404, body: body}} ->
+          error_msg = Jason.decode!(body)
+          %{is_found: false, response: error_msg["exc_type"]}
+
+        {_status, _response} ->
+          %{is_found: false, response: "Invalid response"}
+      end
+    end
+  end
+
   def webhook(_, _fields), do: %{}
 
   @spec get_header(String.t()) :: list()
@@ -111,4 +131,17 @@ defmodule Glific.Clients.ReapBenefit do
       {"Content-Type", "application/json"}
     ]
   end
+
+  defp validate_fetch_attrs(
+         %{
+           "doctype" => doctype,
+           "doctype_id" => doctype_id,
+           "token" => token
+         } = _fields
+       ) do
+    %{is_valid: true, attrs: {doctype, doctype_id, token}}
+  end
+
+  defp validate_fetch_attrs(_fields),
+    do: %{is_valid: false, message: "Add Doctype, Doctype_id and Token in body to proceed"}
 end
