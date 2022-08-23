23e712c7a4cf95a948184458c2c906d78a3f3d3b
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-08-23 15:21:14 +0530

    adding message conversation

---
+++ lib/glific/messages/message_conversation.ex
@@ -0,0 +1,61 @@
+defmodule Glific.Messages.MessageConversation do
+  @moduledoc """
+  Message conversation are mapped with a message
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias __MODULE__
+
+  alias Glific.{
+    Contacts.Contact,
+    Messages.Message,
+    Partners.Organization
+  }
+
+  @required_fields [
+    :deduction_type,
+    :contact_id,
+    :message_id,
+    :organization_id
+  ]
+
+  @optional_fields[]
+
+  @type t() :: %__MODULE__{
+    __meta__: Ecto.Schema.Metadata.t(),
+    id: non_neg_integer | nil,
+    conversation_id: String.t() | nil,
+    deduction_type: String.t() | nil,
+    is_billable: boolean() | false,
+    payload: map() | nil,
+    organization_id: non_neg_integer | nil,
+    organization: Organization.t() | Ecto.Association.NotLoaded.t() | nil
+    contact: Contact.t() | Ecto.Association.NotLoaded.t() | nil,
+    message: Message.t() | Ecto.Association.NotLoaded.t() | nil,
+  }
+
+  schema "messages_conversations" do
+    field :conversation_id, :string
+    field :deduction_type, :string
+    field :is_billable, :boolean
+    field :payload, :map
+
+    belongs_to :contact, Contact
+    belongs_to :message, Message
+    belongs_to :organization, Organization
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc """
+  Standard changeset pattern we use for all data types
+  """
+  @spec changeset(MessageConversation.t(), map()) :: Ecto.Changeset.t()
+  def changeset(message_conversation, attrs) do
+    message_conversation
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+  end
+end
