7c7db4f3b879c132fe758ef213a6e4ca42f0b251
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-08-26 13:08:16 +0530

    added flow id to messages table as well

--- priv/repo/migrations/20220826061242_add_profile_id_to_flow_tables.exs
+++ priv/repo/migrations/20220826061242_add_profile_id_to_flow_tables.exs
@@ -5,6 +5,7 @@ defmodule Glific.Repo.Migrations.AddProfileIdToFlowTables do
     add_profile_to_flow_results()
     add_profile_to_flow_contexts()
     add_profile_to_contact_histories()
+    add_profile_to_messages()
   end
 
   defp add_profile_to_flow_results() do
@@ -21,6 +22,12 @@ defmodule Glific.Repo.Migrations.AddProfileIdToFlowTables do
 
   defp add_profile_to_contact_histories() do
     alter table(:contact_histories) do
+      add :profile_id, references(:profiles, on_delete: :delete_all), null: true
+    end
+  end
+
+  defp add_profile_to_messages()() do
+    alter table(:messages) do
       add :profile_id, references(:profiles, on_delete: :delete_all), null: true
     end
   end
