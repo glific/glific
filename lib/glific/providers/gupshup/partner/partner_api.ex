58905182085112812c1debf3a3920c84f25f4a28
Pankaj Agrawal <pankaj@coloredcow.com>
2022-08-26 23:49:41 +0530

    Quick fixes

--- lib/glific/providers/gupshup/partner/partner_api.ex
+++ lib/glific/providers/gupshup/partner/partner_api.ex
@@ -9,10 +9,8 @@ defmodule Glific.Providers.Gupshup.PartnerAPI do
     Partners.Saas
   }
 
-  alias Plug.Conn.Query
-
   use Tesla
-  plug(Tesla.Middleware.Logger)
+  alias Plug.Conn.Query
 
   plug(Tesla.Middleware.FormUrlencoded,
     encode: &Query.encode/1
@@ -21,6 +19,54 @@ defmodule Glific.Providers.Gupshup.PartnerAPI do
   @partner_url "https://partner.gupshup.io/partner/account"
   @app_url "https://partner.gupshup.io/partner/app/"
   @global_organization_id 0
+
+  @doc """
+    Fetch App details based on API key and App name
+  """
+  @spec fetch_app_details(non_neg_integer()) :: any()
+  def fetch_app_details(org_id) do
+    organization = Partners.organization(org_id)
+    gupshup_secrets = organization.services["bsp"].secrets
+
+    post_request(
+      @partner_url <> "/api/appLink",
+      %{
+        apiKey: gupshup_secrets["api_key"],
+        appName: gupshup_secrets["app_name"]
+      },
+      token_type: :partner_token
+    )
+    |> case do
+      {:ok, res} ->
+        res["partnerApps"]
+
+      error ->
+        error
+    end
+  end
+
+  @doc """
+  Fetches Partner token and App Access token to get tier information
+  for an organization with input app id
+  """
+  @spec get_quality_rating(non_neg_integer(), String.t()) :: {:error, any} | {:ok, map()}
+  def get_quality_rating(organization_id, _phone) do
+    (@app_url <> app_id!(organization_id) <> "/ratings")
+    |> get_request(token_type: :app_token, org_id: organization_id)
+    |> case do
+      {:ok, res} ->
+        {:ok,
+         %{
+           current_limit: res["currentLimit"],
+           event: res["event"],
+           event_time: res["eventTime"],
+           previous_limit: res["oldLimit"]
+         }}
+
+      error ->
+        {:error, "Error while getting the ratings. #{inspect(error)}"}
+    end
+  end
 
   # Get Partner token
   @spec get_partner_token :: {:ok, map()} | {:error, any}
@@ -56,18 +102,69 @@ defmodule Glific.Providers.Gupshup.PartnerAPI do
     end
   end
 
-  defp make_request(:post, url, data) do
-    with {:ok, %{partner_token: partner_token}} <- get_partner_token() do
-      default_headers = [{"token", partner_token}]
+  @spec get_partner_app_token(non_neg_integer) ::
+          {:error, String.t()} | {:ok, %{partner_app_token: any}}
+  defp get_partner_app_token(org_id) do
+    {:ok, partner_app_token} = Caches.get(org_id, "partner_app_token", refresh_cache: false)
 
-      post(url, data, headers: default_headers)
-      |> case do
-        {:ok, %Tesla.Env{status: status, body: body}} when status in 200..299 ->
-          {:ok, Jason.decode!(body)}
+    if partner_app_token,
+      do: {:ok, %{partner_app_token: partner_app_token}},
+      else: fetch_partner_app_token(org_id)
+  end
 
-        err ->
-          {:error, "#{inspect(err)}"}
-      end
+  @spec fetch_partner_app_token(non_neg_integer) ::
+          {:error, String.t()} | {:ok, %{partner_app_token: any}}
+  defp fetch_partner_app_token(org_id) do
+    url = @app_url <> app_id!(org_id) <> "/token"
+
+    get_request(url, token_type: :partner_token)
+    |> case do
+      {:ok, res} ->
+        app_token = get_in(res, ["token", "token"])
+        Caches.set(org_id, "partner_app_token", app_token, ttl: :timer.hours(22))
+        {:ok, %{partner_app_token: app_token}}
+
+      {:error, error} ->
+        {:error, "Could not fetch the partner app token #{inspect(error)}"}
+    end
+  end
+
+  @spec headers(atom(), Keyword.t()) :: list()
+  defp headers(:app_token, opts) do
+    org_id = Keyword.get(opts, :org_id)
+    {:ok, %{partner_app_token: partner_app_token}} = get_partner_app_token(org_id)
+
+    [{"token", partner_app_token}, {"Authorization", partner_app_token}]
+  end
+
+  defp headers(:partner_token, _opts) do
+    {:ok, %{partner_token: partner_token}} = get_partner_token()
+    [{"token", partner_token}, {"Authorization", partner_token}]
+  end
+
+  defp post_request(url, data, opts) do
+    req_headers = headers(Keyword.get(opts, :token_type), opts)
+
+    post(url, data, headers: req_headers)
+    |> case do
+      {:ok, %Tesla.Env{status: status, body: body}} when status in 200..299 ->
+        {:ok, Jason.decode!(body)}
+
+      err ->
+        {:error, "#{inspect(err)}"}
+    end
+  end
+
+  defp get_request(url, opts) do
+    req_headers = headers(Keyword.get(opts, :token_type), opts)
+
+    get(url, headers: req_headers)
+    |> case do
+      {:ok, %Tesla.Env{status: status, body: body}} when status in 200..299 ->
+        {:ok, Jason.decode!(body)}
+
+      err ->
+        {:error, "#{inspect(err)}"}
     end
   end
 
@@ -76,10 +173,10 @@ defmodule Glific.Providers.Gupshup.PartnerAPI do
     organization = Partners.organization(org_id)
     gupshup_secrets = organization.services["bsp"].secrets
 
-    if gupshup_secrets["app_id"] not in [nil, ""] do
-      {:ok, gupshup_secrets["app_id"]}
-    else
+    if gupshup_secrets["app_id"] in [nil, ""] do
       {:error, "App Id not found."}
+    else
+      {:ok, gupshup_secrets["app_id"]}
     end
   end
 
@@ -87,128 +184,5 @@ defmodule Glific.Providers.Gupshup.PartnerAPI do
   defp app_id!(org_id) do
     {:ok, app_id} = app_id(org_id)
     app_id
-  end
-
-  @doc """
-    Fetch App details based on API key and App name
-  """
-  @spec fetch_app_details(non_neg_integer()) :: any()
-  def fetch_app_details(org_id) do
-    organization = Partners.organization(org_id)
-    gupshup_secrets = organization.services["bsp"].secrets
-
-    make_request(:post, @partner_url <> "/api/appLink", %{
-      apiKey: gupshup_secrets["api_key"],
-      appName: gupshup_secrets["app_name"]
-    })
-    |> case do
-      {:ok, res} ->
-        res["partnerApps"]
-
-      error ->
-        error
-    end
-  end
-
-  @doc """
-  Get the handle id for reusable media
-  """
-  @spec get_media_id(non_neg_integer(), String.t(), String.t()) :: tuple()
-  def get_media_id(org_id, type, file_url) do
-    app_id = app_id!(org_id)
-    url = @app_url <> "#{app_id}/upload/media"
-    make_request(:post, url, %{file: file_url, file_type: type})
-  end
-
-  # fetches app id from phone using partner API
-  @spec get_apps_details(non_neg_integer(), String.t()) :: {:ok, String.t()} | {:error, any}
-  defp get_apps_details(organization_id, phone) do
-    # Need to use Caches.fetch and do_get_apps_details should be used as fallback fn but somehow test cases are failing when used
-    {:ok, app_id} = Caches.get(organization_id, {:app_id, phone})
-
-    if app_id == false,
-      do: do_get_apps_details(organization_id, phone),
-      else: {:ok, app_id}
-  end
-
-  @spec do_get_apps_details(non_neg_integer(), String.t()) :: {:ok, String.t()} | {:error, any}
-  defp do_get_apps_details(organization_id, phone) do
-    url = @partner_url <> "/api/partnerApps"
-
-    with {:ok, %{partner_token: partner_token}} <- get_partner_token(),
-         {:ok, %Tesla.Env{status: 200, body: body}} = _response <-
-           get(url, headers: [{"token", partner_token}]) do
-      app_list =
-        body
-        |> Jason.decode!()
-        |> Map.get("partnerAppsList")
-
-      app = Enum.find(app_list, fn app -> app["phone"] == phone end)
-
-      if app,
-        do: Caches.set(organization_id, {:app_id, phone}, app["id"]),
-        else: {:error, "App not found"}
-    end
-  end
-
-  # fetches partner token first to get app access token
-  @spec get_app_token(non_neg_integer(), String.t()) :: {:ok, map()} | {:error, any}
-  defp get_app_token(organization_id, app_id) do
-    with {:ok, %{partner_token: partner_token}} <- get_partner_token() do
-      # Need to use Caches.fetch and do_get_apps_details should be used as fallback fn but somehow testcases are failing when used
-      {:ok, app_token} = Caches.get(organization_id, {:app_token, app_id})
-
-      if app_token == false,
-        do: do_get_app_token(organization_id, partner_token, app_id),
-        else: {:ok, %{app_token: app_token}}
-    end
-  end
-
-  @spec do_get_app_token(non_neg_integer(), String.t(), String.t()) ::
-          {:ok, map()} | {:error, any}
-  defp do_get_app_token(organization_id, partner_token, app_id) do
-    url = @app_url <> app_id <> "/token"
-
-    get(url, headers: [{"token", partner_token}])
-    |> case do
-      {:ok, %Tesla.Env{status: 200, body: body}} ->
-        {:ok, app_token} =
-          Jason.decode!(body)
-          |> then(&Caches.set(organization_id, {:app_token, app_id}, &1["token"]["token"]))
-
-        {:ok, %{app_token: app_token}}
-
-      {_status, _response} ->
-        {:error, "invalid credentials or app id}"}
-    end
-  end
-
-  @doc """
-  Fetches Partner token and App Access token to get tier information for an organization with input app id
-  """
-  @spec get_quality_rating(non_neg_integer(), String.t()) :: {:error, any} | {:ok, map()}
-  def get_quality_rating(organization_id, phone) do
-    with {:ok, app_id} <- get_apps_details(organization_id, phone),
-         {:ok, %{app_token: app_token}} <- get_app_token(organization_id, app_id) do
-      url = @app_url <> app_id <> "/ratings"
-      param = %{"phone" => phone, "isBlocked" => "true"}
-
-      get(url, body: param, headers: [{"Authorization", app_token}])
-      |> case do
-        {:ok, %Tesla.Env{status: 200, body: body}} ->
-          response = Jason.decode!(body)
-
-          data = %{
-            current_limit: response["currentLimit"],
-            previous_limit: response["oldLimit"],
-            event: response["event"]
-          }
-
-          {:ok, data}
-
-        {_status, _response} ->
-          {:error, "Invalid App Id or Phone}"}
-      end
-    end
   end
 end
