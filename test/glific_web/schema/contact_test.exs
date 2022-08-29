db8c13161cead7a4cabe101d4e0c59eb8bd3836f
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-08-29 14:54:12 +0530

    added testcases

--- test/glific_web/schema/contact_test.exs
+++ test/glific_web/schema/contact_test.exs
@@ -684,7 +684,8 @@ defmodule GlificWeb.Schema.ContactTest do
   end
 
   test "contacts history returns list of contacts history in asc order", %{staff: user} = attrs do
-    [contact | _tail] = Contacts.list_contacts(attrs)
+    profile = Fixtures.profile_fixture()
+    contact = Contacts.get_contact!(profile.contact_id)
     [flow | _tail] = Flows.list_flows(attrs)
     {:ok, _flow} = Flows.start_contact_flow(flow, contact)
 
@@ -693,6 +694,28 @@ defmodule GlificWeb.Schema.ContactTest do
         variables: %{
           "opts" => %{"order" => "ASC"},
           "filter" => %{"contact_id" => contact.id}
+        }
+      )
+
+    assert {:ok, query_data} = result
+    contact_history = get_in(query_data, [:data, "contactHistory"])
+    assert length(contact_history) > 0
+
+    result =
+      auth_query_gql_by(:contact_history_list, user,
+        variables: %{
+          "filter" => %{"profile_id" => profile.id}
+        }
+      )
+
+    assert {:ok, query_data} = result
+    contact_history = get_in(query_data, [:data, "contactHistory"])
+    assert length(contact_history) > 0
+
+    result =
+      auth_query_gql_by(:contact_history_list, user,
+        variables: %{
+          "filter" => %{"event_label" => "Flow Started"}
         }
       )
 
@@ -726,8 +749,6 @@ defmodule GlificWeb.Schema.ContactTest do
       )
 
     assert count_from_db == count
-
-    # contact_history = get_in(query_data, [:data, "contactHistory"])
-    # assert length(contact_history) > 0
+    assert get_in(query_data, [:data, "countContactHistory"]) > 0
   end
 end
