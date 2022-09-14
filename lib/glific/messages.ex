9bd1d368fcae97fe4e0d0d841fdc8166a28202a5
Pankaj Agrawal <pankaj@coloredcow.com>
2022-09-14 21:05:54 +0530

    fixes

--- lib/glific/messages.ex
+++ lib/glific/messages.ex
@@ -511,12 +511,6 @@ defmodule Glific.Messages do
       |> Templates.parse_buttons(is_translated, session_template.has_buttons)
       |> parse_template_vars(parameters)
 
-    IO.inspect("updated_template")
-    IO.inspect(updated_template)
-
-    IO.inspect("params")
-    IO.inspect(parameters)
-
     %{
       body: updated_template.body,
       type: updated_template.type,
