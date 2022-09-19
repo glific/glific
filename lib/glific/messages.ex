242bc9bd3e66a74fa0661cc73166d776ed50e6b0
Pankaj Agrawal <pankaj@coloredcow.com>
2022-09-19 14:28:50 +0530

    updates

--- lib/glific/messages.ex
+++ lib/glific/messages.ex
@@ -684,10 +684,10 @@ defmodule Glific.Messages do
 
     {:ok, group_message} =
       if message_params[:is_hsm] in [nil, false],
-        do: create_group_message(message_params |> Map.put(:type, :text)),
+        do: create_group_message(message_params |> Map.put_new(:type, :text)),
         else:
           message_params
-          |> Map.put(:type, :text)
+          |> Map.put_new(:type, :text)
           |> Map.put(
             :body,
             "Sending HSM template #{message_params.template_id}, params: #{message_params.parameters}"
