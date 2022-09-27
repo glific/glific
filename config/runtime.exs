86a7a85cc031033a1f09b19e670591583befb93b
Pankaj Agrawal <pankaj@coloredcow.com>
2022-09-27 11:53:35 +0530

    updates

--- config/runtime.exs
+++ config/runtime.exs
@@ -44,7 +44,8 @@ config :appsignal, :config,
   active: env!("APPSIGNAL_ACTIVE", :boolean, false),
   revision: Application.spec(:glific, :vsn) |> to_string(),
   push_api_key: env!("APPSIGNAL_PUSH_API_KEY", :string!),
-  ecto_repos: []
+  ecto_repos: [],
+  ignore_namespaces: ["gupshup_webhooks", "gupshup_enterprise_webhooks"]
 
 config :glific, Glific.Vault,
   ciphers: [
