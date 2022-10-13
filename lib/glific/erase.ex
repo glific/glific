c3cf681193f007399acc7334657d4f8c41f154e1
Pankaj Agrawal <pankaj@coloredcow.com>
2022-10-13 20:06:11 +0530

    Disable unwanted updates table (#2521)
    * fixes
    
    * push
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * fixes
    
    * fixes
    
    * updates
    
    * updates
    
    * updates

--- lib/glific/erase.ex
+++ lib/glific/erase.ex
@@ -6,6 +6,10 @@ defmodule Glific.Erase do
 
   alias Glific.Repo
 
+  alias Glific.Seeds.SeedsMigration
+
+  require Logger
+
   @period "month"
 
   @doc """
@@ -102,5 +106,31 @@ defmodule Glific.Erase do
       # need such a large timeout specifically to vacuum the messages
       &Repo.query!(&1, [], timeout: 300_000, skip_organization_id: true)
     )
+  end
+
+  @doc """
+  Keep latest 200 messages for a contact
+  """
+  @spec clean_old_messages(non_neg_integer(), boolean()) :: list
+  def clean_old_messages(org_id, skip_delete \\ false) do
+    limit = 200
+
+    contact_query =
+      "select id, last_message_number from contacts where organization_id = #{org_id} and last_message_number > #{limit}"
+
+    Repo.query!(contact_query).rows
+    |> Enum.map(fn [contact_id, last_message_number] ->
+      Logger.info("message cleanup started for #{contact_id}")
+      SeedsMigration.fix_message_number_for_contact(contact_id)
+      message_to_delete = last_message_number - limit
+
+      delete_message_query =
+        "delete from messages where organization_id = #{org_id} and contact_id = #{contact_id} and message_number < #{message_to_delete}"
+
+      if skip_delete != false do
+        Repo.query!(delete_message_query, [], timeout: 300_000, skip_organization_id: true)
+        SeedsMigration.fix_message_number_for_contact(contact_id)
+      end
+    end)
   end
 end
