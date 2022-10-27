60a8e26ac9f40f2996ca028c9a91c91e0745a1ff
Pankaj Agrawal <pankaj@coloredcow.com>
2022-10-27 22:02:52 +0530

    updates

--- lib/glific_web/live/glific_dashboard_live.ex
+++ lib/glific_web/live/glific_dashboard_live.ex
@@ -2,7 +2,16 @@ defmodule GlificWeb.GlificDashboardLive do
   use GlificWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, contact_count: 0, page_title: "Glific Dashboard")
+    socket =
+      assign(socket,
+        page_title: "Glific Dashboard",
+        conversation_count: 40,
+        active_flow_count: 50,
+        contact_count: 50,
+        opted_in_contacts_count: 50,
+        opted_out_contacts_count: 50
+      )
+
     {:ok, socket}
   end
 end
