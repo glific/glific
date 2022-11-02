4f083f0cfebb0ca30d985ba5f04392c743996b6c
Pankaj Agrawal <pankaj@coloredcow.com>
2022-11-03 00:27:53 +0530

    fixes

--- lib/glific/reports.ex
+++ lib/glific/reports.ex
@@ -4,101 +4,19 @@ defmodule Glific.Reports do
   """
 
   import Ecto.Query, warn: false
-  alias Glific.Repo
+  # alias Glific.Repo
 
-  alias Glific.Reports.Report
-
-  @doc """
-  Returns the list of reports.
-
-  ## Examples
-
-      iex> list_reports()
-      [%Report{}, ...]
-
-  """
-  def list_reports do
-    Repo.all(Report, skip_organization_id: true)
+  def get_kpi(_kpi) do
+    Enum.random(1..1000)
   end
 
-  @doc """
-  Gets a single report.
-
-  Raises `Ecto.NoResultsError` if the Report does not exist.
-
-  ## Examples
-
-      iex> get_report!(123)
-      %Report{}
-
-      iex> get_report!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_report!(id), do: Repo.get!(Report, id, skip_organization_id: true)
-
-  @doc """
-  Creates a report.
-
-  ## Examples
-
-      iex> create_report(%{field: value})
-      {:ok, %Report{}}
-
-      iex> create_report(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_report(attrs \\ %{}) do
-    %Report{}
-    |> Report.changeset(attrs)
-    |> Repo.insert(skip_organization_id: true)
-  end
-
-  @doc """
-  Updates a report.
-
-  ## Examples
-
-      iex> update_report(report, %{field: new_value})
-      {:ok, %Report{}}
-
-      iex> update_report(report, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_report(%Report{} = report, attrs) do
-    report
-    |> Report.changeset(attrs)
-    |> Repo.update(skip_organization_id: true)
-  end
-
-  @doc """
-  Deletes a report.
-
-  ## Examples
-
-      iex> delete_report(report)
-      {:ok, %Report{}}
-
-      iex> delete_report(report)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_report(%Report{} = report) do
-    Repo.delete(report, skip_organization_id: true)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking report changes.
-
-  ## Examples
-
-      iex> change_report(report)
-      %Ecto.Changeset{data: %Report{}}
-
-  """
-  def change_report(%Report{} = report, attrs \\ %{}) do
-    Report.changeset(report, attrs)
+  def kpi_list() do
+    [
+      :conversation_count,
+      :active_flow_count,
+      :contact_count,
+      :opted_in_contacts_count,
+      :opted_out_contacts_count
+    ]
   end
 end
--- lib/glific_web/live/glific_dashboard_live.ex
+++ lib/glific_web/live/glific_dashboard_live.ex
@@ -1,6 +1,8 @@
 defmodule GlificWeb.GlificDashboardLive do
   use GlificWeb, :live_view
 
+  alias Glific.Reports
+
   def mount(_params, _session, socket) do
     if(connected?(socket)) do
       :timer.send_interval(1000, self(), :refresh)
@@ -11,39 +13,20 @@ defmodule GlificWeb.GlificDashboardLive do
   end
 
   def handle_info(:refresh, socket) do
-    socket = assign_stats(socket, :call)
-    # socket = assign(socket, :contact_count, 2000)
-    {:noreply, socket}
+    {:noreply, assign_stats(socket, :call)}
   end
 
   def handle_info({:get_stats, kpi}, socket) do
-    count = Enum.random(1..1000)
-    socket = assign(socket, kpi, count)
-    {:noreply, socket}
+    {:noreply, assign(socket, kpi, Reports.get_kpi(kpi))}
   end
 
   defp assign_stats(socket, :init) do
-    assign(socket,
-      page_title: "Glific Dashboard",
-      conversation_count: 10,
-      active_flow_count: 20,
-      contact_count: 0,
-      opted_in_contacts_count: 0,
-      opted_out_contacts_count: 0
-    )
+    stats = Enum.map(Reports.kpi_list(), &{&1, "loading.."})
+    assign(socket, Keyword.merge(stats, page_title: "Glific Dashboard"))
   end
 
   defp assign_stats(socket, :call) do
-    stats = [
-      :conversation_count,
-      :active_flow_count,
-      :contact_count,
-      :opted_in_contacts_count,
-      :opted_out_contacts_count
-    ]
-
-    Enum.map(stats, &send(self(), {:get_stats, &1}))
-
+    Enum.map(Reports.kpi_list(), &send(self(), {:get_stats, &1}))
     socket
   end
 end
