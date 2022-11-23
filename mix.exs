81fe6f9ff938878cc4d1cc84062b030879d5b058
Pankaj Agrawal <pankaj@coloredcow.com>
2022-11-23 20:06:13 +0530

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Glific.MixProject do
   def project do
     [
       app: :glific,
-      version: "4.9.3",
+      version: "4.9.4",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       dialyzer: [
