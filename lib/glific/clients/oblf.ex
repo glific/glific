5f8a5137a7017a5e2df5016e4c005f2d4dbc566f
Mohit Gusain <mohitgusain@Mohits-MacBook-Pro.local>
2022-11-24 13:44:02 +0530

    dynamic interactive messages

--- lib/glific/clients/oblf.ex
+++ lib/glific/clients/oblf.ex
@@ -8,6 +8,28 @@ defmodule Glific.Clients.Oblf do
   additional functionality as needed
   """
   @spec webhook(String.t(), map()) :: map()
+  def webhook("get_question_buttons", fields) do
+
+    buttons =
+      fields["daily_question"]
+      |> String.split("|")
+      |> Enum.with_index()
+      |> Enum.map(fn {answer, index} -> {"button_#{index + 1}", String.trim(answer)} end)
+      |> Enum.into(%{})
+
+    %{
+      buttons: buttons,
+      button_count: length(Map.keys(buttons)),
+      is_valid: true
+    }
+  end
+
+  def webhook("check_response", fields) do
+    %{
+      response: String.equivalent?(fields["correct_response"], fields["user_response"])
+    }
+  end
+
   def webhook(_, _fields),
     do: %{}
 end
