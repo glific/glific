2a333f584b04694ec5d8c361c9fe1f287c3b0ffe
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-11-24 12:57:43 +0530

    added docs

--- lib/glific/clients/oblf.ex
+++ lib/glific/clients/oblf.ex
@@ -3,7 +3,11 @@ defmodule Glific.Clients.Oblf do
   Custom webhook implementation specific to OBLF use case
   """
 
+  @doc """
+  Create a webhook with different signatures, so we can easily implement
+  additional functionality as needed
+  """
+  @spec webhook(String.t(), map()) :: map()
   def webhook(_, _fields),
     do: %{}
-
 end
