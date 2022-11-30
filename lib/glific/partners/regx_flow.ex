1f720e7ec88eb89f5fe6662e52bed95b9bde9785
Pankaj Agrawal <pankaj@coloredcow.com>
2022-11-30 18:59:30 +0530

    fixes

---
+++ lib/glific/partners/regx_flow.ex
@@ -0,0 +1,39 @@
+defmodule Glific.Partners.OrganizationSettings.RegxFlow do
+  @moduledoc """
+  The Glific abstraction to represent the regular expression flow
+  """
+  alias __MODULE__
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Glific.Flows.Flow
+
+  @optional_fields []
+
+  @required_fields [
+    :flow_id,
+    :regx
+  ]
+
+  @type t() :: %__MODULE__{
+          flow_id: non_neg_integer | nil,
+          regx: String.t() | nil
+        }
+
+  @primary_key false
+  embedded_schema do
+    field :regx, :string
+    belongs_to :flow, Flow
+  end
+
+  @doc """
+  Standard changeset pattern for embedded schema
+  """
+  @spec regx_flow_changeset(RegxFlow.t(), map()) :: Ecto.Changeset.t()
+  def regx_flow_changeset(regx_flow, attrs) do
+    regx_flow
+    |> cast(attrs, @optional_fields ++ @required_fields)
+    |> validate_required(@required_fields)
+  end
+end
