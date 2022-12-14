f233045f1170c4d0d742c2cce8939079d8c9d128
Pankaj Agrawal <pankaj@coloredcow.com>
2022-12-14 13:15:16 +0530

    fixes

--- build_scripts/gigalixir/staging_build.sh
+++ build_scripts/gigalixir/staging_build.sh
@@ -14,12 +14,12 @@ echo "pushing data to staging server"
 git push staging tag_$1:master
 
 
-echo "Going for a quick nap for next 40 second"
+echo "Going for a quick nap for next 30 second"
 sleep 30
+
+echo "Running the migration"
+gigalixir ps:migrate --migration_app_name glific -a glific-staging
 
 # echo "SSH to portal"
 echo "clean SSH server for Staging"
 gigalixir ps:ssh -a glific-staging < ./build_scripts/ssh_profile.sh
-
-echo "Running the migration"
-gigalixir ps:migrate --migration_app_name=glific
