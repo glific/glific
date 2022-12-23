34bf5bf98f587f856d2e5b1ae678bc8e9a56f0b5
Pankaj Agrawal <pankaj@coloredcow.com>
2022-12-23 18:22:02 +0530

    fixes

--- lib/glific/partners.ex
+++ lib/glific/partners.ex
@@ -750,12 +750,14 @@ defmodule Glific.Partners do
   list == [] (empty list) - the action should be performed for all organizations
   list == [ values ] - the actions should be performed only for organizations in the values list
   """
-  @spec perform_all((... -> nil), map() | nil, list() | [] | nil, boolean) :: any
-  def perform_all(handler, handler_args, list, only_recent \\ false)
+  @spec perform_all((... -> nil), map() | nil, list() | [] | nil, Keyword.t()) :: any
+  def perform_all(handler, handler_args, list, opts \\ [])
 
-  def perform_all(_handler, _handler_args, nil, _only_recent), do: nil
+  def perform_all(_handler, _handler_args, nil, _opts), do: nil
 
-  def perform_all(handler, handler_args, list, only_recent) do
+  def perform_all(handler, handler_args, list, opts) do
+    only_recent = Keyword.get(opts, :only_recent, false)
+    async = Keyword.get(opts, :async, false)
     # We need to do this for all the active organizations
     list
     |> active_organizations()
@@ -765,11 +767,7 @@ defmodule Glific.Partners do
 
       if is_nil(handler_args),
         do: handler.(id),
-        else:
-          handler.(
-            id,
-            Map.put(handler_args, :organization_name, name)
-          )
+        else: handler.(id, Map.put(handler_args, :organization_name, name))
     end)
   rescue
     # If we fail, we need to mark the organization as failed
