e1412896adf843184d0ad5656dafff8e683dde42
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-12-26 16:43:04 +0530

    updating sample sheet link

--- mix.exs
+++ mix.exs
@@ -97,6 +97,7 @@ defmodule Glific.MixProject do
       {:floki, ">= 0.27.0", only: @test_envs},
       {:gettext, "~> 0.20"},
       {:decimal, "~> 2.0"},
+      {:cowboy, "~> 2.1"},
       {:jason, "~> 1.4"},
       {:plug_cowboy, "~> 2.0"},
       {:ecto_enum, "~> 1.4"},
