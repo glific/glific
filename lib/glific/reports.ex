7cd274a0a164c6fbb26dbc45be74930eb988c98e
AkhileshNegi <akhilesh.negi@coloredcow.in>
2023-01-10 22:53:46 +0530

    restructuring quereis

--- lib/glific/reports.ex
+++ lib/glific/reports.ex
@@ -5,16 +5,7 @@ defmodule Glific.Reports do
 
   import Ecto.Query, warn: false
 
-  alias Glific.{
-    Contacts.Contact,
-    Messages.MessageConversation,
-    Repo
-  }
-
-  @kpis %{
-    contacts: Contact,
-    conversation: MessageConversation
-  }
+  alias Glific.Repo
 
   @doc false
   @spec get_kpi(String.t()) :: integer()
@@ -39,52 +30,59 @@ defmodule Glific.Reports do
 
     ## Examples
 
-      iex> get_kpi_data(1, :contacts)
-          %{
-            "04-01-2023" => 0,
-            "05-01-2023" => 0,
-            "06-01-2023" => 0,
-            "07-01-2023" => 0,
-            "08-01-2023" => 2,
-            "09-01-2023" => 3,
-            "10-01-2023" => 10
-          }
+    iex> Glific.Reports.get_kpi_data(1, "contacts")
+      %{
+        "04-01-2023" => 0,
+        "05-01-2023" => 0,
+        "06-01-2023" => 0,
+        "07-01-2023" => 0,
+        "08-01-2023" => 2,
+        "09-01-2023" => 3,
+        "10-01-2023" => 10
+      }
+    iex> Glific.Reports.get_kpi_data(1, "messages_conversations")
   """
-  @spec get_kpi_data(non_neg_integer(), atom()) :: map()
-  def get_kpi_data(organization_id, kpi) do
-    get_kpi_date_keys()
-    |> get_kpi_data_stats(organization_id, kpi)
-  end
+  @spec get_kpi_data(non_neg_integer(), String.t()) :: map()
+  def get_kpi_data(org_id, kpi) do
+    {today, last_day, date_map} = get_query_date()
 
-  # Returns last 7 days datetime which can then be used in query
-  @spec get_kpi_date_keys() :: list()
-  defp get_kpi_date_keys() do
-    Enum.reduce(0..6, [], fn day, acc ->
-      DateTime.utc_now()
-      |> Timex.shift(days: -day)
-      |> Timex.beginning_of_day()
-      |> then(&(acc ++ [&1]))
+    query_data =
+      """
+      SELECT
+      date_trunc('day', inserted_at) as date,
+      COUNT(id) as count
+      FROM #{kpi}
+      WHERE  inserted_at > '#{last_day}' and inserted_at <= '#{today}' and organization_id = #{org_id}
+      GROUP BY date
+      """
+      |> Repo.query!([])
+
+    Enum.reduce(query_data.rows, date_map, fn [date, count], acc ->
+      Map.put(acc, Timex.format!(date, "{0D}-{0M}-{YYYY}"), count)
     end)
   end
 
-  # Query the KPI data and stitches it together in a map
-  @spec get_kpi_data_stats(list(), non_neg_integer(), atom()) :: map()
-  defp get_kpi_data_stats(days, organization_id, kpi) do
-    Enum.reduce(days, %{}, fn day, acc ->
-      next_day = Timex.shift(day, days: 1)
-      date_key = Timex.format!(day, "{0D}-{0M}-{YYYY}")
-      module = Map.get(@kpis, kpi)
+  @spec get_query_date(DateTime.t()) :: tuple()
+  defp get_query_date(time \\ DateTime.utc_now()) do
+    today = shifted_time(time, 1) |> Timex.format!("{YYYY}-{0M}-{0D}")
 
-      Repo.one(
-        from(p in module,
-          select: count("*"),
-          where:
-            p.organization_id == ^organization_id and
-              p.inserted_at >= ^day and
-              p.inserted_at < ^next_day
-        )
-      )
-      |> then(&Map.put(acc, date_key, &1))
-    end)
+    last_day = shifted_time(time, -6) |> Timex.format!("{YYYY}-{0M}-{0D}")
+
+    date_map =
+      Enum.reduce(0..6, %{}, fn day, acc ->
+        time
+        |> shifted_time(-day)
+        |> Timex.format!("{0D}-{0M}-{YYYY}")
+        |> then(&Map.put(acc, &1, 0))
+      end)
+
+    {today, last_day, date_map}
+  end
+
+  @spec shifted_time(DateTime.t(), integer()) :: DateTime.t()
+  defp shifted_time(time, days) do
+    time
+    |> Timex.beginning_of_day()
+    |> Timex.shift(days: days)
   end
 end
