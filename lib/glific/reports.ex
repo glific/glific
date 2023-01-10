2d73ded8eada10bfe358f7bd221323b50a50d5d8
AkhileshNegi <akhilesh.negi@coloredcow.in>
2023-01-10 15:55:59 +0530

    added function to fetch kpi data

--- lib/glific/reports.ex
+++ lib/glific/reports.ex
@@ -4,10 +4,13 @@ defmodule Glific.Reports do
   """
 
   import Ecto.Query, warn: false
-  # alias Glific.Repo
+
+  alias Glific.{
+    Contacts.Contact,
+    Repo
+  }
 
   @doc false
-
   @spec get_kpi(String.t()) :: integer()
   def get_kpi(_kpi) do
     Enum.random(100..1000)
@@ -23,5 +26,42 @@ defmodule Glific.Reports do
       :opted_in_contacts_count,
       :opted_out_contacts_count
     ]
+  end
+
+  @doc """
+  Returns last 7 days kpi data map with keys as date and value as count
+  """
+  @spec get_kpi_data(atom()) :: list()
+  def get_kpi_data(:contacts) do
+    get_kpi_date_keys()
+    |> get_kpi_data_stats(:contacts)
+  end
+
+  # Returns last 7 days datetime which can then be used in query
+  @spec get_kpi_date_keys() :: list()
+  defp get_kpi_date_keys() do
+    Enum.reduce(0..6, [], fn day, acc ->
+      DateTime.utc_now()
+      |> Timex.shift(days: -day)
+      |> Timex.beginning_of_day()
+      |> then(&(acc ++ [&1]))
+    end)
+  end
+
+  # Query the KPI data and stitches it together in a map
+  @spec get_kpi_data_stats(list(), atom()) :: map()
+  defp get_kpi_data_stats(days, :contacts) do
+    Enum.reduce(days, %{}, fn day, acc ->
+      next_day = Timex.shift(day, days: 1)
+      date_key = Timex.format!(day, "{0D}-{0M}-{YYYY}")
+
+      Repo.one(
+        from(p in Contact,
+          select: count("*"),
+          where: p.inserted_at >= ^day and p.inserted_at < ^next_day
+        )
+      )
+      |> then(&Map.put(acc, date_key, &1))
+    end)
   end
 end
