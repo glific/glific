91c60666b752f5da8e1299f063ae42c0c43e30c8
AkhileshNegi <akhilesh.negi@coloredcow.in>
2023-02-08 15:53:27 +0530

    updating type

--- lib/glific_web/schema/session_template_type.ex
+++ lib/glific_web/schema/session_template_type.ex
@@ -22,7 +22,7 @@ defmodule GlificWeb.Schema.SessionTemplateTypes do
   end
 
   object :bulk_apply_templates_result do
-    field :message, :json
+    field :csv_rows, :string
     field :errors, list_of(:input_error)
   end
 
