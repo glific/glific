d57ebcb0bf7ae335281918c22eab419af55747b4
Donald A Lobo <409069+dlobo@users.noreply.github.com>
2023-04-01 18:12:33 -0700

    make low_balance email a bit more proactive for larger amounts (#2734)

--- lib/glific/mails/balance_alert_mail.ex
+++ lib/glific/mails/balance_alert_mail.ex
@@ -8,7 +8,7 @@ defmodule Glific.Mails.BalanceAlertMail do
   @spec low_balance_alert(Organization.t(), integer()) :: Swoosh.Email.t()
   def low_balance_alert(org, bsp_balance) do
     subject = """
-    [URGENT Low balance] : Messages on Glific will stop soon
+    [URGENT Low balance $#{bsp_balance}] : Messages on Glific will stop soon
     """
 
     body = """
