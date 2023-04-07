ace31b2e6426fc39ef867df9f97bf1ae6485dd58
AkhileshNegi <akhileshnegi.an3@gmail.com>
2023-04-07 13:28:22 +0530

    queuing contact

--- lib/glific/providers/gupshup/contacts.ex
+++ lib/glific/providers/gupshup/contacts.ex
@@ -12,12 +12,9 @@ defmodule Glific.Providers.GupshupContacts do
   alias Glific.{
     Contacts,
     Contacts.Contact,
-    Partners,
-    Partners.Organization,
-    Providers.Gupshup.ApiClient
+    Providers.Gupshup.ApiClient,
+    Providers.Gupshup.ContactWorker
   }
-
-  @days_shift -14
 
   @doc """
     Update a contact phone as opted in
@@ -47,26 +44,27 @@ defmodule Glific.Providers.GupshupContacts do
   """
   @spec fetch_opted_in_contacts(map()) :: :ok | {:error, String.t()}
   def fetch_opted_in_contacts(attrs) do
-    organization = Partners.organization(attrs.organization_id)
-    fetch_opted_in_contacts(organization, @per_page_limit, 1)
+    fetch_opted_in_contacts(attrs.organization_id, @per_page_limit, 1)
   end
 
-  def fetch_opted_in_contacts(organization, @per_page_limit, page) do
-    ApiClient.fetch_opted_in_contacts(organization.id, page)
+  def fetch_opted_in_contacts(org_id, @per_page_limit, page) do
+    ApiClient.fetch_opted_in_contacts(org_id, page)
     |> validate_opted_in_contacts()
     |> case do
       {:ok, users} ->
         Enum.chunk_every(users, 1000)
-        |> Enum.each(fn users -> update_contacts(users, organization) end)
+        |> Enum.each(fn users ->
+          ContactWorker.make_job(users, org_id)
+        end)
 
-        fetch_opted_in_contacts(organization, length(users), page + 1)
+        fetch_opted_in_contacts(org_id, length(users), page + 1)
 
       error ->
         error
     end
   end
 
-  def fetch_opted_in_contacts(_organization, _user_count, _page), do: :ok
+  def fetch_opted_in_contacts(_org_id, _user_count, _page), do: :ok
 
   @doc """
   Perform the gupshup API call and parse the results for downstream functions.
@@ -90,52 +88,6 @@ defmodule Glific.Providers.GupshupContacts do
 
       {:error, %Tesla.Error{reason: reason}} ->
         {:error, dgettext("errors", "Reason: %{reason}", reason: reason)}
-    end
-  end
-
-  @spec update_contacts(list() | nil, Organization.t() | nil) :: :ok | any()
-  defp update_contacts(users, organization) do
-    Enum.each(users, fn user ->
-      if user["optinStatus"] == "OPT_IN" do
-        # handle scenario when contact has not sent a message yet
-        last_message_at = last_message_at(user["lastMessageTimeStamp"])
-
-        {:ok, optin_time} = DateTime.from_unix(user["optinTimeStamp"], :millisecond)
-
-        phone = user["countryCode"] <> user["phoneCode"]
-
-        Contacts.upsert(%{
-          phone: phone,
-          last_message_at: last_message_at,
-          optin_time: optin_time |> DateTime.truncate(:second),
-          optin_status: true,
-          optin_method: user["optinSource"],
-          bsp_status: check_bsp_status(last_message_at),
-          organization_id: organization.id,
-          language_id: organization.default_language_id,
-          last_communication_at: last_message_at
-        })
-      end
-    end)
-  end
-
-  @spec last_message_at(non_neg_integer()) :: DateTime.t()
-  defp last_message_at(0) do
-    Timex.shift(DateTime.utc_now(), days: @days_shift)
-  end
-
-  defp last_message_at(time) do
-    DateTime.from_unix(time, :millisecond)
-    |> elem(1)
-    |> DateTime.truncate(:second)
-  end
-
-  @spec check_bsp_status(DateTime.t()) :: atom()
-  defp check_bsp_status(last_message_at) do
-    if Timex.diff(DateTime.utc_now(), last_message_at, :hours) < 24 do
-      :session_and_hsm
-    else
-      :hsm
     end
   end
 end
