da1f51eef50491cdb238efdbe406a3deb0eb842a
AkhileshNegi <akhileshnegi.an3@gmail.com>
2023-05-17 15:18:50 +0530

    added specs

--- test/support/fixtures/tickets_fixtures.ex
+++ test/support/fixtures/tickets_fixtures.ex
@@ -3,10 +3,12 @@ defmodule Glific.TicketsFixtures do
   This module defines test helpers for creating
   entities via the `Glific.Tickets` context.
   """
+  alias Glific.Tickets.Ticket
 
   @doc """
   Generate a ticket.
   """
+  @spec ticket_fixture(map()) :: Ticket.t()
   def ticket_fixture(attrs \\ %{}) do
     {:ok, ticket} =
       attrs
