39d1fd1f62f313cb2b829da0f104e0bda7a5755b
AkhileshNegi <akhileshnegi.an3@gmail.com>
2023-05-24 15:01:33 +0530

    rearranging things

--- priv/repo/migrations/20230522105210_create_topic.exs
+++ priv/repo/migrations/20230522105210_create_topic.exs
@@ -2,18 +2,9 @@ defmodule Glific.Repo.Migrations.CreateTopic do
   use Ecto.Migration
 
   def change do
-    create table(:topics) do
-      add :uuid, :uuid, null: false, comment: "Unique ID for each topic"
-      add :name, :string, comment: "Name of the topic"
-
-      # foreign key to organization restricting scope of this table to this organization only
-      add :organization_id, references(:organizations, on_delete: :delete_all),
-        null: false,
-        comment: "Unique organization ID"
-
-      timestamps(type: :utc_datetime)
+    alter table(:flow_labels) do
+      add(:type, :string,
+        comment: "Flow label type for now can be flow or ticket"
+      )
     end
-
-    create unique_index(:topics, [:name, :organization_id])
-  end
 end
