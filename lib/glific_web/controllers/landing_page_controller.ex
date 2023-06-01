d098f115c901029d7c71e8bd41fc96e7eaccbbfe
Adwait <adwaitngodbole@gmail.com>
2023-06-02 00:07:44 +0530

    ci/cd fix and routes test added

--- lib/glific_web/controllers/landing_page_controller.ex
+++ lib/glific_web/controllers/landing_page_controller.ex
@@ -1,6 +1,8 @@
 defmodule GlificWeb.LandingPageController do
+  @moduledoc false
   use GlificWeb, :controller
 
+  @doc false
   def index(conn,_params) do
     render(conn,"landing_page.html", page_title: "Glific Backend")
   end
