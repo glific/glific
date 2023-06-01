bdf1cc0db3fe5f26b63da611bb0c95a68b072e9b
Adwait <adwaitngodbole@gmail.com>
2023-06-02 00:35:07 +0530

    space after semicolon

--- lib/glific_web/controllers/landing_page_controller.ex
+++ lib/glific_web/controllers/landing_page_controller.ex
@@ -4,6 +4,6 @@ defmodule GlificWeb.LandingPageController do
 
   @doc false
   def index(conn,_params) do
-    render(conn,"landing_page.html", page_title: "Glific Backend")
+    render(conn, "landing_page.html", page_title: "Glific Backend")
   end
 end
--- test/glific_web/page_controller_test.exs
+++ test/glific_web/page_controller_test.exs
@@ -1,8 +1,8 @@
-defmodule GlificWeb.LandingPageControllerTest do
+defmodule GlificWeb.Landing PageControllerTest do
   use GlificWeb.ConnCase
 
-  test "GET / renders the landing page", %{conn:conn} do
-    conn = get(conn,"/")
-    assert html_response(conn,200) =- "Welcome to Glific Backend!"
+  test "GET / renders the landing page", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =- "Welcome to Glific Backend!"
   end
 end
