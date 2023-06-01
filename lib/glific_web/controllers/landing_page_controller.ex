7b077a2bbe7a50461df5c5bbfcf6a5bbead56c52
Adwait Godbole <68469605+adwait-godbole@users.noreply.github.com>
2023-06-02 02:01:44 +0530

    Glific Backend Landing Page (#2808)
    * Glific Backend Landing Page
    
    * ci/cd fix and routes test added
    
    * space after semicolon
    
    * space fixes
    
    * Update page_controller_test.exs
    
    * fix syntax error
    
    ---------
    
    Co-authored-by: Donald A Lobo <409069+dlobo@users.noreply.github.com>
    Co-authored-by: Donald A. Lobo <donald.lobo@gmail.com>

---
+++ lib/glific_web/controllers/landing_page_controller.ex
@@ -0,0 +1,9 @@
+defmodule GlificWeb.LandingPageController do
+  @moduledoc false
+  use GlificWeb, :controller
+
+  @doc false
+  def index(conn, _params) do
+    render(conn, "landing_page.html", page_title: "Glific Backend")
+  end
+end
