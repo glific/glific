f4b41ed47c8300cc5140d0e0186d9024824fdc69
Donald A. Lobo <donald.lobo@gmail.com>
2023-06-10 22:15:33 -0700

    small steps forward

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -3,8 +3,8 @@
 # It initializes the DB and then runs the server
 
 GLIFIC_FIRST_STARTUP="GLIFIC_FIRST_STARTUP"
-if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
-    touch /$CONTAINER_FIRST_STARTUP
+if [ ! -e /$GLIFIC_FIRST_STARTUP ]; then
+    touch /$GLIFIC_FIRST_STARTUP
     # lets set things up
     mix setup
 fi
