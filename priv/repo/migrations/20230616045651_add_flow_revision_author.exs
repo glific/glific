2f1f75f0ca033e08ba543053adaa91756a618da8
AkhileshNegi <akhileshnegi.an3@gmail.com>
2023-06-16 10:38:06 +0530

    updating schema

--- priv/repo/migrations/20230616045651_add_flow_revision_author.exs
+++ priv/repo/migrations/20230616045651_add_flow_revision_author.exs
@@ -4,6 +4,7 @@ defmodule Glific.Repo.Migrations.AddFlowRevisionAuthor do
   def change do
     alter table(:flow_revisions) do
       # author of flow revision
-      add(:user_id, references(:users, on_delete: :nilify_all), null: true)
+      add(:user_id, references(:users, on_delete: :nilify_all), null: true, comment: "User ID of user who created this flow revision")
+    end
   end
 end
