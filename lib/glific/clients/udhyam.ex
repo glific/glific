05356412f4f91a52bcebb2ddf690b712feea9511
AkhileshNegi <akhileshnegi.an3@gmail.com>
2023-07-12 18:20:43 +0530

    setting GCS folder based on flows

--- lib/glific/clients/udhyam.ex
+++ lib/glific/clients/udhyam.ex
@@ -10,23 +10,25 @@ defmodule Glific.Clients.Udhyam do
   }
 
   @doc """
-  Tweak GCS Bucket name based Lahi usecase
+  Tweak GCS Bucket name based Udhyam usecase
   """
   @spec gcs_file_name(map()) :: String.t()
   def gcs_file_name(media) do
     flow_id = media["flow_id"]
 
-    flow_name =
-      Flow
-      |> where([f], f.id == ^flow_id)
-      |> select([fr], %{
-        name: fr.name
-      })
-      |> Repo.one()
-      |> then(& &1.name)
-
-    if is_nil(flow_name),
+    if flow_id == 0,
       do: "Udhyam" <> "/" <> media["remote_name"],
-      else: flow_name <> "/" <> media["remote_name"]
+      else: do_gcs_file_name(flow_id) <> "/" <> media["remote_name"]
+  end
+
+  @spec do_gcs_file_name(non_neg_integer()) :: String.t()
+  defp do_gcs_file_name(flow_id) do
+    Flow
+    |> where([f], f.id == ^flow_id)
+    |> select([fr], %{
+      name: fr.name
+    })
+    |> Repo.one()
+    |> then(& &1.name)
   end
 end
