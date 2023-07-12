df4f837199972b29f7ab4f5a73073e9853aabd00
AkhileshNegi <akhileshnegi.an3@gmail.com>
2023-07-12 18:27:16 +0530

    credo fixes

--- lib/glific/clients.ex
+++ lib/glific/clients.ex
@@ -17,7 +17,7 @@ defmodule Glific.Clients do
   @dev %{
     id: 1,
     name: "Glific",
-    gcs_file_name: Glific.Clients.Udhyam,
+    gcs_file_name: Glific.Clients.Tap,
     broadcast: Glific.Clients.Weunlearn,
     webhook: Glific.Clients.Tap,
     daily_tasks: Glific.Clients.DigitalGreen,
