85714b695ff5ddcfc8924c61e5dfdb286ac5d266
AkhileshNegi <akhileshnegi.an3@gmail.com>
2023-07-20 20:04:44 +0530

    tweaks to query

--- lib/glific/reports.ex
+++ lib/glific/reports.ex
@@ -51,10 +51,11 @@ defmodule Glific.Reports do
 
   defp get_count_query(org_id, :flows_started),
     do:
-      "SELECT SUM(flows_started) FROM stats WHERE organization_id = #{org_id} and period = 'day' and date_trunc('month', inserted_at) = date_trunc('month', CURRENT_DATE)"
+      "SELECT SUM(flows_started) FROM stats WHERE organization_id = #{org_id} and period = 'day' and EXTRACT(MONTH FROM date) = EXTRACT(MONTH FROM CURRENT_DATE)"
+
   defp get_count_query(org_id, :flows_completed),
     do:
-      "SELECT SUM(flows_completed) FROM stats WHERE organization_id = #{org_id} and period = 'day' and date_trunc('month', inserted_at) = date_trunc('month', CURRENT_DATE)"
+      "SELECT SUM(flows_completed) FROM stats WHERE organization_id = #{org_id} and period = 'day' and EXTRACT(MONTH FROM date) = EXTRACT(MONTH FROM CURRENT_DATE)"
 
   defp get_count_query(org_id, :valid_contact_count),
     do: "SELECT COUNT(id) FROM contacts WHERE organization_id = #{org_id} and status = 'valid'"
