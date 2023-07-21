534ce585085d7b7b51beba4c7e458ea23f54e17a
Donald A Lobo <409069+dlobo@users.noreply.github.com>
2023-07-21 02:41:18 -0700

    Feature/simplify migrations (#2890)
    * klets get tests to work
    
    * lets squash all migrations, get rid of airtel
    
    * Let get a bulk export API going
    
    * tweak to structure.sql to fix test
    
    * tweak to structure.sql to fix test and format
    
    * add empty migrations directory
    
    * update structure to latest DB, let's merge asap
    
    * update to new db structure

--- priv/repo/migrations/20230710133911_add_auto_sync_to_sheets.exs
+++
@@ -1,9 +0,0 @@
-defmodule Glific.Repo.Migrations.AddAutoSyncToSheets do
-  use Ecto.Migration
-
-  def change do
-    alter table(:sheets) do
-      add(:auto_sync, :boolean, default: false, comment: "Auto Sync Sheets data in some interval")
-    end
-  end
-end
