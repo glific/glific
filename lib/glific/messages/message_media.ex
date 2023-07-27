1228a835eccb817f963b12b294dd8098fcff02c9
Donald A. Lobo <donald.lobo@gmail.com>
2023-07-26 17:36:42 -0700

    fix mix check errors

--- lib/glific/messages/message_media.ex
+++ lib/glific/messages/message_media.ex
@@ -29,7 +29,6 @@ defmodule Glific.Messages.MessageMedia do
     :gcs_url,
     :content_type
   ]
-  alias Glific.Enums.{MessageFlow}
 
   @type t() :: %__MODULE__{
           __meta__: Ecto.Schema.Metadata.t(),
