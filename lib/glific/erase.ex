94290ddb7d10a68f7b286bc2df887a1bca8b3df5
AmishaBisht <amishabisht33@gmail.com>
2023-08-01 12:46:54 +0530

    fix limit

--- lib/glific/erase.ex
+++ lib/glific/erase.ex
@@ -133,7 +133,7 @@ defmodule Glific.Erase do
     |> Repo.query!([], timeout: 60_000, skip_organization_id: true)
   end
 
-  @limit 100
+  @limit 500
 
   @doc """
   Keep latest limited messages for a contact
