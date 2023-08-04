2afa5b4126271794b2c6d28cb5bea3fd339e4f68
AkhileshNegi <akhileshnegi.an3@gmail.com>
2023-08-04 11:05:19 +0530

    removing tag dependency

--- lib/glific/processor/consumer_tagger.ex
+++ lib/glific/processor/consumer_tagger.ex
@@ -5,7 +5,6 @@ defmodule Glific.Processor.ConsumerTagger do
 
   alias Glific.{
     Messages.Message,
-    Processor.Helper,
     Taggers,
     Taggers.Status
   }
@@ -33,54 +32,9 @@ defmodule Glific.Processor.ConsumerTagger do
   @spec new_contact_tagger({atom() | Message.t(), map()}) :: {Message.t(), map()}
   defp new_contact_tagger({message, state}) do
     if Status.is_new_contact(message) do
-      add_status_tag(message, "newcontact", state)
       {message, state |> Map.put(:tagged, true) |> Map.put(:newcontact, true)}
     else
       {message, state}
     end
   end
-
-  _ = """
-  Commenting out the next few functions as we eliminate work that we are not using
-
-  @spec numeric_tagger({atom() | Message.t(), map()}, String.t()) :: {Message.t(), map()}
-  defp numeric_tagger({message, state}, body) do
-    case Numeric.tag_body(body, state.numeric_map) do
-      {:ok, value} ->
-        {
-          Helper.add_tag(message, state.numeric_tag_id, value),
-          Map.put(state, :tagged, true)
-        }
-
-      _ ->
-        {message, state}
-    end
-  end
-  """
-
-  _ = ~c"""
-  alias Glific.Dialogflow.Sessions
-
-  @spec dialogflow_tagger({Message.t(), map()}) :: {Message.t(), map()}
-  # dialog flow only accepts messages less than 255 characters for intent
-  defp dialogflow_tagger({%{body: body} = message, %{tagged: false} = state})
-       when byte_size(body) > 255,
-       do: {message, state}
-
-  defp dialogflow_tagger({message, %{tagged: false} = state}) do
-    # only do the query if we have a valid credentials file for dialogflow
-    if FunWithFlags.enabled?(:dialogflow,
-         for: %{organization_id: message.organization_id}
-       ),
-       do: Sessions.detect_intent(message, state.dialogflow_session_id)
-
-    {message, state}
-  end
-
-  defp dialogflow_tagger({message, state}), do: {message, state}
-  """
-
-  @spec add_status_tag(Message.t(), String.t(), map()) :: Message.t()
-  defp add_status_tag(message, status, state),
-    do: Helper.add_tag(message, state.status_map[status])
 end
