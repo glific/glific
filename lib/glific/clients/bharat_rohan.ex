c2a07cc3aba0e2cdf803db3bae4799950d02639f
mohitgusain <mohit.gusain@coloredcow.com>
2023-08-08 13:47:46 +0530

    bharat rohan weather integration

--- lib/glific/clients/bharat_rohan.ex
+++ lib/glific/clients/bharat_rohan.ex
@@ -31,6 +31,43 @@ defmodule Glific.Clients.BharatRohan do
     %{success: true}
   end
 
+  def webhook("parse_weather_report", fields) do
+    weather_report = fields["results"]["weather_report"]
+    %{report_msg: get_report_msg(weather_report)}
+  end
+
   def webhook(_, _fields),
     do: %{}
+
+  @doc """
+    Get weather report message
+  """
+  @spec get_report_msg(map()) :: String.t()
+  def get_report_msg(weather_report) do
+    days_map = Map.get(weather_report, "days", %{})
+    days = Map.values(days_map)
+
+    _formatted_output =
+      Enum.reduce(days, "", fn day, acc ->
+        windspeed = Map.get(day, "windspeed", "")
+        winddir = Map.get(day, "winddir", "")
+        tempmin = Map.get(day, "tempmin", "")
+        tempmax = Map.get(day, "tempmax", "")
+        datetime = Map.get(day, "datetime", "")
+        precipitation = Map.get(day, "precip", "")
+        humidity = Map.get(day, "humidity", "")
+
+        line = """
+        *Date:* #{datetime}
+        *Min Temperature:* #{tempmin} °C
+        *Max Temperature:* #{tempmax} °C
+        *Precipitation:* #{precipitation} mm
+        *Humidity:* #{humidity} %
+        *Wind speed:* #{windspeed} km/h
+        *Wind Direction:* #{winddir} °
+        """
+
+        acc <> line <> "\n\n"
+      end)
+  end
 end
