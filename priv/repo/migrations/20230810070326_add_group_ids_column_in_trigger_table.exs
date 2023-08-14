ffc9e7bf3bcc9b3c96909afebf2b28c1b8072786
AmishaBisht <amishabisht33@gmail.com>
2023-08-14 13:56:29 +0530

    removed unnecessary code

--- priv/repo/migrations/20230810070326_add_group_ids_column_in_trigger_table.exs
+++
@@ -1,9 +0,0 @@
-defmodule Glific.Repo.Migrations.AddGroupIdsColumnInTriggerTable do
-  use Ecto.Migration
-
-  def change do
-    alter table(:triggers) do
-      add :group_ids, {:array, :integer}, default: []
-    end
-  end
-end
