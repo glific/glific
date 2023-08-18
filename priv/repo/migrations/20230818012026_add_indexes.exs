c6a60e167af531a254e04e562fa9ccc6b0438356
Donald A. Lobo <donald.lobo@gmail.com>
2023-08-18 13:49:43 -0700

    drop unused big indexes

--- priv/repo/migrations/20230818012026_add_indexes.exs
+++ priv/repo/migrations/20230818012026_add_indexes.exs
@@ -5,5 +5,8 @@ defmodule Glific.Repo.Migrations.AddIndexes do
     create_if_not_exists index(:contact_histories, :profile_id, where: "profile_id IS NOT NULL")
     create_if_not_exists index(:bigquery_jobs, [:organization_id, :table])
     create_if_not_exists index(:session_templates, :message_media_id, where: "message_media_id IS NOT NULL")
+
+    drop_if_exists index(:contact_histories, [:contact_id, :updated_at])
+    drop_if_exists index(:contact_histories, [:updated_at])
   end
 end
