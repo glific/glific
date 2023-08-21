87ed5b2402af141cfae4de8e825feda5fa564d54
Darshan2003 <darshanjain727@gmail.com>
2023-08-21 16:09:58 +0530

    removed migration

--- priv/repo/migrations/20230818121359_modify_setting_default.exs
+++
@@ -1,9 +0,0 @@
-defmodule Glific.Repo.Migrations.ModifySettingDefault do
-  use Ecto.Migration
-
-  def change do
-    alter table(:organizations) do
-      modify :setting, :jsonb, default: "{}"
-    end
-  end
-end
