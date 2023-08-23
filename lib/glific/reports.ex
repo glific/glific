2fab4d6a5a8c87397c742720a16da132db305bd6
Darshan2003 <darshanjain727@gmail.com>
2023-08-23 23:09:34 +0530

    bug fixes

--- lib/glific/reports.ex
+++ lib/glific/reports.ex
@@ -130,13 +130,14 @@ defmodule Glific.Reports do
   @spec get_day_range(String.t()) :: tuple()
   defp get_day_range(duration) do
     day = shifted_time(NaiveDateTime.utc_now(), -1) |> NaiveDateTime.to_date()
+    last_7 = shifted_time(NaiveDateTime.utc_now(), -7) |> NaiveDateTime.to_date()
 
     case duration do
       "MONTHLY" ->
         {"month", Date.beginning_of_month(day), Date.end_of_month(day)}
 
       "WEEKLY" ->
-        {"week", Date.beginning_of_week(day), Date.end_of_week(day)}
+        {"day", last_7, day}
 
       "DAILY" ->
         {"day", day, day}
