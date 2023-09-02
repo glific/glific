692e49fbd61c27ba499a6a3edd7e56e4d1a10622
AkhileshNegi <akhileshnegi.an3@gmail.com>
2023-09-02 22:40:51 +0530

    updated testcases

--- test/glific/tracker_test.exs
+++ test/glific/tracker_test.exs
@@ -1,5 +1,6 @@
 defmodule Glific.TrackersTest do
   use Glific.DataCase
+
   alias Glific.{
     Seeds.SeedsDev,
     Trackers,
@@ -18,14 +19,20 @@ defmodule Glific.TrackersTest do
     test "create_tracker/1 creates a new tracker", attrs do
       tracker_attrs = %{
         organization_id: attrs.organization_id,
-        counts: %{},
+        counts: %{"Webhook" => 5, "Flows Started" => 2},
         date: Date.utc_today(),
         period: "day"
       }
 
-      {:ok, tracker} = Trackers.create_tracker(tracker_attrs)
-      assert %Tracker{} = tracker
-      assert Repo.get_by(Tracker, organization_id: attrs.organization_id)
+      {:ok, _tracker} = Trackers.create_tracker(tracker_attrs)
+
+      assert {:ok, tracker} =
+               Repo.fetch_by(Tracker, %{
+                 organization_id: attrs.organization_id
+               })
+
+      assert tracker.counts == %{"Webhook" => 5, "Flows Started" => 2}
+      assert tracker.period == "day"
     end
 
     test "update_tracker/2 updates an existing tracker", attrs do
@@ -37,21 +44,33 @@ defmodule Glific.TrackersTest do
       }
 
       {:ok, tracker} = Trackers.create_tracker(tracker_attrs)
-      updated_attrs = %{counts: %{sms: 10}}
+      updated_attrs = %{counts: %{"Webhook" => 5, "Flows Started" => 2}}
       {:ok, updated_tracker} = Trackers.update_tracker(tracker, updated_attrs)
 
-      assert %{sms: 10} = updated_tracker.counts
+      assert updated_tracker.counts == %{"Webhook" => 5, "Flows Started" => 2}
+      assert updated_tracker.period == "day"
     end
 
     test "upsert_tracker/4 inserts a new tracker when it doesn't exist", attrs do
-      counts = %{sms: 5}
+      counts = %{"Webhook" => 5, "Flows Started" => 2}
       organization_id = attrs.organization_id
       date = Date.utc_today()
       period = "day"
 
-      assert {:ok, _} = Trackers.upsert_tracker(counts, organization_id, date, period)
-      assert %Tracker{} = Repo.get_by(Tracker, organization_id: organization_id)
-    end
+      # Upsert should create a new entry when called for first time
+      assert {:ok, tracker} = Trackers.upsert_tracker(counts, organization_id, date, period)
+      assert tracker.counts == %{"Webhook" => 5, "Flows Started" => 2}
+      assert tracker.period == "day"
 
+      # Upsert should append counts to existing entry when called for second time
+
+      new_count = %{"Webhook" => 8, "Flows Started" => 10}
+
+      assert {:ok, updated_tracker} =
+               Trackers.upsert_tracker(new_count, organization_id, date, period)
+
+      assert updated_tracker.counts == %{"Webhook" => 13, "Flows Started" => 12}
+      assert updated_tracker.period == "day"
+    end
   end
 end
