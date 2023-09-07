437c0054b22f278a83abc975e67cc9a8ca13c93d
AkhileshNegi <akhileshnegi.an3@gmail.com>
2023-09-07 10:06:55 +0530

    added common

--- lib/glific/clients/common_webhook.ex
+++ lib/glific/clients/common_webhook.ex
@@ -81,6 +81,20 @@ defmodule Glific.Clients.CommonWebhook do
     end
   end
 
+  def webhook("openllm", fields) do
+    mp = Multipart.add_field(Multipart.new(), "prompt", fields["prompt"])
+
+    Tesla.post(fields["url"], mp, opts: [adapter: [recv_timeout: 100_000]])
+    |> case do
+      {:ok, %Tesla.Env{status: 200..299, body: body}} ->
+        Jason.decode!(body)
+        |> Map.take(["answer", "session_id"])
+
+      {_status, response} ->
+        %{success: false, response: "Invalid response #{response}"}
+    end
+  end
+
   def webhook("jugalbandi-voice", %{"query_text" => query_text} = fields),
     do: query_jugalbandi_api(fields, query_text: query_text)
 
