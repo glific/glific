1ab1be2fd99faf7680ea71dea7c9c785bb21334f
Chaitanya <thakur.chaitanya25@gmail.com>
2023-09-09 12:23:45 +0530

    fix broadcast table bug

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -1,41 +1,5 @@
 let Hooks = {};
 
-function createTable(element, headers, data) {
-  let table = document.createElement("table");
-  let thead = document.createElement("thead");
-  let tbody = document.createElement("tbody");
-
-  let headerRow = document.createElement("tr");
-  headers.forEach((header) => {
-    let th = document.createElement("th");
-    th.textContent = header;
-    headerRow.appendChild(th);
-  });
-  thead.appendChild(headerRow);
-
-  data.forEach((item) => {
-    let row = document.createElement("tr");
-    Object.values(item).forEach((value) => {
-      let td = document.createElement("td");
-      td.textContent = value;
-      row.appendChild(td);
-    });
-    tbody.appendChild(row);
-  });
-
-  table.appendChild(thead);
-  table.appendChild(tbody);
-  element.appendChild(table);
-}
-
-Hooks.table = {
-  mounted() {
-    let tableData = JSON.parse(this.el.dataset.tableData);
-    let tableHeaders = JSON.parse(this.el.dataset.tableHeaders);
-    createTable(this.el, tableHeaders, tableData);
-  },
-};
-
 Hooks.dateInput = {
   mounted() {
     flatpickr(this.el, {
