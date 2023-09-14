c8680ba4f94fb266b0c11004b5943902803dc5a5
AmishaBisht <amishabisht33@gmail.com>
2023-09-14 19:37:23 +0530

    resolve review

--- lib/glific/flows/contact_action.ex
+++ lib/glific/flows/contact_action.ex
@@ -375,7 +375,7 @@ defmodule Glific.Flows.ContactAction do
 
     {_cid, message_vars} = resolve_cid(context, cid)
 
-    db_url = from(m in MessageMedia, where: m.url == ^url) |> Repo.one()
+    db_url = from(m in MessageMedia, where: m.url == ^url) |> limit(1) |> Repo.one()
 
     if is_nil(url) do
       FlowContext.notification(context, "Could not send message to contact: Empty media URL")
