f75353b4e0a4b4a4143f9864212802f549ed02d0
AmishaBisht <amishabisht33@gmail.com>
2023-11-17 19:51:35 +0530

    fix code

--- lib/glific/providers/balance_worker.ex
+++ lib/glific/providers/balance_worker.ex
@@ -22,7 +22,7 @@ defmodule Glific.Jobs.BSPBalanceWorker do
   def perform_periodic(organization_id) do
     organization = Repo.get!(Organization, organization_id)
     threshold = organization.setting.low_balance_threshold
-    bsp_balance_limit = organization.setting.bsp_balance_limit
+    critical_balance_threshold = organization.setting.report_frequency
 
     Logger.info("Checking BSP balance: organization_id: '#{organization_id}'")
 
@@ -31,7 +31,7 @@ defmodule Glific.Jobs.BSPBalanceWorker do
       {:ok, data} ->
         bsp_balance = data["balance"]
 
-        send_low_balance_notification(bsp_balance, organization_id, threshold, bsp_balance_limit)
+        send_low_balance_notification(bsp_balance, organization_id, threshold, critical_balance_threshold)
 
         # We should move this to an embedded schema
         # and then fix the function in publish_data. Basically have a periodic
@@ -49,16 +49,22 @@ defmodule Glific.Jobs.BSPBalanceWorker do
     :ok
   end
 
-  @spec send_low_balance_notification(integer(), non_neg_integer(), non_neg_integer(), non_neg_integer()) ::
+  @spec send_low_balance_notification(
+          integer(),
+          non_neg_integer(),
+          non_neg_integer(),
+          non_neg_integer()
+        ) ::
           nil | {:ok, any}
   defp send_low_balance_notification(bsp_balance, organization_id, nil, nil),
     do: send_low_balance_notification(bsp_balance, organization_id, 10, 3)
 
-  defp send_low_balance_notification(bsp_balance, organization_id, threshold, bsp_balance_limit)
+  defp send_low_balance_notification(bsp_balance, organization_id, threshold, critical_balance_threshold)
        when bsp_balance < threshold do
     # start sending a warning message when the balance is lower than $10
     # we can tweak this over time
-    go_back = if bsp_balance < bsp_balance_limit , do: 48, else: 7 * 24
+    # Checking if bsp_balance is less than the critical_balance_threshold, which by default is 3
+    go_back = if bsp_balance < critical_balance_threshold, do: 48, else: 7 * 24
 
     ## We need to check if we have already sent this notification in last go_back time
     category = "low_bsp_balance"
