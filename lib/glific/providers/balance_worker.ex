18802b9e5ae51c00378f0f387513aa6f99372394
Amisha Bisht <amishabisht33@gmail.com>
2023-11-20 12:51:48 +0000

    Email:Low balance alert mail frequency (#3202)
    * send mail twice a week
    
    * send mail twice a week
    
    * fix code
    
    * fix code
    
    * fix code
    
    * resolve review

--- lib/glific/providers/balance_worker.ex
+++ lib/glific/providers/balance_worker.ex
@@ -22,6 +22,7 @@ defmodule Glific.Jobs.BSPBalanceWorker do
   def perform_periodic(organization_id) do
     organization = Repo.get!(Organization, organization_id)
     threshold = organization.setting.low_balance_threshold
+    critical_balance_threshold = organization.setting.critical_balance_threshold
 
     Logger.info("Checking BSP balance: organization_id: '#{organization_id}'")
 
@@ -30,7 +31,12 @@ defmodule Glific.Jobs.BSPBalanceWorker do
       {:ok, data} ->
         bsp_balance = data["balance"]
 
-        send_low_balance_notification(bsp_balance, organization_id, threshold)
+        send_low_balance_notification(
+          bsp_balance,
+          organization_id,
+          threshold,
+          critical_balance_threshold
+        )
 
         # We should move this to an embedded schema
         # and then fix the function in publish_data. Basically have a periodic
@@ -48,16 +54,27 @@ defmodule Glific.Jobs.BSPBalanceWorker do
     :ok
   end
 
-  @spec send_low_balance_notification(integer(), non_neg_integer(), non_neg_integer()) ::
+  @spec send_low_balance_notification(
+          integer(),
+          non_neg_integer(),
+          non_neg_integer(),
+          non_neg_integer()
+        ) ::
           nil | {:ok, any}
-  defp send_low_balance_notification(bsp_balance, organization_id, nil),
-    do: send_low_balance_notification(bsp_balance, organization_id, 10)
+  defp send_low_balance_notification(bsp_balance, organization_id, nil, nil),
+    do: send_low_balance_notification(bsp_balance, organization_id, 10, 3)
 
-  defp send_low_balance_notification(bsp_balance, organization_id, threshold)
+  defp send_low_balance_notification(
+         bsp_balance,
+         organization_id,
+         threshold,
+         critical_balance_threshold
+       )
        when bsp_balance < threshold do
-    # start sending a warning message when the balance is lower than $10
+    # start sending a warning message when the balance is lower than a certain threshold default is $10
     # we can tweak this over time
-    go_back = if bsp_balance < 3, do: 24, else: 48
+    # If the balance is below a certain threshold or it's critically low (below $3 by default), trigger a warning notification.
+    go_back = if bsp_balance < critical_balance_threshold, do: 48, else: 7 * 24
 
     ## We need to check if we have already sent this notification in last go_back time
     category = "low_bsp_balance"
@@ -76,5 +93,5 @@ defmodule Glific.Jobs.BSPBalanceWorker do
     end
   end
 
-  defp send_low_balance_notification(_, _, _), do: nil
+  defp send_low_balance_notification(_, _, _, _), do: nil
 end
