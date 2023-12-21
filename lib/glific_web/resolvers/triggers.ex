e00867ec53b65b2aa323b950d3d597402bca1e0f
AmishaBisht <amishabisht33@gmail.com>
2023-12-21 16:30:47 +0530

    fix test case

--- lib/glific_web/resolvers/triggers.ex
+++ lib/glific_web/resolvers/triggers.ex
@@ -51,9 +51,10 @@ defmodule GlificWeb.Resolvers.Triggers do
   @spec check_trigger_warnings(Absinthe.Resolution.t(), %{input: map()}, %{context: map()}) ::
           {:ok, any} | {:error, any}
   def check_trigger_warnings(_, %{input: params}, _) do
-    with {:ok, _} <- Triggers.check_trigger_warnings(params) do
-      {:ok, %{errors: nil}}
-    else
+    case Triggers.check_trigger_warnings(params) do
+      {:ok, _} ->
+        {:ok, %{errors: nil}}
+
       {:warning, warning} ->
         {:ok, %{errors: [%{key: "warning", message: warning}]}}
     end
