cc199601905997988da7c1b6c79ec2a4ab4287e1
Akhilesh Negi <akhileshnegi.an3@gmail.com>
2024-01-11 22:14:38 +0530

    Auto Translate: Bugfixes (#3287)
    * moving to translating one string at time
    
    * updating testcases

--- lib/glific/flows/translate/open_ai.ex
+++ lib/glific/flows/translate/open_ai.ex
@@ -23,33 +23,23 @@ defmodule Glific.Flows.Translate.OpenAI do
           {:ok, [String.t()]} | {:error, String.t()}
   def translate(strings, src, dst) do
     strings
-    |> chunk()
+    |> check_large_strings()
     |> Enum.reduce([], &[do_translate(&1, src, dst) | &2])
-    |> Enum.flat_map(& &1)
     |> then(&{:ok, &1})
   end
 
   # Making API call to open ai to translate list of string from src language to dst
-  @spec do_translate([String.t()], String.t(), String.t()) ::
-          {:ok, [String.t()]} | {:error, String.t()}
+  @spec do_translate([String.t()], String.t(), String.t()) :: [String.t()] | {:error, String.t()}
   defp do_translate(strings, src, dst) do
-    length = Enum.count(strings)
-
     prompt =
       """
-      I'm going to give you a template for your output. CAPITALIZED WORDS are my placeholders.
-      Please preserve the overall formatting of my template to convert list of strings from #{src} to #{dst}.Each comma separated strings can be multi-lined where linebreak can be \n or \n\n. Keep the translated message also multi-lined
-
-      ***["CONVERTED_TEXT_1", "CONVERTED_TEXT_2","CONVERTED_TEXT_3"]***
-
-      Please return only the list. Here's sample
-
-      User: ["hello there", "oops wrong answer", "Great to meet you"]
-      Think: there are 3 comma separated strings list in english convert it to 3 comma separated list of string in hindi
-      System: ["नमस्ते", "उफ़ ग़लत उत्तर", "बड़ा अच्छा लगा आपसे मिल के"]
-      User: ["welcome", "correct answer, keep it up", "you won 1 point"]
-      Think: there are 3 comma separated strings list in english convert it to 3 comma separated strings list in tamil
-      System: ["வரவேற்பு", "சரியான பதில், தொடருங்கள்", "நீங்கள் 1 புள்ளியை வென்றீர்கள்"]
+      Translate the text from #{src} to #{dst}. Return only translated text
+      User: "hello there"
+      Think: Translate the text from english to hindi
+      System: "नमस्ते"
+      User: "you won 1 point"
+      Think: Translate the text from english to tamil
+      System: "நீங்கள் 1 புள்ளியை வென்றீர்கள்"
       """
 
     Glific.get_open_ai_key()
@@ -57,14 +47,14 @@ defmodule Glific.Flows.Translate.OpenAI do
       """
       #{prompt}
       User: #{strings}
-      Think: there are #{length} comma separated strings list in #{src} convert it to #{length} comma separated strings list in #{dst}
+      Think: Translate the text from #{src} to #{dst}
       System:
       """,
       @open_ai_params
     )
     |> case do
       {:ok, result} ->
-        Jason.decode!(result)
+        result
 
       {:error, error} ->
         Logger.error("Error translating: #{error} String: #{strings}")
@@ -73,31 +63,20 @@ defmodule Glific.Flows.Translate.OpenAI do
   end
 
   @doc """
-  Chunking list of strings based on the size
+  Cleanup up string list replacing long text exceeding token threshold with warning
+  This reverses the order of string which is reversed again in next function
   """
-  @spec chunk([String.t()]) :: [String.t()]
-  def chunk(strings), do: do_chunk(strings, [], 0, [])
+  @spec check_large_strings([String.t()]) :: [String.t()]
+  def check_large_strings(strings) do
+    strings
+    |> Enum.reduce([], fn string, acc ->
+      string_size = Gpt3Tokenizer.token_count(string)
 
-  @spec do_chunk([String.t()], list(), non_neg_integer(), list()) :: [String.t()]
-  defp do_chunk([], [], _, acc), do: Enum.reverse(acc)
-  defp do_chunk([], chunk, _, acc), do: Enum.reverse([Enum.reverse(chunk) | acc])
-
-  defp do_chunk([head | tail], chunk, current_size, acc) do
-    string_size = Gpt3Tokenizer.token_count(head)
-
-    cond do
-      # Replacing long text with default message that translation not available
-      string_size > @token_chunk_size ->
-        total_size = current_size + 6
-        do_chunk(tail, ["translation not available for long messages" | chunk], total_size, acc)
-
-      # Splitting chunks based on total size
-      current_size + string_size > @token_chunk_size ->
-        do_chunk([head | tail], [], 0, [Enum.reverse(chunk) | acc])
-
-      # Default case: add head to chunk and continue
-      true ->
-        do_chunk(tail, [head | chunk], current_size + string_size, acc)
-    end
+      if string_size > @token_chunk_size do
+        ["translation not available for long messages" | acc]
+      else
+        [string | acc]
+      end
+    end)
   end
 end
