fc68b6065d5e0efb97297cf9ef3b08532cc4de28
AkhileshNegi <akhileshnegi.an3@gmail.com>
2024-01-11 19:15:45 +0530

    moving to translating one string at time

--- test/glific/flows/translate/open_ai_test.exs
+++ test/glific/flows/translate/open_ai_test.exs
@@ -35,28 +35,37 @@ defmodule Glific.Flows.Translate.OpenAITest do
     assert translated_text == ["हमारे एनजीओ चैटबॉट में आपका स्वागत है", "हमें अपने बारे में परिचय देने के लिए धन्यवाद"]
   end
 
-  test "chunk/1 should chunk a list of strings based on length" do
+  test "check_large_strings/1 should replace long strings with warning" do
     long_text = Faker.Lorem.sentence(250)
 
     # when long text is at the middle
     response = [
-      ["thankyou for joining", "translation not available for long messages", "correct answer"]
+      "thankyou for joining",
+      "translation not available for long messages",
+      "correct answer"
     ]
 
-    assert response == OpenAI.chunk(["thankyou for joining", long_text, "correct answer"])
+    assert response ==
+             OpenAI.check_large_strings(["correct answer", long_text, "thankyou for joining"])
 
     # when long text is at the end
     response = [
-      ["thankyou for joining", "correct answer", "translation not available for long messages"]
+      "thankyou for joining",
+      "correct answer",
+      "translation not available for long messages"
     ]
 
-    assert response == OpenAI.chunk(["thankyou for joining", "correct answer", long_text])
+    assert response ==
+             OpenAI.check_large_strings([long_text, "correct answer", "thankyou for joining"])
 
     # when long text is at the beginning
     response = [
-      ["translation not available for long messages", "thankyou for joining", "correct answer"]
+      "correct answer",
+      "thankyou for joining",
+      "translation not available for long messages"
     ]
 
-    assert response == OpenAI.chunk([long_text, "thankyou for joining", "correct answer"])
+    assert response ==
+             OpenAI.check_large_strings([long_text, "thankyou for joining", "correct answer"])
   end
 end
