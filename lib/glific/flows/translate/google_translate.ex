333d3deadecd86bd31873e46a6ca817a8b2b1b0e
AmishaBisht <amishabisht33@gmail.com>
2024-01-16 20:40:45 +0530

    structure of google translate

--- lib/glific/flows/translate/google_translate.ex
+++ lib/glific/flows/translate/google_translate.ex
@@ -1,10 +1,83 @@
 defmodule Glific.Flows.Translate.GoogleTranslate do
-  use GoogleApi.Translate
+  @moduledoc """
+  Code to translate using google translate as the translation engine
+  """
+  @behaviour Glific.Flows.Translate.Translate
+  @google_translate_params %{"temperature" => 0, "max_tokens" => 12_000}
+  @token_chunk_size 200
+
   alias Glific.GoogleTranslate.Translate
+  require Logger
 
+  @doc """
+  Translate a list of strings from language 'src' to language 'dst'
+  Returns, either ok with the translated list in the same order,
+  or error with a error message
 
-  @spec translate(String.t(), String.t(), String.t())
-  def translate(string, src, dst) do
+  ## Examples
 
+  iex> Glific.Flows.Translate.GoogleTranslate.translate(["thankyou for joining", "correct answer"], "english", "hindi")
+    {:ok, ["hindi thankyou for joining english", "hindi correct answer english"]}
+  """
+  @spec translate([String.t()], String.t(), String.t()) ::
+          {:ok, [String.t()]} | {:error, String.t()}
+  def translate(strings, src, dst) do
+    strings
+    |> check_large_strings()
+    |> Enum.reduce([], &[do_translate(&1, src, dst) | &2])
+    |> then(&{:ok, &1})
+  end
+
+  # Making API call to google translate to translate list of string from src language to dst
+  @spec do_translate([String.t()], String.t(), String.t()) :: [String.t()] | {:error, String.t()}
+  defp do_translate(strings, src, dst) do
+    prompt =
+      """
+      Translate the text from #{src} to #{dst}. Return only translated text
+      User: "hello there"
+      Think: Translate the text from english to hindi
+      System: "नमस्ते"
+      User: "you won 1 point"
+      Think: Translate the text from english to tamil
+      System: "நீங்கள் 1 புள்ளியை வென்றீர்கள்"
+      """
+
+    Glific.get_google_translate_key()
+    |> Translate.parse(
+      """
+      #{prompt}
+      User: #{strings}
+      Think: Translate the text from #{src} to #{dst}
+      System:
+      """,
+      @google_translate_params
+    )
+    |> case do
+      {:ok, result} ->
+        result
+
+      {:error, error} ->
+        Logger.error("Error translating: #{error} String: #{strings}")
+        ["Could not translate, Try again"]
+    end
+  end
+
+  @doc """
+  Cleanup up string list replacing long text exceeding token threshold with warning
+  This reverses the order of string which is reversed again in next function
+  """
+  @spec check_large_strings([String.t()]) :: [String.t()]
+  def check_large_strings(strings) do
+    strings
+    |> Enum.reduce([], fn string, acc ->
+      # we ca use the gptTokenizer to count the token
+      string_size = Gpt3Tokenizer.token_count(string)
+
+      if string_size > @token_chunk_size do
+        ["translation not available for long messages" | acc]
+      else
+        [string | acc]
+      end
+    end)
   end
 end
--- lib/glific/third_party/google_translate/translate.ex
+++ lib/glific/third_party/google_translate/translate.ex
@@ -1,8 +1,84 @@
 defmodule Glific.GoogleTranslate.Translate do
   @moduledoc """
-  Glific googleTranslate module for all API calls to googleTranslate
+  Glific Google Translate module for all API calls to Google Translate
   """
 
+  alias Glific.Partners
 
-@endpoint "https://translation.googleapis.com/language/translate/v2"
+  @endpoint "https://translation.googleapis.com/language/translate/v2"
+
+  @default_params %{
+    "temperature" => 0.7,
+    "max_tokens" => 250,
+    "top_p" => 1,
+    "frequency_penalty" => 0,
+    "presence_penalty" => 0
+  }
+
+  @doc """
+  API call to google translate
+  """
+  @spec parse(String.t(), String.t(), map()) :: tuple()
+  def parse(api_key, question_text, params \\ %{}) do
+    data =
+      @default_params
+      |> Map.merge(params)
+      |> Map.merge(%{
+        "messages" => [
+          %{
+            "role" => "system",
+            "content" => question_text
+          }
+        ]
+      })
+
+    middleware = [
+      Tesla.Middleware.JSON,
+      {Tesla.Middleware.Headers,
+       [{"Content-Type", "application/json"}, {"X-Goog-Api-Key", api_key}]}
+    ]
+
+    middleware
+    |> Tesla.client()
+    |> Tesla.post(@endpoint, data, opts: [adapter: [recv_timeout: 120_000]])
+    |> handle_response()
+  end
+
+  @spec handle_response(tuple()) :: tuple()
+  defp handle_response(response) do
+    response
+    |> case do
+      {:ok, %Tesla.Env{status: 200, body: %{"data" => %{"translations" => translations}}}} ->
+        {:ok, translations |> Enum.map(& &1["translatedText"])}
+
+      {:ok, %Tesla.Env{status: 200, body: body}} ->
+        {:error, "Unexpected response format: #{inspect(body)}"}
+
+      {_status, response} ->
+        {:error, "Invalid response: #{inspect(response)}"}
+    end
+  end
+
+  @doc """
+    Get the API key with existing configurations.
+  """
+  @spec get_api_key(non_neg_integer()) :: String.t()
+  def get_api_key(org_id) do
+    {:ok, %{api_key: api_key}} = credentials(org_id)
+    api_key
+  end
+
+  @spec credentials(non_neg_integer()) :: tuple()
+  defp credentials(org_id) do
+    organization = Partners.organization(org_id)
+
+    organization.services["google-translate"]
+    |> case do
+      nil ->
+        {:error, "Secret not found."}
+
+      credentials ->
+        {:ok, %{api_key: credentials.secrets["api_key"]}}
+    end
+  end
 end
