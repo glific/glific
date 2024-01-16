fbabd6f6fc921b54314ec00d1e9cea714cb8eb1d
AmishaBisht <amishabisht33@gmail.com>
2024-01-16 18:06:08 +0530

    first step in google translate integration

---
+++ lib/glific/flows/translate/google_translate.ex
@@ -0,0 +1,10 @@
+defmodule Glific.Flows.Translate.GoogleTranslate do
+  use GoogleApi.Translate
+  alias Glific.GoogleTranslate.Translate
+
+
+  @spec translate(String.t(), String.t(), String.t())
+  def translate(string, src, dst) do
+
+  end
+end
---
+++ lib/glific/third_party/google_translate/translate.ex
@@ -0,0 +1,8 @@
+defmodule Glific.GoogleTranslate.Translate do
+  @moduledoc """
+  Glific googleTranslate module for all API calls to googleTranslate
+  """
+
+
+@endpoint "https://translation.googleapis.com/language/translate/v2"
+end
