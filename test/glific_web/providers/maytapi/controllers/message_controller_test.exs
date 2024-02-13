60ef5996966ef9c2dfe5a977e871f8bac16ccb80
madclaws <pavanan.anandu63@gmail.com>
2024-02-13 11:44:09 +0530

    fix: test wip, checking out

--- test/glific_web/providers/maytapi/controllers/message_controller_test.exs
+++ test/glific_web/providers/maytapi/controllers/message_controller_test.exs
@@ -31,8 +31,8 @@ defmodule GlificWeb.Providers.Maytapi.Controllers.MessageControllerTest do
   }
 
   @text_message_webhook %{
-    "product_id" => "5351f38b-c0ae-49c4-9e43-427cb901b0f7",
-    "phone_id" => 42_908,
+    "product_id" => "ce2a5bf0-7a8d-4cc3-8202-a645dd5deccb",
+    "phone_id" => 1_150,
     "message" => %{
       "type" => "text",
       "text" => "It's like a mini-sprint- Almost half of the team is there",
@@ -51,14 +51,44 @@ defmodule GlificWeb.Providers.Maytapi.Controllers.MessageControllerTest do
     "timestamp" => 1_707_216_634,
     "type" => "message",
     "reply" =>
-      "https =>//api.maytapi.com/api/5351f38b-c0ae-49c4-9e43-427cb901b0f5/42906/sendMessage",
-    "productId" => "5351f38b-c0ae-49c4-9e43-427cb901b0f7",
-    "phoneId" => 42_908
+      "https =>//api.maytapi.com/api/5351f38b-c0ae-49c4-9e43-427cb901b0f5/1150/sendMessage",
+    "productId" => "ce2a5bf0-7a8d-4cc3-8202-a645dd5deccb",
+    "phoneId" => 1_150
+  }
+
+  # TODO Add a text handling message send by self.
+
+  @media_message_webhook %{
+      "product_id" => "ce2a5bf0-7a8d-4cc3-8202-a645dd5deccb",
+      "phone_id" => 1150,
+      "message" => %{
+        "type" => "image",
+        "url" => "https://cdnydm.com/wh/x7Yr1HQYy_m9RZ_xcJ6dw.jpeg?size=1280x960",
+        "mime" => "image/jpeg",
+        "filename" => "false_120363027326493365@g.us_0C623FCC2528444570C488FB229F7628_919917443994@c.us.jpeg",
+        "caption" => "",
+        "id" => "false_120363027326493365@g.us_0C623FCC2528444570C488FB229F7628_919917443994@c.us",
+        "_serialized" => "false_120363027326493365@g.us_0C623FCC2528444570C488FB229F7628_919917443994@c.us",
+        "fromMe" => false
+      },
+      "user" => %{
+        "id" => "919917443994@c.us",
+        "name" => "name_a",
+        "phone" => "919917443994"
+      },
+      "conversation" => "120363027326493365@g.us",
+      "conversation_name" => "Tech4Dev Team",
+      "receiver" => "919917443955",
+      "timestamp" => 1707216553,
+      "type" => "message",
+      "reply" => "https://api.maytapi.com/api/ce2a5bf0-7a8d-4cc3-8202-a645dd5deccb/1150/sendMessage",
+      "productId" => "ce2a5bf0-7a8d-4cc3-8202-a645dd5deccb",
+      "phoneId" => 1150
   }
 
   @text_message_webhook_invalid_group %{
-    "product_id" => "5351f38b-c0ae-49c4-9e43-427cb901b0f7",
-    "phone_id" => 42_908,
+    "product_id" => "ce2a5bf0-7a8d-4cc3-8202-a645dd5deccb",
+    "phone_id" => 1_150,
     "message" => %{
       "type" => "text",
       "text" => "It's like a mini-sprint- Almost half of the team is there",
@@ -77,9 +107,9 @@ defmodule GlificWeb.Providers.Maytapi.Controllers.MessageControllerTest do
     "timestamp" => 1_707_216_634,
     "type" => "message",
     "reply" =>
-      "https =>//api.maytapi.com/api/5351f38b-c0ae-49c4-9e43-427cb901b0f5/42906/sendMessage",
-    "productId" => "5351f38b-c0ae-49c4-9e43-427cb901b0f7",
-    "phoneId" => 42_908
+      "https =>//api.maytapi.com/api/5351f38b-c0ae-49c4-9e43-427cb901b0f5/1150/sendMessage",
+    "productId" => "ce2a5bf0-7a8d-4cc3-8202-a645dd5deccb",
+    "phoneId" => 1_150
   }
 
   setup do
@@ -199,6 +229,178 @@ defmodule GlificWeb.Providers.Maytapi.Controllers.MessageControllerTest do
       assert message.group.label == "Tech4Dev Team"
     end
 
+    test "Updating the contact_type to WABA+WA due to sender contact already existing", %{
+      conn: conn,
+      message_params: message_params
+    } do
+      conn_bak = conn
+      # handling a message from gupshup, so that the phone number will be already existing
+      # in contacts table.
+      conn = post(conn, "/gupshup", message_params)
+      assert conn.halted
+      bsp_message_id = get_in(message_params, ["payload", "id"])
+
+      {:ok, message} =
+        Repo.fetch_by(Message, %{
+          bsp_message_id: bsp_message_id,
+          organization_id: conn.assigns[:organization_id]
+        })
+
+      message = Repo.preload(message, [:receiver, :sender, :media])
+
+      # Provider message id should be updated
+      assert message.bsp_status == :delivered
+      assert message.flow == :inbound
+
+      # ensure the message has been received by the mock
+      assert_receive :received_message_to_process
+
+      assert message.sender.last_message_at != nil
+      assert true == Glific.in_past_time(message.sender.last_message_at, :seconds, 10)
+
+      # Sender should be stored into the db
+      assert message.sender.phone ==
+               get_in(message_params, ["payload", "sender", "phone"])
+
+      # handling text message from maytapi
+
+      text_webhook_params =
+        @text_message_webhook
+        |> put_in(["user", "phone"], get_in(message_params, ["payload", "sender", "phone"]))
+
+      conn = assign(conn_bak, :organization_id, 1)
+      conn = post(conn, "/maytapi", text_webhook_params)
+
+      assert conn.halted
+
+      bsp_message_id = get_in(text_webhook_params, ["message", "id"])
+
+      {:ok, message} =
+        Repo.fetch_by(Message, %{
+          bsp_message_id: bsp_message_id,
+          organization_id: conn.assigns[:organization_id]
+        })
+
+      message = Repo.preload(message, [:receiver, :sender, :media, :contact, :group])
+
+      # Provider message id should be updated
+      assert message.bsp_status == :delivered
+      assert message.flow == :inbound
+
+      # ensure the message has been received by the mock
+      assert_receive :received_message_to_process
+
+      assert message.sender.last_message_at != nil
+      assert true == Glific.in_past_time(message.sender.last_message_at, :seconds, 10)
+
+      # Sender should be stored into the db
+      assert message.sender.phone ==
+               get_in(text_webhook_params, ["user", "phone"])
+
+      assert message.contact.contact_type == "WABA+WA"
+      assert message.group.label == "Tech4Dev Team"
+    end
+
+    test "Incoming text message should be stored in the database, but group doesnt exist", %{
+      conn: conn
+    } do
+      conn = post(conn, "/maytapi", @text_message_webhook_invalid_group)
+      assert conn.halted
+
+      bsp_message_id = get_in(@text_message_webhook_invalid_group, ["message", "id"])
+
+      {:ok, message} =
+        Repo.fetch_by(Message, %{
+          bsp_message_id: bsp_message_id,
+          organization_id: conn.assigns[:organization_id]
+        })
+
+      message = Repo.preload(message, [:receiver, :sender, :media, :contact])
+
+      # Provider message id should be updated
+      assert message.bsp_status == :delivered
+      assert message.flow == :inbound
+
+      # ensure the message has been received by the mock
+      assert_receive :received_message_to_process
+
+      assert message.sender.last_message_at != nil
+      assert true == Glific.in_past_time(message.sender.last_message_at, :seconds, 10)
+
+      # Sender should be stored into the db
+      assert message.sender.phone ==
+               get_in(@text_message_webhook_invalid_group, ["user", "phone"])
+
+      # contact_type and message_type should be updated for wa groups
+      assert message.contact.contact_type == "WA"
+      assert message.message_type == "WA"
+      assert message.group_id == nil
+    end
+  end
+
+  describe "media" do
+    setup do
+      message_payload = %{
+        "text" => "Inbound Message"
+      }
+
+      message_params =
+        @message_request_params
+        |> put_in(["payload", "type"], "text")
+        |> put_in(["payload", "id"], Faker.String.base64(36))
+        |> put_in(["payload", "payload"], message_payload)
+
+      %{message_params: message_params}
+    end
+
+    @tag :maymedia
+    test "Incoming media message without phone should raise exception", %{conn: conn} do
+      media_msg_webhook = Map.delete(@media_message_webhook, "user")
+      assert_raise RuntimeError, fn -> post(conn, "/maytapi", media_msg_webhook) end
+
+      media_msg_webhook = put_in(@media_message_webhook, ["user", "phone"], nil)
+      assert_raise RuntimeError, fn -> post(conn, "/maytapi", media_msg_webhook) end
+
+      media_msg_webhook = put_in(@media_message_webhook, ["user", "phone"], "")
+      assert_raise RuntimeError, fn -> post(conn, "/maytapi", media_msg_webhook) end
+    end
+
+    @tag :skip
+    test "Incoming text message should be stored in the database, new contact", %{conn: conn} do
+      conn = post(conn, "/maytapi", @text_message_webhook)
+      assert conn.halted
+
+      bsp_message_id = get_in(@text_message_webhook, ["message", "id"])
+
+      {:ok, message} =
+        Repo.fetch_by(Message, %{
+          bsp_message_id: bsp_message_id,
+          organization_id: conn.assigns[:organization_id]
+        })
+
+      message = Repo.preload(message, [:receiver, :sender, :media, :contact, :group])
+
+      # Provider message id should be updated
+      assert message.bsp_status == :delivered
+      assert message.flow == :inbound
+
+      # ensure the message has been received by the mock
+      assert_receive :received_message_to_process
+
+      assert message.sender.last_message_at != nil
+      assert true == Glific.in_past_time(message.sender.last_message_at, :seconds, 10)
+
+      # Sender should be stored into the db
+      assert message.sender.phone ==
+               get_in(@text_message_webhook, ["user", "phone"])
+
+      # contact_type and message_type should be updated for wa groups
+      assert message.contact.contact_type == "WA"
+      assert message.message_type == "WA"
+      assert message.group.label == "Tech4Dev Team"
+    end
+
+    @tag :skip
     test "Updating the contact_type to WABA+WA due to sender contact already existing", %{
       conn: conn,
       message_params: message_params
