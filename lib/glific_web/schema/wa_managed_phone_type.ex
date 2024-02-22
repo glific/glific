4836c52d5db1853949d502321a4f4349e85ca5ce
Akhilesh Negi <akhileshnegi.an3@gmail.com>
2024-02-22 15:47:58 +0530

    WA Group: Trigger and APIs for wa_managed_phone (#3393)
    * first stab at creating wa_message trigger
    
    * trigger cleanups
    
    * adding API and resolver for wa_managed_phones
    
    * updating tables
    
    * updated testcases
    
    * credo fixes

---
+++ lib/glific_web/schema/wa_managed_phone_type.ex
@@ -0,0 +1,60 @@
+defmodule GlificWeb.Schema.WAManagedPhoneTypes do
+  @moduledoc """
+  GraphQL Representation of Glific's WAManagedPhone DataType
+  """
+  use Absinthe.Schema.Notation
+  import Absinthe.Resolution.Helpers
+
+  alias Glific.Repo
+  alias GlificWeb.Resolvers
+  alias GlificWeb.Schema.Middleware.Authorize
+
+  object :wa_managed_phone_result do
+    field :wa_managed_phone, :wa_managed_phone
+    field :errors, list_of(:input_error)
+  end
+
+  object :wa_managed_phone do
+    field :id, :id
+    field :phone, :string
+    field :phone_id, :integer
+    field :label, :string
+    field :is_active, :boolean
+    field :inserted_at, :datetime
+    field :updated_at, :datetime
+
+    field :contact, :contact do
+      resolve(dataloader(Repo, use_parent: true))
+    end
+
+    field :organization, :organization do
+      resolve(dataloader(Repo))
+    end
+  end
+
+  @desc "Filtering options for wa_managed_phones"
+  input_object :wa_managed_phone_filter do
+    @desc "Match the label"
+    field :label, :string
+
+    @desc "Match the phone"
+    field :phone, :string
+  end
+
+  object :wa_managed_phone_queries do
+    @desc "Get a list of all wa_managed_phones filtered by various criteria"
+    field :wa_managed_phones, list_of(:wa_managed_phone) do
+      arg(:filter, :wa_managed_phone_filter)
+      arg(:opts, :opts)
+      middleware(Authorize, :manager)
+      resolve(&Resolvers.WAManagedPhones.wa_managed_phones/3)
+    end
+
+    @desc "Get a count of all wa_managed_phones filtered by various criteria"
+    field :count_wa_managed_phones, :integer do
+      arg(:filter, :wa_managed_phone_filter)
+      middleware(Authorize, :manager)
+      resolve(&Resolvers.WAManagedPhones.count_wa_managed_phones/3)
+    end
+  end
+end
---
+++ priv/repo/migrations/20240222064744_add_trigger_for_wa_message_number.exs
@@ -0,0 +1,62 @@
+defmodule Glific.Repo.Migrations.AddTriggerForWaMessageNumber do
+  use Ecto.Migration
+
+  def change do
+    wa_message_before_insert_trigger()
+  end
+
+  defp wa_message_before_insert_trigger do
+    execute("""
+    CREATE OR REPLACE FUNCTION public.wa_message_before_insert_callback()
+    RETURNS trigger
+    LANGUAGE plpgsql
+    AS $function$
+    DECLARE now TIMESTAMP WITH TIME ZONE;
+    DECLARE var_message_number BIGINT;
+    DECLARE var_context_id BIGINT;
+    BEGIN
+      CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+      IF (TG_OP = 'INSERT') THEN
+        now := (CURRENT_TIMESTAMP at time zone 'utc');
+          SELECT last_message_number INTO var_message_number
+          FROM wa_groups WHERE organization_id = NEW.organization_id AND id = NEW.wa_group_id LIMIT 1;
+          var_message_number = var_message_number + 1;
+          IF (NEW.flow = 'inbound') THEN
+            IF (NEW.context_id IS NOT NULL) THEN
+              SELECT id INTO var_context_id
+              FROM wa_messages
+              WHERE bsp_id = NEW.context_id;
+              NEW.context_message_id = var_context_id;
+            END IF;
+            UPDATE wa_groups SET
+                last_communication_at = now,
+                last_message_number = var_message_number,
+                updated_at = now
+                WHERE id = NEW.wa_group_id;
+          ELSE
+            UPDATE wa_groups
+              SET
+                last_communication_at = now,
+                last_message_number = var_message_number,
+                updated_at = now
+              WHERE id = NEW.wa_group_id;
+          END IF;
+          NEW.message_number = var_message_number;
+        RETURN NEW;
+      END IF;
+      RETURN NEW;
+    END;
+    $function$
+    """)
+
+    execute("DROP TRIGGER IF EXISTS wa_message_before_insert_trigger ON wa_messages;")
+
+    execute("""
+    CREATE TRIGGER wa_message_before_insert_trigger
+    BEFORE INSERT
+    ON wa_messages
+    FOR EACH ROW
+    EXECUTE PROCEDURE wa_message_before_insert_callback();
+    """)
+  end
+end
