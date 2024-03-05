accb365d7b3e9f4d499a9421adb474e9236d7f22
AkhileshNegi <akhileshnegi.an3@gmail.com>
2024-03-05 13:13:33 +0530

    added system prompt and example text update support

--- lib/glific/third_party/open_llm/openLLM.ex
+++ lib/glific/third_party/open_llm/openLLM.ex
@@ -2,15 +2,16 @@ defmodule Glific.OpenLLM do
   @moduledoc """
   Glific OpenLLM module for all API calls to OpenLLM
   """
+
   alias Glific.Partners
 
-  @doc """
-  API call to OpenLLM
-  """
-  @spec parse(String.t(), String.t(), map()) :: tuple()
-  def parse(api_key, api_url, params) do
-    data = set_params(params)
+  use Tesla
 
+  @doc """
+  Making API call to OpenLLM  and adding Authorization token in header
+  """
+  @spec open_llm_post(String.t(), any(), String.t()) :: Tesla.Env.result()
+  def open_llm_post(url, payload, api_key) do
     middleware = [
       Tesla.Middleware.JSON,
       {Tesla.Middleware.Headers, [{"Authorization", api_key}]}
@@ -18,11 +19,22 @@ defmodule Glific.OpenLLM do
 
     middleware
     |> Tesla.client()
-    |> Tesla.post(
-      api_url,
-      data,
+    |> post(
+      url,
+      payload,
       opts: [adapter: [recv_timeout: 120_000]]
     )
+  end
+
+  @doc """
+  API call to OpenLLM
+  """
+  @spec parse(String.t(), String.t(), map()) :: tuple()
+  def parse(api_key, url, params) do
+    data = set_params(params)
+    chat_url = url <> "/api/chat"
+
+    open_llm_post(chat_url, data, api_key)
     |> handle_response()
   end
 
@@ -50,7 +62,7 @@ defmodule Glific.OpenLLM do
   @doc """
     Get the credentials for Open LLM with existing configurations.
   """
-  @spec get_credentials(non_neg_integer()) :: String.t()
+  @spec get_credentials(non_neg_integer()) :: {:ok, map()} | {:error, String.t()}
   def get_credentials(org_id) do
     organization = Partners.organization(org_id)
 
@@ -61,6 +73,29 @@ defmodule Glific.OpenLLM do
 
       credentials ->
         {:ok, %{api_key: credentials.secrets["api_key"], api_url: credentials.secrets["api_url"]}}
+    end
+  end
+
+  @doc """
+    Set system prompt for Open LLM with existing configurations.
+  """
+  @spec set_system_prompt(non_neg_integer(), String.t()) :: {:ok, map()} | {:error, String.t()}
+  def set_system_prompt(org_id, system_prompt) do
+    with {:ok, %{api_key: api_key, api_url: api_url}} <- get_credentials(org_id) do
+      url = api_url <> "/api/system_prompt"
+      open_llm_post(url, %{system_prompt: system_prompt}, api_key)
+    end
+  end
+
+  @doc """
+    Set examples text for Open LLM with existing configurations.
+    Glific.OpenLLM.set_examples_text(1, arc)
+  """
+  @spec set_examples_text(non_neg_integer(), String.t()) :: {:ok, map()} | {:error, String.t()}
+  def set_examples_text(org_id, examples_text) do
+    with {:ok, %{api_key: api_key, api_url: api_url}} <- get_credentials(org_id) do
+      url = api_url <> "/api/examples_text"
+      open_llm_post(url, %{examples_text: examples_text}, api_key)
     end
   end
 end
