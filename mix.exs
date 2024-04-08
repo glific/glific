4e54c526b71fc622c6347f1b9d52ef64d1b131b2
AkhileshNegi <akhileshnegi.an3@gmail.com>
2024-04-08 20:44:27 +0530

    updating mix version

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Glific.MixProject do
   def project do
     [
       app: :glific,
-      version: "6.0.1",
+      version: "6.0.2",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       dialyzer: [
