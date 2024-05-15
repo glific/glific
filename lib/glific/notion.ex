b86b06e6ce9cb346b4f99b3539d0ac862170baf7
madclaws <pavanan.anandu63@gmail.com>
2024-05-15 17:10:24 +0530

    creating row in notion db WIP

--- lib/glific/notion.ex
+++ lib/glific/notion.ex
@@ -1,4 +1,7 @@
 defmodule Glific.Notion do
+  alias Glific.Partners.Organization
+  alias Glific.Registrations.Registration
+
   @doc """
   Utilities to interact with Notion
   """
@@ -10,8 +13,8 @@ defmodule Glific.Notion do
 
   use Tesla
 
-  @spec headers(String.t()) :: list()
-  defp headers(token),
+  @spec headers() :: list()
+  defp headers(),
     do: [
       {"Content-Type", "application/json"},
       {"Authorization", "Bearer " <> Application.get_env(:glific, :google_translate)}
@@ -27,10 +30,84 @@ defmodule Glific.Notion do
     # create page
     # then hit post req
     # handle the response
+    with {:ok, %{"properties" => properties} = database} <- fetch_database() do
+    end
   end
 
   @spec fetch_database :: {:ok, map()} | {:error, String.t()}
   defp fetch_database do
+    (@notion_base_url <> "/databases/#{@ngo_database_id}")
+    |> get(headers: headers())
+    |> parse_response()
+  end
 
+  @spec create_page(map()) :: {:ok, map()} | {:error, String.t()}
+  defp create_page(properties) do
+    body = %{
+      parent: %{
+        database_id: @ngo_database_id
+      },
+      properties: properties
+    }
+
+    post(@notion_base_url <> "/pages", body)
+    |> parse_response()
+  end
+
+  @spec parse_response(Tesla.Env.result()) :: {:ok, map()} | {:error, String.t()}
+  defp parse_response({:ok, %{body: resp_body, status: status}})
+       when status >= 200 and status < 300 do
+    {:ok, resp_body}
+  end
+
+  defp parse_response({:ok, %{body: resp_body, status: status}}) do
+    {:error, inspect(resp_body)}
+  end
+
+  defp parse_response({:error, message}, _), do: {:error, inspect(message)}
+
+  defp create_table_properties(org, %Registration{} = registration) do
+    %{
+      "Org Name" => %{
+        "type" => "title",
+        "title" => [
+          %{"type" => "text", "text" => %{"content" => "#{registration.org_details["name"]}"}}
+        ]
+      },
+      "NGO POC email id" => %{
+        "type" => "rich_text",
+        "rich_text" => %{
+          "type" => "text",
+          "text" => %{"content" => "#{registration.org_details["email"]}"}
+        }
+      },
+      "Current office location- City" => %{
+        "type" => "rich_text",
+        "rich_text" => %{
+          "type" => "text",
+          "text" => %{"content" => "#{registration.org_details["current_address"]}"}
+        }
+      },
+      "Finance POC Details" => %{
+        "type" => "rich_text",
+        "rich_text" => %{
+          "type" => "text",
+          "text" => %{"content" => "#{convert_details_to_string(registration.finance_poc)}"}
+        }
+      },
+      "BOT number" => %{
+        "type" => "number",
+        "number" => "345"
+      }
+      # TODO: add the bot number
+    }
+  end
+
+  @spec convert_details_to_string(map()) :: String.t()
+  defp convert_details_to_string(details) do
+    details
+    |> Enum.reduce("", fn {k, v}, str ->
+      str <> "| #{k}: #{v} "
+    end)
   end
 end
