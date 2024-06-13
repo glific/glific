3fbb5bbf111ba06d966c79baf0c803130c1322bb
Anandu Pavanan <pavanan.anandu63@gmail.com>
2024-06-13 20:05:56 +0530

    WA Groups: Setting maytapi webhook api for org  (#3468)
    * chore: checking out
    
    * feat: added auto maytapi webhook setting
    
    * formatting
    
    * formatting

--- test/glific/groups/whatsapp_group_test.exs
+++ test/glific/groups/whatsapp_group_test.exs
@@ -73,26 +73,65 @@ defmodule Glific.Groups.WAGroupsTest do
     assert group.bsp_id == "120363218884368889@g.us"
   end
 
-  test "fetch_wa_groups/1 fetch groups for empty group label", attrs do
-    Tesla.Mock.mock(fn _env ->
-      %Tesla.Env{
-        status: 200,
-        body:
-          "{\"count\":79,\"data\":[{\"admins\":[\"917834811115@c.us\"],\"config\":{\"disappear\":false,\"edit\":\"all\",\"send\":\"all\"},\"id\":\"120363213149844251@g.us\",\"name\":\"marketing\",\"participants\":[\"917834811116@c.us\",\"917834811115@c.us\",\"917834811114@c.us\"]},{\"admins\":[\"917834811114@c.us\",\"917834811115@c.us\"],\"config\":{\"disappear\":false,\"edit\":\"all\",\"send\":\"all\"},\"id\":\"120363203450035277@g.us\",\"name\":\"admin group\",\"participants\":[\"917834811116@c.us\",\"917834811115@c.us\",\"917834811114@c.us\"]},{\"admins\":[\"917834811114@c.us\"],\"config\":{\"disappear\":false,\"edit\":\"all\",\"send\":\"all\"},\"id\":\"120363218884368888@g.us\",\"name\":\"\",\"participants\":[\"917834811114@c.us\"]}],\"limit\":500,\"success\":true,\"total\":79}"
-      }
+  test "setting maytapi webhook endpoint, success", attrs do
+    Tesla.Mock.mock(fn
+      %{method: :post} ->
+        %Tesla.Env{
+          status: 200,
+          body: %{
+            "pid" => "dc01968f-####-####-####-7cfcf51aa423",
+            "webhook" => "https://myserver.com/send/callback/here",
+            "ack_delivery" => true,
+            "phone_limit" => 2
+          }
+        }
     end)
 
-    assert :ok == WAGroups.fetch_wa_groups(attrs.organization_id)
-
-    assert {:ok, group} = Repo.fetch_by(WAGroup, %{label: "marketing"})
-    assert group.label == "marketing"
-    assert group.bsp_id == "120363213149844251@g.us"
-
-    assert {:ok, group} = Repo.fetch_by(WAGroup, %{label: "admin group"})
-    assert group.label == "admin group"
-    assert group.bsp_id == "120363203450035277@g.us"
-
-    # group with an empty name is not created
-    assert is_nil(Repo.get_by(WAGroup, label: ""))
+    assert :ok =
+             WAGroups.set_webhook_endpoint(%{
+               id: attrs.organization_id,
+               shortcode: "maytapi"
+             })
   end
+
+  test "setting maytapi webhook endpoint, failed", attrs do
+    Tesla.Mock.mock(fn
+      %{method: :post} ->
+        %Tesla.Env{
+          status: 400,
+          body: %{
+            "message" => "error"
+          }
+        }
+    end)
+
+    assert {:error, _} =
+             WAGroups.set_webhook_endpoint(%{
+               id: attrs.organization_id,
+               shortcode: "maytapi"
+             })
+
+  end
+    test "fetch_wa_groups/1 fetch groups for empty group label", attrs do
+      Tesla.Mock.mock(fn _env ->
+        %Tesla.Env{
+          status: 200,
+          body:
+            "{\"count\":79,\"data\":[{\"admins\":[\"917834811115@c.us\"],\"config\":{\"disappear\":false,\"edit\":\"all\",\"send\":\"all\"},\"id\":\"120363213149844251@g.us\",\"name\":\"marketing\",\"participants\":[\"917834811116@c.us\",\"917834811115@c.us\",\"917834811114@c.us\"]},{\"admins\":[\"917834811114@c.us\",\"917834811115@c.us\"],\"config\":{\"disappear\":false,\"edit\":\"all\",\"send\":\"all\"},\"id\":\"120363203450035277@g.us\",\"name\":\"admin group\",\"participants\":[\"917834811116@c.us\",\"917834811115@c.us\",\"917834811114@c.us\"]},{\"admins\":[\"917834811114@c.us\"],\"config\":{\"disappear\":false,\"edit\":\"all\",\"send\":\"all\"},\"id\":\"120363218884368888@g.us\",\"name\":\"\",\"participants\":[\"917834811114@c.us\"]}],\"limit\":500,\"success\":true,\"total\":79}"
+        }
+      end)
+
+      assert :ok == WAGroups.fetch_wa_groups(attrs.organization_id)
+
+      assert {:ok, group} = Repo.fetch_by(WAGroup, %{label: "marketing"})
+      assert group.label == "marketing"
+      assert group.bsp_id == "120363213149844251@g.us"
+
+      assert {:ok, group} = Repo.fetch_by(WAGroup, %{label: "admin group"})
+      assert group.label == "admin group"
+      assert group.bsp_id == "120363203450035277@g.us"
+
+      # group with an empty name is not created
+      assert is_nil(Repo.get_by(WAGroup, label: ""))
+    end
 end
