a00cd8c0903bfbd916a41d6b374f274df86c2cd4
AmishaBisht <amishabisht33@gmail.com>
2024-06-21 16:11:45 +0530

    import interactive template for quick reply with translations

--- lib/glific_web/schema/interactive_template_types.ex
+++ lib/glific_web/schema/interactive_template_types.ex
@@ -146,6 +146,7 @@ defmodule GlificWeb.Schema.InteractiveTemplateTypes do
 
     field :import_interactive_template, :interactive_template_result do
       arg(:id, non_null(:id))
+      arg(:translation, :string)
       middleware(Authorize, :manager)
       resolve(&Resolvers.InteractiveTemplates.import_interactive_template/3)
     end
