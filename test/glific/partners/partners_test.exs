a7d0436f57cdd056017441c76cad8b5c04318fb2
AkhileshNegi <akhileshnegi.an3@gmail.com>
2024-07-01 21:10:39 +0530

    updated testcases

--- test/glific/partners/partners_test.exs
+++ test/glific/partners/partners_test.exs
@@ -156,27 +156,29 @@ defmodule Glific.PartnersTest do
               Jason.encode!(%{
                 "partnerAppUsageList" => [
                   %{
-                    "appId" => "eea57ae3-0260-4596-8469-a5e1fbad2dde",
-                    "appName" => "testthursday",
-                    "authentication" => 2,
-                    "cumulativeBill" => 20.45,
-                    "date" => "2023-06-01",
-                    "discount" => 10.0,
-                    "fep" => 270,
-                    "ftc" => 16,
-                    "gsCap" => 500.0,
-                    "gsFees" => 43,
-                    "incomingMsg" => 172,
-                    "marketing" => 34,
-                    "outgoingMediaMsgSKU" => 32,
-                    "outgoingMsg" => 277,
-                    "service" => 23,
-                    "templateMediaMsgSKU" => 265,
-                    "templateMsg" => 143,
-                    "totalFees" => 87,
-                    "totalMsg" => 150,
-                    "utility" => 100,
-                    "waFees" => 44
+                    "appId" => "test-appID-e991-41c7-9fd7-b3cd58a8aedb",
+                    "appName" => "2023TestApp",
+                    "authentication" => 0,
+                    "cumulativeBill" => 0.089,
+                    "currency" => "USD",
+                    "date" => "2024-06-03",
+                    "discount" => 0.0,
+                    "fep" => 0,
+                    "ftc" => 4,
+                    "gsCap" => 75.0,
+                    "gsFees" => 0.064,
+                    "incomingMsg" => 27,
+                    "internationalAuthentication" => 0,
+                    "marketing" => 0,
+                    "outgoingMediaMsg" => 0,
+                    "outgoingMsg" => 37,
+                    "service" => 0,
+                    "templateMediaMsg" => 0,
+                    "templateMsg" => 0,
+                    "totalFees" => 0.064,
+                    "totalMsg" => 64,
+                    "utility" => 0,
+                    "waFees" => 0.0
                   }
                 ]
               })
@@ -184,7 +186,12 @@ defmodule Glific.PartnersTest do
       end)
 
       {:ok, result} = PartnerAPI.get_app_usage(org.id, "2022-03-01", "2022-04-01")
-      assert hd(result)["incomingMsg"] == 172
+      app_usage = get_in(result, [Access.at(0)])
+      assert app_usage["cumulativeBill"] == 0.089
+      assert app_usage["gsFees"] == 0.064
+      assert app_usage["incomingMsg"] == 27
+      assert app_usage["outgoingMsg"] == 37
+      assert app_usage["totalFees"] == 0.064
     end
 
     test "enable template messaging for an app" do
