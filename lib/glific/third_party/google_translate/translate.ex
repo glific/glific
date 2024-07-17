7cdf82826af2ec14a56d4bebafe29fa0c6a283fc
AmishaBisht <amishabisht33@gmail.com>
2024-07-17 18:10:10 +0530

    not translating the contact variables

--- lib/glific/third_party/google_translate/translate.ex
+++ lib/glific/third_party/google_translate/translate.ex
@@ -10,8 +10,17 @@ defmodule Glific.GoogleTranslate.Translate do
   """
   @spec parse(String.t(), String.t(), map()) :: tuple()
   def parse(api_key, question_text, languages) do
+    IO.inspect(question_text)
+    #contact variables are non_translatable
+    {translatable_segments, non_translatable_segments} =
+      question_text
+      |> String.split("\n")
+      |> Enum.split_with(&(!String.starts_with?(&1, "@results")))
+
+    translatable_text = Enum.join(translatable_segments, "\n")
+
     data = %{
-      "q" => question_text,
+      "q" => translatable_text,
       "source" => languages["source"],
       "target" => languages["target"],
       "format" => "text"
@@ -26,19 +35,28 @@ defmodule Glific.GoogleTranslate.Translate do
     middleware
     |> Tesla.client()
     |> Tesla.post(@endpoint, data, opts: [adapter: [recv_timeout: 120_000]])
-    |> handle_response()
+    |> handle_response(non_translatable_segments)
+    |> IO.inspect()
   end
 
-  @spec handle_response(tuple()) :: tuple()
-  defp handle_response(response) do
+  @spec handle_response(tuple(), list()) :: tuple()
+  defp handle_response(response, non_translatable_segments) do
     response
     |> case do
       {:ok, %Tesla.Env{status: 200, body: %{"data" => %{"translations" => translations}}}} ->
         translated_texts =
           translations
           |> Enum.map(fn translation -> translation["translatedText"] end)
+          |> Enum.join("\n")
 
-        {:ok, hd(translated_texts)}
+        # Combine translated and non-translatable segments
+        combined_texts =
+          non_translatable_segments
+          |> Enum.reduce(translated_texts, fn seg, acc ->
+            acc <> seg
+          end)
+
+        {:ok, combined_texts}
 
       {:ok, %Tesla.Env{status: 200, body: body}} ->
         {:error, "Unexpected response format: #{inspect(body)}"}
