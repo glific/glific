8c3bc9dd87c084b645014c264af1f1d45b77a4bc
AmishaBisht <amishabisht33@gmail.com>
2024-07-24 12:00:01 +0530

    refactoring flow in seeder

--- config/dev.exs
+++ config/dev.exs
@@ -30,8 +30,6 @@ config :glific, GlificWeb.Endpoint,
 # config :absinthe, Absinthe.Logger,
 #   pipeline: true,
 #   level: :debug
-config :logger,
-  level: :emergency
 
 # Watch static and templates for browser reloading.
 config :glific, GlificWeb.Endpoint,
