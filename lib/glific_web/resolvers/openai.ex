90fb4b78e0cce1c82c042d87eee4b8dc76d0d2c3
vgull574 <160291520+vgull574@users.noreply.github.com>
2024-08-18 22:31:58 -0500

    Resolved comments

--- lib/glific_web/resolvers/openai.ex
+++ lib/glific_web/resolvers/openai.ex
@@ -4,7 +4,7 @@ defmodule GlificWeb.Resolvers.OpenAI do
   one or more calls to resolve the incoming queries.
   """
 
-  alias Glific.OpenAI.ChatGPT
+  alias Glific.OpenAI
 
   @doc """
   Create a Vector Store
@@ -12,7 +12,7 @@ defmodule GlificWeb.Resolvers.OpenAI do
   @spec create_vector_store(Absinthe.Resolution.t(), map(), %{context: map()}) ::
           {:ok, map()} | {:error, any()}
   def create_vector_store(_, %{name: name} = _args, _),
-    do: ChatGPT.create_vector_store(name)
+    do: OpenAI.create_vector_store(name)
 
   @doc """
   Delete a Vector Store
@@ -20,7 +20,7 @@ defmodule GlificWeb.Resolvers.OpenAI do
   @spec delete_vector_store(Absinthe.Resolution.t(), map(), %{context: map()}) ::
           {:ok, map()} | {:error, any()}
   def delete_vector_store(_, %{vector_store_id: vector_store_id} = _args, _),
-    do: ChatGPT.delete_vector_store(vector_store_id)
+    do: OpenAI.delete_vector_store(vector_store_id)
 
   @doc """
   Create an assistant
@@ -28,7 +28,7 @@ defmodule GlificWeb.Resolvers.OpenAI do
   @spec create_assistant(Absinthe.Resolution.t(), map(), %{context: map()}) ::
           {:ok, map()} | {:error, any()}
   def create_assistant(_, args, _),
-    do: ChatGPT.create_assistant(args)
+    do: OpenAI.create_assistant(args)
 
   @doc """
   Delete Assistant
@@ -36,5 +36,5 @@ defmodule GlificWeb.Resolvers.OpenAI do
   @spec delete_assistant(Absinthe.Resolution.t(), map(), %{context: map()}) ::
           {:ok, map()} | {:error, any()}
   def delete_assistant(_, %{assistant_id: assistant_id} = _args, _),
-    do: ChatGPT.delete_assistant(assistant_id)
+    do: OpenAI.delete_assistant(assistant_id)
 end
