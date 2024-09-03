d6f8a1b78ea8097b82d6d9ab3db5f0aa855bc333
AmishaBisht <amishabisht33@gmail.com>
2024-09-03 12:20:42 +0530

    allowing the hinding content in template

--- lib/glific/templates.ex
+++ lib/glific/templates.ex
@@ -211,7 +211,10 @@ defmodule Glific.Templates do
   end
 
   defp contains_emoji?(text) do
-    Regex.match?(~r/[^a-zA-Z0-9\s]/, text)
+    emoji_regex =
+      ~r/[\x{1F600}-\x{1F64F}|\x{1F300}-\x{1F5FF}|\x{1F680}-\x{1F6FF}|\x{1F700}-\x{1F77F}|\x{1F780}-\x{1F7FF}|\x{1F800}-\x{1F8FF}|\x{1F900}-\x{1F9FF}|\x{1FA00}-\x{1FA6F}|\x{1FA70}-\x{1FAFF}|\x{2600}-\x{26FF}|\x{2700}-\x{27BF}|\x{1F1E6}-\x{1F1FF}]/u
+
+    Regex.match?(emoji_regex, text)
   end
 
   defp contains_formatting?(text) do
