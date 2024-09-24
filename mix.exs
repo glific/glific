ee4c1f9d25932b41eaade1794b306d30ece75bee
madclaws <pavanan.anandu63@gmail.com>
2024-09-24 08:06:25 +0530

    build: bumo version to 6.1.13

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Glific.MixProject do
   def project do
     [
       app: :glific,
-      version: "6.1.12",
+      version: "6.1.13",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       dialyzer: [
