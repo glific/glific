63823ca04ecd6649a3100fe8238cbb0fbee278f0
AmishaBisht <amishabisht33@gmail.com>
2025-01-07 15:40:06 +0530

    validate options

--- lib/glific/wa_poll.ex
+++ lib/glific/wa_poll.ex
@@ -19,8 +19,32 @@ defmodule Glific.WaPoll do
   @spec create_wa_poll(map()) ::
           {:ok, WaPoll.t()} | {:error, Ecto.Changeset.t()}
   def create_wa_poll(attrs) do
-    %WaPoll{}
-    |> WaPoll.changeset(attrs)
-    |> Repo.insert()
+    with :ok <-
+           validate_options(attrs) do
+      %WaPoll{}
+      |> WaPoll.changeset(attrs)
+      |> Repo.insert()
+    end
+  end
+
+  @spec validate_options(map()) :: :ok | {:error, String.t()}
+  defp validate_options(attrs) do
+    options = attrs[:poll_content]["options"]
+    options_count = length(options)
+
+    # Check if there are more than 12 options
+    if options_count > 12 do
+      {:error, "The number of options should be up to 12 only, but got #{options_count}."}
+    else
+      # Check for duplicate option names
+      option_names = Enum.map(options, fn option -> option["name"] end)
+      duplicates = option_names -- Enum.uniq(option_names)
+
+      if duplicates != [] do
+        {:error, "Duplicate options detected: #{Enum.join(duplicates, ", ")}."}
+      else
+        :ok
+      end
+    end
   end
 end
