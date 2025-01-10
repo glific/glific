cae58e2a08d6a016e178de4da5434307f10ac3c8
Donald A. Lobo <donald.lobo@gmail.com>
2025-01-09 18:44:49 -0800

    lets ensure we do migrations, fix dialyzer

--- priv/repo/migrations/20250109212933_add_oban_pro.exs
+++ priv/repo/migrations/20250109212933_add_oban_pro.exs
@@ -1,7 +1,7 @@
 defmodule Glific.Repo.Migrations.AddObanPro do
   use Ecto.Migration
 
-  def up, do: Oban.Pro.Migration.up()
+  def up, do: Oban.Pro.Migration.up(prefix: "global")
 
-  def down, do: Oban.Pro.Migration.down()
+  def down, do: Oban.Pro.Migration.down(prefix: "global")
 end
--- priv/repo/migrations/20250109213637_upgrade_oban_jobs_to_v12.exs
+++ priv/repo/migrations/20250109213637_upgrade_oban_jobs_to_v12.exs
@@ -2,12 +2,12 @@ defmodule Glific.Repo.Migrations.UpgradeObanJobsToV12 do
   use Ecto.Migration
 
   def up do
-    Oban.Migration.up(version: 12)
+    Oban.Migration.up(version: 12, prefix: "golbal")
   end
 
   # We specify `version: 1` in `down`, ensuring that we'll roll all the way back down if
   # necessary, regardless of which version we've migrated `up` to.
   def down do
-    Oban.Migration.down(version: 1)
+    Oban.Migration.down(version: 1, prefix: "golbal")
   end
 end
