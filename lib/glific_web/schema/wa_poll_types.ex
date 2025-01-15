82b57cfd05d097d516bfb56290c48e203b39b5f2
Amisha Bisht <amishabisht33@gmail.com>
2025-01-15 11:56:44 +0530

    Wa groups: wa poll APIs (#3977)
    * create wa_polls table
    
    * api for create wa poll
    
    * adding support for multiple answers
    
    * validate options
    
    * add schema test cases
    
    * add function test case
    
    * refactor
    
    * fetch wa poll by id api
    
    * refactoring
    
    * refactoring
    
    * refactoring
    
    * refactoring
    
    * refactoring
    
    * refactoring
    
    * refactoring
    
    * listing api with filters
    
    * listing api with filters
    
    * listing api with filters
    
    * wa_groups: create wa_polls API (#3972)
    
    * create wa_polls table
    
    * api for create wa poll
    
    * adding support for multiple answers
    
    * validate options
    
    * add schema test cases
    
    * add function test case
    
    * refactor
    
    * refactoring
    
    * refactoring
    
    * refactoring
    
    * api for create wa poll
    
    * add schema test cases
    
    * add function test case
    
    * refactoring
    
    * validate options
    
    * refactoring
    
    * add apis and test case
    
    * add apis and test case
    
    * add apis and test case
    
    * fix: minor typo
    
    ---------
    
    Co-authored-by: madclaws <pavanan.anandu63@gmail.com>

--- lib/glific_web/schema/wa_poll_types.ex
+++ lib/glific_web/schema/wa_poll_types.ex
@@ -22,6 +22,15 @@ defmodule GlificWeb.Schema.WaPollTypes do
     field :errors, list_of(:input_error)
   end
 
+  @desc "Filtering options for wa polls"
+  input_object :wa_poll_filter do
+    @desc "Match the label"
+    field(:label, :string)
+
+    @desc "Match the allow multiple answer"
+    field(:allow_multiple_answer, :boolean)
+  end
+
   input_object :wa_poll_input do
     field :id, :id
     field :label, :string
@@ -32,11 +41,48 @@ defmodule GlificWeb.Schema.WaPollTypes do
     field :updated_at, :datetime
   end
 
+  object :wa_poll_queries do
+    @desc "get the details of one whatsapp poll"
+    field :wa_poll, :wa_poll_result do
+      arg(:id, non_null(:id))
+      middleware(Authorize, :staff)
+      resolve(&Resolvers.WaPoll.wa_poll/3)
+    end
+
+    @desc "Get a list of all wa polls filtered by various criteria"
+    field :wa_polls, list_of(:wa_poll) do
+      arg(:filter, :wa_poll_filter)
+      arg(:opts, :opts)
+      middleware(Authorize, :staff)
+      resolve(&Resolvers.WaPoll.wa_polls/3)
+    end
+
+    @desc "Get a count of all wa polls filtered by various criteria"
+    field :count_wa_polls, :integer do
+      arg(:filter, :wa_poll_filter)
+      middleware(Authorize, :manager)
+      resolve(&Resolvers.WaPoll.count_wa_polls/3)
+    end
+  end
+
   object :wa_poll_mutations do
     field :create_wa_poll, :wa_poll_result do
       arg(:input, non_null(:wa_poll_input))
       middleware(Authorize, :manager)
       resolve(&Resolvers.WaPoll.create_wa_poll/3)
+    end
+
+    field :delete_wa_poll, :wa_poll_result do
+      arg(:id, non_null(:id))
+      middleware(Authorize, :manager)
+      resolve(&Resolvers.WaPoll.delete_wa_poll/3)
+    end
+
+    field :copy_wa_poll, :wa_poll_result do
+      arg(:id, non_null(:id))
+      arg(:input, :wa_poll_input)
+      middleware(Authorize, :manager)
+      resolve(&Resolvers.WaPoll.copy_wa_poll/3)
     end
   end
 end
--- test/glific_web/schema/wa_poll_test.exs
+++ test/glific_web/schema/wa_poll_test.exs
@@ -2,7 +2,14 @@ defmodule GlificWeb.Schema.WAPollTest do
   use GlificWeb.ConnCase
   use Wormwood.GQLCase
 
+  alias Glific.{Repo, WaGroup.WaPoll}
+
   load_gql(:create, GlificWeb.Schema, "assets/gql/wa_poll/create.gql")
+  load_gql(:fetch, GlificWeb.Schema, "assets/gql/wa_poll/fetch.gql")
+  load_gql(:copy, GlificWeb.Schema, "assets/gql/wa_poll/copy.gql")
+  load_gql(:delete, GlificWeb.Schema, "assets/gql/wa_poll/delete.gql")
+  load_gql(:list, GlificWeb.Schema, "assets/gql/wa_poll/list.gql")
+  load_gql(:count, GlificWeb.Schema, "assets/gql/wa_poll/count.gql")
 
   test "create a poll and test possible scenarios", %{manager: user} do
     result =
@@ -22,7 +29,7 @@ defmodule GlificWeb.Schema.WAPollTest do
 
     assert wa_poll["label"] == "Test Poll"
 
-    # by default the allow multiple answer should be false
+    # by default the allow multiple answers should be false
     assert wa_poll["allowMultipleAnswer"] == false
 
     # allow multiple answer should be true
@@ -80,5 +87,201 @@ defmodule GlificWeb.Schema.WAPollTest do
 
     assert {:ok, result_data} = result
     assert result_data |> get_in([:data, "CreateWaPoll"]) == nil
+  end
+
+  test "fetch the wa_poll using id", %{manager: user} do
+    result =
+      auth_query_gql_by(:create, user,
+        variables: %{
+          "input" => %{
+            "label" => "Test Poll",
+            "poll_content" =>
+              "{\"options\":[{\"id\":0,\"name\":\"okay\",\"voters\":[],\"votes\":0},{\"id\":1,
+              \"name\":\"huh\",\"voters\":[],\"votes\":0}],\"text\":\"testing poll\"}"
+          }
+        }
+      )
+
+    assert {:ok, query_data} = result
+    wa_poll = query_data |> get_in([:data, "CreateWaPoll", "waPoll"])
+    label = wa_poll["label"]
+
+    {:ok, wa_poll} =
+      Repo.fetch_by(WaPoll, %{label: label, organization_id: user.organization_id})
+
+    result = auth_query_gql_by(:fetch, user, variables: %{"id" => wa_poll.id})
+
+    assert {:ok, query_data} = result
+    fetched_wa_poll = query_data |> get_in([:data, "waPoll", "waPoll"])
+    assert fetched_wa_poll["label"] == label
+  end
+
+  test "count wa_polls with and without filters", %{manager: user} do
+    for i <- 1..3 do
+      auth_query_gql_by(:create, user,
+        variables: %{
+          "input" => %{
+            "label" => "Test Poll #{i}",
+            "poll_content" =>
+              "{\"options\":[{\"id\":0,\"name\":\"option1\",\"voters\":[],\"votes\":0}],\"text\":\"poll #{i}\"}",
+            "allow_multiple_answer" => i == 3
+          }
+        }
+      )
+    end
+
+    # Count all polls without filters
+    {:ok, query_data} = auth_query_gql_by(:count, user, variables: %{})
+    total_count = query_data |> get_in([:data, "countWaPolls"])
+    assert total_count == 3
+
+    # Filter polls by label
+    {:ok, query_data} =
+      auth_query_gql_by(:count, user,
+        variables: %{
+          "filter" => %{"label" => "Test Poll 1"}
+        }
+      )
+
+    filtered_count = query_data |> get_in([:data, "countWaPolls"])
+    assert filtered_count == 1
+
+    # Filter polls by `allow_multiple_answer`
+    {:ok, query_data} =
+      auth_query_gql_by(:count, user,
+        variables: %{
+          "filter" => %{"allow_multiple_answer" => true}
+        }
+      )
+
+    filtered_count = query_data |> get_in([:data, "countWaPolls"])
+    assert filtered_count == 1
+
+    # Fetch polls with invalid filter
+    {:ok, query_data} =
+      auth_query_gql_by(:count, user,
+        variables: %{
+          "filter" => %{"label" => "Nonexistent Poll"}
+        }
+      )
+
+    filtered_count = query_data |> get_in([:data, "countWaPolls"])
+    assert filtered_count == 0
+  end
+
+  test "list wa_polls with and without filters", %{manager: user} do
+    for i <- 1..3 do
+      auth_query_gql_by(:create, user,
+        variables: %{
+          "input" => %{
+            "label" => "Test Poll #{i}",
+            "poll_content" =>
+              "{\"options\":[{\"id\":0,\"name\":\"option1\",\"voters\":[],\"votes\":0}],\"text\":\"poll #{i}\"}",
+            "allow_multiple_answer" => i == 3
+          }
+        }
+      )
+    end
+
+    # Fetch all polls without filters
+    result = auth_query_gql_by(:list, user, variables: %{})
+    assert {:ok, query_data} = result
+
+    polls = query_data |> get_in([:data, "waPolls"])
+    assert length(polls) == 3
+
+    # Filter polls by label
+    result =
+      auth_query_gql_by(:list, user,
+        variables: %{
+          "filter" => %{"label" => "Test Poll 1"}
+        }
+      )
+
+    assert {:ok, query_data} = result
+    filtered_polls = query_data |> get_in([:data, "waPolls"])
+    assert length(filtered_polls) == 1
+    assert hd(filtered_polls)["label"] == "Test Poll 1"
+
+    # Filter polls by `allow_multiple_answer`
+    result =
+      auth_query_gql_by(:list, user,
+        variables: %{
+          "filter" => %{"allow_multiple_answer" => true}
+        }
+      )
+
+    assert {:ok, query_data} = result
+    filtered_polls = query_data |> get_in([:data, "waPolls"])
+    assert length(filtered_polls) == 1
+    assert hd(filtered_polls)["label"] == "Test Poll 3"
+
+    # Fetch polls with invalid filter
+    result =
+      auth_query_gql_by(:list, user,
+        variables: %{
+          "filter" => %{"label" => "Nonexistent Poll"}
+        }
+      )
+
+    assert {:ok, query_data} = result
+    filtered_polls = query_data |> get_in([:data, "waPolls"])
+    assert Enum.empty?(filtered_polls) == true
+  end
+
+  test "copy an existing wa_poll", %{manager: user} do
+    result =
+      auth_query_gql_by(:create, user,
+        variables: %{
+          "input" => %{
+            "label" => "Poll to Copy",
+            "poll_content" =>
+              "{\"options\":[{\"id\":0,\"name\":\"opt1\",\"voters\":[],\"votes\":0}],\"text\":\"poll to copy\"}"
+          }
+        }
+      )
+
+    assert {:ok, query_data} = result
+    wa_poll = query_data |> get_in([:data, "CreateWaPoll", "waPoll"])
+
+    result =
+      auth_query_gql_by(:copy, user,
+        variables: %{
+          "copyWaPollId" => wa_poll["id"],
+          "input" => %{"label" => "Copied Poll"}
+        }
+      )
+
+    assert {:ok, query_data} = result
+    copied_wa_poll = query_data |> get_in([:data, "copyWaPoll", "waPoll"])
+    assert copied_wa_poll["label"] == "Copied Poll"
+  end
+
+  test "delete an existing wa_poll and possible scenarios", %{manager: user} do
+    result =
+      auth_query_gql_by(:create, user,
+        variables: %{
+          "input" => %{
+            "label" => "Poll to Delete",
+            "poll_content" =>
+              "{\"options\":[{\"id\":0,\"name\":\"delete_opt\",\"voters\":[],\"votes\":0}],\"text\":\"poll to delete\"}"
+          }
+        }
+      )
+
+    assert {:ok, query_data} = result
+    wa_poll = query_data |> get_in([:data, "CreateWaPoll", "waPoll"])
+
+    result = auth_query_gql_by(:delete, user, variables: %{"deleteWaPollId" => wa_poll["id"]})
+
+    assert {:ok, query_data} = result
+    assert get_in(query_data, [:data, "deleteWaPoll", "errors"]) == nil
+    result = auth_query_gql_by(:delete, user, variables: %{"deleteWaPollId" => 123_444})
+    assert {:ok, query_data} = result
+
+    message =
+      get_in(query_data, [:data, "deleteWaPoll", "errors", Access.at(0), "message"])
+
+    assert message == "Resource not found"
   end
 end
