b7da47c62dd72ff2a5d7a6c3fc899356ec4838db
AkhileshNegi <akhileshnegi.an3@gmail.com>
2025-02-22 11:53:37 +0530

    setting up flow

--- test/glific_web/flows/flow_resume_controller_test.exs
+++ test/glific_web/flows/flow_resume_controller_test.exs
@@ -2,7 +2,24 @@ defmodule GlificWeb.Flows.FlowResumeControllerTest do
   use GlificWeb.ConnCase
 
   # alias GlificWeb.Flows.FlowResumeController
-  alias Glific.Seeds.SeedsDev
+  alias Glific.{
+    Fixtures,
+    Messages,
+    Repo,
+    Seeds.SeedsDev
+  }
+
+  alias Glific.Flows.{
+    Flow,
+    FlowContext
+  }
+
+  @valid_attrs %{
+    flow_id: 1,
+    flow_uuid: Ecto.UUID.generate(),
+    uuid_map: %{},
+    node_uuid: Ecto.UUID.generate()
+  }
 
   setup do
     SeedsDev.seed_organizations()
@@ -24,9 +41,25 @@ defmodule GlificWeb.Flows.FlowResumeControllerTest do
         "organization_id" => organization_id,
         "signature" => "b0bbfcc5e80830da9ce8603a2f1ee4089723544d44d748fa7fc73ca5106e6bf6",
         "status" => "success",
-        "thread_id" => "thread_yJxZazJ0bcXvFAF2TLIgM3VX",
+        "thread_id" => "thread_yJxZazJ0bcXvFAsglific",
         "timestamp" => 1_740_203_086_735_839
       }
+
+      flow = Flow.get_loaded_flow(organization_id, "published", %{keyword: "help"})
+      [node | _tail] = flow.nodes
+
+      message = Messages.create_temp_message(organization_id, "1")
+      wakeup_at = Timex.shift(Timex.now(), minutes: +3)
+
+      flow_context =
+        flow_context_fixture(%{
+          node_uuid: node.uuid,
+          is_await_result: true,
+          wakeup_at: wakeup_at,
+          is_background_flow: true,
+          flow_uuid: flow.uuid,
+          flow_id: flow.id
+        })
 
       conn =
         conn
@@ -34,5 +67,20 @@ defmodule GlificWeb.Flows.FlowResumeControllerTest do
 
       assert json_response(conn, 200) == ""
     end
+  end
+
+  def flow_context_fixture(attrs \\ %{}) do
+    contact = Fixtures.contact_fixture()
+
+    {:ok, flow_context} =
+      attrs
+      |> Map.put(:contact_id, contact.id)
+      |> Map.put(:organization_id, contact.organization_id)
+      |> Enum.into(@valid_attrs)
+      |> FlowContext.create_flow_context()
+
+    flow_context
+    |> Repo.preload(:contact)
+    |> Repo.preload(:flow)
   end
 end
