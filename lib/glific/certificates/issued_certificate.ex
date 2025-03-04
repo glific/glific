8c39a9106bb24b37a878c9cbd8a2025088d17491
madclaws <pavanan.anandu63@gmail.com>
2025-03-05 03:56:24 +0530

    feat: Added issued_certificate schema

---
+++ lib/glific/certificates/issued_certificate.ex
@@ -0,0 +1,77 @@
+defmodule Glific.Certificates.IssuedCertificate do
+  @moduledoc false
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Glific.Repo
+  alias Glific.Certificates.IssuedCertificate
+
+  alias Glific.{
+    Certificates.CertificateTemplate,
+    Contacts.Contact,
+    Partners.Organization
+  }
+
+  @required_fields [
+    :certificate_template_id,
+    :contact_id,
+    :organization_id
+  ]
+
+  @optional_fields [
+    :gcs_url,
+    :errors,
+    :status
+  ]
+
+  @type t() :: %__MODULE__{
+          __meta__: Ecto.Schema.Metadata.t(),
+          id: non_neg_integer | nil,
+          certificate_template_id: non_neg_integer() | nil,
+          certificate_template: CertificateTemplate.t() | nil,
+          contact_id: non_neg_integer() | nil,
+          contact: Contact.t() | nil,
+          gcs_url: String.t() | nil,
+          errors: map() | nil,
+          status: String.t() | nil,
+          organization_id: non_neg_integer | nil,
+          organization: Organization.t() | Ecto.Association.NotLoaded.t() | nil,
+          inserted_at: :utc_datetime | nil,
+          updated_at: :utc_datetime | nil
+        }
+
+  schema "issued_certificates" do
+    field :gcs_url, :string
+    field :errors, :map
+    field :status, :string
+    belongs_to :certificate_template, CertificateTemplate
+    belongs_to :contact, Contact
+    belongs_to :organization, Organization
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc """
+  Standard changeset pattern we use for all data types
+  """
+  @spec changeset(CertificateTemplate.t(), map()) :: Ecto.Changeset.t()
+  def changeset(wa_poll, attrs) do
+    wa_poll
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+    |> foreign_key_constraint(:organization_id)
+    |> foreign_key_constraint(:contact_id)
+    |> foreign_key_constraint(:certificate_template_id)
+  end
+
+  @doc """
+  Creates an certificate_template
+  """
+  @spec create_issued_certificate(map()) ::
+          {:ok, IssuedCertificate.t()} | {:error, Ecto.Changeset.t()}
+  def create_issued_certificate(attrs) do
+    %IssuedCertificate{}
+    |> changeset(attrs)
+    |> Repo.insert()
+  end
+end
--- priv/repo/migrations/20250227212728_certificate_templates.exs
+++ priv/repo/migrations/20250227212728_certificate_templates.exs
@@ -3,7 +3,12 @@ defmodule Glific.Repo.Migrations.CertificateTemplates do
 
   def change do
     Glific.Enums.CertificateTemplateType.create_type()
+    create_certificate_template()
+    create_issued_certfifcates()
+  end
 
+  @spec create_certificate_template() :: any()
+  defp create_certificate_template() do
     create table(:certificate_templates) do
       add :label, :string, null: false, comment: "Title of the certificate template"
       add :url, :string, null: false, comment: "Url of the certificate template"
@@ -21,5 +26,32 @@ defmodule Glific.Repo.Migrations.CertificateTemplates do
     end
 
     create unique_index(:certificate_templates, [:label, :organization_id])
+  end
+
+  defp create_issued_certfifcates do
+    create table(:issued_certificates) do
+      # we doing cascade delete, since we sync these tables to BQ, so will be available for tracking
+      add :certificate_template_id, references(:certificate_templates, on_delete: :delete_all),
+        null: false,
+        comment: "Unique certificate template ID"
+
+      add :contact_id, references(:contacts, on_delete: :delete_all),
+        null: false,
+        comment: "Unique contact ID"
+
+      add :gcs_url, :string, comment: "GCS url of the final generated certificate"
+
+      add :errors, :map, default: %{}, comment: "Errors during certificate generation"
+
+      add :status, :string,
+        comment:
+          "Incremental status while certificate generation, available options are: copied_to_drive, replaced_text, thumbnail_created, gcs_uploaded"
+
+      add :organization_id, references(:organizations, on_delete: :delete_all),
+        null: false,
+        comment: "Unique organization ID."
+
+      timestamps(type: :utc_datetime)
+    end
   end
 end
