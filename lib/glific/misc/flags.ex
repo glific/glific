9bc0428001cab6aad7f5c65961ca9421d2aae86d
Anandu Pavanan <pavanan.anandu63@gmail.com>
2025-03-20 05:03:06 +0530

    Custom Certificates: Added feature flag, metrics and tables to BQ  (#4072)
    * wip: feature flag
    
    * feat: Added feature-flags
    
    * feat: Added certificate_issued metric
    
    * refactor

--- lib/glific/misc/flags.ex
+++ lib/glific/misc/flags.ex
@@ -211,6 +211,27 @@ defmodule Glific.Flags do
   end
 
   @doc """
+  Get custom certificate value for organization flag
+  """
+  @spec get_custom_certificate_enabled(map()) :: boolean
+  def get_custom_certificate_enabled(organization) do
+    app_env = Application.get_env(:glific, :environment)
+
+    cond do
+      FunWithFlags.enabled?(:is_custom_certificate_enabled,
+        for: %{organization_id: organization.id}
+      ) ->
+        true
+
+      Glific.trusted_env?(app_env, organization.id) ->
+        true
+
+      true ->
+        false
+    end
+  end
+
+  @doc """
   Get OpenAI auto translation value for organization flag
   """
   @spec get_open_ai_auto_translation_enabled(map()) :: boolean
@@ -259,6 +280,18 @@ defmodule Glific.Flags do
       organization,
       :is_whatsapp_group_enabled,
       get_whatsapp_group_enabled(organization)
+    )
+  end
+
+  @doc """
+  Set fun_with_flag toggle forcustom certificate for an organization
+  """
+  @spec set_custom_certificate_enabled(map()) :: map()
+  def set_custom_certificate_enabled(organization) do
+    Map.put(
+      organization,
+      :is_custom_certificate_enabled,
+      get_custom_certificate_enabled(organization)
     )
   end
 
@@ -349,7 +382,8 @@ defmodule Glific.Flags do
       :is_ticketing_enabled,
       :is_open_ai_auto_translation_enabled,
       :is_google_auto_translation_enabled,
-      :is_whatsapp_group_enabled
+      :is_whatsapp_group_enabled,
+      :is_custom_certificate_enabled
     ]
     |> Enum.each(fn flag ->
       if !FunWithFlags.enabled?(
--- lib/glific/partners/organization.ex
+++ lib/glific/partners/organization.ex
@@ -171,6 +171,9 @@ defmodule Glific.Partners.Organization do
     # A virtual field for now to conditionally enable whatsapp group feature for an organization
     field(:is_whatsapp_group_enabled, :boolean, default: false, virtual: true)
 
+    # A virtual field for now to conditionally enable custom certificate feature for an organization
+    field(:is_custom_certificate_enabled, :boolean, default: false, virtual: true)
+
     timestamps(type: :utc_datetime)
   end
 
