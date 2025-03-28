c3b9cab0e973577b9cb2c3145a0b476190f0a50f
madclaws <pavanan.anandu63@gmail.com>
2025-03-28 18:13:38 +0530

    fix: Added strict slides url validation

--- lib/glific/third_party/google_slide/slide.ex
+++ lib/glific/third_party/google_slide/slide.ex
@@ -57,6 +57,36 @@ defmodule Glific.ThirdParty.GoogleSlide.Slide do
     end
   end
 
+  @doc """
+  Parses the slides url
+
+  ## Examples
+      iex> Glific.ThirdParty.GoogleSlide.Slide.parse_slides_url("https://docs.google.com/presentation/d/1aF1ldS4zjEHmM4LqHfGEW7TfGacBL4dyBfgIadp4/edit#slide=id.p")
+      {:error, "Invalid url"}
+      iex> Glific.ThirdParty.GoogleSlide.Slide.parse_slides_url("https://docs.google.com/presentation/d/1UllxeYCFhetMS6_WwkmuKeWRiwbttXfJU1bp22aiaOk/edit#slide=id.g33e095ed7be_2_75")
+      {:ok, %{presentation_id: "1UllxeYCFhetMS6_WwkmuKeWRiwbttXfJU1bp22aiaOk", page_id: "g33e095ed7be_2_75"}}
+      iex> Glific.ThirdParty.GoogleSlide.Slide.parse_slides_url("https://docs.google.com/presentation/d/1UllxeYCFhetMS6_WwkmuKeWRiwbttXfJU1bp22aiaOk/edit#slide=id.g33e095ed7be_2_75")
+      {:ok, %{presentation_id: "1UllxeYCFhetMS6_WwkmuKeWRiwbttXfJU1bp22aiaOk", page_id: "g33e095ed7be_2_75"}}
+      iex> Glific.ThirdParty.GoogleSlide.Slide.parse_slides_url("https://docs.google.com/presentation/d/1qHobEu0HbRHpiHJbqAEvu_M13YvJ1qWIWiAcWXXXBFk/edit?slide=id.g33e0d0c1143_0_0#slide=id.g33e0d0c1143_0_0")
+      {:ok, %{presentation_id: "1qHobEu0HbRHpiHJbqAEvu_M13YvJ1qWIWiAcWXXXBFk", page_id: "g33e0d0c1143_0_0"}}
+      iex> Glific.ThirdParty.GoogleSlide.Slide.parse_slides_url("https://docs.google.com/presentation/d/1qHobEu0HbRHpiHJbqAEvu_M13YvJ1qWIWiAcWXXXBFk/edit?slide=id.g33e0d0c1143_0_0")
+      {:ok, %{presentation_id: "1qHobEu0HbRHpiHJbqAEvu_M13YvJ1qWIWiAcWXXXBFk", page_id: "g33e0d0c1143_0_0"}}
+  """
+  @spec parse_slides_url(String.t()) :: {:ok, map()} | {:error, String.t()}
+  def parse_slides_url(url) do
+    with [_, presentation_id] <-
+           Regex.run(~r{https://docs.google.com/presentation/d/([a-zA-Z0-9_-]+)/}, url),
+         [_, page_id] <- Regex.run(~r/slide=id\.(g[a-zA-Z0-9_-]+)/, url) do
+      {:ok,
+       %{
+         presentation_id: presentation_id,
+         page_id: page_id
+       }}
+    else
+      _ -> {:error, "Invalid url"}
+    end
+  end
+
   @spec copy_slide(String.t(), String.t()) :: {:ok, map()} | {:error, String.t()}
   defp copy_slide(token, presentation_id) do
     url = "#{@drive_url}/#{presentation_id}/copy"
