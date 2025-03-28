79dce402b5cc6d85b94639087f7768a321e13993
Akansha Sakhre <asakhre2002@gmail.com>
2025-03-28 12:02:58 +0530

    ran mix format

--- test/glific/templates/interactive_template_test.exs
+++ test/glific/templates/interactive_template_test.exs
@@ -560,13 +560,29 @@ defmodule Glific.Templates.InteractiveTemplateTest do
   end
 
   test "create_interactive_template/1 returns error for markdown characters in translations",
-    %{organization_id: org_id} do
-
-      # for interactive list
-      attrs = %{
-        label: "List Label",
-        type: :list,
-        interactive_content: %{
+       %{organization_id: org_id} do
+    # for interactive list
+    attrs = %{
+      label: "List Label",
+      type: :list,
+      interactive_content: %{
+        "title" => "Interactive list",
+        "body" => "some body",
+        "globalButtons" => [%{"type" => "text", "title" => "button text"}],
+        "items" => [
+          %{
+            "title" => "Item Title",
+            "subtitle" => "Subtitle",
+            "options" => [
+              %{"type" => "text", "title" => "Option 1", "description" => "Description"},
+              %{"type" => "text", "title" => "Option 2", "description" => "Description"}
+            ]
+          }
+        ]
+      },
+      translations: %{
+        "1" => %{
+          "type" => "list",
           "title" => "Interactive list",
           "body" => "some body",
           "globalButtons" => [%{"type" => "text", "title" => "button text"}],
@@ -576,71 +592,53 @@ defmodule Glific.Templates.InteractiveTemplateTest do
               "subtitle" => "Subtitle",
               "options" => [
                 %{"type" => "text", "title" => "Option 1", "description" => "Description"},
-                %{"type" => "text", "title" => "Option 2", "description" => "Description"}
+                %{"type" => "text", "title" => "**Option 2**", "description" => "Description"}
               ]
             }
           ]
-        },
-        translations: %{
-          "1" => %{
-            "type" => "list",
-            "title" => "Interactive list",
-            "body" => "some body",
-            "globalButtons" => [%{"type" => "text", "title" => "button text"}],
-            "items" => [
-              %{
-                "title" => "Item Title",
-                "subtitle" => "Subtitle",
-                "options" => [
-                  %{"type" => "text", "title" => "Option 1", "description" => "Description"},
-                  %{"type" => "text", "title" => "**Option 2**", "description" => "Description"}
-                ]
-              }
-            ]
-          }
-        },
-        organization_id: org_id,
-        language_id: language_fixture().id
-      }
+        }
+      },
+      organization_id: org_id,
+      language_id: language_fixture().id
+    }
 
-      assert {:error,
-              "Button text cannot contain any markdown characters (e.g., **bold**, _italics_, etc)."} ==
-              InteractiveTemplates.create_interactive_template(attrs)
+    assert {:error,
+            "Button text cannot contain any markdown characters (e.g., **bold**, _italics_, etc)."} ==
+             InteractiveTemplates.create_interactive_template(attrs)
 
-      # for quick reply
-      attrs = %{
-        label: "A label",
-        type: :quick_reply,
-        interactive_content: %{
+    # for quick reply
+    attrs = %{
+      label: "A label",
+      type: :quick_reply,
+      interactive_content: %{
+        "content" => %{"text" => "some text", "type" => "text"},
+        "options" => [
+          %{"title" => "Option 1", "type" => "text"},
+          %{"title" => "Option 2", "type" => "text"}
+        ],
+        "type" => "quick_reply"
+      },
+      translations: %{
+        "1" => %{
+          "type" => "quick_reply",
           "content" => %{"text" => "some text", "type" => "text"},
           "options" => [
-            %{"title" => "Option 1", "type" => "text"},
+            %{"title" => "**Option 1**", "type" => "text"},
             %{"title" => "Option 2", "type" => "text"}
-          ],
-          "type" => "quick_reply"
-        },
-        translations: %{
-          "1" => %{
-            "type" => "quick_reply",
-            "content" => %{"text" => "some text", "type" => "text"},
-            "options" => [
-              %{"title" => "**Option 1**", "type" => "text"},
-              %{"title" => "Option 2", "type" => "text"}
-            ],
-          }
-        },
-        organization_id: org_id,
-        language_id: language_fixture().id
-      }
+          ]
+        }
+      },
+      organization_id: org_id,
+      language_id: language_fixture().id
+    }
 
-      assert {:error,
-        "Button text cannot contain any markdown characters (e.g., **bold**, _italics_, etc)."} ==
-        InteractiveTemplates.create_interactive_template(attrs)
+    assert {:error,
+            "Button text cannot contain any markdown characters (e.g., **bold**, _italics_, etc)."} ==
+             InteractiveTemplates.create_interactive_template(attrs)
   end
 
   test "update_interactive_template\1 returns error for markdown characters in translations",
-  %{organization_id: org_id} do
-
+       %{organization_id: org_id} do
     interactive_attrs = %{
       label: "List Label",
       type: :list,
@@ -726,7 +724,7 @@ defmodule Glific.Templates.InteractiveTemplateTest do
 
     assert {:error,
             "Button text cannot contain any markdown characters (e.g., **bold**, _italics_, etc)."} ==
-            InteractiveTemplates.update_interactive_template(interactive, attrs)
+             InteractiveTemplates.update_interactive_template(interactive, attrs)
 
     # for quick reply
     attrs = %{
@@ -747,7 +745,7 @@ defmodule Glific.Templates.InteractiveTemplateTest do
           "options" => [
             %{"title" => "**Option 1**", "type" => "text"},
             %{"title" => "Option 2", "type" => "text"}
-          ],
+          ]
         }
       },
       organization_id: org_id,
@@ -755,7 +753,7 @@ defmodule Glific.Templates.InteractiveTemplateTest do
     }
 
     assert {:error,
-      "Button text cannot contain any markdown characters (e.g., **bold**, _italics_, etc)."} ==
-      InteractiveTemplates.update_interactive_template(interactive, attrs)
-      end
+            "Button text cannot contain any markdown characters (e.g., **bold**, _italics_, etc)."} ==
+             InteractiveTemplates.update_interactive_template(interactive, attrs)
+  end
 end
