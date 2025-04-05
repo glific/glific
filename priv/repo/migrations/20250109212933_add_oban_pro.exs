8d352491bec9caa54663fad96fadfd282cb3b675
madclaws <pavanan.anandu63@gmail.com>
2025-04-06 01:09:25 +0530

    fix: moved pro from rc + other changes

--- priv/repo/migrations/20250109212933_add_oban_pro.exs
+++ priv/repo/migrations/20250109212933_add_oban_pro.exs
@@ -1,7 +1,7 @@
 defmodule Glific.Repo.Migrations.AddObanPro do
   use Ecto.Migration
 
-  def up, do: Oban.Pro.Migration.up(prefix: "global")
+  def up, do: Oban.Pro.Migration.up(version: "1.5.0", prefix: "global")
 
   def down, do: Oban.Pro.Migration.down(prefix: "global")
 end
