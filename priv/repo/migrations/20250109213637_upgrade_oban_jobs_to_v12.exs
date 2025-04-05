530bd6c2b729d2320403099e5b699b8042761d71
madclaws <pavanan.anandu63@gmail.com>
2025-04-05 21:09:07 +0530

    fix: typo in prefix

--- priv/repo/migrations/20250109213637_upgrade_oban_jobs_to_v12.exs
+++ priv/repo/migrations/20250109213637_upgrade_oban_jobs_to_v12.exs
@@ -2,12 +2,12 @@ defmodule Glific.Repo.Migrations.UpgradeObanJobsToV12 do
   use Ecto.Migration
 
   def up do
-    Oban.Migration.up(version: 12, prefix: "golbal")
+    Oban.Migration.up(version: 12, prefix: "global")
   end
 
   # We specify `version: 1` in `down`, ensuring that we'll roll all the way back down if
   # necessary, regardless of which version we've migrated `up` to.
   def down do
-    Oban.Migration.down(version: 1, prefix: "golbal")
+    Oban.Migration.down(version: 1, prefix: "global")
   end
 end
