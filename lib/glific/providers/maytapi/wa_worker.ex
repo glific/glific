b1e1bae84b38a1954409636647965f4aaad90252
AmishaBisht <amishabisht33@gmail.com>
2025-04-15 15:22:51 +0530

    CI pass

--- lib/glific/providers/maytapi/wa_worker.ex
+++ lib/glific/providers/maytapi/wa_worker.ex
@@ -98,7 +98,7 @@ defmodule Glific.Providers.Maytapi.WAWorker do
   end
 
   @spec update_credentials(non_neg_integer()) :: :ok | {:error, String.t()}
-  def update_credentials(org_id) do
+  defp update_credentials(org_id) do
     with :ok <- WAManagedPhones.delete_existing_wa_managed_phones(org_id),
          :ok <- WAManagedPhones.fetch_wa_managed_phones(org_id),
          :ok <- WAGroups.fetch_wa_groups(org_id) do
