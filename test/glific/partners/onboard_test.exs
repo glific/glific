2da4a3cab7bc597ab4d0f4f1fa5997ebc53b3d79
madclaws <pavanan.anandu63@gmail.com>
2025-05-23 16:25:51 +0530

    test: more coversge

--- test/glific/partners/onboard_test.exs
+++ test/glific/partners/onboard_test.exs
@@ -684,6 +684,40 @@ defmodule Glific.OnboardTest do
       |> Map.put("shortcode", "NEW_Glific")
       |> Map.put("phone", "919917443995")
 
-    _result = Onboard.setup(attrs)
+    assert %{is_valid: false} = Onboard.setup(attrs)
+  end
+
+  test "Partnerapps api returns error" do
+    Tesla.Mock.mock(fn
+      %{method: :get} ->
+        {:error,
+        %Tesla.Env{
+          status: 400,
+          body:
+            Jason.encode!(%{
+              "error" => "some error"
+            })
+        }}
+
+      %{method: :post} ->
+        %Tesla.Env{
+          status: 200,
+          body:
+            Jason.encode!(%{
+              "token" => "ks_test_token",
+              "status" => "success",
+              "templates" => [],
+              "template" => %{"id" => Ecto.UUID.generate(), "status" => "PENDING"}
+            })
+        }
+    end)
+
+    attrs =
+      @valid_attrs
+      |> Map.put("name", " First")
+      |> Map.put("shortcode", "NEW_Glific")
+      |> Map.put("phone", "919917443995")
+
+    assert %{is_valid: false} = Onboard.setup(attrs)
   end
 end
