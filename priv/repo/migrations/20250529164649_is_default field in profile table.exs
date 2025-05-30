db809e0b9eb13d4ddf02664e2c9b200a9b3ecf17
AmishaBisht <amishabisht33@gmail.com>
2025-05-30 11:58:39 +0530

    test: add the test case for default profile

--- priv/repo/migrations/20250529164649_is_default field in profile table.exs
+++ priv/repo/migrations/20250529164649_is_default field in profile table.exs
@@ -2,7 +2,7 @@ defmodule Glific.Repo.Migrations.IsDefaultFieldInProfileTable do
   use Ecto.Migration
 
   def change do
-alter table(:profiles) do
+    alter table(:profiles) do
       add :is_default, :boolean, default: false
     end
   end
