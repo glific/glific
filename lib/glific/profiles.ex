f7d9cbf67cedd9add4dca0c30b82e199c115de91
AmishaBisht <amishabisht33@gmail.com>
2025-05-30 17:15:27 +0530

    refactor: add the force preload for active profile

--- lib/glific/profiles.ex
+++ lib/glific/profiles.ex
@@ -138,21 +138,23 @@ defmodule Glific.Profiles do
 
     with {:ok, index} <- Glific.parse_maybe_integer(profile_index),
          {profile, _index} <- fetch_indexed_profile(contact, index),
-         {:ok, _updated_contact} <-
+         {:ok, updated_contact} <-
            Contacts.update_contact(contact, %{
              active_profile_id: profile.id,
              language_id: profile.language_id,
              fields: profile.fields
            }) do
-      ContactField.do_add_contact_field(
-        contact,
-        "active_profile_name",
-        "active_profile_name",
-        profile.name,
-        "string"
-      )
+      contact =
+        ContactField.do_add_contact_field(
+          updated_contact,
+          "active_profile_name",
+          "active_profile_name",
+          profile.name,
+          "string"
+        )
+        |> Repo.preload([:active_profile], force: true)
 
-      {:ok, Contacts.get_contact!(contact.id)}
+      {:ok, contact}
     else
       _ -> {:error, contact}
     end
--- test/glific/profiles_test.exs
+++ test/glific/profiles_test.exs
@@ -147,11 +147,13 @@ defmodule Glific.ProfilesTest do
         contact_with_profile_2
         |> Profiles.switch_profile("1")
 
-      updated_contact = Repo.preload(contact_with_profile_1, [:active_profile])
-      assert updated_contact.active_profile_id == new_profile.id
-      assert updated_contact.active_profile.name == "Profile 2"
+      assert contact_with_profile_1.active_profile_id == new_profile.id
+      assert contact_with_profile_1.active_profile.name == "Profile 2"
+
       # active_profile name should be in the contact fields
-      assert updated_contact.fields["active_profile_name"]["value"] == "Profile 2"
+      fields = get_in(contact_with_profile_1.fields)
+      profile_name = Map.get(fields["active_profile_name"], :value)
+      assert profile_name == "Profile 2"
     end
   end
 end
