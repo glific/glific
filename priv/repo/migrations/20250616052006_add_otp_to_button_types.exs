b78ab3e8c3ac74f10ce98035e6cef3f5c5d19631
AmishaBisht <amishabisht33@gmail.com>
2025-06-18 18:32:58 +0530

    refactor: add otp in button type in session_templates

--- priv/repo/migrations/20250616052006_add_otp_to_button_types.exs
+++
@@ -1,9 +0,0 @@
-defmodule Glific.Repo.Migrations.AddOtpToButtonTypes do
-  use Ecto.Migration
-
-  def change do
-    alter table(:session_templates) do
-      modify :button_type, :string
-    end
-  end
-end
