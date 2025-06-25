c73f180ef9882840b32f2d09b8fbf20f9c400001
madclaws <pavanan.anandu63@gmail.com>
2025-06-25 18:00:36 +0530

    test: resolved merge conflicts

--- test/glific/state_test.exs
+++ test/glific/state_test.exs
@@ -106,7 +106,7 @@ defmodule Glific.StateTest do
 
     # we have 21 flows in our dev seeder
     # two template flows
-    assert length(free_flows) == 21
+    assert length(free_flows) == 22
     assert Enum.empty?(busy_flows)
   end
 
