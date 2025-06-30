615b69b4bf61581c9958534e15e04573f54fc01c
AmishaBisht <amishabisht33@gmail.com>
2025-06-30 17:16:13 +0530

    test: add test case for otp parameter

--- lib/glific/messages.ex
+++ lib/glific/messages.ex
@@ -464,7 +464,7 @@ defmodule Glific.Messages do
         organization_id: contact.organization_id
       })
 
-    parameters = ["registration", otp]
+    parameters = [otp]
 
     %{template_id: session_template.id, receiver_id: contact.id, parameters: parameters}
     |> create_and_send_hsm_message()
