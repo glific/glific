6c722c9112aa5eecc20b641e92e2fe388faeef73
AmishaBisht <amishabisht33@gmail.com>
2025-06-30 15:15:10 +0530

    refactor: fix parameter

--- lib/glific/messages.ex
+++ lib/glific/messages.ex
@@ -464,7 +464,7 @@ defmodule Glific.Messages do
         organization_id: contact.organization_id
       })
 
-    parameters = ["Registration", otp]
+    parameters = [otp]
 
     %{template_id: session_template.id, receiver_id: contact.id, parameters: parameters}
     |> create_and_send_hsm_message()
