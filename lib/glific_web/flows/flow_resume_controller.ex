773d545a3aeff116126036faf19e0680179b9f5b
AmishaBisht <amishabisht33@gmail.com>
2025-08-01 19:21:22 +0530

    refactor: handle the wait for result case in flow resume controller

--- lib/glific_web/flows/flow_resume_controller.ex
+++ lib/glific_web/flows/flow_resume_controller.ex
@@ -20,6 +20,7 @@ defmodule GlificWeb.Flows.FlowResumeController do
     # Map the response_id to thread_id, since we treat response_id as the thread ID in Glific
     # and use thread_id throughout the platform for OpenAI conversation support
     thread_id = if is_map(response), do: Map.get(response, "response_id"), else: nil
+    response = Map.put(response, "thread_id", thread_id)
 
     organization = Partners.organization(organization_id)
     Repo.put_process_state(organization.id)
