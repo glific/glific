93929dd73db3ac8607202a8d7ab5619bddd72d2b
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-08-07 17:56:44 +0530

    test: add footer field to WhatsApp HSM template in attach_footer test

--- test/glific/templates_test.exs
+++ test/glific/templates_test.exs
@@ -2220,6 +2220,7 @@ defmodule Glific.TemplatesTest do
           "data" => "Your train ticket no. {{1}}",
           "elementName" => "ticket_update_status",
           "id" => whatspp_hsm_uuid,
+          "footer" => "footer",
           "languageCode" => "en",
           "languagePolicy" => "deterministic",
           "master" => true,
