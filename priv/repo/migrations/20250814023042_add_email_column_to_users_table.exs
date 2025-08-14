81a773250d296af5b4d2fd07062238cca6f7efb6
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-08-14 09:29:15 +0530

    * feat: add email field to User schema and update required fields

---
+++ priv/repo/migrations/20250814023042_add_email_column_to_users_table.exs
@@ -0,0 +1,11 @@
+defmodule Glific.Repo.Migrations.AddEmailColumnToUsersTable do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :email, :string
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
