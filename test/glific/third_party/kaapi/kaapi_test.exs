889fb186e67ee079be4ad84b4571a32b3be66b7b
Shijith Karumathil <shijithjayan@gmail.com>
2025-08-22 01:08:59 +0530

    test: :white_check_mark: Added new Kaapi API tests and used describe blocks
    Group related tests into describe blocks, fix test descriptions, add
    delete_assistant tests, and replace credential setup with module
    constant for API key.

--- test/glific/third_party/kaapi/kaapi_test.exs
+++ test/glific/third_party/kaapi/kaapi_test.exs
@@ -1,6 +1,4 @@
 defmodule Glific.ThirdParty.Kaapi.ApiClienTest do
-  alias Glific.Partners
-
   use GlificWeb.ConnCase
   import Tesla.Mock
   alias Glific.ThirdParty.Kaapi.ApiClient
@@ -11,6 +9,8 @@ defmodule Glific.ThirdParty.Kaapi.ApiClienTest do
     organization_name: "GLific_org",
     project_name: "Glific"
   }
+
+  @org_kaapi_api_key "sk_3fa22108-f464-41e5-81d9-d8a298854430"
 
   test "onboard_to_kaapi/1 returns {:ok, %{api_key: key}} on 200 with api_key" do
     mock(fn
@@ -25,11 +25,11 @@ defmodule Glific.ThirdParty.Kaapi.ApiClienTest do
     assert key == "ApiKey XoxxxxabcDefGhfKSDrs"
   end
 
-  test "returns {:error, msg} when API returns body with error field (200)" do
+  test "returns {:error, msg} when API returns body with error field" do
     mock(fn
       %Tesla.Env{method: :post} ->
         %Tesla.Env{
-          status: 200,
+          status: 422,
           body: %{error: "API key already exists for this user and project."}
         }
     end)
@@ -71,113 +71,157 @@ defmodule Glific.ThirdParty.Kaapi.ApiClienTest do
     assert {:error, "API request failed"} = ApiClient.onboard_to_kaapi(@params)
   end
 
-  test "create_assistant/2 successfully creates assistant in kaapi", %{user: user} do
-    enable_kaapi(%{organization_id: user.organization_id})
+  describe "create_assistant/2" do
+    test "successfully creates assistant in kaapi" do
+      params = %{
+        name: "Assistant-f11ead89",
+        instructions: "this is a story telling assistant that tells story",
+        id: "asst_123",
+        model: "gpt-4o",
+        temperature: 1.0
+      }
 
-    params = %{
-      name: "Assistant-f11ead89",
-      instructions: "this is a story telling assistant that tells story",
-      id: "asst_123",
-      model: "gpt-4o",
-      temperature: 1.0
-    }
-
-    mock(fn
-      %Tesla.Env{method: :post, url: "This is not a secret/api/v1/assistant/"} ->
-        %Tesla.Env{
-          status: 200,
-          body: %{
-            error: nil,
-            data: %{
-              id: 86,
-              name: "Assistant-f78f4392",
-              instructions: "you are a helpful asssitant",
-              organization_id: 1,
-              project_id: 1,
-              assistant_id: "asst_5TtScw1DwabcDBjvrvY",
-              vector_store_ids: [],
-              temperature: 0.1,
-              model: "gpt-4o",
-              is_deleted: false,
-              deleted_at: nil
-            },
-            metadata: nil,
-            success: true
+      mock(fn
+        %Tesla.Env{method: :post} ->
+          %Tesla.Env{
+            status: 200,
+            body: %{
+              error: nil,
+              data: %{
+                id: 86,
+                name: "Assistant-f78f4392",
+                instructions: "you are a helpful asssitant",
+                organization_id: 1,
+                project_id: 1,
+                assistant_id: "asst_5TtScw1DwabcDBjvrvY",
+                vector_store_ids: [],
+                temperature: 0.1,
+                model: "gpt-4o",
+                is_deleted: false,
+                deleted_at: nil
+              },
+              metadata: nil,
+              success: true
+            }
           }
-        }
-    end)
+      end)
 
-    assert {:ok, resp} = ApiClient.create_assistant(params, 1)
-    assert resp.name == "Assistant-f78f4392"
-    assert resp.id == "asst_5TtScw1DwabcDBjvrvY"
+      assert {:ok, resp} = ApiClient.create_assistant(params, @org_kaapi_api_key)
+      assert resp.data.name == "Assistant-f78f4392"
+      assert resp.data.assistant_id == "asst_5TtScw1DwabcDBjvrvY"
+    end
+
+    test "returns error if Kaapi fails" do
+      params = %{
+        name: "Repeated-Assistant-f11ead89",
+        instructions: "this is a story telling assistant that tells story",
+        id: "asst_123",
+        model: "gpt-4o",
+        temperature: 1.0
+      }
+
+      mock(fn
+        %Tesla.Env{method: :post} ->
+          %Tesla.Env{
+            status: 409,
+            body: %{
+              error: "Assistant already exists",
+              data: %{},
+              metadata: nil,
+              success: true
+            }
+          }
+      end)
+
+      assert {:error, resp} = ApiClient.create_assistant(params, @org_kaapi_api_key)
+      assert resp == "Assistant already exists"
+    end
   end
 
-  test "update_assistant/2 successfully updates assistant in kaapi", %{user: user} do
-    enable_kaapi(%{organization_id: user.organization_id})
-
-    mock(fn
-      %Tesla.Env{
-        method: :patch,
-        url: "This is not a secret/api/v1/assistant/asst_5TtScw1DwabcDBjvrv"
-      } ->
+  describe "update_assistant/3" do
+    test "successfully updates assistant in kaapi" do
+      mock(fn
         %Tesla.Env{
-          status: 200,
-          body: %{
-            error: nil,
-            data: %{
-              id: 86,
-              name: "Assistant-f78f4392",
-              instructions: "you are a helpful asssitant",
-              organization_id: 1,
-              project_id: 1,
-              assistant_id: "asst_5TtScw1DwabcDBjvrvY",
-              vector_store_ids: ["vs_1"],
-              temperature: 0.1,
-              model: "gpt-4o",
-              is_deleted: false,
-              deleted_at: nil
-            },
-            metadata: nil,
-            success: true
+          method: :patch
+        } ->
+          %Tesla.Env{
+            status: 200,
+            body: %{
+              error: nil,
+              data: %{
+                id: 86,
+                name: "Assistant-f78f4392",
+                instructions: "you are a helpful asssitant",
+                organization_id: 1,
+                project_id: 1,
+                assistant_id: "asst_5TtScw1DwabcDBjvrvY",
+                vector_store_ids: ["vs_1"],
+                temperature: 0.1,
+                model: "gpt-4o",
+                is_deleted: false,
+                deleted_at: nil
+              },
+              metadata: nil,
+              success: true
+            }
           }
-        }
-    end)
+      end)
 
-    params = %{
-      id: "asst_5TtScw1DwabcDBjvrv",
-      name: "Updated Assistant",
-      model: "gpt-4o",
-      instructions: "new instructions",
-      temperature: 0.7,
-      tool_resources: %{file_search: %{vector_store_ids: ["vs_1"]}}
-    }
+      params = %{
+        name: "Updated Assistant",
+        model: "gpt-4o",
+        instructions: "new instructions",
+        temperature: 0.7,
+        tool_resources: %{file_search: %{vector_store_ids: ["vs_1"]}}
+      }
 
-    assert {:ok, resp} = ApiClient.update_assistant(params, 1)
-    assert resp.id == "asst_5TtScw1DwabcDBjvrvY"
-    assert resp.vector_store_ids == ["vs_1"]
+      assert {:ok, resp} =
+               ApiClient.update_assistant("asst_5TtScw1DwabcDBjvrvY", params, @org_kaapi_api_key)
+
+      assert resp.data.assistant_id == "asst_5TtScw1DwabcDBjvrvY"
+      assert resp.data.vector_store_ids == ["vs_1"]
+    end
+
+    test "update assistant with invalid id" do
+      params = %{
+        name: "Updated Assistant",
+        model: "gpt-4o",
+        instructions: "new instructions",
+        temperature: 0.7,
+        tool_resources: %{file_search: %{vector_store_ids: ["vs_1"]}}
+      }
+
+      mock(fn %Tesla.Env{method: :patch} ->
+        %Tesla.Env{status: 404, body: %{error: "Not Found", data: %{}}}
+      end)
+
+      assert {:error, res} = ApiClient.update_assistant("invalid_id", params, @org_kaapi_api_key)
+      assert res == "Not Found"
+    end
   end
 
-  defp enable_kaapi(attrs) do
-    {:ok, credential} =
-      Partners.create_credential(%{
-        organization_id: attrs.organization_id,
-        shortcode: "kaapi",
-        keys: %{},
-        secrets: %{
-          "api_key" => "sk_3fa22108-f464-41e5-81d9-d8a298854430"
+  describe "delete_assistant/1" do
+    test "successfully updates assistant in kaapi" do
+      mock(fn %Tesla.Env{method: :delete} ->
+        %Tesla.Env{
+          status: 200,
+          body: %{error: nil, data: "Deleted", metadata: nil, success: true}
         }
-      })
+      end)
 
-    valid_update_attrs = %{
-      keys: %{},
-      secrets: %{
-        "api_key" => "sk_3fa22108-f464-41e5-81d9-d8a298854430"
-      },
-      is_active: true,
-      organization_id: attrs.organization_id,
-      shortcode: "kaapi"
-    }
+      assert {:ok, resp} =
+               ApiClient.delete_assistant("asst_5TtScw1DwabcDBjvrvY", @org_kaapi_api_key)
 
-    Partners.update_credential(credential, valid_update_attrs)
+      assert resp.data == "Deleted"
+    end
+
+    test "update assistant with invalid id" do
+      mock(fn %Tesla.Env{method: :delete} ->
+        %Tesla.Env{status: 404, body: %{error: "Not Found", data: %{}}}
+      end)
+
+      assert {:error, res} = ApiClient.delete_assistant("invalid_id", @org_kaapi_api_key)
+      assert res == "Not Found"
+    end
   end
 end
