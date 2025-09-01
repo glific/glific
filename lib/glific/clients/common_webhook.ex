db3023083db382c4cac8c3147fea83d248998bca
Shijith Karumathil <shijithjayan@gmail.com>
2025-09-01 08:04:52 +0530

    Add metrics to Kaapi Requests (#4393)

--- lib/glific/clients/common_webhook.ex
+++ lib/glific/clients/common_webhook.ex
@@ -66,6 +66,8 @@ defmodule Glific.Clients.CommonWebhook do
       |> maybe_put_response_id(fields)
       |> Jason.encode!()
 
+    Glific.Metrics.increment("Kaapi Requests")
+
     client =
       Tesla.client([
         {Tesla.Middleware.JSON, engine_opts: [keys: :atoms]},
@@ -84,10 +86,12 @@ defmodule Glific.Clients.CommonWebhook do
         Map.merge(%{success: true}, body)
 
       {:ok, %Tesla.Env{status: _status, body: body}} ->
+        Glific.Metrics.increment("Kaapi Failed")
         reason = Jason.encode!(body)
         %{success: false, reason: reason}
 
       {:error, reason} ->
+        if reason == :timeout, do: Glific.Metrics.increment("Kaapi Timeout")
         %{success: false, reason: inspect(reason)}
     end
   end
