5eb8040e4efdf297466d86a7a2cbfc940e27ba97
AmishaBisht <amishabisht33@gmail.com>
2025-10-01 14:40:57 +0530

    refactor: fix the failure message

--- lib/glific/flows/flow_context.ex
+++ lib/glific/flows/flow_context.ex
@@ -945,23 +945,26 @@ defmodule Glific.Flows.FlowContext do
       )
 
     message =
-      cond do
-        not is_nil(message) ->
-          message
+      if is_nil(message) do
+        cond do
+          FunWithFlags.enabled?(:is_kaapi_enabled,
+            for: %{organization_id: context.organization_id}
+          ) ->
+            webhook_log =
+              WebhookLog
+              |> where([w], w.flow_context_id == ^context.id)
+              |> order_by([w], desc: w.inserted_at)
+              |> limit(1)
+              |> Repo.one()
 
-        FunWithFlags.enabled?(:is_kaapi_enabled, for: %{organization_id: context.organization_id}) ->
-          webhook_log =
-            WebhookLog
-            |> where([w], w.flow_context_id == ^context.id)
-            |> order_by([w], desc: w.inserted_at)
-            |> limit(1)
-            |> Repo.one()
+            Webhook.update_log(webhook_log.id, "Timeout: taking long to process response")
+            Messages.create_temp_message(context.organization_id, "Failure")
 
-          Webhook.update_log(webhook_log.id, "Timeout: taking long to process response")
-          Messages.create_temp_message(context.organization_id, "Failure")
-
-        true ->
-          Messages.create_temp_message(context.organization_id, "No Response")
+          true ->
+            Messages.create_temp_message(context.organization_id, "No Response")
+        end
+      else
+        message
       end
 
     context
