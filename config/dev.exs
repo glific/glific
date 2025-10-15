34c36d1ec9da88de960bfdc75a3f5f41e05d107c
madclaws <pavanan.anandu63@gmail.com>
2025-10-15 16:35:08 +0530

    chore: undoed dev logger config

--- config/dev.exs
+++ config/dev.exs
@@ -49,9 +49,6 @@ config :phoenix, :stacktrace_depth, 20
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
 
-config :logger,
-  level: :error
-
 config :appsignal, :config,
   otp_app: :glific,
   active: false,
