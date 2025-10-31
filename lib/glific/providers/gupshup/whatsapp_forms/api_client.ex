eeb6d0e087bf41e6d92ea2ad412fdb3d53bbecd7
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-10-31 20:04:30 +0530

    fix: add telemetry and retry mechanism for the whatsform api

--- lib/glific/providers/gupshup/whatsapp_forms/api_client.ex
+++ lib/glific/providers/gupshup/whatsapp_forms/api_client.ex
@@ -14,12 +14,15 @@ defmodule Glific.Providers.Gupshup.WhatsappForms.ApiClient do
   @endpoint "https://partner.gupshup.io/partner/app/"
 
   defp client(opts) do
-    Tesla.client([
-      {Tesla.Middleware.BaseUrl, @endpoint},
-      {Tesla.Middleware.Headers, PartnerAPI.headers(:app_token, opts)},
-      {Tesla.Middleware.JSON, engine_opts: [keys: :atoms]},
-      Tesla.Middleware.Telemetry
-    ])
+    Tesla.client(
+      [
+        {Tesla.Middleware.BaseUrl, @endpoint},
+        {Tesla.Middleware.Headers, PartnerAPI.headers(:app_token, opts)},
+        {Tesla.Middleware.JSON, engine_opts: [keys: :atoms]},
+        {Tesla.Middleware.Telemetry,
+         metadata: %{provider: "gupshup_whatsapp_forms", sampling_scale: 10}}
+      ] ++ Glific.get_tesla_retry_middleware()
+    )
   end
 
   defp parse_response({:ok, %Tesla.Env{status: status, body: body}})
@@ -35,5 +38,11 @@ defmodule Glific.Providers.Gupshup.WhatsappForms.ApiClient do
       _ ->
         {:error, "Something went wrong"}
     end
+  end
+
+  @spec get_tesla_middlewares :: list()
+  defp get_tesla_middlewares do
+    [{Tesla.Middleware.Telemetry, metadata: %{provider: "Gupshup", sampling_scale: 10}}] ++
+      Glific.get_tesla_retry_middleware()
   end
 end
