bb2b6cea85eefb3a2b396f0f81a855d0376bb04e
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-11-03 18:54:12 +0530

    refactor: make a separate function for updating the status of the WhatsApp form

--- lib/glific/whatsapp_forms.ex
+++ lib/glific/whatsapp_forms.ex
@@ -16,26 +16,12 @@ defmodule Glific.WhatsappForms do
   def publish_whatsapp_form(%WhatsappForm{} = form) do
     case ApiClient.publish_wa_form(form.meta_flow_id, form.organization_id) do
       {:ok, _response} ->
-        updated_changeset = form |> Ecto.Changeset.change(status: :published)
-
-        case Repo.update(updated_changeset) do
-          {:ok, updated_form} ->
-            {:ok, updated_form}
-
-          {:error, changeset} ->
-            {:error, "Failed to update form status: #{inspect(changeset.errors)}"}
-        end
+        update_form_status(form, :published)
 
       {:error, reason} ->
         Logger.error("Failed to publish WhatsApp form #{form.id}: #{inspect(reason)}")
         {:error, reason}
     end
-  end
-
-  @doc false
-  @spec get_whatsapp_form_by_meta_flow_id(String.t()) :: WhatsappForm.t() | nil
-  def get_whatsapp_form_by_meta_flow_id(meta_flow_id) do
-    Repo.get_by(WhatsappForm, meta_flow_id: meta_flow_id)
   end
 
   @doc false
@@ -47,9 +33,28 @@ defmodule Glific.WhatsappForms do
         {:error, "WhatsApp form not found"}
 
       %WhatsappForm{} = form ->
-        form
-        |> Ecto.Changeset.change(status: :inactive)
-        |> Repo.update()
+        update_form_status(form, :inactive)
+    end
+  end
+
+  @doc false
+  @spec get_whatsapp_form_by_meta_flow_id(String.t()) :: WhatsappForm.t() | nil
+  def get_whatsapp_form_by_meta_flow_id(meta_flow_id) do
+    Repo.get_by(WhatsappForm, meta_flow_id: meta_flow_id)
+  end
+
+  @spec update_form_status(WhatsappForm.t(), atom()) ::
+          {:ok, WhatsappForm.t()} | {:error, String.t()}
+  defp update_form_status(%WhatsappForm{} = form, new_status) do
+    form
+    |> Ecto.Changeset.change(status: new_status)
+    |> Repo.update()
+    |> case do
+      {:ok, updated_form} ->
+        {:ok, updated_form}
+
+      {:error, changeset} ->
+        {:error, "Failed to update form status: #{inspect(changeset.errors)}"}
     end
   end
 end
