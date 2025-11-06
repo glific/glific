2aab705f2593344f1fa9c58782b8b57e661e0cb9
Akansha Sakhre <asakhre2002@gmail.com>
2025-11-06 20:47:29 +0530

    Update WhatsApp form tests: enhance form creation and update scenarios with improved mock responses and metadata handling

--- test/glific/third_party/whatsapp_form/whatsapp_form_test.exs
+++ test/glific/third_party/whatsapp_form/whatsapp_form_test.exs
@@ -5,8 +5,90 @@ defmodule Glific.ThirdParty.WhatsappForm.ApiClienTest do
     Providers.Gupshup.WhatsappForms.ApiClient
   }
 
-  @flow_id "flow-8f91de44-b123-482e-bb52-77f1c3a78df0"
+  @meta_flow_id "flow-8f91de44-b123-482e-bb52-77f1c3a78df0"
   @org_id 1
+  @form_json %{
+    version: "7.2",
+    screens: [
+      %{
+        id: "RECOMMEND",
+        title: "Feedback 1 of 2",
+        data: %{},
+        layout: %{},
+      },
+      %{
+        id: "RATE",
+        title: "Feedback 2 of 2",
+        data: %{},
+        terminal: true,
+        success: true,
+        layout: %{},
+      },
+    ],
+  }
+
+  test "successfully creates WhatsApp form" do
+    form_params = %{
+      name: "Test Form",
+      categories: ["other"],
+      description: "This is a test form",
+      form_json: @form_json,
+      organization_id: @org_id
+    }
+
+    Tesla.Mock.mock(fn
+      %{method: :post} ->
+        %Tesla.Env{
+          status: 201,
+          body: %{
+            status: "success",
+            body: %{
+              id: 1,
+              name: "Test Form",
+              categories: ["other"],
+              meta_flow_id: @meta_flow_id
+            }
+          }
+        }
+    end)
+
+    assert {:ok, response} = ApiClient.create_whatsapp_form(form_params)
+    assert response.status == "success"
+    assert response.body.name == "Test Form"
+    assert response.body.categories == ["other"]
+  end
+
+  test "successfully updates WhatsApp form" do
+    form_id = 1
+    update_params = %{
+      name: "Updated Test Form",
+      description: "This is an updated test form",
+      categories: ["customer_support"],
+      form_json: @form_json,
+      organization_id: @org_id
+
+    }
+
+    Tesla.Mock.mock(fn
+      %{method: :put} ->
+        %Tesla.Env{
+          status: 200,
+          body: %{
+            status: "success",
+            body: %{
+              id: form_id,
+              name: "Updated Test Form",
+              description: "This is an updated test form",
+              categories: ["customer_support"]
+            }
+          }
+        }
+    end)
+
+    assert {:ok, response} = ApiClient.update_whatsapp_form(form_id, update_params)
+    assert response.status == "success"
+    assert response.body.name == "Updated Test Form"
+  end
 
   test "successfully publishes WhatsApp form" do
     Tesla.Mock.mock(fn
@@ -22,9 +104,9 @@ defmodule Glific.ThirdParty.WhatsappForm.ApiClienTest do
         }
     end)
 
-    assert {:ok, response} = ApiClient.publish_wa_form(@flow_id, @org_id)
+    assert {:ok, response} = ApiClient.publish_wa_form(@meta_flow_id, @org_id)
     assert response.status == "success"
-    assert response.body.meta_flow_id == @flow_id
+    assert response.body.meta_flow_id == @meta_flow_id
   end
 
   test "fails to publish WhatsApp form due to invalid request" do
@@ -36,7 +118,7 @@ defmodule Glific.ThirdParty.WhatsappForm.ApiClienTest do
         }
     end)
 
-    assert {:error, body} = ApiClient.publish_wa_form(@flow_id, @org_id)
+    assert {:error, body} = ApiClient.publish_wa_form(@meta_flow_id, @org_id)
     assert body.error == "Invalid flow ID"
   end
 
@@ -49,7 +131,7 @@ defmodule Glific.ThirdParty.WhatsappForm.ApiClienTest do
         }
     end)
 
-    assert {:error, body} = ApiClient.publish_wa_form(@flow_id, @org_id)
+    assert {:error, body} = ApiClient.publish_wa_form(@meta_flow_id, @org_id)
     assert body.error == "Internal server error"
   end
 
@@ -60,7 +142,7 @@ defmodule Glific.ThirdParty.WhatsappForm.ApiClienTest do
     end)
 
     assert {:error, "%Tesla.Error{env: nil, stack: [], reason: :timeout}"} =
-             ApiClient.publish_wa_form(@flow_id, @org_id)
+             ApiClient.publish_wa_form(@meta_flow_id, @org_id)
   end
 
   test "handles unexpected HTTP status codes" do
@@ -72,7 +154,7 @@ defmodule Glific.ThirdParty.WhatsappForm.ApiClienTest do
         }
     end)
 
-    assert {:error, body} = ApiClient.publish_wa_form(@flow_id, @org_id)
+    assert {:error, body} = ApiClient.publish_wa_form(@meta_flow_id, @org_id)
     assert body.error == "Unauthorized request"
   end
 end
