4d8d05286f445f6680f55523d6137df2c80cb706
Akansha Sakhre <asakhre2002@gmail.com>
2025-11-07 21:37:20 +0530

    Remove unnecessary blank line in WhatsApp form types schema

--- lib/glific_web/schema/whatsapp_form_types.ex
+++ lib/glific_web/schema/whatsapp_form_types.ex
@@ -33,7 +33,6 @@ defmodule GlificWeb.Schema.WhatsappFormTypes do
     field :description, :string
   end
 
-
   object :whatsapp_form_queries do
     @desc "Get a WhatsApp form by ID"
     field :whatsapp_form, :whatsapp_form_result do
