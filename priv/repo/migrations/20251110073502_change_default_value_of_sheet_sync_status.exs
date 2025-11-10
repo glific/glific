aa9e45998f29ae914c04998702583c413f5cf496
Shijith Karumathil <shijithjayan@gmail.com>
2025-11-10 15:10:50 +0530

    fix: :bug: Update sheet sync status migration query
    - Change the WHERE condition to set sync_status to 'success' for all
    records that are not 'failed', rather than only targeting NULL or
    'pending' values.
    
    - This fixes ` (Postgrex.Error) ERROR 22P02
    (invalid_text_representation) invalid input value for enum
    sheet_sync_status_enum: "pending"` error in CI - which happens becaue we
    removed `pending` from the sync_status type.

--- priv/repo/migrations/20251110073502_change_default_value_of_sheet_sync_status.exs
+++ priv/repo/migrations/20251110073502_change_default_value_of_sheet_sync_status.exs
@@ -2,7 +2,7 @@ defmodule Glific.Repo.Migrations.ChangeDefaultValueOfSheetSyncStatus do
   use Ecto.Migration
 
   def change do
-    execute "UPDATE sheets SET sync_status = 'success' WHERE sync_status IS NULL OR sync_status = 'pending';"
+    execute "UPDATE sheets SET sync_status = 'success' WHERE sync_status != 'failed';"
 
     alter table(:sheets) do
       modify :sync_status, Glific.Enums.SheetSyncStatus.type(), default: "success"
