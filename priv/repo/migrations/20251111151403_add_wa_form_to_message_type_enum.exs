e94bb032f1dc227c6a5415205bf652a78fa0a578
Akansha Sakhre <asakhre2002@gmail.com>
2025-11-11 20:47:57 +0530

    refactor

--- priv/repo/migrations/20251111151403_add_wa_form_to_message_type_enum.exs
+++ priv/repo/migrations/20251111151403_add_wa_form_to_message_type_enum.exs
@@ -4,10 +4,4 @@ defmodule Glific.Repo.Migrations.AddWaFormToMessageTypeEnum do
   def up do
     execute "ALTER TYPE message_type_enum ADD VALUE 'wa_form'"
   end
-
-  def down do
-    # Note: PostgreSQL doesn't support removing enum values
-    # You would need to recreate the enum type to remove a value
-    raise "Cannot remove enum value"
-  end
 end
