f8e4e41f7a9ae335aa1a94c5e5126a441f783a78
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-11-11 16:40:46 +0530

    refactor: add down function to migration

--- priv/repo/migrations/20251105035933_add_new_button_type_to_session_templates.exs
+++ priv/repo/migrations/20251105035933_add_new_button_type_to_session_templates.exs
@@ -4,4 +4,19 @@ defmodule Glific.Repo.Migrations.AddNewButtonTypeToSessionTemplates do
   def up do
     execute("ALTER TYPE template_button_type_enum ADD VALUE IF NOT EXISTS 'whatsapp_form'")
   end
+
+  def down do
+    execute("""
+    CREATE TYPE template_button_type_enum_new AS ENUM ('call_to_action', 'quick_reply', 'otp');
+    """)
+
+    execute("""
+    ALTER TABLE session_templates
+    ALTER COLUMN button_type TYPE template_button_type_enum_new
+    USING button_type::text::template_button_type_enum_new;
+    """)
+
+    execute("DROP TYPE template_button_type_enum;")
+    execute("ALTER TYPE template_button_type_enum_new RENAME TO template_button_type_enum;")
+  end
 end
