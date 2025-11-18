2044ed78c78de15ee6be2c6c54b92b9679e9205c
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-11-18 18:16:10 +0530

    refactor: add alias for glific web

--- test/glific_web/schema/whatsapp_form_test.exs
+++ test/glific_web/schema/whatsapp_form_test.exs
@@ -8,45 +8,49 @@ defmodule GlificWeb.Schema.WhatsappFormTest do
     WhatsappForms.WhatsappForm
   }
 
+  alias GlificWeb.{
+    Schema
+  }
+
   load_gql(
     :publish_whatsapp_form,
-    GlificWeb.Schema,
+    Schema,
     "assets/gql/whatsapp_forms/publish_whatsapp_form.gql"
   )
 
   load_gql(
     :deactivate_whatsapp_form,
-    GlificWeb.Schema,
+    Schema,
     "assets/gql/whatsapp_forms/deactivate_whatsapp_form.gql"
   )
 
   load_gql(
     :activate_whatsapp_form,
-    GlificWeb.Schema,
+    Schema,
     "assets/gql/whatsapp_forms/activate.gql"
   )
 
   load_gql(
     :count_whatsapp_forms,
-    GlificWeb.Schema,
+    Schema,
     "assets/gql/whatsapp_forms/count.gql"
   )
 
   load_gql(
     :list_whatsapp_forms,
-    GlificWeb.Schema,
+    Schema,
     "assets/gql/whatsapp_forms/list.gql"
   )
 
   load_gql(
     :whatsapp_form,
-    GlificWeb.Schema,
+    Schema,
     "assets/gql/whatsapp_forms/get.gql"
   )
 
   load_gql(
     :delete_whatsapp_form,
-    GlificWeb.Schema,
+    Schema,
     "assets/gql/whatsapp_forms/delete.gql"
   )
 
