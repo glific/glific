f07b40a5ddcfefac62b05e67813c400dde1b78f8
Akansha Sakhre <asakhre2002@gmail.com>
2025-12-17 13:28:29 +0530

    Refactor WhatsApp form responses tests; enhance response creation logic and improve header preparation

--- test/glific/whatsapp_form_responses.test.exs
+++ test/glific/whatsapp_form_responses.test.exs
@@ -3,9 +3,11 @@ defmodule Glific.WhatsappFormResponsesTest do
   import Mock
 
   alias Glific.{
+    Repo,
     Partners,
     Seeds.SeedsDev,
     Templates,
+    WhatsappForms.WhatsappForm,
     WhatsappFormsResponses
   }
 
@@ -21,7 +23,7 @@ defmodule Glific.WhatsappFormResponsesTest do
       %{method: :get, url: nil} ->
         {:error, :invalid_url}
 
-      %{method: :post, url: url} ->
+      %{method: :post, url: _url} ->
         %Tesla.Env{
           status: 200,
           body:
@@ -42,10 +44,47 @@ defmodule Glific.WhatsappFormResponsesTest do
     organization = SeedsDev.seed_organizations(default_provider)
     SeedsDev.seed_whatsapp_forms(organization)
 
+    {:ok, _temp} =
+      Templates.create_session_template(%{
+        label: "Whatsapp Form Template",
+        type: :text,
+        body: "Hello World",
+        language_id: 1,
+        organization_id: organization.id,
+        uuid: "3982792f-a178-442d-be4b-3eadbb804726",
+        buttons: [
+          %{
+            "text" => "RATE",
+            "type" => "FLOW",
+            "flow_id" => "flow-8f91de44-b123-482e-bb52-77f1c3a78df0",
+            "flow_action" => "NAVIGATE",
+            "navigate_screen" => "RATE"
+          }
+        ]
+      })
+
     %{organization_id: organization.id}
   end
 
-  @valid_attrs %{
+  @valid_attrs_for_create %{
+    status: :received,
+    type: :whatsapp_form_response,
+    body: "",
+    organization_id: 1,
+    flow: :inbound,
+    bsp_status: :delivered,
+    sender: %{name: "abc", phone: "9876543210_1", contact_type: "WABA"},
+    bsp_message_id: "wamid.HBgMOTE5NDI1MDEwNDQ5FQIAEhgUM0EzRTUwQjRGQzg4NTgxOTBCODMA",
+    sender_id: 2,
+    receiver_id: 1,
+    context_id: "031U5mGKSgDZdXxdrtBEa2",
+    template_id: "3982792f-a178-442d-be4b-3eadbb804726",
+    raw_response:
+      "{\"flow_token\":\"unused\",\"screen_1_Customer_service_2\":\"2_Average\",\"screen_0_Choose_one_0\":\"0_Yes\",\"screen_1_Delivery_and_setup_1\":\"0_Excellent\",\"screen_1_Purchase_experience_0\":\"1_Good\"}",
+    submitted_at: "1765956142"
+  }
+
+  @valid_attrs_for_headers %{
     id: 1,
     organization_id: 1,
     submitted_at: ~U[2025-12-16 08:24:15.000000Z],
@@ -64,7 +103,24 @@ defmodule Glific.WhatsappFormResponsesTest do
     updated_at: ~U[2025-12-16 12:31:58.211190Z]
   }
 
-  test "create_whatsapp_form_response/1 creates a response successfully and write to google sheet if it exists",
+  test "create_whatsapp_form_response/1 creates a whatsapp form response",
+       %{organization_id: organization_id} do
+    attrs = Map.put(@valid_attrs_for_create, :organization_id, organization_id)
+
+    whatsapp_form =
+      Repo.get_by(WhatsappForm, %{meta_flow_id: "flow-8f91de44-b123-482e-bb52-77f1c3a78df0"})
+
+    {:ok, whatsapp_form_response} =
+      WhatsappFormsResponses.create_whatsapp_form_response(attrs)
+
+    assert whatsapp_form_response.whatsapp_form_id == whatsapp_form.id
+    assert whatsapp_form_response.contact_id == attrs.sender_id
+
+    assert whatsapp_form_response.raw_response ==
+             Jason.decode!(attrs.raw_response)
+  end
+
+  test "prepare_row_from_headers/1 creates a correct headers",
        %{organization_id: organization_id} do
     with_mock(
       Goth.Token,
@@ -90,26 +146,6 @@ defmodule Glific.WhatsappFormResponsesTest do
 
       Partners.create_credential(sheet_attrs)
 
-      {:ok, _temp} =
-        Templates.create_session_template(%{
-          label: "Whatsapp Form Template",
-          type: :text,
-          body: "Hello World",
-          language_id: 1,
-          organization_id: organization_id,
-          uuid: "3982792f-a178-442d-be4b-3eadbb804726",
-          buttons: [
-            %{
-              "text" => "RATE",
-              "type" => "FLOW",
-              "flow_id" => "flow-8f91de44-b123-482e-bb52-77f1c3a78df0",
-              "flow_action" => "NAVIGATE",
-              "navigate_screen" => "RATE"
-            }
-          ]
-        })
-
-      # Test prepare_row_from_headers with mocked GoogleSheets.get_headers
       expected_headers = [
         "timestamp",
         "contact_phone_number",
@@ -127,12 +163,15 @@ defmodule Glific.WhatsappFormResponsesTest do
         get_headers: fn _org_id, _spreadsheet_id -> {:ok, expected_headers} end
       ) do
         {:ok, ordered_row} =
-          WhatsappFormsResponses.prepare_row_from_headers(@valid_attrs, "1234678904004")
+          WhatsappFormsResponses.prepare_row_from_headers(
+            @valid_attrs_for_headers,
+            "1234678904004"
+          )
 
         assert length(ordered_row) == length(expected_headers)
 
-        assert Enum.at(ordered_row, 1) == to_string(@valid_attrs.contact_number)
-        assert Enum.at(ordered_row, 2) == to_string(@valid_attrs.whatsapp_form_id)
+        assert Enum.at(ordered_row, 1) == to_string(@valid_attrs_for_headers.contact_number)
+        assert Enum.at(ordered_row, 2) == to_string(@valid_attrs_for_headers.whatsapp_form_id)
       end
     end
   end
