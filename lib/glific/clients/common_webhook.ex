a88726739910372fc87907fd4c1294a58df03aa1
Shijith Karumathil <shijithjayan@gmail.com>
2025-12-17 20:13:23 +0530

    feat: :sparkles: Add option to forcce use OpenAI text-to-speech for nmt_tts_with_bhasini webhook

--- lib/glific/clients/common_webhook.ex
+++ lib/glific/clients/common_webhook.ex
@@ -231,6 +231,9 @@ defmodule Glific.Clients.CommonWebhook do
     speech_engine = Map.get(fields, "speech_engine", "")
 
     cond do
+      speech_engine == "open_ai" ->
+        ChatGPT.text_to_speech_with_open_ai(org_id, text)
+
       speech_engine == "bhashini" && source_language == target_language ->
         Glific.Bhasini.text_to_speech_with_bhashini(source_language, org_id, text)
 
