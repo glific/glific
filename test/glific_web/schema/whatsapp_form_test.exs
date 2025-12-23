14953c0a2f7f074c63a9aa72445f8313f0a9e752
Akansha Sakhre <asakhre2002@gmail.com>
2025-12-23 09:11:55 +0530

    WhatsApp Forms: Google sheet integration (#4639)
    * feat: Add sheet_id reference to whatsapp_forms schema and migration
    
    * fix: Update optional fields to include sheet_id in whatsapp_forms schema
    
    * feat: Integrate Google Sheets functionality in WhatsApp forms and update GraphQL schema
    
    * feat: Add specifications for Google Sheets creation functions in WhatsApp forms module
    
    * fixed formatting
    
    * feat: Implement functionality to append headers to Google Sheets for WhatsApp forms
    
    * Add sheetId field to WhatsApp form schema and update related tests
    
    * updating tests for google sheets
    
    * added @doc false for seed_sheets func
    
    * feat: Add header building and insertion functions for Google Sheets
    
    * feat: Add specifications for find_complete_action and extract_payload_from_child functions
    
    * create unique index on sheet_id in whatsapp_forms migration
    
    * enhance Google Sheets integration with validation and error handling
    
    * update default headers in build_headers function to include whatsapp_form_name
    
    * refactor: update extract_spreadsheet_id function visibility and improve append_headers_to_sheet logic
    
    * added missing function
    
    * Add documentation for append_headers_to_sheet function
    
    * Refactor seed_sheets function to private and update spec; enhance error handling for Google Sheet access
    
    * Refactor maybe_create_google_sheet function for improved readability; simplify URL handling logic
    
    * minor fixes
    
    * Remove unnecessary @doc false annotation from seed_sheets function
    
    * Update update_sheet function to conditionally validate URL presence
    
    * Remove redundant validate_sheet clause for improved clarity
    
    * Refactor validate_sheet function for improved URL validation; update tests for error handling
    
    * Remove redundant sheet field resolver for improved clarity
    
    * Refactor update_sheet function for improved readability; add test for updating WhatsApp form's URL and its effect on the corresponding sheet record
    
    * Enhance WhatsApp form functionality: add tests for sheet URL conflicts and ensure updates reflect in associated sheets

--- test/glific_web/schema/whatsapp_form_test.exs
+++ test/glific_web/schema/whatsapp_form_test.exs
@@ -1,10 +1,14 @@
 defmodule GlificWeb.Schema.WhatsappFormTest do
   use GlificWeb.ConnCase
   use Wormwood.GQLCase
+  import Mock
 
   alias Glific.{
+    Partners,
     Repo,
     Seeds.SeedsDev,
+    Sheets.Sheet,
+    WhatsappForms,
     WhatsappForms.WhatsappForm
   }
 
@@ -56,7 +60,7 @@ defmodule GlificWeb.Schema.WhatsappFormTest do
     SeedsDev.seed_whatsapp_forms(organization)
   end
 
-  test "published a whatsapp form and updates its status to published",
+  test "publishes a whatsapp form and updates its status to published",
        %{manager: user} do
     Tesla.Mock.mock(fn
       %{method: :post} ->
@@ -141,8 +145,7 @@ defmodule GlificWeb.Schema.WhatsappFormTest do
     {:ok, %{data: %{"deactivateWhatsappForm" => %{"errors" => [error | _]}}}} =
       auth_query_gql_by(:deactivate_whatsapp_form, user, variables: %{"id" => "318182039810832"})
 
-    assert error["message"] ==
-             "Resource not found"
+    assert error["message"] == "Resource not found"
   end
 
   test "fails to publish WhatsApp form if the form does not exist",
@@ -150,8 +153,7 @@ defmodule GlificWeb.Schema.WhatsappFormTest do
     {:ok, %{data: %{"publishWhatsappForm" => %{"errors" => [error | _]}}}} =
       auth_query_gql_by(:publish_whatsapp_form, user, variables: %{"id" => "318182039810832"})
 
-    assert error["message"] ==
-             "Resource not found"
+    assert error["message"] == "Resource not found"
   end
 
   test "count returns the number of whatsapp forms", %{manager: user} do
@@ -316,5 +318,80 @@ defmodule GlificWeb.Schema.WhatsappFormTest do
     assert form_with_sheet2["sheet"]["label"] == "Contact Responses"
 
     assert form_without_sheet["sheet"] == nil
+  end
+
+  test "if an existing sheet is associated with a WhatsApp form, updating the form updates the sheet details",
+       %{user: user} do
+    Tesla.Mock.mock(fn
+      %{method: :put} ->
+        %Tesla.Env{
+          status: 200,
+          body: %{status: "success", success: true}
+        }
+
+      %{method: :post, url: url} when is_binary(url) ->
+        cond do
+          String.contains?(url, "googleapis.com") && String.contains?(url, ":append") ->
+            %Tesla.Env{
+              status: 200,
+              body:
+                Jason.encode!(%{
+                  "spreadsheetId" => "1A2B3C4D5E6F7G8H9I0J",
+                  "updates" => %{
+                    "spreadsheetId" => "1A2B3C4D5E6F7G8H9I0J",
+                    "updatedRange" => "A1:A1",
+                    "updatedRows" => 1,
+                    "updatedColumns" => 1,
+                    "updatedCells" => 1
+                  }
+                })
+            }
+        end
+    end)
+
+    with_mock(
+      Goth.Token,
+      [],
+      fetch: fn _url ->
+        {:ok, %{token: "0xFAKETOKEN_Q=", expires: System.system_time(:second) + 120}}
+      end
+    ) do
+      sheet_attrs = %{
+        shortcode: "google_sheets",
+        secrets: %{
+          "service_account" =>
+            Jason.encode!(%{
+              project_id: "DEFAULT PROJECT ID",
+              private_key_id: "DEFAULT API KEY",
+              client_email: "DEFAULT CLIENT EMAIL",
+              private_key: "DEFAULT PRIVATE KEY"
+            })
+        },
+        is_active: true,
+        organization_id: user.organization_id
+      }
+
+      Partners.create_credential(sheet_attrs)
+
+      {:ok, whatsapp_form_1} =
+        Repo.fetch_by(WhatsappForm, %{meta_flow_id: "flow-8f91de44-b123-482e-bb52-77f1c3a78df0"})
+
+      {:ok, sheet} = Repo.fetch_by(Sheet, %{label: "User Data Sheet"})
+
+      whatsapp_form_1 = Repo.preload(whatsapp_form_1, [:sheet])
+
+      valid_attrs = %{
+        name: whatsapp_form_1.name,
+        description: whatsapp_form_1.description,
+        form_json: whatsapp_form_1.definition,
+        categories: ["other"],
+        organization_id: whatsapp_form_1.organization_id,
+        google_sheet_url: sheet.url
+      }
+
+      result = WhatsappForms.update_whatsapp_form(whatsapp_form_1, valid_attrs)
+
+      assert {:error, %Ecto.Changeset{}} = result
+    end
   end
 end
