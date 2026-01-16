2825f2663f1426a8632677f9c88b8da8ebf4c3eb
priyanshu6238 <priyanshusingh0520@gmail.com>
2026-01-16 10:39:41 +0530

    fix: timeout error

--- lib/glific/providers/gupshup/whatsapp_forms/api_client.ex
+++ lib/glific/providers/gupshup/whatsapp_forms/api_client.ex
@@ -82,8 +82,10 @@ defmodule Glific.Providers.Gupshup.WhatsappForms.ApiClient do
         headers: [{"content-type", "application/json"}]
       )
 
+    opts = [adapter: [recv_timeout: 60_000]]
+
     client(url: url, headers: headers)
-    |> Tesla.put("/flows/#{form.meta_flow_id}/assets", multipart)
+    |> Tesla.put("/flows/#{form.meta_flow_id}/assets", multipart, opts: opts)
     |> parse_response("update_whatsapp_form_json")
   end
 
