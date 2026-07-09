1968dd71b530a9a8fc57b53bcb1ac148495f3887
Vignesh Rajasekaran <vignesh@rvignesh.io>
2026-07-09 09:26:42 +0530

    undo appsignal absinthe tracking as it doesn't work reliably

--- lib/glific/application.ex
+++ lib/glific/application.ex
@@ -78,9 +78,6 @@ defmodule Glific.Application do
     # Add this :telemetry.attach/4 for Ecto query timing:
     attach_repo_telemetry_event()
 
-    # Attach AppSignal's built-in Absinthe instrumentation for GraphQL operation timing:
-    attach_absinthe_telemetry_event()
-
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Glific.Supervisor]
@@ -184,9 +181,5 @@ defmodule Glific.Application do
       &Glific.Appsignal.handle_event/4,
       []
     )
-  end
-
-  defp attach_absinthe_telemetry_event do
-    Appsignal.Absinthe.attach()
   end
 end
