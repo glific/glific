c689ea56c1f50e833552e6642d032b0bf71ae333
nthapa000 <nishantthapa0000@gmail.com>
2026-07-30 08:50:29 +0530

    fix

--- test/glific_web/schema/user_test.exs
+++ test/glific_web/schema/user_test.exs
@@ -192,6 +192,23 @@ defmodule GlificWeb.Schema.UserTest do
     assert key == "OTP"
   end
 
+  test "update current user cannot change email without a verified otp", %{manager: user} do
+    Fixtures.otp_hsm_fixture()
+    original_email = Repo.get!(User, user.id).email
+
+    result =
+      auth_query_gql_by(:update_current, user,
+        variables: %{"input" => %{"otp" => "000000", "email" => "demo@domain.com"}}
+      )
+
+    assert {:ok, query_data} = result
+
+    assert get_in(query_data, [:data, "updateCurrentUser", "errors", Access.at(0), "key"]) ==
+             "OTP"
+
+    assert Repo.get!(User, user.id).email == original_email
+  end
+
   test "delete a user", %{manager: user_auth} do
     user = Fixtures.user_fixture()
 
