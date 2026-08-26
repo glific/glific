7fc9dc1659f611bfc75a664007890981119c79b5
priyanshu6238 <priyanshusingh0520@gmail.com>
2026-08-26 08:55:13 +0530

    feat(migrations): enable concurrent index creation on message_broadcast_id in wa_messages table

--- priv/repo/migrations/20260826084104_add_index_on_message_broadcast_id_in_wa_messages.exs
+++ priv/repo/migrations/20260826084104_add_index_on_message_broadcast_id_in_wa_messages.exs
@@ -1,10 +1,14 @@
 defmodule Glific.Repo.Migrations.AddIndexOnMessageBroadcastIdInWaMessages do
   use Ecto.Migration
 
+  @disable_ddl_transaction true
+  @disable_migration_lock true
+
   def change do
     create index(:wa_messages, [:message_broadcast_id],
              name: "wa_messages_message_broadcast_id_index",
-             where: "message_broadcast_id IS NOT NULL"
+             where: "message_broadcast_id IS NOT NULL",
+             concurrently: true
            )
   end
 end
