d5c47f6798d67f454dcb705fd796aebacf787a97
priyanshu6238 <priyanshusingh0520@gmail.com>
2026-08-27 14:34:35 +0530

    refactor(sheets): update API client to use path parameters for Google Sheets API calls

--- lib/glific/third_party/sheets/api_client.ex
+++ lib/glific/third_party/sheets/api_client.ex
@@ -3,15 +3,23 @@ defmodule Glific.Sheets.ApiClient do
   Http API client to interact with Gupshup
   """
 
+  @csv_export_base_url "https://docs.google.com/spreadsheets/d"
+
   @doc """
-  Get the CSV content from the url.
+  Get the CSV content for a spreadsheet's public export URL.
   """
-  @spec get_csv_content(Keyword.t()) :: Keyword.t()
-  def get_csv_content([url: url] = _opts) do
+  @spec get_csv_content(String.t(), keyword()) :: Keyword.t()
+  def get_csv_content(spreadsheet_id, query_params \\ [])
+
+  def get_csv_content(spreadsheet_id, query_params) when is_binary(spreadsheet_id) do
+    query_string = URI.encode_query([{:format, "csv"} | query_params])
+
     {:ok, response} =
       get_tesla_middlewares()
       |> Tesla.client()
-      |> Tesla.get(url)
+      |> Tesla.get("/:spreadsheet_id/export?#{query_string}",
+        opts: [path_params: [spreadsheet_id: spreadsheet_id]]
+      )
 
     {:ok, stream} = StringIO.open(response.body)
 
@@ -19,12 +27,15 @@ defmodule Glific.Sheets.ApiClient do
     |> CSV.decode(headers: true, field_transform: &String.trim/1, escape_max_lines: 50)
   end
 
-  def get_csv_content(_opts), do: [ok: %{}]
+  def get_csv_content(_spreadsheet_id, _query_params), do: [ok: %{}]
 
   @spec get_tesla_middlewares :: list()
   defp get_tesla_middlewares do
     [
+      {Tesla.Middleware.BaseUrl, @csv_export_base_url},
       Tesla.Middleware.FollowRedirects,
+      Tesla.Middleware.KeepRequest,
+      Tesla.Middleware.PathParams,
       {Tesla.Middleware.Telemetry, metadata: %{provider: "google_sheets", sampling_scale: 10}}
     ] ++
       Glific.get_tesla_retry_middleware()
