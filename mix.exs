edaba1029bddadcdefb161f6a974ee48bb05d3f4
Priyanshu singh <111607560+priyanshu6238@users.noreply.github.com>
2026-09-22 21:10:28 +0530

    Bump version from 8.12.9 to 8.13.0 (#5795)

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Glific.MixProject do
   def project do
     [
       app: :glific,
-      version: "8.12.9",
+      version: "8.13.0",
       elixir: "~> 1.18.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       dialyzer: [
